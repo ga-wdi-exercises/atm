b55b69e3466df153ac3044729b4546bec565c241
Samuel Key <s.key08@gmail.com>
2015-07-08 16:36:19 -0400

    added Id's and created variables

--- atm.js
+++ atm.js
@@ -1,21 +1,46 @@
-var checkingBalance = document.querySelector("div#checking_balance");
-checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+var balanceField = $("#balanceField");
+var textField = $("#textField");
+var depositButton = $("#depositButton");
+var withdrawlButton = $("#withdrawlButton");
+var enteredAmount;
 
-var checkingDeposit = function(amount){
-  checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
-
+function addMoney(){
+  enteredAmount=$("#textField").val();
+  console.log("val()")
 }
 
-function checkingWithdrawal(amount){
+textField.on("click", addMoney);
 
-}
+//user clicks text field
+//user enters a numnerical value into text field
+//user presses either deopsit or withdrawlButton
+  //this amount is captured
+//the number entered is either added to or subtracted from the current amount
 
-function savingsDeposit(amount){
 
-}
 
-function savingsWithdrawal(amount){
 
-}
+
+
+
+// var checkingBalance = document.querySelector("div#checking_balance");
+// checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+//
+// var checkingDeposit = function(amount){
+//   checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+//
+// }
+//
+// function checkingWithdrawal(amount){
+//
+// }
+//
+// function savingsDeposit(amount){
+//
+// }
+//
+// function savingsWithdrawal(amount){
+//
+// }
 
 // an eventListerner for each button, each one a "click"
