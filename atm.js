41cde0bd65effbf568b91785aad4fb7d372b8fc1
LEIGH VARLEY <LEIGHVARLEY@GMAIL.COM>
2015-07-08 19:53:49 -0400

    added checking deposit and withdraw button functions

--- atm.js
+++ atm.js
@@ -1,21 +1,32 @@
-var checkingBalance = document.querySelector("div#checking_balance");
-checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+//var checkingBalance = document.querySelector("div#checking_balance");
+//checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+//var checkingDeposit = function(amount){
+//  checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+//}
 
-var checkingDeposit = function(amount){
-  checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+cBal = 0
+sBal = 0
 
-}
+//Checking Account - deposit button
+  //When user enters a value and clicks the deposit button, update account balance
+    //User enters value in cUserInput field
+    //user clicks cDepositButton
+    //Update checkingBalance to reflect user deposit
 
-function checkingWithdrawal(amount){
+$( "cDepositButton" ).on("click", function(){
+  var cUserInput = parseInt($( "#cUserInput" ).val())
+})
 
-}
+//Checking account - withdrawButton
+$( "cWithdrawButton" ).on("click", function(){
+  var cUserInput = parseInt($( "#cUserInput" ).val())
+})
 
-function savingsDeposit(amount){
 
-}
+//function checkingWithdrawal(amount){}
 
-function savingsWithdrawal(amount){
+//function savingsDeposit(amount){}
 
-}
+//function savingsWithdrawal(amount){}
 
 // an eventListerner for each button, each one a "click"
