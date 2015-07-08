966065d4d7f78c32c396b6890aaa41fbf594b3f5
Edward Park <parkes.edward@gmail.com>
2015-07-08 16:35:27 -0400

    added comments

--- atm.js
+++ atm.js
@@ -1,8 +1,8 @@
 
-var checkingBalance = 0;
-var savingsBalance = 0;
 
 // CHECKING ACCOUNT
+var checkingBalance = 10;
+
 var checkingDisplay = $(".checking_account .balance");
 var checkingInput = $(".checking_input");
 function checkingDeposit(){
@@ -27,6 +27,8 @@ function checkingWithdrawal(){
 
  //SAVINGS ACCOUNT
 
+var savingsBalance = 10;
+
 var savingsDisplay = $(".savings_account .balance");
 var savingsInput = $(".savings_input");
 
@@ -47,7 +49,12 @@ function savingsWithdrawal(){
   savingsDisplay.css("background-color","red");
 }
 
+savingsDisplay.html("$"+ savingsBalance);
+checkingDisplay.html("$"+checkingBalance);
+
 $(".checking_account input[value=Deposit]").on("click",checkingDeposit);
 $(".savings_account input[value=Deposit]").on("click",savingsDeposit);
 $(".checking_account input[value=Withdraw]").on("click",checkingWithdrawal);
 $(".savings_account input[value=Withdraw]").on("click",savingsWithdrawal);
+
+//when I withdraw with it being zero 
