3167c8b9dc66d5fedacc008da404b7aeaa6399ba
Edward Park <parkes.edward@gmail.com>
2015-07-08 14:33:57 -0400

    finished checking deposit

--- atm.js
+++ atm.js
@@ -1,20 +1,29 @@
 
-var checkingDeposit = function(){
+var checkingBalance = 0;
+var checkingDisplay = $(".checking_account .balance");
+var checkingInput = $(".checking_input");
+function checkingDeposit(){
+  var depositAmount = parseFloat($(checkingInput).val());
+  checkingBalance += depositAmount;
+  checkingInput.val("");
+  checkingDisplay.html("$"+checkingBalance);
+  checkingDisplay.css("background-color","#61c419");
+
 
   //target and get value inside text box
 }
 
-function checkingWithdrawal(amount){
-
-}
-
-function savingsDeposit(amount){
-
-}
-
-function savingsWithdrawal(amount){
-
-}
+// function checkingWithdrawal(amount){
+//
+// }
+//
+// function savingsDeposit(amount){
+//
+// }
+//
+// function savingsWithdrawal(amount){
+//
+// }
 
 // an eventListerner for each button, each one a "click"
 $(".checking_account input[value=Deposit]").on("click",checkingDeposit);
