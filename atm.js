c6adf42b23115f7a8ba485ccb6b9f89e2e0d9ffe
JARED ARKIN <ARKINJB@GMAIL.COM>
2015-07-08 16:21:21 -0400

    working on click event listeners for all buttons

--- atm.js
+++ atm.js
@@ -1,10 +1,10 @@
-var checkingBalance = document.querySelector("div#checking_balance");
+/*var checkingBalance = document.querySelector("div#checking_balance");
 checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
 
 var checkingDeposit = function(amount){
   checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
 
-}
+}*/
 
 function checkingWithdrawal(amount){
 
@@ -18,4 +18,20 @@ function savingsWithdrawal(amount){
 
 }
 
-// an eventListerner for each button, each one a "click"
+// an eventListener for each button, each one a "click"
+// click checking deposit
+$(":button").eq(0).on("click", function(){
+  console.log("click check dep");
+})
+// click checking withdrawal
+$(":button").eq(1).on("click", function(){
+  console.log("click check wd");
+})
+// click checking deposit
+$(":button").eq(2).on("click", function(){
+  console.log("click sav dep");
+})
+// click checking deposit
+$(":button").eq(3).on("click", function(){
+  console.log("click sav wd");
+})
