3e8a60d8e71da5333456b54c8428884868a8fd5c
Jeff Jones <jeff.jones1@gmail.com>
2015-07-08 17:23:35 -0400

    functionality of ignore transaction to go negative and edit bank account background

--- atm.js
+++ atm.js
@@ -1,4 +1,4 @@
-
+$(document).ready (function(){
 $('#checking_deposit').on('click', function() {
     cInput =  $('#checking_input');
     cBalance = $('.balance').eq(0)
@@ -7,20 +7,44 @@ $('#checking_deposit').on('click', function() {
 
     cBalance.html('$' + newChecking)
     currentChecking = cBalance.html();
+    $(cBalance).removeClass('zero')
     cInput.val('')
 })
 
-// var cInput =  $('#checking_input');
-// var cBalance = $('.balance').eq(0)
-// var currentChecking = cBalance.html();
-// var newChecking = (parseFloat(cBalance.html().substr(1)) + parseInt(cInput.val()))
 
 $('#checking_withdraw').on('click', function(){
 
   newChecking = parseFloat(cBalance.html().substr(1)) - parseInt(cInput.val())
-  cBalance.html('$' + newChecking)
 
-  currentChecking = cBalance.html();
-  cInput.val('')
+    if (newChecking > 0){
+        cBalance.html('$' + newChecking)
+        currentChecking = cBalance.html();
+        cInput.val('')
+      }
+})
 
+$('#savings_deposit').on('click', function() {
+    sInput =  $('#savings_input');
+    sBalance = $('.balance').eq(1)
+
+    newSavings = parseFloat(sBalance.html().substr(1)) + parseInt(sInput.val())
+
+    sBalance.html('$' + newSavings)
+    currentSavings = sBalance.html();
+    $(sBalance).removeClass('zero')
+    sInput.val('')
+})
+
+$('#savings_withdraw').on('click', function() {
+    sInput =  $('#savings_input');
+    sBalance = $('.balance').eq(1)
+
+    newSavings = parseFloat(sBalance.html().substr(1)) - parseInt(sInput.val())
+  if(newSavings > 0){
+    sBalance.html('$' + newSavings)
+    currentSavings = sBalance.html();
+    sInput.val('')
+  }
+
+  })
 })
