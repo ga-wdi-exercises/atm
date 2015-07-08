00b50a35857d49e78f36e5d484377f2668111922
Rachel Warren <raelwa@gmail.com>
2015-07-08 17:49:56 -0400

    variables for deposit input and checking balance

--- atm.js
+++ atm.js
@@ -1,58 +1,34 @@
-// var checkingBalance = document.querySelector("div#checking_balance");
-// checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
-//
-// var checkingDeposit = function(amount){
-//   checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
-//
-// }
-//
-// function checkingWithdrawal(amount){
-//
-// }
-//
-// function savingsDeposit(amount){
-//
-// }
-//
-// function savingsWithdrawal(amount){
-//
-// }
-
-// an eventListerner for each button, each one a "click"
-
-
-//Directions
-// As a user, I want to deposit money into one of the bank accounts
-// As a user, I want to withdraw money from one of the bank accounts
-// Make sure the balance in an account can't go negative. If a user tries to withdraw more money than exists in the account, ignore the transaction.
-
-
-//
-
 $(document).ready(function() {  ///DON'T DELETE
 
-// var checkingBalance = function(amount) {
-//   alert("I worked!")
-// }
+// As a user, I want to deposit money into one of the bank accounts
+  //user inputs new number amount (a variable) checkingAmount
+  //user clicks deposit button
+    //new number amount + old number amount (default = 0)
+    //total should replace $0
+    //input field should go back to default
 
-// var cDepositButton = $("#cDepositButton")
-// var checkingInput;
+var cBal = 0;
 
-$("#cDepositButton").on("click", function(checkingBalance) {
-  alert("button worked!")
+$("#cDepositButton").on("click", function() {
+  var cAmt = parseInt($("#cplaceholder").val());
+  alert("$" + (cAmt + cBal)) //returning $NaN0 .. cAmt is NaN?
 })
-//omg i got the button to work. FIRST: created an ID in the HTML for the deposit button so that I could identify it.  once that was done i made the event listener (with alert) with an anonymous function.  then i entered checkingBalance in the function. unsure if i need to do this.
-// $("#cdepositButton").on("click", function(checkingBalance) {
-//   alert("button worked!")
-// })
 
-
-
-
-
-
-
+// Make sure the balance in an account can't go negative. If a user tries to withdraw more money than exists in the account, ignore the transaction.
+// var cBal = parseInt($("#checkingBalance").text().replace("$", ""));
 
 
 
 }) ///CLOSING TAG DON'T DELETE
+
+
+
+//Notes to self
+
+
+//omg i got the button to work. FIRST: created an ID in the HTML for the deposit button so that I could identify it.  once that was done i made the event listener (with alert) with an anonymous function.  then i entered checkingBalance in the function. unsure if i need to do this.
+// $("#cDepositButton").on("click", function(checkingBalance) {
+//   alert("button worked!")
+// })
+
+//had the hardest time with the variables..they were ahead of the function, but kept being returned not defined in the DOM or NaN in my popup... was advised that as the page loads, it determines the outcome of the function (clicking the deposit button in this case) before the user actually enters a number, which was why it was returning NaN. put the var cAmt variable within the function so it would process the variable at the same time. Voila.
