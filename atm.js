775bfe1edcc18fcc94a2b8724cd9cf33075d3181
Matt Curtis <Curtismn87@yahoo.com>
2015-07-09 19:01:57 -0400

    all buttons add values

--- atm.js
+++ atm.js
@@ -1,21 +1,37 @@
-var checkingBalance = document.querySelector("div#checking_balance");
-checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+var totalCheckingBalance = 0;
+var totalSavingBalance = 0;
+//deposit button for Checking
+$("#depositCheckingButton").on("click", function (){
+  console.log(totalCheckingBalance)
+  var addingBalance = parseInt($("#checkingInput").val());
+return totalCheckingBalance +=  addingBalance})
+//Withdraw button for Checking
+$("#withdrawCheckingButton").on("click", function (){
+  console.log(totalCheckingBalance)
+  var withdrawingBalance = parseInt($("#checkingInput").val());
+  return totalCheckingBalance -=  withdrawingBalance})
+//deposit button for Savings
+$("#depositSavingsButton").on("click", function (){
+  console.log(totalSavingBalance);
+  var depositBalance = parseInt($("#savingsInput").val());
+return totalSavingBalance +=  depositBalance})
+//withdraw button for savings
+$("#withdrawSavingsButton").on("click", function (){
+  console.log(totalSavingBalance)
+    var withdrawSavingBalance = parseInt($("#savingsInput").val());
+    return totalSavingBalance -=  withdrawSavingBalance})
 
+
+/* var checkingBalance = checkingBalance + $("checkingInput").val();
+var checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
 var checkingDeposit = function(amount){
   checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
-
 }
-
 function checkingWithdrawal(amount){
-
 }
-
 function savingsDeposit(amount){
-
 }
-
 function savingsWithdrawal(amount){
-
-}
+}*/
 
 // an eventListerner for each button, each one a "click"
