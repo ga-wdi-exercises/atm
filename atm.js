1acec02f8a792eb345b9da8f5915f9fb00223db2
Silvia Brown <siltastic@gmail.com>
2015-07-09 10:46:42 -0400

    Added functionality to the deposit buttons.

--- atm.js
+++ atm.js
@@ -1,21 +1,70 @@
-var checkingBalance = document.querySelector("div#checking_balance");
-checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+// var checkingBalance = document.querySelector("div#checking_balance");
+// checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "")
 
-var checkingDeposit = function(amount){
-  checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+// User chooses either the checking or savings account.
 
-}
+$(document).ready(function() {
+	console.log("Bank is ready.");
 
-function checkingWithdrawal(amount){
+	// Add an event to checking's deposit button.
+	$("#addToChecking").on("click", function() {
+		// Use parseInt to receive the value of the input you are targeting.
+		var depositMoney = parseInt($("#checkingInput").val());
+		// Use parseInt to ...
+		var accountBalance = parseInt($("#checkingBalance").text().replace("$", "")); // Had lots ofhelp here.
+			console.log("Checking balance.")
+		// Create a string for the new balance.
+		// Add ()s to avoid repeating numbers.
+		var newBalance = "$" + (accountBalance + depositMoney); // Had help here.
+		// Total balance and show the new balance.
+		$("#checkingBalance").text(newBalance);
+			console.log("Added $" + depositMoney + " to the checking account.");
+	});
 
-}
+	// Repeat the same funtion as the checking account.
+	$("#addToSavings").on("click", function() {
+		var depositMoney = parseInt($("#savingsInput").val());
+		var accountBalance = parseInt($("#savingsBalance").text().replace("$", ""));
+			console.log("Checking balance.")
+		var newBalance = "$" + (accountBalance + depositMoney);
+		$("#savingsBalance").text(newBalance);
+			console.log("Added $" + depositMoney + " to the savings account.");
+	});
 
-function savingsDeposit(amount){
 
-}
+	// Repeat the same funtion as the checking account.
+	$("#withdrawFromSavings").on("click", function() {
+		var accountBalance = parseInt($("#checkingBalance").text().replace("$", ""));
+			console.log("Checking balance.")
+		var newBalance = "$" + (accountBalance - withdrawMoney);
+		$("#checkingBalance").text(newBalance);
+			console.log("Withdrew $" + depositMoney + " from the checking account.");
+	});
+});
 
-function savingsWithdrawal(amount){
 
-}
+
+
+// User wants to deposit money.
+
+
+
+
+// var checkingDeposit = function(amount){
+//   checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+
+// }
+
+// function checkingWithdrawal(amount){
+
+// }
+
+// function savingsDeposit(amount){
+
+// }
+
+// function savingsWithdrawal(amount){
+
+// }
 
 // an eventListerner for each button, each one a "click"
