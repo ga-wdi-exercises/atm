2cc0bc6b7546ee69bf48a5f0a45f15a6d1db2af8
Rachel Warren <raelwa@gmail.com>
2015-07-09 18:03:07 -0400

    figured out transferring money b/t accts. and added sweet prompts

--- atm.js
+++ atm.js
@@ -14,12 +14,39 @@ $("#cWithdrawButton").on("click", function() {
   // alert("withdraw button!")
   // alert(cBal);
   var cAmt = parseInt($("#cplaceholder").val());
+
   if (cBal > cAmt) {
   cBal = (cBal - cAmt);
   $("#checkingBalance").text("$" + cBal);
   }
+  else if (cBal === cAmt) {
+    cBal = cBal - cAmt;
+    $("#checkingBalance").text("$" + cBal);
+
+    alert("You can't have a zero balance. We'll transfer $1 from your savings because nobody likes negative numbers.")
+    cBal = 1
+    $("#checkingBalance").text("$" + cBal);
+
+    sBal = sBal - 1;
+    $("#savingsBalance").text("$" + sBal);
+  }
+
+  else if (cBal < cAmt && sBal > cAmt ) {
+    var cDiff = (cAmt - cBal);
+    cBal = (cBal - cAmt);
+    $("#checkingBalance").text("$" + cBal);
+
+    var sDiff = cDiff + 1
+    alert("You're overdrawing by $" + cDiff + " but you have money in savings to cover it.  We will transfer $" + sDiff + " to bring your balance to $1")
+
+    cBal = cBal + sDiff;
+    $("#checkingBalance").text("$" + cBal);
+
+    sBal = sBal - sDiff;
+    $("#savingsBalance").text("$" + sBal);
+  }
   else {
-    alert("you can't do that!")
+      alert("You can't withdraw because you don't have enough funds in your accounts. Sorry not sorry.")
   }
 })
 
@@ -36,7 +63,7 @@ $("#sWithdrawButton").on("click", function() {
   $("#savingsBalance").text("$" + sBal);
   }
   else {
-    alert("YOU HAVE NO MONEY. womp womp")
+    alert("YOU DON'T HAVE ENOUGH MONEY. DO NOT BUY THOSE SHOES!")
   }
 })
 
