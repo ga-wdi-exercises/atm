b9ac52949d0255b2d537f6a0dc611f7503db9e52
tomBeach <tbeach2k@gmail.com>
2015-07-09 17:01:32 -0400

    works! single function called for each transaction

--- atm.js
+++ atm.js
@@ -9,19 +9,23 @@ var Sbalance = parseInt($('.balance').eq(1).html().substring(1));
 
 // ======= buttons
 var Cdeposit = $('.account input').eq(1).on('click', function(event) {
-  make_transaction('checking', 'deposit');
+	console.log('make_transaction');
+	make_transaction('checking', 'deposit');
 });
 
 var Sdeposit = $('.account input').eq(4).on('click', function(event) {
-  make_transaction('savings', 'deposit')
+	console.log('Sdeposit');
+	make_transaction('savings', 'deposit')
 });
 
 var Cwithdraw = $('.account input').eq(2).on('click', function(event){
-  make_transaction('checking', 'withdraw');
+	console.log('Cwithdraw');
+	make_transaction('checking', 'withdraw');
 });
 
-var Swithdraw = $('.account input').eq(2).on('click', function(event){
-  make_transaction('savings', 'withdraw');
+var Swithdraw = $('.account input').eq(5).on('click', function(event){
+	console.log('Swithdraw');
+	make_transaction('savings', 'withdraw');
 });
 
 function make_transaction(whichAccount, dep_Or_wdrw) {
@@ -34,7 +38,7 @@ function make_transaction(whichAccount, dep_Or_wdrw) {
 	if ((isNaN(CdepositAmount) && CdepositAmount != '') || (isNaN(SdepositAmount) && SdepositAmount != '')) {
 		$('.balance').eq(0).html('numbers only please');
 	} else {
-		if (dep_Or_wdrw == 'deposit') {
+		if (dep_Or_wdrw == 'deposit') {						// deposit code
 			if (whichAccount == 'checking') {
 				Cbalance = Cbalance + parseInt(CdepositAmount);
 				$('.balance').eq(0).html('$' + Cbalance);
@@ -42,134 +46,42 @@ function make_transaction(whichAccount, dep_Or_wdrw) {
 				Sbalance = Sbalance + parseInt(SdepositAmount);
 				$('.balance').eq(1).html('$' + Sbalance);
 			}
-		} else {
+		} else {											// withdrawal code
 			if (whichAccount == 'checking') {
 				Cbalance = Cbalance - parseInt(CdepositAmount);
-				console.log('Cbalance: ' + Cbalance);
+				console.log('Cbalance_A: ' + Cbalance);
 				if (Cbalance < 0) {
 					console.log('C < 0');
 					Sbalance = parseInt($('.balance').eq(1).html().substring(1));
 					console.log('Sbalance: ' + Sbalance);
-					console.log('Cbalance: ' + Cbalance);
+					console.log('Cbalance_B: ' + Cbalance);
 					if (-Cbalance < Sbalance) {
 					// if ((Sbalance + Cbalance) >= 0) {
 						console.log('C < S');
 						Sbalance = Sbalance + Cbalance;
 						Cbalance = 0;
-						console.log('Cbalance: ' + Cbalance);
 						console.log('Sbalance: ' + Sbalance);
+						console.log('Cbalance_C: ' + Cbalance);
 						$('.balance').eq(0).html('$' + Cbalance);
 						$('.balance').eq(1).html('$' + Sbalance);
 					} else {
 						$('.balance').eq(0).html('cancelled: negative account balance');
 					}
+				} else {
+					console.log('C > 0');
+					console.log('Cbalance_C: ' + Cbalance);
+					$('.balance').eq(0).html('$' + Cbalance);
 				}
-				$('.balance').eq(0).html('$' + Cbalance);
 			} else {
 				Sbalance = Sbalance - parseInt(SdepositAmount);
 				if (Sbalance < 0) {
-					$('.balance').eq(0).html('cancelled: negative account balance');
+					console.log('S < 0');
+					$('.balance').eq(1).html('cancelled: negative account balance');
 				} else {
-					$('.balance').eq(0).html('$' + Sbalance);
+					$('.balance').eq(1).html('$' + Sbalance);
 				}
 			}
 		}
 	}
 }
 
-// $('.account input').eq(1).on('click', CmakeDeposit);
-// $('.account input').eq(2).on('click', CmakeWithdrawal);
-// $('.account input').eq(4).on('click', SmakeDeposit);
-// $('.account input').eq(5).on('click', SmakeWithdrawal);
-
-// function makeDeposit(whichAccount) {
-// 	console.log('makeDeposit');
-// 	if (whichAccount == 'checking') {
-// 		if (!isNaN($('.account input').eq(0).val())) {
-// 			CdepositAmount = parseInt($('.account input').eq(0).val());
-// 			Cbalance = Cbalance + CdepositAmount;
-// 			$('.balance').eq(0).html('$' + Cbalance);
-// 			console.log('Cbalance: ' + Cbalance);
-// 		} else {
-// 			$('.balance').eq(0).html('numbers only please');
-// 		}
-// 	}
-// }
-
-
-
-// function CmakeDeposit(event) {
-// 	console.log('CmakeDeposit');
-// 	if (!isNaN($('.account input').eq(0).val())) {
-// 		CdepositAmount = parseInt($('.account input').eq(0).val());
-// 		Cbalance = Cbalance + CdepositAmount;
-// 		$('.balance').eq(0).html('$' + Cbalance);
-// 		console.log('Cbalance: ' + Cbalance);
-// 	} else {
-// 		$('.balance').eq(0).html('numbers only please');
-// 	}
-// }
-
-// function CmakeWithdrawal(event) {
-// 	console.log('CmakeWithdrawal');
-// 	if (!isNaN($('.account input').eq(0).val())) {
-// 		CdepositAmount = parseInt($('.account input').eq(0).val());
-// 		Cbalance = Cbalance - CdepositAmount;
-// 		console.log('Cbalance: ' + Cbalance);
-// 		if (Cbalance < 0) {
-// 				Sbalance = parseInt($('.balance').eq(1).html().substring(1));
-// 				console.log('Sbalance: ' + Sbalance);
-// 				console.log('-Cbalance: ' + -Cbalance);
-// 				if (-Cbalance < Sbalance) {
-// 					console.log('C < S');
-// 					Sbalance = Sbalance + Cbalance;
-// 					Cbalance = 0;
-// 					console.log('Cbalance: ' + Cbalance);
-// 					console.log('Sbalance: ' + Sbalance);
-// 				}
-// 			}
-// 		$('.balance').eq(0).html('$' + Cbalance);
-// 		$('.balance').eq(1).html('$' + Sbalance);
-// 	} else {
-// 		$('.balance').eq(0).html('numbers only please');
-// 	}
-// }
-
-// function SmakeDeposit(event) {
-// 	console.log('SmakeDeposit');
-// 	if (!isNaN($('.account input').eq(3).val())) {
-// 		SdepositAmount = parseInt($('.account input').eq(3).val());
-// 		Sbalance = Sbalance + SdepositAmount;
-// 		$('.balance').eq(1).html('$' + Sbalance);
-// 		console.log('Sbalance: ' + Sbalance);
-// 	} else {
-// 		$('.balance').eq(1).html('numbers only please');
-// 	}
-// }
-
-// function SmakeWithdrawal(event) {
-
-// }
-
-
-
-// var checkingBalance = document.querySelector("div#checking_balance");
-// // checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
-// var checkingDeposit = function(amount){
-//   checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
-
-// }
-
-// function checkingWithdrawal(amount){
-
-// }
-
-// function savingsDeposit(amount){
-
-// }
-
-// function savingsWithdrawal(amount){
-
-// }
-
-// an eventListerner for each button, each one a "click"
