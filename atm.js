72b89468c3926346b7b83c9c0f300de0a543b985
Matt Ficke <matthew.ficke@gmail.com>
2015-07-09 18:03:40 -0400

    overdraft features, color change on zero balance

--- atm.js
+++ atm.js
@@ -8,14 +8,25 @@ var checkingDeposit = function(amount){
  }
  $("#checkingBalance").html("$"+checkingBalance);
  $("#checkingAmount").val($(this).attr('placeholder'));
-
 }
 
 function checkingWithdrawal(amount){
-  var amount = parseInt($("#checkingAmount").val());
-  if (amount && amount <= checkingBalance) {
+  amount = amount || parseInt($("#checkingAmount").val());
+
+  if(!amount) {
+    return;
+  }
+  if (amount > savingsBalance + checkingBalance) {
+    return;
+  }
+  if (amount > checkingBalance) {
+    savingsWithdrawal(amount - checkingBalance);
+    checkingBalance = 0;
+  }
+  else if (amount <= checkingBalance) {
     checkingBalance = checkingBalance - amount;
   }
+
   $("#checkingBalance").html("$"+checkingBalance);
   $("#checkingAmount").val($(this).attr('placeholder'));
 }
@@ -30,28 +41,65 @@ function savingsDeposit(amount){
 }
 
 function savingsWithdrawal(amount){
-  var amount = parseInt($("#savingsAmount").val());
-  if (amount && amount <= savingsBalance) {
+  amount = amount || parseInt($("#savingsAmount").val());
+
+  if (!amount) {
+    return;
+  }
+  if (amount > savingsBalance + checkingBalance) {
+    return;
+  }
+  if (amount > savingsBalance) {
+    checkingWithdrawal(amount - savingsBalance);
+    savingsBalance = 0;
+  }
+  else if (amount <= savingsBalance) {
     savingsBalance = savingsBalance - amount;
   }
+
   $("#savingsBalance").html("$"+savingsBalance);
   $("#savingsAmount").val($(this).attr('placeholder'));
+}
+
+var balanceCheck = function() {
+  if (checkingBalance === 0) {
+    $("#checkingBalance").addClass("zero");
+  }
+  else {
+    $("#checkingBalance").removeClass("zero");
+  }
+
+  if (savingsBalance === 0) {
+    $("#savingsBalance").addClass("zero");
+  }
+  else {
+    $("#savingsBalance").removeClass("zero");
+  }
+
 }
 
 // an eventListerner for each button, each one a "click"
 
 $("#checkingDeposit").on("click", function() {
   checkingDeposit();
+  balanceCheck();
 });
 
 $("#checkingWithdrawal").on("click", function() {
   checkingWithdrawal();
+  balanceCheck();
 });
 
 $("#savingsDeposit").on("click", function() {
   savingsDeposit();
+  balanceCheck();
 });
 
 $("#savingsWithdrawal").on("click", function() {
   savingsWithdrawal();
+  balanceCheck();
 });
+
+$(document).ready(function() {
+  balanceCheck();
+})
