9913b6175f839fe6b69e55f6f5d09eaac546054d
Alex Bannon <alexbannon@gmail.com>
2015-07-09 14:56:47 -0400

    made an atm where deposit works for both savings and checking

--- atm.js
+++ atm.js
@@ -1,21 +1,15 @@
-var checkingBalance = document.querySelector("div#checking_balance");
-checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+$("input").eq(1).on("click", depositIntoChecking);
+$("input").eq(4).on("click", depositIntoSavings);
 
-var checkingDeposit = function(amount){
-  checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
-
+function depositIntoChecking(event) {
+  event.preventDefault();
+  var checkingAccountUserInput = Number(parseFloat($("input").eq(0).val()).toFixed(2));
+  var checkingTotal = (parseFloat($("div.balance").eq(0).html().substr(1))) + checkingAccountUserInput;
+  $("div.balance").eq(0).html("$" + checkingTotal);
 }
-
-function checkingWithdrawal(amount){
-
+function depositIntoSavings(event) {
+  event.preventDefault();
+  var savingsAccountUserInput = Number(parseFloat($("input").eq(3).val()).toFixed(2));
+  var savingsTotal = (parseFloat($("div.balance").eq(1).html().substr(1))) + savingsAccountUserInput;
+  $("div.balance").eq(1).html("$" + savingsTotal);
 }
-
-function savingsDeposit(amount){
-
-}
-
-function savingsWithdrawal(amount){
-
-}
-
-// an eventListerner for each button, each one a "click"
