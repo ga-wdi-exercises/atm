82a6040a01787d13aa156d78cc689923d2446fbc
Ksenia Coulter <kzherdeva@ucdavis.edu>
2015-07-08 21:22:59 -0400

    prevent default refresh when button is clicked

--- atm.js
+++ atm.js
@@ -1,21 +1,47 @@
-var checkingBalance = document.querySelector("div#checking_balance");
-checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+//Checking account
+//Define players
+$(document).ready(function() {
+  var accountBalance = 0;
+  var inputBox = $("input:first")[0];
+  var userAmount = $("input:first").val();
+  var depositButton = $("input").eq(1);
+  var withdrawButton = $("input").eq(2);
+  //$("input").eq(1).attr("value") is "Deposit"
+  //$("input").eq(2).attr("value") is "Withdraw"
 
-var checkingDeposit = function(amount){
-  checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+  //When Deposit button is clicked, read user input and add it to the total
+  depositButton.on("click", function(){
+    event.preventDefault();
+  })
 
-}
 
-function checkingWithdrawal(amount){
+//Whin withdraw button is clicked, read user input and subtract it from the total
+//add logic to make sure the balance is not zero
 
-}
+// var checkingBalance = document.querySelector("div#checking_balance");
+// checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+//
+// var checkingDeposit = function(amount){
+//   checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+//
+// }
+//
+// function checkingWithdrawal(amount){
+//
+// }
+//
+// function savingsDeposit(amount){
+//
+// }
+//
+// function savingsWithdrawal(amount){
+//
+// }
+//
+// // an eventListerner for each button, each one a "click"
 
-function savingsDeposit(amount){
 
-}
 
-function savingsWithdrawal(amount){
 
-}
 
-// an eventListerner for each button, each one a "click"
+});
