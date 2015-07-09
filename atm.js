2777770429fb4ab3a20873bcc4398a49519a5e93
LEIGH VARLEY <LEIGHVARLEY@GMAIL.COM>
2015-07-08 21:42:03 -0400

    checking and savings deposit and withdraw buttons working except for functionality to prevent overdraw

--- atm.js
+++ atm.js
@@ -15,14 +15,39 @@ $( "#cDepositButton" ).on("click", function(){
   $( "#checkingBalance" ).text( "$" + cBal );
 })
 
-//Checking account - withdrawButton
 $( "#cWithdrawButton" ).on("click", function(){
   var cUserInput = parseInt($( "#cUserInput" ).val());
   cBal = cBal - cUserInput;
   $( "#checkingBalance" ).text( "$" + cBal );
 })
 
+//Savings Account - deposit and withdraw buttons
+  //When user enters a value and clicks the deposit button, update account balance
+    //User enters value in sUserInput field
+    //user clicks sDepositButton - update savingsBalance to reflect user deposit
+    //user clicks sWithdrawButton - update savingsBalance to reflect user withdraw
+
+$( "#sDepositButton" ).on("click", function(){
+  var sUserInput = parseInt($( "#sUserInput" ).val());
+  sBal = sBal + sUserInput;
+  $( "#savingsBalance" ).text( "$" + sBal );
+})
+
+$( "#sWithdrawButton" ).on("click", function(){
+  var sUserInput = parseInt($( "#sUserInput" ).val());
+  sBal = sBal - sUserInput;
+  $( "#savingsBalance" ).text( "$" + sBal );
+})
+
+
+
+
+
+
+
 //Need functionality for preventing account from getting to negative balance
+
+
 
 
 
