dd7a3114e4e24cd58eff06a038897fc6108908a4
Erica Andersen <andersenek@gmail.com>
2015-07-09 19:30:18 -0400

    Added functionality for user to deposit money into one of the bank accounts

--- atm.js
+++ atm.js
@@ -1,10 +1,21 @@
-var checkingBalance = document.querySelector("div#checking_balance");
-checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+// As a user, I want to deposit money into one of the bank accounts
+var balanceDisplay = $(".account").children().eq(1).html(); // Selects Balance HTML Area (cannot be called in function?)
+var userInput = $(".account").children().eq(2); // Selects User Input
+var checkDepositButton = $(".account").children().eq(3); // Checking Deposit Button
 
-var checkingDeposit = function(amount){
-  checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+$(checkDepositButton).click(function() { // Set up Checking Deposit button
+  event.preventDefault(); // Prevents Default Refresh
+  var depAmount = parseInt($( userInput ).val()); // New Total Value
+  var balance = parseInt($( "#check-balance" ).text().replace("$", "")) // Does not work with stored variable
+  var newBalance = "$" + (balance + depAmount)
+  $( "#check-balance" ).html(newBalance); // Does not work with stored variable
 
-}
+  console.log("depAmount is: " + depAmount)
+  console.log("depAmount type is: " + typeof(depAmount))
+  console.log("balanceDisplay is: " + balanceDisplay)
+  console.log("newbalance is: " + newBalance)
+  console.log("Check Deposit button is working");
+});
 
 function checkingWithdrawal(amount){
 
@@ -18,4 +29,10 @@ function savingsWithdrawal(amount){
 
 }
 
-// an eventListerner for each button, each one a "click"
+// An eventListener for each button, each one a "click"
+
+
+// Code to discuss:
+// var checkingBalance = document.querySelector("div#checking_balance");
+// checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+// checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
