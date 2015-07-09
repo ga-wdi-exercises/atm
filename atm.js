7837ae18779980208b0d8beaa7cd70d7ab35cd50
LEIGH VARLEY <LEIGHVARLEY@GMAIL.COM>
2015-07-08 21:15:48 -0400

    deposit and withdraw buttons working

--- atm.js
+++ atm.js
@@ -1,32 +1,29 @@
-//var checkingBalance = document.querySelector("div#checking_balance");
-//checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
-//var checkingDeposit = function(amount){
-//  checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
-//}
+$( document ).ready( function(){
 
-cBal = 0
-sBal = 0
+var cBal = 0
+var sBal = 0
 
-//Checking Account - deposit button
+//Checking Account - deposit and withdraw buttons
   //When user enters a value and clicks the deposit button, update account balance
     //User enters value in cUserInput field
-    //user clicks cDepositButton
-    //Update checkingBalance to reflect user deposit
+    //user clicks cDepositButton - update checkingBalance to reflect user deposit
+    //user clicks cWithdrawButton - update checkingBalance to reflect user withdraw
 
-$( "cDepositButton" ).on("click", function(){
-  var cUserInput = parseInt($( "#cUserInput" ).val())
+$( "#cDepositButton" ).on("click", function(){
+  var cUserInput = parseInt($( "#cUserInput" ).val());
+  cBal = cBal + cUserInput;
+  $( "#checkingBalance" ).text( "$" + cBal );
 })
 
 //Checking account - withdrawButton
-$( "cWithdrawButton" ).on("click", function(){
-  var cUserInput = parseInt($( "#cUserInput" ).val())
+$( "#cWithdrawButton" ).on("click", function(){
+  var cUserInput = parseInt($( "#cUserInput" ).val());
+  cBal = cBal - cUserInput;
+  $( "#checkingBalance" ).text( "$" + cBal );
 })
 
+//Need functionality for preventing account from getting to negative balance
 
-//function checkingWithdrawal(amount){}
 
-//function savingsDeposit(amount){}
 
-//function savingsWithdrawal(amount){}
-
-// an eventListerner for each button, each one a "click"
+})
