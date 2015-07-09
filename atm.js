a52324dc407be478a5565718810826b0eaf0bf60
Erica Andersen <andersenek@gmail.com>
2015-07-09 19:42:26 -0400

    Added functionality for user to withdraw money from one of the bank accounts

--- atm.js
+++ atm.js
@@ -2,24 +2,33 @@
 var balanceDisplay = $(".account").children().eq(1).html(); // Selects Balance HTML Area (cannot be called in function?)
 var userInput = $(".account").children().eq(2); // Selects User Input
 var checkDepositButton = $(".account").children().eq(3); // Checking Deposit Button
+var checkWithdrawlButton = $(".account").children().eq(4); // Checking Withdrawl Button
 
 $(checkDepositButton).click(function() { // Set up Checking Deposit button
   event.preventDefault(); // Prevents Default Refresh
-  var depAmount = parseInt($( userInput ).val()); // New Total Value
-  var balance = parseInt($( "#check-balance" ).text().replace("$", "")) // Does not work with stored variable
-  var newBalance = "$" + (balance + depAmount)
-  $( "#check-balance" ).html(newBalance); // Does not work with stored variable
+  var depAmount = parseInt($( userInput ).val()); // Get Deposit Value
+  var balance = parseInt($( "#check-balance" ).text().replace("$", "")) // Remove '$' from Balance HTML
+  var newBalance = "$" + (balance + depAmount) // New Balance Value + '$'
+  $( "#check-balance" ).html(newBalance); // Replace Balance HTML with New Balance Value *Does not work with stored variable
 
   console.log("depAmount is: " + depAmount)
   console.log("depAmount type is: " + typeof(depAmount))
+  console.log("balance is: " + balance)
   console.log("balanceDisplay is: " + balanceDisplay)
   console.log("newbalance is: " + newBalance)
   console.log("Check Deposit button is working");
 });
 
-function checkingWithdrawal(amount){
+$(checkWithdrawlButton).click(function() {
 
-}
+  event.preventDefault(); // Prevents Default Refresh
+  var withAmount = parseInt($( userInput ).val()); // Get Withdrawl Value
+  var balance = parseInt($( "#check-balance" ).text().replace("$", "")) // Remove $ from Balance HTML
+  var newBalance = "$" + (balance - withAmount) // New Balance Value + '$'
+  $( "#check-balance" ).html(newBalance); // Replace Balance HTML with New Balance Value
+
+  console.log("Check Withdrawl button is working");
+});
 
 function savingsDeposit(amount){
 
