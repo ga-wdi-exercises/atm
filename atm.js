427508ad8497582d53f3cc03ec4b03b1afcc0778
Matt Ficke <matthew.ficke@gmail.com>
2015-07-09 15:20:59 -0400

    adding event listeners for each button with jQuery

--- atm.js
+++ atm.js
@@ -1,21 +1,36 @@
-var checkingBalance = document.querySelector("div#checking_balance");
-checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+// var checkingBalance = document.querySelector("div#checking_balance");
+// checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
 
 var checkingDeposit = function(amount){
-  checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
-
+  alert("CD");
 }
 
 function checkingWithdrawal(amount){
-
+  alert("CW");
 }
 
 function savingsDeposit(amount){
-
+  alert("SD");
 }
 
 function savingsWithdrawal(amount){
-
+  alert("SW");
 }
 
 // an eventListerner for each button, each one a "click"
+
+$("#checking .deposit").on("click", function() {
+  checkingDeposit();
+});
+
+$("#checking .withdrawal").on("click", function() {
+  checkingWithdrawal();
+});
+
+$("#savings .deposit").on("click", function() {
+  savingsDeposit();
+});
+
+$("#savings .withdrawal").on("click", function() {
+  savingsWithdrawal();
+});
