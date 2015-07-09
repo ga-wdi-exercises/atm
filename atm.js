a79d4dc75cb24eb2778cf6370124edfafeeea062
Omari Mayers-Walker <omari@fastmail.fm>
2015-07-08 20:49:47 -0400

    added all event listeners; set up global vars; begins of deposit()

--- atm.js
+++ atm.js
@@ -1,21 +1,81 @@
-var checkingBalance = document.querySelector("div#checking_balance");
-checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+// document ready
 
-var checkingDeposit = function(amount){
-  checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+$(document).ready(function() {
+    console.log("ready");
+});
 
+//Global Vars
+
+var checkingInput = $('#checkingInput');
+var checkDepositButton = $('#checkDepositButton');
+var checkWithdrawButton = $('#checkWithdrawButton');
+
+var savingsInput = $('savingsInput');
+var saveDepositButton = $('saveDepositButton');
+var saveWithdrawButton = $('saveWithdrawButton');
+
+var checkingBalance = $("#checking_balance");
+var savingsBalance = $('#savings_balance');
+// checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+
+// var checkingDeposit = function(amount){
+  // checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+
+// };
+
+function deposit (amount){
+    //be able to deposit depending on which button is pushed
+
+    if (checkingInput == true){
+        // add amount to checking balance
+        // triggered by eventListerner
+        // checkingBalance = parseInt(checkingBalance.html());
+    }
+    return checkingBalance;
+
+    if (savingsInput == true){
+        // add amount to savings balance
+        // triggered by eventListerner
+        // savingsBalance = parseInt(checkingBalance.html());
+    }
+    return savingsBalance;
 }
 
-function checkingWithdrawal(amount){
+// event listeners 
 
-}
+// checking deposit button
 
-function savingsDeposit(amount){
+$('#checkDepositButton').click('click', function(event){
+    console.log('checkDepositButton');
+});
 
-}
+// withdraw deposit button
 
-function savingsWithdrawal(amount){
+$('#checkWithdrawButton').click('click', function(event){
+    console.log('checkWithdrawButton');
+});
 
-}
+// save deposit button
+
+$('#saveDepositButton').click('click', function(event){
+    console.log('saveDepositButton');
+});
+
+// withdraw savings button
+
+$('#saveWithdrawButton').click('click', function(event){
+    console.log('saveWithdrawButton');
+});
+// function checkingWithdrawal(amount){
+
+// }
+
+// function savingsDeposit(amount){
+
+// }
+
+// function savingsWithdrawal(amount){
+
+// }
 
 // an eventListerner for each button, each one a "click"
