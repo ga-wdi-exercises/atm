f6a5cab779de16d880794d1389a3a6dcb6cd3aa4
lanak1301 <musiconstellation@gmail.com>
2015-07-08 21:45:44 -0400

    deposit function (checking account)is working

--- atm.js
+++ atm.js
@@ -1,3 +1,14 @@
+//javascript, html, dom, css
+//get input from input field
+//when you click on Deposit button
+	//get number from input field
+	//create new element of total 
+		//update total adding to input
+//when you click on Withdraw button
+	//get number from input field
+	//create total subtracting grom input
+
+/*
 var checkingBalance = document.querySelector("div#checking_balance");
 checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
 
@@ -19,3 +30,43 @@ function savingsWithdrawal(amount){
 }
 
 // an eventListerner for each button, each one a "click"
+*/	
+
+var $deposit = $("input#deposit") ; //deposit button
+var $withdraw = $("input#withdraw") ; //withdraw button
+var $userInput = $("input#input") ; //input field
+var $checkingBalance = $("div#checking_balance") ;
+var $balance = $("div.balance") ; 
+
+// var currentTotal = function() {
+// 	$balance = $userInput.val() + $checkingBalance.substring(1) ; 
+// }
+
+$deposit.click( function() {
+	var currentTotal = parseInt($userInput.val()) + parseInt($("div.balance").html().substring(1)) ;
+	console.log( currentTotal) ;
+	$balance.html( "$" + currentTotal ) ;
+	$userInput.val(null) ;
+
+})	
+
+
+
+
+
+$withdraw.click( function() {
+	console.log( "withdraw" )
+})	
+
+
+
+
+
+
+
+
+
+
+
+
+
