1bf96282a282ada17f2879cbc177e22ea4c07e3a
tomBeach <tbeach2k@gmail.com>
2015-07-09 18:57:40 -0400

    final version???

--- atm.js
+++ atm.js
@@ -1,83 +1,95 @@
 
 
-// ======= init
-var checkingTotal = 0;
-
-// ======= balance fields
+// ======= init ======= 
 var Cbalance = parseInt($('.balance').eq(0).html().substring(1));
 var Sbalance = parseInt($('.balance').eq(1).html().substring(1));
 
-// ======= buttons
+
+// ======= buttons ======= 
 var Cdeposit = $('.account input').eq(1).on('click', function(event) {
-	console.log('make_transaction');
 	make_transaction('checking', 'deposit');
 });
 
 var Sdeposit = $('.account input').eq(4).on('click', function(event) {
-	console.log('Sdeposit');
 	make_transaction('savings', 'deposit')
 });
 
 var Cwithdraw = $('.account input').eq(2).on('click', function(event){
-	console.log('Cwithdraw');
 	make_transaction('checking', 'withdraw');
 });
 
 var Swithdraw = $('.account input').eq(5).on('click', function(event){
-	console.log('Swithdraw');
 	make_transaction('savings', 'withdraw');
 });
 
-function make_transaction(whichAccount, dep_Or_wdrw) {
-	console.log('make_transaction');
 
-	// ======= input fields
+// ======= make_transaction =======	
+function make_transaction(whichAccount, dep_Or_wdrw) {
+
+	// ======= input fields ======= 
 	var CdepositAmount = $('.account input').eq(0).val();
 	var SdepositAmount = $('.account input').eq(3).val();
 
+	// ======= validation (text entry) ======= 
 	if ((isNaN(CdepositAmount) && CdepositAmount != '') || (isNaN(SdepositAmount) && SdepositAmount != '')) {
+		$('.balance').eq(0).addClass("message");
 		$('.balance').eq(0).html('numbers only please');
 	} else {
-		if (dep_Or_wdrw == 'deposit') {						// deposit code
+
+		// ======= deposits =======
+		if (dep_Or_wdrw == 'deposit') {	
 			if (whichAccount == 'checking') {
 				Cbalance = Cbalance + parseInt(CdepositAmount);
+				$('.balance').eq(0).removeClass("message");
 				$('.balance').eq(0).html('$' + Cbalance);
 			} else {
 				Sbalance = Sbalance + parseInt(SdepositAmount);
 				$('.balance').eq(1).html('$' + Sbalance);
 			}
-		} else {											// withdrawal code
+
+		// ======= withdrawals =======	
+		} else {
+
+			// ======= checking =======
 			if (whichAccount == 'checking') {
 				Cbalance = Cbalance - parseInt(CdepositAmount);
-				console.log('Cbalance_A: ' + Cbalance);
+
+				// ======= low checking balance =======
 				if (Cbalance < 0) {
-					console.log('C < 0');
 					Sbalance = parseInt($('.balance').eq(1).html().substring(1));
-					console.log('Sbalance: ' + Sbalance);
-					console.log('Cbalance_B: ' + Cbalance);
+
+					// ======= check savings balance (enough to cover checking?) =======
 					if (-Cbalance < Sbalance) {
-					// if ((Sbalance + Cbalance) >= 0) {
-						console.log('C < S');
 						Sbalance = Sbalance + Cbalance;
 						Cbalance = 0;
-						console.log('Sbalance: ' + Sbalance);
-						console.log('Cbalance_C: ' + Cbalance);
+						$('.balance').eq(0).removeClass("message");
 						$('.balance').eq(0).html('$' + Cbalance);
 						$('.balance').eq(1).html('$' + Sbalance);
+
+					// ======= not enough =======		
 					} else {
+						$('.balance').eq(0).addClass("message");
 						$('.balance').eq(0).html('cancelled: negative account balance');
 					}
+
+				// ======= enough to cover =======		
 				} else {
-					console.log('C > 0');
-					console.log('Cbalance_C: ' + Cbalance);
+					$('.balance').eq(0).removeClass("message");
 					$('.balance').eq(0).html('$' + Cbalance);
 				}
+
+			// ======= savings =======
 			} else {
 				Sbalance = Sbalance - parseInt(SdepositAmount);
+
+				// ======= not enough =======
 				if (Sbalance < 0) {
-					console.log('S < 0');
+					$('.balance').eq(1).addClass("message");
 					$('.balance').eq(1).html('cancelled: negative account balance');
+
+				// ======= enough to cover =======	
 				} else {
+					$('.balance').eq(1).removeClass("message");
 					$('.balance').eq(1).html('$' + Sbalance);
 				}
 			}
