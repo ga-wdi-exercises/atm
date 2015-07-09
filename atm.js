e7328854a496520a0bc636301015f931a4e16ca9
Mostafa Habib <MostafaSHabib@gmail.com>
2015-07-09 18:15:55 -0400

    got the left side working

--- atm.js
+++ atm.js
@@ -1,21 +1,50 @@
-var checkingBalance = document.querySelector("div#checking_balance");
-checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+//my likely bad code
+// var checkBal = $('.balance').eq(0).html();
+// var checkSave = $('.balance').eq(1).html();
 
+
+var checkBalance = 0;
+
+//left side number, currently null
+// var checkingBalance = document.querySelector("div#checking_balance");
+//     checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+
+
+//right side number in a varible and function
 var checkingDeposit = function(amount){
-  checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+    checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
 
-}
+    }
 
 function checkingWithdrawal(amount){
-
+//withdraw from  left side
 }
 
 function savingsDeposit(amount){
-
+//deposit right side - take the input.val - add to savings total
 }
 
 function savingsWithdrawal(amount){
-
+//withdraw from right side
 }
 
 // an eventListerner for each button, each one a "click"
+
+$('#leftDeposit').on('click' , function(){
+  console.log('Deposit');
+  userLeftAmount = $('#leftAmount').val();
+  console.log(userLeftAmount);
+  checkBalance = parseInt(userLeftAmount) + parseInt(checkBalance);
+  console.log(checkBalance);
+
+});
+
+
+
+$('#leftWithdraw').on('click' , function(){
+  console.log('withdraw');
+
+});
+
+//another likely wrong way
+//$('.balance').eq(1).html($("input").val());
