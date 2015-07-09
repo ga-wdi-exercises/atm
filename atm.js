52d3514983f081583f1242374a6411bbc4f6b373
Preston Davies <pdavies88@gmail.com>
2015-07-09 17:48:08 -0400

    added in buttons and replacing text

--- atm.js
+++ atm.js
@@ -1,21 +1,27 @@
-var checkingBalance = document.querySelector("div#checking_balance");
-checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+// var checkingBalance = document.querySelector("div#checking_balance");
+// checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+//
+// var checkingDeposit = function(amount){
+//  checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
 
-var checkingDeposit = function(amount){
-  checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+//}
+$(".checkingDeposit").on("click", function checkingDeposit(amount){
+  console.log("checking Deposit")
+  var inputAmount = $("input").val();
+  $(".balance").text("$" + inputAmount)
+})
 
-}
+$(".checkingWithdrawal").on("click", function checkingWithdrawal(amount){
+console.log("checking withdrawal")
+})
 
-function checkingWithdrawal(amount){
+$(".savingsDeposit").on("click", function savingsDeposit(amount){
+console.log("savings Deposit")
+})
 
-}
+$(".savingsWithdrawal").on("click", function savingsWithdrawal(amount){
+console.log("savings withdrawal")
+})
 
-function savingsDeposit(amount){
-
-}
-
-function savingsWithdrawal(amount){
-
-}
 
 // an eventListerner for each button, each one a "click"
