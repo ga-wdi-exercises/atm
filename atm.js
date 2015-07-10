2ad12d6119806cdc4c1442ec733ec44ceb0fd1c6
Nayana Davis <nayana487@gmail.com>
2015-07-10 07:53:20 -0400

    ATM application

--- atm.js
+++ atm.js
@@ -1,21 +1,90 @@
-var checkingBalance = document.querySelector("div#checking_balance");
-checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
-
-var checkingDeposit = function(amount){
-  checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
-
+$( document ).ready(function(){
+var checkingDeposit = function(){
+  var checkingBalance = $("#checkingBalance").html();
+  checkingBalance = parseInt( checkingBalance.replace("$", "") );
+  var deposit = parseInt($("#checkingAmount").val());
+  var total = deposit + checkingBalance;
+  $("#checkingBalance").html("$" + total);
+  if(total > 0){
+    $("#checkingBalance").removeClass("zero");
+  }
 }
 
-function checkingWithdrawal(amount){
 
+function checkingWithdrawal(){
+  var checkingBalance = $("#checkingBalance").html();
+  checkingBalance = parseInt( checkingBalance.replace("$", "") );
+  var withdrawal = parseInt($("#checkingAmount").val());
+  var total = checkingBalance - withdrawal;
+  var savingsBalance = $("#savingsBalance").html();
+  savingsBalance = parseInt( savingsBalance.replace("$", "") );
+  if(total >= 0){
+      $("#checkingBalance").html("$" + total);
+      if(total === 0){
+        $("#checkingBalance").addClass("zero");
+      }
+  } else if(total + savingsBalance >= 0) {
+      $("#checkingBalance").html("$0");
+      $("#savingsBalance").html("$" + (savingsBalance + total));
+      $("#checkingBalance").addClass("zero");
+      if(withdrawal === (checkingBalance + savingsBalance)){
+        $("#savingsBalance").addClass("zero");
+      }
+
+  }
 }
 
-function savingsDeposit(amount){
-
+function savingsDeposit(){
+  var savingsBalance = $("#savingsBalance").html();
+  savingsBalance = parseInt( savingsBalance.replace("$", "") );
+  var deposit = parseInt($("#savingsAmount").val());
+  var total = deposit + savingsBalance;
+  $("#savingsBalance").html("$" + total);
+  if(total > 0){
+    $("#savingsBalance").removeClass("zero");
+  }
 }
 
-function savingsWithdrawal(amount){
+function savingsWithdrawal(){
+  var savingsBalance = $("#savingsBalance").html();
+  savingsBalance = parseInt( savingsBalance.replace("$", "") );
+  var withdrawal = parseInt($("#savingsAmount").val());
+  var total = savingsBalance - withdrawal;
+  var checkingBalance = $("#checkingBalance").html();
+  checkingBalance = parseInt( checkingBalance.replace("$", "") );
+  if(total >= 0){
+      $("#savingsBalance").html("$" + total);
+      if(total === 0){
+        $("#savingsBalance").addClass("zero");
+      }
+  } else if(total + checkingBalance >= 0) {
+      $("#savingsBalance").html("$0");
+      $("#checkingBalance").html("$" + (checkingBalance + total));
+      $("#savingsBalance").addClass("zero");
+      if(withdrawal === (savingsBalance + checkingBalance)){
+        $("#checkingBalance").addClass("zero");
+      }
 
+  }
 }
 
 // an eventListerner for each button, each one a "click"
+
+$("#checkingDeposit").on("click", function(){
+    checkingDeposit();
+  });
+
+
+$("#savingsDeposit").on("click", function(){
+    savingsDeposit();
+    });
+
+$("#checkingWithdrawal").on("click",function(){
+  checkingWithdrawal();
+});
+
+$("#savingsWithdrawal").on("click",function(){
+  savingsWithdrawal();
+});
+
+});
