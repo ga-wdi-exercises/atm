9604a4152bb878aac2a8ce52509e9e956dd98dce
Walter <walter.augustine10@gmail.com>
2015-07-10 02:03:26 -0400

    almost completely made to specs. the only thing missing is preventing a negative value in the savings account when the checking account is overdrawn

--- atm.js
+++ atm.js
@@ -1,51 +1,67 @@
 $( document ).ready(function() {
     console.log( "ready!" );
-});
+    var totalBalance = parseInt(checking + savings)
 
 
-//add value of inputChecking to #checking
-$("#checkingDeposit").click(function(){
-  var inputChecking = parseInt($("#inputChecking").val());
-  var checking = $("#checking").text();
-  checking = parseInt(checking.replace("$", ""))
-  var newBalance = checking + inputChecking
-  console.log(newBalance);
-  $("#checking").text("$" + newBalance)
-})
+  //add value of inputChecking to #checking
+  $("#checkingDeposit").click(function(){
+    var inputChecking = parseInt($("#inputChecking").val());
+    var checking = parseInt($("#checking").text().replace("$", ""));
+    var newBalance = checking + inputChecking
+    console.log(newBalance);
+    $("#checking").text("$" + newBalance)
+    if (checking > 0) {
+      $(".account").removeClass("zero")
 
-//subtract inputChecking from #checking
-$("#checkingWithdrawal").click(function(){
-  var inputChecking = parseInt($("#inputChecking").val());
-  var checking = $("#checking").text();
-  checking = parseInt(checking.replace("$", ""))
-  var newBalance = checking - inputChecking
+    }
+  })
+
+  //subtract inputChecking from #checking
+  $("#checkingWithdrawal").click(function(){
+    var inputChecking = parseInt($("#inputChecking").val());
+    var checking = parseInt($("#checking").text().replace("$", ""));
+    var savings = parseInt($("#savings").text().replace("$", ""));
+    var newBalance = checking - inputChecking
+
+    if((checking - inputChecking) < 0){
+      var overDraft = inputChecking - checking
+      $("#checking").text("$0")
+      var newSavingsBalance = (savings - overDraft)
+      $("#savings").text("$" + newSavingsBalance)
+      $(".account").addClass("zero")
+    }
+
+    //I could not get the savings to prevent a negative balance when overdraft
+    else if ((newBalance == 0) && (newSavingsBalance ==0)){
+        console.log("oops");
+        return;
+      }
+    //////////////////////////////////
+    else{
+    console.log(newBalance);
+    $("#checking").text("$" + newBalance)
+    }
+
+  })
+
+  //add inputSavings to savings
+  $("#savingsDeposit").click(function(){
+    var inputSavings = parseInt($("#inputSavings").val());
+    var savings = parseInt($("#savings").text().replace("$", ""));
+    var newBalance = savings + inputSavings
+    console.log(newBalance);
+    $("#savings").text("$" + newBalance)
+  })
+
+  //subtract inputSavings from savingsBalance
+  $("#savingsWithdrawal").click(function(){
+    var inputSavings = parseInt($("#inputSavings").val());
+    var savings = $("#savings").text().replace("$", "");
+    var newBalance = savings - inputSavings
     if (newBalance <0) { //prevents negative balances
       return;
     }
-  console.log(newBalance);
-  $("#checking").text("$" + newBalance)
-
-})
-
-//add inputSavings to savings
-$("#savingsDeposit").click(function(){
-  var inputSavings = parseInt($("#inputSavings").val());
-  var savings = $("#savings").text();
-  savings = parseInt(savings.replace("$", ""))
-  var newBalance = savings + inputSavings
-  console.log(newBalance);
-  $("#savings").text("$" + newBalance)
-})
-
-//subtract inputSavings from savingsBalance
-$("#savingsWithdrawal").click(function(){
-  var inputSavings = parseInt($("#inputSavings").val());
-  var savings = $("#savings").text();
-  savings = parseInt(savings.replace("$", ""))
-  var newBalance = savings - inputSavings
-  if (newBalance <0) { //prevents negative balances
-    return;
-  }
-  console.log(newBalance);
-  $("#savings").text("$" + newBalance)
-})
+    console.log(newBalance);
+    $("#savings").text("$" + newBalance)
+  })
+});
