06472adc437bbf6566e471007f0f8edad978217c
Malissa Cruz Romero <romero.malissa@gmail.com>
2015-07-10 07:56:19 -0400

    added code to checkingand savings accounts

--- atm.js
+++ atm.js
@@ -1,21 +1,81 @@
-var checkingBalance = document.querySelector("div#checking_balance");
-checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+$(document).ready(function(){
 
-var checkingDeposit = function(amount){
-  checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+  var checkingDepositButton = $("#checkingDeposit");
+  var checkingWithdrawButton = $("#checkingWithdraw");
+  var checkingBalance = $("#checkingBalance");
+  var checkingRunningBalance = 0;
+  var checkingEnterAnAmount = $("#checkingEnterAnAmount");
 
-}
+  $("#checkingDepositButton").on("click", function(){
+    var userInput = parseFloat(checkingEnterAnAmount.val());
+    checkingRunningBalance = checkingRunningBalance + userInput;
+    $("#checkingBalance").text("$" + checkingRunningBalance)
+    console.log(checkingEnterAnAmount.val());
+  });
 
-function checkingWithdrawal(amount){
+  $("#checkingWithdrawButton").on("click",function(){
+    var userInput = parseFloat(checkingEnterAnAmount.val());
+    console.log(checkingEnterAnAmount.val());
+    // var newTotalBalance = checkingBalance = savingBalance
+    if (userInput <= checkingRunningBalance){
+      checkingRunningBalance = checkingRunningBalance - userInput;
+      $("#checkingBalance").text("$" + checkingRunningBalance)
+    }
+    else {
+    // ((newTotalBalance - userInput) > 0){
+    // //     var overDraft = userInput - checkingBalance
+    // //     $("#checkingBalance").text("$0")
+    // //     var newSavingsBalance = "$" + (savingsBalance - overDraft)
+    // //     $("#savingsBalance").text(newSavingsBalance)
+        alert("You have insufficient funds.");
+    }
+  });
 
-}
+var savingDepositButton = $("#savingDeposit");
+var savingWithdrawButton = $("#savingWithraw");
+var savingBalance = $("#savingBalance");
+var savingRunningBalance = 0;
+var savingEnterAnAmount = $("#savingEnterAnAmount")
 
-function savingsDeposit(amount){
+  $("#savingDepositButton").on("click", function(){
+    var userInput = parseFloat(savingEnterAnAmount.val());
+    savingRunningBalance = savingRunningBalance + userInput;
+    $("#savingBalance").text("$" + savingRunningBalance)
+    console.log(savingEnterAnAmount.val());
+  });
 
-}
+  $("#savingWithdrawButton").on("click",function(){
+    var userInput = parseFloat(savingEnterAnAmount.val());
+    console.log(savingEnterAnAmount.val());
+    if (userInput <= savingRunningBalance){
+      savingRunningBalance = savingRunningBalance - userInput;
+      $("#savingBalance").text("$" + savingRunningBalance)
+    }
+    else {
+      alert("You have insufficient funds.");
+    }
 
-function savingsWithdrawal(amount){
 
-}
+  });
+});
 
-// an eventListerner for each button, each one a "click"
+// [x] Step 1
+// i want to check my checking balance
+// i want to desposit money
+// i will enter the amount i want to deposit
+// and press a deposit button so that it takes it
+// i want to see my updated balance
+//
+// [X] Step 2
+// [x]balance cant be less
+// [x]user types in more money in enter an amount
+// // ignore the user input with a if statement
+// if userinput > checkingBalance
+// subtract user input from balance
+// // promt not enough funds
+// []step 3
+// if user enters an amount in checking
+// then subtract it out of the savingBalance
+//
+// if userinput <= checkingRunningBalance
+// checkingrunningbalance - savingsrunningbalance = checkingbalance
