be760f1b8854b3227c158d47bef275db29da2ad3
Ryan Kahle <jrkahle@hotmail.com>
2015-07-10 10:08:19 -0400

    Put into object

--- atm.js
+++ atm.js
@@ -1,115 +1,86 @@
 
 var atm = {
+sumChecking: 0,
+sumSavings: 0,
+listen:function() {
 
-  
+      $( "#checking input[value=Deposit]" ).on( "click" , atm.functions.DepositC );
+      $( "#checking input[value=Withdraw]" ).on("click" , atm.functions.WithdrawC );
+      $( "#savings input[value=Deposit]" ).on( "click" , atm.functions.DepositS );
+      $( "#savings input[value=Withdraw]" ).on("click" , atm.functions.WithdrawS );
 
+},
+
+functions: {
+  DepositC: function() {
+            var depositChecking = $( "#checking input[type=text]" ).val();
+            atm.sumChecking += parseInt(depositChecking);
+
+            $( "#checking div.balance" ).html("$" + atm.sumChecking);
+            if(atm.sumSavings > 0) {
+                $( "#checking .balance").removeClass( "zero");
+            }
+          },
+
+  WithdrawC: function () {
+              var withdrawChecking = $( "#checking input[type=text]" ).val();
+              atm.sumChecking -= parseInt(withdrawChecking);
+
+              if (atm.sumChecking < 0) {
+                $( "#checking .balance").addClass( "zero");
+              }
+
+              if (atm.sumChecking < 0 && atm.sumSavings >= 0) {
+                if (atm.sumSavings <= 0) {
+                  $( "#savings .balance").addClass( "zero");
+                  atm.sumChecking = 0;
+                  return alert("You don't have enough money for this transaction.")
+                }
+                // Overdraft amount ( ex. -10 + 50 = 40)
+                atm.sumSavings += atm.sumChecking;
+                atm.sumChecking = 0;
+
+                $( "#savings div.balance" ).html("$" + atm.sumSavings);
+              }
+
+              $( "#checking div.balance" ).html("$" + atm.sumChecking);
+
+            },
+  DepositS: function() {
+            var depositSavings = $( "#savings input[type=text]" ).val();
+            atm.sumSavings += parseInt(depositSavings);
+
+            $( "#savings div.balance" ).html("$" + atm.sumSavings);
+            if(atm.sumSavings > 0) {
+                $( "#savings .balance").removeClass( "zero");
+            }
+
+          },
+  WithdrawS: function() {
+              var withdrawSavings = $( "#savings input[type=text]" ).val();
+              atm.sumSavings -= parseInt(withdrawSavings);
+
+              // Turn acct red if 0
+              if (atm.sumSavings <= 0) {
+                $( "#savings .balance").addClass( "zero");
+              }
+
+              if (atm.sumSavings < 0 && atm.sumChecking >= 0) {
+                if (atm.sumChecking <= 0) {
+                  $( "#checking .balance").addClass( "zero");
+                  atm.sumSavings = 0;
+                  return alert("You don't have enough money for this transaction.")
+                }
+                // Overdraft amount ( ex. -10 + 50 = 40)
+                atm.sumChecking += atm.sumSavings;
+                atm.sumSavings = 0;
+
+                $( "#checking div.balance" ).html("$" + atm.sumChecking);
+              }
+
+              $( "#savings div.balance" ).html("$" + atm.sumSavings);
+
+            }
+        }
 }
-
-
-// Listeners
-
-$( "#checking input[value=Deposit]" ).on( "click" , DepositC );
-
-$( "#checking input[value=Withdraw]" ).on("click" , WithdrawC );
-
-$( "#savings input[value=Deposit]" ).on( "click" , DepositS );
-
-$( "#savings input[value=Withdraw]" ).on("click" , WithdrawS );
-
-
-// Should be able to make this into one function for each[checking,savings], yes?
-// if statement to go down diff. paths?
-// Put into object
-
-
-// Checking ***************
-
-var sumChecking = 0;
-
-function DepositC() {
-  var depositChecking = $( "#checking input[type=text]" ).val();
-  sumChecking += parseInt(depositChecking);
-
-  $( "#checking div.balance" ).html("$" + sumChecking);
-  if(sumSavings > 0) {
-      $( "#checking .balance").removeClass( "zero");
-  }
-}
-
-
-
-
-function WithdrawC() {
-  var withdrawChecking = $( "#checking input[type=text]" ).val();
-  sumChecking -= parseInt(withdrawChecking);
-
-  if (sumChecking < 0) {
-    $( "#checking .balance").addClass( "zero");
-  }
-
-  if (sumChecking < 0 && sumSavings >= 0) {
-    if (sumSavings <= 0) {
-      $( "#savings .balance").addClass( "zero");
-      sumChecking = 0;
-      return alert("You don't have enough money for this transaction.")
-    }
-    // Overdraft amount ( ex. -10 + 50 = 40)
-    sumSavings += sumChecking;
-    sumChecking = 0;
-
-    $( "#savings div.balance" ).html("$" + sumSavings);
-  }
-
-  $( "#checking div.balance" ).html("$" + sumChecking);
-
-}
-
-
-
-// Savings ***************
-
-var sumSavings = 0;
-
-
-
-function DepositS() {
-  var depositSavings = $( "#savings input[type=text]" ).val();
-  sumSavings += parseInt(depositSavings);
-
-  $( "#savings div.balance" ).html("$" + sumSavings);
-  if(sumSavings > 0) {
-      $( "#savings .balance").removeClass( "zero");
-  }
-
-}
-
-
-
-
-function WithdrawS() {
-  var withdrawSavings = $( "#savings input[type=text]" ).val();
-  sumSavings -= parseInt(withdrawSavings);
-
-  // Turn acct red if 0
-  if (sumSavings <= 0) {
-    $( "#savings .balance").addClass( "zero");
-  }
-
-  if (sumSavings < 0 && sumChecking >= 0) {
-    if (sumChecking <= 0) {
-      $( "#checking .balance").addClass( "zero");
-      sumSavings = 0;
-      return alert("You don't have enough money for this transaction.")
-    }
-    // Overdraft amount ( ex. -10 + 50 = 40)
-    sumChecking += sumSavings;
-    sumSavings = 0;
-
-    $( "#checking div.balance" ).html("$" + sumChecking);
-  }
-
-
-
-  $( "#savings div.balance" ).html("$" + sumSavings);
-
-}
+atm.listen()
