cab01e94186a4ef7ad1836b48b8026a1f9675f08
LEIGH VARLEY <LEIGHVARLEY@GMAIL.COM>
2015-07-09 23:07:02 -0400

    final version

--- atm.js
+++ atm.js
@@ -3,22 +3,35 @@ $( document ).ready( function(){
 var cBal = 0
 var sBal = 0
 
+//Had issues making this work
+// $("div.zero").css({
+//       "background-color": "000000",
+//     });
+
 //Checking Account - deposit and withdraw buttons
   //When user enters a value and clicks the deposit button, update account balance
     //User enters value in cUserInput field
     //user clicks cDepositButton - update checkingBalance to reflect user deposit
     //user clicks cWithdrawButton - update checkingBalance to reflect user withdraw
+    //if userinput withdraw makes checkingBalance go below zero, do not allow
 
-$( "#cDepositButton" ).on("click", function(){
-  var cUserInput = parseInt($( "#cUserInput" ).val());
-  cBal = cBal + cUserInput;
-  $( "#checkingBalance" ).text( "$" + cBal );
-})
+//Checking Account Deposit
+    $( "#cDepositButton" ).on("click", function(){
+      var cDepositUserInput = parseInt($( "#cUserInput" ).val());
+      cBal = cBal + cDepositUserInput;
+      $( "#checkingBalance" ).text( "$" + cBal );
+    })
 
+//Checking Account Withdraw
 $( "#cWithdrawButton" ).on("click", function(){
-  var cUserInput = parseInt($( "#cUserInput" ).val());
-  cBal = cBal - cUserInput;
-  $( "#checkingBalance" ).text( "$" + cBal );
+  var cWithdrawUserInput = parseInt($( "#cUserInput" ).val());
+  if(cBal >= cWithdrawUserInput) {
+  cBal = cBal - cWithdrawUserInput;
+    $( "#checkingBalance" ).text( "$" + cBal );
+  }
+  else if(cBal < cWithdrawUserInput) {
+    var answer = confirm("Insufficient Funds!")
+  }
 })
 
 //Savings Account - deposit and withdraw buttons
@@ -27,28 +40,23 @@ $( "#cWithdrawButton" ).on("click", function(){
     //user clicks sDepositButton - update savingsBalance to reflect user deposit
     //user clicks sWithdrawButton - update savingsBalance to reflect user withdraw
 
+//Savings Account Deposit
 $( "#sDepositButton" ).on("click", function(){
   var sUserInput = parseInt($( "#sUserInput" ).val());
   sBal = sBal + sUserInput;
   $( "#savingsBalance" ).text( "$" + sBal );
 })
 
+//Savings Account Withdraw
 $( "#sWithdrawButton" ).on("click", function(){
-  var sUserInput = parseInt($( "#sUserInput" ).val());
-  sBal = sBal - sUserInput;
+  var sWithdrawUserInput = parseInt($( "#sUserInput" ).val());
+  if(sBal >= sWithdrawUserInput) {
+  sBal = sBal - sWithdrawUserInput;
   $( "#savingsBalance" ).text( "$" + sBal );
+}
+  else if(sBal < sWithdrawUserInput) {
+    var answer = confirm("Insufficient Funds!")
+  }
 })
-
-
-
-
-
-
-
-//Need functionality for preventing account from getting to negative balance
-
-
-
-
 
 })
