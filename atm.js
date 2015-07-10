7861924f574433f0c19ee2a2052b7b426c36670e
Lisa <lscottcook@hotmail.com>
2015-07-09 22:14:58 -0400

    final

--- atm.js
+++ atm.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-//Deposit Money
+//Checking Deposit
   $(".cdepositButton").on("click",function(){
     console.log("i clicked the deposit button")
     var deposit = parseInt($(".input").val())
@@ -11,24 +11,32 @@ $(document).ready(function(){
   var newBalance= deposit + balance
  //console.log(balance)
  $(".cBalance").text("$" + newBalance)
-// var changeColor = function(obj){
+ //I wasn't sure what instructions meant regarding changing the color to reflect the balance
+ if (newBalance >= 500){
+ $(".cBalance").css("background-color", "#6df15d")
+ }
 
-//  if(NewBalance.value <= 0){
-//    o.style.backgroundColor = 'green';--  start here
-//  }
+
  })
 
-//Withdraw Money
+//Checking Withdrawal
 $(".cwithdrawalButton").on("click",function(){
   console.log("i clicked the withdrawal button")
 var withdrawal = parseInt($(".input").val())
 var balance=$(".cBalance").text()
+if (withdrawal > balance) { //unable to figure out the code to ignore transaction if withdrawal > deposit total 
+var balance=$(".cBalance").text()
+}
 balance= parseInt(balance.replace("$", ""))
   var newBalance= balance - withdrawal
    $(".cBalance").text("$" + newBalance)
+   if (newBalance === 0){
+   $(".cBalance").css("background-color", "#de3ea8")
+
+}
 })
 
-//Saving Money
+//Savings Deposit
 $(".sdepositButton").on("click",function(){
   console.log("i clicked the deposit button")
   var deposit = parseInt($(".sInput").val())
@@ -39,9 +47,12 @@ $(".sdepositButton").on("click",function(){
 var newBalance= deposit + balance
 //console.log(balance)
 $(".sBalance").text("$" + newBalance)
+if (newBalance >= 500){
+$(".sBalance").css("background-color", "#6df15d")
+}
 })
 
-//Withdraw Money
+//Savings Withdrawal
 $(".swithdrawalButton").on("click",function(){
 console.log("i clicked the withdrawal button")
 var withdrawal = parseInt($(".sInput").val())
@@ -49,6 +60,10 @@ var balance=$(".sBalance").text()
 balance= parseInt(balance.replace("$", ""))
 var newBalance= balance - withdrawal
  $(".sBalance").text("$" + newBalance)
+ if (newBalance === 0){
+   console.log ("account is < zero")
+ $(".sBalance").css("background-color", "#de3ea8")
+ }
 })
 
 })
