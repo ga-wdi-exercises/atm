cd79b1c6cb239bcab6509eee19b57905fa8f44b7
Preston Davies <pdavies88@gmail.com>
2015-07-09 22:05:47 -0400

    created color changes to backrounds on overdraft

--- atm.js
+++ atm.js
@@ -3,18 +3,20 @@ $(".checkingDeposit").on("click", function checkingDeposit(amount){
   console.log("checking Deposit")
   var inputAmount = parseInt($("input.checkingAmount").val(),10);
   checkDepositTotal += inputAmount;
-  $("#checking_balance.balance").html("$" + checkDepositTotal)
+  $("#checking_balance.balance").html("$" + checkDepositTotal);
+  $(".checkingAccount").removeClass("zero");
 });
 
 
 $(".checkingWithdrawal").on("click", function checkingWithdrawal(amount){
 console.log("checking withdrawal")
-if(checkDepositTotal - parseInt($("input.checkingAmount").val(),10) > 0 ) {
+if(checkDepositTotal - parseInt($("input.checkingAmount").val(),10) >= 0 ) {
   var inputAmount = parseInt($("input.checkingAmount").val(),10);
   checkDepositTotal -= inputAmount;
   $("#checking_balance.balance").html("$" + checkDepositTotal);
 }
 else{
+  $(".checkingAccount").addClass("zero");
   alert("Not enough funds");
   }
 });
@@ -24,17 +26,19 @@ $(".savingsDeposit").on("click", function savingsDeposit(amount){
 console.log("savings Deposit")
 var inputAmount = parseInt($("input.savingsAmount").val(),10);
 savingsDepositTotal += inputAmount;
-$("#savings_balance.balance").html("$" + savingsDepositTotal)
+$("#savings_balance.balance").html("$" + savingsDepositTotal);
+$(".savingsAccount").removeClass("zero");
 });
 
 $(".savingsWithdrawal").on("click", function savingsWithdrawal(amount){
 console.log("savings withdrawal")
-if(savingsDepositTotal - parseInt($("input.savingsAmount").val(),10) > 0 ) {
+if(savingsDepositTotal - parseInt($("input.savingsAmount").val(),10) >= 0 ) {
   var inputAmount = parseInt($("input.savingsAmount").val(),10);
   savingsDepositTotal -= inputAmount;
   $("#savings_balance.balance").html("$" + savingsDepositTotal)
 }
 else {
-alert("Not enough funds");
+  $(".savingsAccount").addClass("zero");
+  alert("Not enough funds");
   }
 });
