7e8fef6424cc02cb64663d1bdf4bbc34f6792c06
Erica Irving <contact.eirving@gmail.com>
2015-07-09 21:07:52 -0400

    Added pseudo code for all user stories. Tweaked css classes to help add var savingsBalance.

--- atm.js
+++ atm.js
@@ -1,21 +1,65 @@
 var checkingBalance = document.querySelector("div#checking_balance");
 checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
 
+//Instantiate var savingsBalance in the style of var checkingBalance
+var savingsBalance = document.querySelector("div#savings_balance");
+savingsBalance = parseInt( savingsBalance.innerHTML.replace("$", "") );
+
 var checkingDeposit = function(amount){
   checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
-
+  //When the user presses the deposit button in the first "account" div...
+    //Set their input equal to amount
+    //Add amount to checkingBalance, i.e. checkingBalance += amount
+    //Update the "balance" div to reflect the new checkingBalance
+    //Remove class zero
 }
 
 function checkingWithdrawal(amount){
-
+  //When the user presses the withdrawal button in the first "account" div...
+    //Set their input equal to amount
+    //If checkingBalance + savingsBalance minus amount is less than $0
+      //do nothing
+    //Else if checkingBalance minus amount is less than $0
+      //Set checkingBalance to $0 and
+      //Reinstate class zero
+      //Subtract (amount-checkingBalance) from savingsBalance
+      //Update the savings balance div
+    //Else
+      //Subtract amount from checkingBalance
+      //Update the "balance" div to reflect the new checkingBalance
 }
 
 function savingsDeposit(amount){
-
+  //When the user presses the deposit button in the second "account" div...
+    //Set their input equal to amount
+    //Add amount to savingsBalance, i.e. savingsBalance += amount
+    //Update the "balance" div to reflect the new savingsBalance
 }
 
 function savingsWithdrawal(amount){
-
+  //When the user presses the withdrawal button in the second "account" div...
+    //Set their input equal to amount
+    //If checkingBalance + savingsBalance minus amount is less than $0
+      //do nothing
+    //Else if savingsBalance minus amount is less than $0
+      //Set savingsBalance to $0 and
+      //Reinstate class zero
+      //Subtract (amount - savingsBalance) from checkingBalance
+      //Update the checking balance div
+    //Else
+      //Subtract amount from savingsBalance
+      //Update the "balance" div to reflect the new checkingBalance
 }
 
-// an eventListerner for each button, each one a "click"
+// - Are there ways to refactor your code to make it DRYer or more Object-Oriented?
+//
+// ###Tips
+// * Tackle making your accounts work individually first
+// * Then tackle them working together with overdraft protection
+// * Only start working with the DOM after you have the logic down
+//
+// ###Resources
+// * [What is window.onload for?](https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers.onload)
+// * [Adding Event Listeners so things happen when you click](https://developer.mozilla.org/en-US/docs/Web/API/EventTarget.addEventListener)
+// * [Adding or removing a CSS class using JS](https://developer.mozilla.org/en-US/docs/Web/API/Element.classList)
+// * [Changing HTML with JavaScript](https://developer.mozilla.org/en-US/docs/Web/API/Element.innerHTML)
