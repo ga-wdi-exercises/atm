43ab8f210f37ee09ace84c0c3df9665116522f16
Preston Davies <pdavies88@gmail.com>
2015-07-09 21:05:35 -0400

    all buttons work to add or subtract dollar values

--- atm.js
+++ atm.js
@@ -1,33 +1,29 @@
-// var checkingBalance = document.querySelector("div#checking_balance");
-// checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
-//
-// var checkingDeposit = function(amount){
-//  checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
-
-//}
 checkDepositTotal = 0;
 $(".checkingDeposit").on("click", function checkingDeposit(amount){
   console.log("checking Deposit")
-
   var inputAmount = parseInt($("input.checkingAmount").val(),10);
-    checkDepositTotal = inputAmount + checkDepositTotal;
+  checkDepositTotal += inputAmount;
   $("#checking_balance.balance").html("$" + checkDepositTotal)
-
-})
+});
 
 $(".checkingWithdrawal").on("click", function checkingWithdrawal(amount){
 console.log("checking withdrawal")
-})
+var inputAmount = parseInt($("input.checkingAmount").val(),10);
+checkDepositTotal -= inputAmount;
+$("#checking_balance.balance").html("$" + checkDepositTotal)
+});
 
+savingsDepositTotal = 0;
 $(".savingsDeposit").on("click", function savingsDeposit(amount){
 console.log("savings Deposit")
-var inputAmount = $("input.savingsAmount").val();
-$("#savings_balance.balance").html("$" + inputAmount)
-})
+var inputAmount = parseInt($("input.savingsAmount").val(),10);
+savingsDepositTotal += inputAmount;
+$("#savings_balance.balance").html("$" + savingsDepositTotal)
+});
 
 $(".savingsWithdrawal").on("click", function savingsWithdrawal(amount){
 console.log("savings withdrawal")
-})
-
-
-// an eventListerner for each button, each one a "click"
+var inputAmount = parseInt($("input.savingsAmount").val(),10);
+savingsDepositTotal -= inputAmount;
+$("#savings_balance.balance").html("$" + savingsDepositTotal)
+});
