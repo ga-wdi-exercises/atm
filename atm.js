07ec28166dc3d6aa66f771826406e2c6d01ddab5
Stephen Gheysens <segheysens@gmail.com>
2015-07-10 03:41:16 -0400

    Included jQuery. Wrote pseudocode for bank account manipulation functions in atm.js

--- atm.js
+++ atm.js
@@ -1,21 +1,34 @@
-var checkingBalance = document.querySelector("div#checking_balance");
-checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+var checkingBalance = $(".balance")[0];
+var checkingInt = Number.parseInt(checkingBalance.innerHTML.replace("$", ""));
+console.log(checkingInt);
+var savingsBalance = $(".balance")[1];
+var savingsInt = Number.parseInt(savingsBalance.innerHTML.replace("$", ""));
+console.log(savingsInt);
 
 var checkingDeposit = function(amount){
-  checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+  // Check contents (integer form) of text input field
+  // Sum together that number and the number parsed from the current balance
+  // Clear the text input field
 
 }
 
 function checkingWithdrawal(amount){
+  // if amount > checkingBalance, check if the remaining withdrawal request could be taken from savingsBalance
+  //  if remainder can be taken from savingsBalance, empty checkingBalance then take remainder from savingsBalance
+  //clear the text input field
 
 }
 
 function savingsDeposit(amount){
-
+// Check contents (integer form) of text input field
+// Sum together that number and the number parsed from the current balance
+// Clear the text input field
 }
 
 function savingsWithdrawal(amount){
-
+//if amount > savingsBalance, clear the entry field and don't change the balance value
+//else, withdraw the amount
+//clear the text input field
 }
 
 // an eventListerner for each button, each one a "click"
