28094d0423f42c07285b2e254b306467c6bbdb25
Preston Davies <pdavies88@gmail.com>
2015-07-09 22:26:50 -0400

    completed ATM with ability to draw from both accounts if one is overdrawn

--- atm.js
+++ atm.js
@@ -15,9 +15,17 @@ if(checkDepositTotal - parseInt($("input.checkingAmount").val(),10) >= 0 ) {
   checkDepositTotal -= inputAmount;
   $("#checking_balance.balance").html("$" + checkDepositTotal);
 }
-else{
+else if (checkDepositTotal - parseInt($("input.checkingAmount").val(),10) < 0 ) {
+  if(savingsDepositTotal - parseInt($("input.checkingAmount").val(),10) >= 0 ) {
+  var inputAmount = parseInt($("input.checkingAmount").val(),10);
+  savingsDepositTotal -= inputAmount;
+  $("#savings_balance.balance").html("$" + savingsDepositTotal)
+}
+else {
   $(".checkingAccount").addClass("zero");
+  $(".savingsAccount").addClass("zero");
   alert("Not enough funds");
+    }
   }
 });
 
@@ -37,8 +45,16 @@ if(savingsDepositTotal - parseInt($("input.savingsAmount").val(),10) >= 0 ) {
   savingsDepositTotal -= inputAmount;
   $("#savings_balance.balance").html("$" + savingsDepositTotal)
 }
+else if (savingsDepositTotal - parseInt($("input.savingsAmount").val(),10) < 0 ) {
+  if(checkDepositTotal - parseInt($("input.savingsAmount").val(),10) >= 0 ) {
+  var inputAmount = parseInt($("input.savingsAmount").val(),10);
+  checkDepositTotal -= inputAmount;
+  $("#checking_balance.balance").html("$" + checkDepositTotal)
+}
 else {
+  $(".checkingAccount").addClass("zero");
   $(".savingsAccount").addClass("zero");
   alert("Not enough funds");
+    }
   }
 });
