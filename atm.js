1d02f0b72d67b680ae9e84bc4ca442d4ca911f6f
Preston Davies <pdavies88@gmail.com>
2015-07-09 21:52:30 -0400

    overdraft protection activated

--- atm.js
+++ atm.js
@@ -6,11 +6,17 @@ $(".checkingDeposit").on("click", function checkingDeposit(amount){
   $("#checking_balance.balance").html("$" + checkDepositTotal)
 });
 
+
 $(".checkingWithdrawal").on("click", function checkingWithdrawal(amount){
 console.log("checking withdrawal")
-var inputAmount = parseInt($("input.checkingAmount").val(),10);
-checkDepositTotal -= inputAmount;
-$("#checking_balance.balance").html("$" + checkDepositTotal)
+if(checkDepositTotal - parseInt($("input.checkingAmount").val(),10) > 0 ) {
+  var inputAmount = parseInt($("input.checkingAmount").val(),10);
+  checkDepositTotal -= inputAmount;
+  $("#checking_balance.balance").html("$" + checkDepositTotal);
+}
+else{
+  alert("Not enough funds");
+  }
 });
 
 savingsDepositTotal = 0;
@@ -23,7 +29,12 @@ $("#savings_balance.balance").html("$" + savingsDepositTotal)
 
 $(".savingsWithdrawal").on("click", function savingsWithdrawal(amount){
 console.log("savings withdrawal")
-var inputAmount = parseInt($("input.savingsAmount").val(),10);
-savingsDepositTotal -= inputAmount;
-$("#savings_balance.balance").html("$" + savingsDepositTotal)
+if(savingsDepositTotal - parseInt($("input.savingsAmount").val(),10) > 0 ) {
+  var inputAmount = parseInt($("input.savingsAmount").val(),10);
+  savingsDepositTotal -= inputAmount;
+  $("#savings_balance.balance").html("$" + savingsDepositTotal)
+}
+else {
+alert("Not enough funds");
+  }
 });
