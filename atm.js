b8d34f3c66837117caf32d225ce026047e08cbf9
Karl Sakas <donusdonut@Karls-MBP.home>
2015-07-10 07:59:52 -0400

    added fully functioning color scheme to atm. MVP minus dollar signs

--- atm.js
+++ atm.js
@@ -6,14 +6,30 @@ var cInput;
 var cBalance = 0;
 var sBalance = 0;
 
+var inTheRed = function () {
+   if (cBalance === 0) {
+     $(".balance:nth-child(2)").eq(0).addClass("zero")
+   }
+   else {
+     $(".balance:nth-child(2)").eq(0).removeClass("zero")
+   }
+   if (sBalance === 0) {
+     $(".balance:nth-child(2)").eq(1).addClass("zero")
+   }
+   else {
+     $(".balance:nth-child(2)").eq(1).removeClass("zero")
+}}
 
+//Starting in the Ready
+inTheRed();
 
-//CHECKING
+//Deposit CHECKING
 $("#cDepositButton").on("click", function(event){
   cInput = $("#cField").val();
   cBalance = cBalance + parseFloat(cInput) ;
   $(".balance:nth-child(2)").eq(0).html(cBalance);
   $("#cField").val("");
+  inTheRed();
 })
 //Deposit Savings
 $("#sDepositButton").on("click", function(event){
@@ -23,7 +39,7 @@ $("#sDepositButton").on("click", function(event){
   $(".balance:nth-child(2)").eq(1).html(sBalance);
   $("#sField").val("");
   $(".balance:nth-child(2)").eq(1).removeClass("zero")
-
+  inTheRed();
 })
 
 //Withdraw Savings
@@ -32,36 +48,15 @@ $("#sWithdrawButton").on("click", function(event){
       if (parseFloat(sInput) <= sBalance) {
           sBalance = sBalance - parseFloat(sInput) ;
           $(".balance:nth-child(2)").eq(1).html(sBalance);
-          $("#sField").val("");}
+          $("#sField").val("");
+          inTheRed();}
       else {
           alert("Insufficient Funds");
           $("#cField").val("");}
-      if (sBalance === 0) {
-          $(".balance:nth-child(2)").eq(1).addClass("zero")
-          }
         })
 
-// COMMIT FOUR - As a user, I want overdraft protection
-// $("#cWithdrawButton").on("click", function(event){
-//   cInput = $("#cField").val();
-//   var overDraftTotal = cBalance + sBalance;
-//   console.log(overDraftTotal)
-//       if (parseFloat(cInput) <= cBalance) {
-//           cBalance = cBalance - parseFloat(cInput) ;
-//           $(".balance:nth-child(2)").eq(0).html(cBalance);
-//           $("#cField").val("");}
-//       else if (parseFloat(cInput) <= overDraftTotal) {
-//           sBalance = sBalance - parseFloat(cInput) ;
-//           $(".balance:nth-child(2)").eq(1).html(sBalance);
-//           $("#cField").val("");
-//       }
-//       else {
-//           alert("Insufficient Funds");
-//           $("#cField").val("");}
-//
-// })
 
-//Trial
+//Checking Withdraw and Overdraft Protection
 
 $("#cWithdrawButton").on("click", function(event){
   cInput = $("#cField").val();
@@ -69,7 +64,9 @@ $("#cWithdrawButton").on("click", function(event){
       if (parseFloat(cInput) <= cBalance) {
           cBalance = cBalance - parseFloat(cInput) ;
           $(".balance:nth-child(2)").eq(0).html(cBalance);
-          $("#cField").val("");}
+          $("#cField").val("");
+          inTheRed();
+          }
       else if (parseFloat(cInput) <= totalAccount) {
           var carryOverCharge = (cBalance - parseFloat(cInput))
           sBalance = sBalance + carryOverCharge;
@@ -77,70 +74,14 @@ $("#cWithdrawButton").on("click", function(event){
           $(".balance:nth-child(2)").eq(1).html(sBalance);
           $(".balance:nth-child(2)").eq(0).html(cBalance);
           $("#cField").val("");
+          inTheRed();
       }
       else {
           alert("Insufficient Funds");
           $("#cField").val("");}
-
 })
 
-//Color change
-// if (cBalance === 0) {
-//   $(".balance:nth-child(2)").eq(0).addClass("zero")
-// }
-// else {
-//   $(".balance:nth-child(2)").eq(0).removeClass("zero")
-// }
-// if (sBalance === 0) {
-//   $(".balance:nth-child(2)").eq(1).addClass("zero")
-// }
-// else {
-//   $(".balance:nth-child(2)").eq(1).removeClass("zero")
-// }
-// if  (cBalance === 0) {
-//         $(".balance:nth-child(2)").eq(0).css("background-color", "#F52F4F");
-//     }
-// else if  (sBalance === 0) {
-//         $(".balance:nth-child(2)").eq(1).css("background-color", "#F52F4F");
-//     }
-// else {
-//           $(".balance:nth-child(2)").eq(0).css("background-color", "white");
-//     }
-
-// if  (cBalance === 0) {
-//     $(".balance:nth-child(2)").eq(0).css("background-color", "#F52F4F");
-// }
-// else {
-//       $(".balance:nth-child(2)").eq(0).css("background-color", "white");
-// }
-// - What happens when the user wants to withdraw more money from the checking account than is in the account?
-//See above
-
-//- If a withdrawal can be covered by the balances in both accounts, take the balance of the account withdrawn from down to $0 and take the rest of the withdrawal from the other account.
-//See above
-
-// - If the withdrawal amount is more than the combined account balance, ignore it.
-//see above
-
-// COMMIT EIGHT - As a user, I want the color of my back account to reflect it's balance (there's a CSS class called .zero already written for this!)
-
-
-// COMMIT NINE - Are there ways to refactor your code to make it DRYer or more Object-Oriented?
-
-
-//
-// ###Tips
-// * Tackle making your accounts work individually first
-// * Then tackle them working together with overdraft protection
-// * Only start working with the DOM after you have the logic down
-//
-// ###Resources
-// * [What is window.onload for?](https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers.onload)
-// * [Adding Event Listeners so things happen when you click](https://developer.mozilla.org/en-US/docs/Web/API/EventTarget.addEventListener)
-// * [Adding or removing a CSS class using JS](https://developer.mozilla.org/en-US/docs/Web/API/Element.classList)
-// * [Changing HTML with JavaScript](https://developer.mozilla.org/en-US/docs/Web/API/Element.innerHTML)
-
-//ORIGINAL
+//ORIGINAL STARTING CODE IS BELOW
 // var checkingBalance = document.querySelector("div#checking_balance");
 // checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
 //
