471a42c6227e2a6b7fefbb0427bf8356804b5c07
Kevin Cole Jr <KevinColeLr@gmail.com>
2015-07-10 10:38:10 -0400

    initial commit

--- atm.js
+++ atm.js
@@ -1,21 +1,59 @@
-var checkingBalance = document.querySelector("div#checking_balance");
-checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+var checkingBalance = 0;
+var savingsBalance = 0;
 
-var checkingDeposit = function(amount){
-  checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+/*var savingsBalance = document.querySelector("div#savings_balance");
+savingsBalance = parseInt( savingsBalance.innerHTML.replace("$", "") );
+*/
 
-}
+//var checking_amount = parseInt( checkingBalance.innerHTML.replace("$", "") );
+
 
 function checkingWithdrawal(amount){
 
 }
 
 function savingsDeposit(amount){
-
+  $(".savingsDepositBtn").on('click', function(){
+      var dollarAmount = $(".savings_Input").val();
+        $("#savings_Input").text("$" + dollarAmount);
+  });
 }
+
+
+
+
 
 function savingsWithdrawal(amount){
 
 }
 
 // an eventListerner for each button, each one a "click"
+//deposit button
+$(".checkingDepositBtn").on('click', function depositCheckingMoney(){//when button is clicked
+    var dollarAmount = parseInt($(".checking_input").val(),10);//take value placed in the input field
+      $("#checking_balance ").text(dollarAmount);// get checking class text(Giant Box). Change to input field aka dollarAmount
+       newCheckingMoney = dollarAmount += checkingBalance;//add dollar amount plus existing checking balance
+            console.log(newCheckingMoney);
+            ("#checking_balance").html("$" + newCheckingMoney);
+
+});
+
+//withdrawal button
+$(".checkingWithdrawBtn").on('click', function(){
+  alert("you clicked");
+});
+////////////////////////////////////////////////////////////////
+
+//savings Button
+$(".savingsDepositBtn").on('click', function(){
+    var savingsDollarAmount = parseInt($(".savings_Input").val(),10);
+      $("#savings_balance").text(savingsDollarAmount);
+        var newSavingsMoney = (savingsDollarAmount += savingsBalance);
+          console.log(newSavingsMoney);
+});
+
+//savings withdrawal button
+$(".savingsWithdrawBtn").on('click', function(){
+  alert("you clicked");
+});
+//target class of balance
