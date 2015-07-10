a68eedc5ce55e6dbac2db3b4df311fbf2be68347
Erica Andersen <andersenek@gmail.com>
2015-07-09 20:24:43 -0400

    Strictly using .js rather than editing HTML. Added functionality for user to deposit money into both banks accounts

--- atm.js
+++ atm.js
@@ -1,15 +1,20 @@
 // As a user, I want to deposit money into one of the bank accounts
-var balanceDisplay = $(".account").children().eq(1).html(); // Selects Balance HTML Area (cannot be called in function?)
-var userInput = $(".account").children().eq(2); // Selects User Input
+var balanceDisplay = $(".account").children().eq(1); // Selects Checking Balance HTML Area
+var userInputChecking = $(".account").children().eq(2); // Selects Checking User Input
 var checkDepositButton = $(".account").children().eq(3); // Checking Deposit Button
 var checkWithdrawlButton = $(".account").children().eq(4); // Checking Withdrawl Button
 
+var balanceDisplaySavings = $(".account").children().eq(6); // Selects Savings Balance HTML Area
+var userInputSavings = $(".account").children().eq(7); // Selects Savings User Input
+var savingsDepositButton = $(".account").children().eq(8); // Savings Deposit Button
+var savingsDepositButton = $(".account").children().eq(9); // Savings Withdrawl Button
+
 $(checkDepositButton).click(function() { // Set up Checking Deposit button
   event.preventDefault(); // Prevents Default Refresh
-  var depAmount = parseInt($( userInput ).val()); // Get Deposit Value
-  var balance = parseInt($( "#check-balance" ).text().replace("$", "")) // Remove '$' from Balance HTML
+  var depAmount = parseInt($( userInputChecking ).val()); // Get Deposit Value
+  var balance = parseInt(balanceDisplay.html().replace("$", "")) // Remove '$' from Balance HTML
   var newBalance = "$" + (balance + depAmount) // New Balance Value + '$'
-  $( "#check-balance" ).html(newBalance); // Replace Balance HTML with New Balance Value *Does not work with stored variable
+  balanceDisplay.html(newBalance); // Replace Balance HTML with New Balance Value
 
   console.log("depAmount is: " + depAmount)
   console.log("depAmount type is: " + typeof(depAmount))
@@ -21,23 +26,28 @@ $(checkDepositButton).click(function() { // Set up Checking Deposit button
 
 $(checkWithdrawlButton).click(function() {
   event.preventDefault(); // Prevents Default Refresh
-  var withAmount = parseInt($( userInput ).val()); // Get Withdrawl Value
-  var balance = parseInt($( "#check-balance" ).text().replace("$", "")) // Remove '$' from Balance HTML
+  var withAmount = parseInt($( userInputChecking ).val()); // Get Withdrawl Value
+  var balance = parseInt(balanceDisplay.html().replace("$", "")) // Remove '$' from Balance HTML
 
   if((balance - withAmount) > 0){
     var newBalance = "$" + (balance - withAmount) // New Balance Value + '$'
-    $( "#check-balance" ).html(newBalance); // Replace Balance HTML with New Balance Value
+    balanceDisplay.html(newBalance); // Replace Balance HTML with New Balance Value
   }
   else {
-    $( "#check-balance" ).html("$0"); // Ensure Balance cannot go below $0
+    balanceDisplay.html("$0"); // Ensure Balance cannot go below $0
   }
   console.log("Check Withdrawl button is working");
 });
 
+$(savingsDepositButton).click(function(){
+  event.preventDefault(); // Prevents Default Refresh
+  var depAmount = parseInt($( userInputSavings ).val()); // Get Deposit Value
+  var balance = parseInt(balanceDisplaySavings.html().replace("$", "")) // Remove '$' from Balance HTML
+  var newBalance = "$" + (balance + depAmount) // New Balance Value + '$'
+  balanceDisplaySavings.html(newBalance); // Replace Balance HTML with New Balance Value
 
-function savingsDeposit(amount){
-
-}
+  console.log("Savings Deposit button is working");
+});
 
 function savingsWithdrawal(amount){
 
