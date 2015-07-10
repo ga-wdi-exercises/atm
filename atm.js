12d8754d1b32f13664bcae20f3c9d4063989a322
Stephen Gheysens <segheysens@gmail.com>
2015-07-10 04:01:59 -0400

    Added event listeners for the 4 buttons

--- atm.js
+++ atm.js
@@ -1,34 +1,45 @@
 var checkingBalance = $(".balance")[0];
 var checkingInt = Number.parseInt(checkingBalance.innerHTML.replace("$", ""));
-console.log(checkingInt);
+// console.log(checkingInt);
 var savingsBalance = $(".balance")[1];
 var savingsInt = Number.parseInt(savingsBalance.innerHTML.replace("$", ""));
-console.log(savingsInt);
+// console.log(savingsInt);
 
-var checkingDeposit = function(amount){
+
+// add eventListener to each button
+$("[value=\"Deposit\"]")[0].click( checkingDeposit(event) );
+$("[value=\"Withdrawal\"]")[0].click( checkingWithdrawal(event) );
+
+// event listeners for savings account buttons
+$("[value=\"Deposit\"]")[1].click( savingsDeposit(event) );
+$("[value=\"Withdrawal\"]")[1].click( savingsWithdrawal(event) );
+
+
+function checkingDeposit(event){
   // Check contents (integer form) of text input field
   // Sum together that number and the number parsed from the current balance
   // Clear the text input field
 
 }
 
-function checkingWithdrawal(amount){
+function checkingWithdrawal(event){
   // if amount > checkingBalance, check if the remaining withdrawal request could be taken from savingsBalance
   //  if remainder can be taken from savingsBalance, empty checkingBalance then take remainder from savingsBalance
   //clear the text input field
 
+  // if checking account balance = 0, change class to 'zero'
 }
 
-function savingsDeposit(amount){
+function savingsDeposit(event){
 // Check contents (integer form) of text input field
 // Sum together that number and the number parsed from the current balance
 // Clear the text input field
 }
 
-function savingsWithdrawal(amount){
+function savingsWithdrawal(event){
 //if amount > savingsBalance, clear the entry field and don't change the balance value
 //else, withdraw the amount
 //clear the text input field
-}
 
-// an eventListerner for each button, each one a "click"
+// if savings account balance = 0, change class to 'zero'
+}
