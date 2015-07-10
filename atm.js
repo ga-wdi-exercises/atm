9dd0051b597f3e5ac53e476fd0759b8eb4be5609
Ksenia Coulter <kzherdeva@ucdavis.edu>
2015-07-09 23:05:44 -0400

    got deposit method to work on both savings and checking. Will change the display in a separate function

--- atm.js
+++ atm.js
@@ -8,31 +8,32 @@ var atm = {
     savings: 0
   },
   listen: function(){
-    document.querySelector(".account").addEventListener("click", function() {
+    $(".account").on("click", function() {
       var actionType = $(event.target).val();//Deposit of withdraw
       var accountType = $(event.target).prevAll("h2").html();//Checking or savings
       var amount = parseInt($(event.target).prevAll("[type=text]").val());//amount
-      if($(event.target).val() === "Deposit"){
-        this.deposit()
+      if( actionType === "Deposit"){
+        this.deposit(accountType,amount)
       }
-      if($(event.target).val() === "Withdraw"){
-        this.withdraw()
+      if( actionType === "Withdraw"){
+        this.withdraw(accountType,amount)
       }
     }.bind(this))
   },
-  deposit: function(transaction,account,amount){
+  deposit: function(account,amount){
     event.preventDefault();
-    accounts.this += parseInt($("input:first").val());
-    $("input:first").val(""); //empty the form after submission
-    //Change the html element that indicates account balance
-    $(".balance").eq(0).html("$" + accountBalance);
+    account = account.toLowerCase();
+    this.accounts[account] += amount;
+    console.log("checking balance is " + atm.accounts.checking);
+    console.log("savings balance is " + atm.accounts.savings);
+    $(event.target).prevAll("[type=text]").val(""); //empty the form after submission
   },
   withdraw: function(transaction,account,amount){
     event.preventDefault();
     var withdrawAmount = $("input:first").val();
     if(accountBalance >= withdrawAmount){
       accountBalance -= parseInt(withdrawAmount);
-      $("input:first").val(""); //empty the form after submission
+      $(event.target).prevAll("[type=text]").val(""); //empty the form after submission
       $(".balance").eq(0).html("$" + accountBalance);
     }
   }
