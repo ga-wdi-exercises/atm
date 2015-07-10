7d47713a716e6be0490fa13a0c35221dcd1a11c7
Stephen Gheysens <segheysens@gmail.com>
2015-07-10 05:27:45 -0400

    Completed functionality and specifications of banking functions, thoroughly tested. Added comments.

--- atm.js
+++ atm.js
@@ -1,8 +1,10 @@
 var checkingBalance = $(".balance")[0];
 var checkingInt = Number.parseInt(checkingBalance.innerHTML.replace("$", ""));
+$(".balance").eq(0).addClass("zero");
 // console.log(checkingInt);
 var savingsBalance = $(".balance")[1];
 var savingsInt = Number.parseInt(savingsBalance.innerHTML.replace("$", ""));
+$(".balance").eq(1).addClass("zero");
 // console.log(savingsInt);
 
 
@@ -15,39 +17,102 @@ $("[value=\"Deposit\"]").eq(1).click( function(event){savingsDeposit(event);} );
 $("[value=\"Withdraw\"]").eq(1).click( function(event){savingsWithdrawal(event);} );
 
 
+
 function checkingDeposit(event){
+  // Return if text input is empty string
+  if($("[type=\"text\"]").eq(0).val() === ""){
+    return;
+  }
   // Check contents (integer form) of text input field
   checkingInt = Number.parseInt(checkingBalance.innerHTML.replace("$", ""));
-  console.log("CHECKING DEPOSIT")
   // Sum together that number and the number parsed from the current balance
+  checkingBalance.innerHTML = "$" + (checkingInt + Number.parseInt($("[type=\"text\"]").eq(0).val()));
   // Clear the text input field
+  $("[type=\"text\"]").eq(0).val("");
 
+  // if the checking account balance > 0, add the class 'zero'
+  if(Number.parseInt(checkingBalance.innerHTML.replace("$", "")) > 0){
+    $(".balance").eq(0).removeClass("zero");
+  }
 }
+
+
 
 function checkingWithdrawal(event){
-  checkingInt = Number.parseInt(checkingBalance.innerHTML.replace("$", ""));
-    console.log("CHECKING withdrawal")
-  // if amount > checkingBalance, check if the remaining withdrawal request could be taken from savingsBalance
-  //  if remainder can be taken from savingsBalance, empty checkingBalance then take remainder from savingsBalance
-  //clear the text input field
+  // Return if text input is empty string
+  if($("[type=\"text\"]").eq(0).val() === ""){
+    return;
+  }
 
-  // if checking account balance = 0, change class to 'zero'
+  // Get current balances:
+  checkingInt = Number.parseInt(checkingBalance.innerHTML.replace("$", ""));
+  savingsInt = Number.parseInt(savingsBalance.innerHTML.replace("$", ""));
+  // if withdraw > checkingBalance, check if the remaining withdrawal request could be taken from savingsBalance
+  if (Number.parseInt($("[type=\"text\"]").eq(0).val()) > checkingInt){
+    console.log("if 1");
+    //  if remainder can be taken from savingsBalance, make checkingBalance zero then take the remainder from savingsBalance
+    if(savingsInt - (Number.parseInt($("[type=\"text\"]").eq(0).val()) - checkingInt) >= 0){
+      console.log("if 2");
+      checkingBalance.innerHTML = "$0";
+      savingsBalance.innerHTML = "$" + (savingsInt - (Number.parseInt($("[type=\"text\"]").eq(0).val()) - checkingInt));
+    }
+  }
+  // else (withdraw is <= checkingBalance), withdraw the money
+  else{
+    checkingBalance.innerHTML = "$" + (checkingInt - Number.parseInt($("[type=\"text\"]").eq(0).val()));
+  }
+
+  // if the checking account balance = 0, add the class 'zero'
+  if(Number.parseInt(checkingBalance.innerHTML.replace("$", "")) === 0){
+    $(".balance").eq(0).addClass("zero");
+  }
+  // if the savings account balance = 0, add the class 'zero'
+  if(Number.parseInt(savingsBalance.innerHTML.replace("$", "")) === 0){
+    $(".balance").eq(1).addClass("zero");
+  }
+
+  // Clear the text input field
+  $("[type=\"text\"]").eq(0).val("");
 }
+
+
 
 function savingsDeposit(event){
-// Check contents (integer form) of text input field
-savingsInt = Number.parseInt(savingsBalance.innerHTML.replace("$", ""));
-  console.log("SAVINGS DEPOSIT")
-// Sum together that number and the number parsed from the current balance
-// Clear the text input field
+  // Return if text input is empty string
+  if($("[type=\"text\"]").eq(1).val() === ""){
+    return;
+  }
+
+  // Check contents (integer form) of text input field
+  savingsInt = Number.parseInt(savingsBalance.innerHTML.replace("$", ""));
+  // Sum together that number and the number parsed from the current balance
+  savingsBalance.innerHTML = "$" + (savingsInt + Number.parseInt($("[type=\"text\"]").eq(1).val()));
+  // Clear the text input field
+  $("[type=\"text\"]").eq(1).val("");
+
+  // if the checking account balance > 0, add the class 'zero'
+  if(Number.parseInt(savingsBalance.innerHTML.replace("$", "")) > 0){
+    $(".balance").eq(1).removeClass("zero");
+  }
 }
 
-function savingsWithdrawal(event){
-savingsInt = Number.parseInt(savingsBalance.innerHTML.replace("$", ""));
-  console.log("SAVINGS withdrawal")
-//if amount > savingsBalance, clear the entry field and don't change the balance value
-//else, withdraw the amount
-//clear the text input field
 
-// if savings account balance = 0, change class to 'zero'
+
+function savingsWithdrawal(event){
+  // Return if text input is empty string
+  if($("[type=\"text\"]").eq(1).val() === ""){
+    return;
+  }
+  savingsInt = Number.parseInt(savingsBalance.innerHTML.replace("$", ""));
+  //if the text input field < savings balancs, withdraw the amount
+  if (Number.parseInt($("[type=\"text\"]").eq(1).val()) < savingsInt){
+    savingsBalance.innerHTML = "$" + (savingsInt - Number.parseInt($("[type=\"text\"]").eq(1).val()));
+  }
+  //clear the text input field
+  $("[type=\"text\"]").eq(1).val("");
+  // if the savings account balance = 0, add the class 'zero'
+  if(Number.parseInt(savingsBalance.innerHTML.replace("$", "")) === 0){
+    $(".balance").eq(1).addClass("zero");
+  }
+
 }
