dacd450023d9a70cf0624b9b1dd486005fa33bdd
Omari Mayers-Walker <omari@fastmail.fm>
2015-07-10 00:11:07 -0400

    scrapped original atm.js; simplified the code; checking and savings are complete

--- atm.js
+++ atm.js
@@ -1,133 +1,46 @@
 // document ready
 
-$(document).ready(function() {
+$(document).ready(function(){
     console.log("ready");
-});
 
 //Global Vars
-
-var checkingInput = $('#checkingInput').val();
-var checkDepositButton = $('#checkDepositButton');
-var checkWithdrawButton = $('#checkWithdrawButton');
-
-var savingsInput = $('#savingsInput').val();
-var saveDepositButton = $('saveDepositButton');
-var saveWithdrawButton = $('saveWithdrawButton');
-
-var checkingBalance = $("#checking_balance");
-var savingsBalance = $('#savings_balance');
 
 var currentCheckBalance = 0;
 var currentSaveBalance = 0;
 
-// checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
-
-// var checkingDeposit = function(amount){
-  // checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
-
-// };
-
-function deposit (deposit){
-    //be able to deposit depending on which button is pushed
-
-    if (deposit == checkingInput){
-        // add amount to checking balance
-        currentCheckBalance = currentCheckBalance + parseInt(deposit);
-        console.log(currentCheckBalance);
-        // triggered by eventListerner
-        // checkingBalance = parseInt(checkingBalance.html());
-    }
-
-    else if (deposit == savingsInput){
-        // add amount to savings balance
-        currentSaveBalance = currentSaveBalance + parseInt(deposit);
-        console.log(currentSaveBalance);
-        // triggered by eventListerner
-        // savingsBalance = parseInt(checkingBalance.html());
-    }
-    return currentCheckBalance || currentSaveBalance;
-}
-
-function withdraw (withdraw){
-    if (withdraw == checkingInput){
-        currentCheckBalance = currentCheckBalance - parseInt(withdraw);
-    }
-
-    else if (withdraw == savingsInput){
-        currentSaveBalance = currentSaveBalance - parseInt(withdraw);
-        console.log('withdraw output');
-    }
-    return currentCheckBalance && currentSaveBalance;
-}
-
-// balance window function
-
-function balanceWindow (accountBalance){
-    var checkBalanceWindow = $('#checkBalanceWindow');
-    var saveBalanceWindow = $('#saveBalanceWindow');
-
-    if (accountBalance == currentCheckBalance){
-        checkBalanceWindow.html(currentCheckBalance);
-    }
-    else if (accountBalance == currentSaveBalance){
-        saveBalanceWindow.html(currentSaveBalance);
-    }
-
-};
-
-balanceWindow(currentCheckBalance);
 // event listeners
 
 // checking deposit button
 
-$('#checkDepositButton').click('click', function(event){
-    event.preventDefault();
-    console.log(checkingInput);
-    // take #checkingingInput; store it; pass it to deposit()
-    deposit(checkingInput);
-    balanceWindow(currentCheckBalance);
+$('#checkDepositButton').click('click', function(){
+    var deposit = $('#checkingInput').val();
+    currentCheckBalance = currentCheckBalance + parseInt(deposit);
+    console.log(currentCheckBalance);
+    $('#checkBalanceWindow').text('$' + currentCheckBalance);
 });
 
 // withdraw deposit button
 
-$('#checkWithdrawButton').click('click', function(event){
-    console.log(checkWithdrawButton);
-    event.preventDefault();
-    withdraw(checkingInput);
-    balanceWindow(currentCheckBalance);
+$('#checkWithdrawButton').click('click', function(){
+    var withdraw = $('#checkingInput').val();
+    currentCheckBalance = currentCheckBalance - parseInt(withdraw);
+    $('#checkBalanceWindow').text('$' + currentCheckBalance);
 });
 
 // save deposit button
 
-$('#saveDepositButton').click('click', function(event){
-    console.log('saveDepositButton');
-    // take #savingsInput; store it; pass it to deposit()
-    deposit(savingsInput);
-    balanceWindow(currentSaveBalance);
+$('#saveDepositButton').click('click', function(){
+    var deposit = $('#savingsInput').val();
+    currentSaveBalance = currentSaveBalance + parseInt(deposit);
+    $('#saveBalanceWindow').text('$' + currentSaveBalance);
 });
 
 // withdraw savings button
 
-$('#saveWithdrawButton').click('click', function(event){
-    console.log('saveWithdrawButton');
-    event.preventDefault();
-    withdraw(savingsInput);
-    balanceWindow(currentSaveBalance);
+$('#saveWithdrawButton').click('click', function(){
+    var withdraw = $('#savingsInput').val();
+    currentSaveBalance = currentSaveBalance - parseInt(withdraw);
+    $('#saveBalanceWindow').text('$' + currentSaveBalance);
 });
 
-
-
-
-// function checkingWithdrawal(amount){
-
-// }
-
-// function savingsDeposit(amount){
-
-// }
-
-// function savingsWithdrawal(amount){
-
-// }
-
-// an eventListerner for each button, each one a "click"
+});
