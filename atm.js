d14d3d5ffc9b169ff334799d51f764b153fb6e93
Andrew Smith-Mui <arsm800@gmail.com>
2016-03-01 18:47:10 -0500

    establish DOM jQuery element variables

--- atm.js
+++ atm.js
@@ -1,9 +1,24 @@
-var checkingBalance = document.querySelector("div#checking_balance");
-checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+var checkingBalance = $(".balance").eq(0);
+// checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
 
-var checkingDeposit = function(amount){
-  checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+var savingsBalance = $(".balance").eq(1);
 
+//var checkingDeposit = function(amount){
+//  checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+//};
+
+var savDepButton = $("input").eq(1);
+var savWitButton = $("input").eq(2);
+var chkDepButton = $("input").eq(4);
+var chkWitButton = $("input").eq(5);
+
+function checkingDeposit() {
+//user enters number into input field
+//hits deposit button
+    //eventListerner "input"
+//total dollar amount updates
+  //update value of div .balance
+    //create variable = 0 for sum total; then do checkingBalance += input
 }
 
 function checkingWithdrawal(amount){
