d929779b0a3821c55ceaa87bab4b4f664ca2e096
Cam <camlatimer@gmail.com>
2016-03-01 18:58:40 -0500

    commit7

--- atm.js
+++ atm.js
@@ -6,77 +6,101 @@ var checkingBoxVal = parseInt(checkingBox.val());
 var savingsBox = $('#savings-input');
 var savingsBoxVal = savingsBox.val();
 var checkBalanceDisplay = $('#checking-display');
+var savingsDisplay = $('#savings-display');
   //checking buttons
 var checkDBtn = $('#checkbtn-d');
 var checkWBtn = $('#checkbtn-w');
   //savings buttons
 var savingsDBtn = $('#savingsbtn-d');
 var savingsWBtn = $('#savingsbtn-w');
+var checkingLeftOver;
+var savingsLeftover;
 
 // As a user, I want to deposit money into one of the bank accounts
-  //make bank accounts
+  //make bank account constructor
 function Accnt() {
     this.totalBal = 0;
     // As a user, I want to withdraw money from one of the bank accounts
     this.deposit = function(dep_amnt) {
-      this.totalBal += dep_amnt;
+      return this.totalBal += dep_amnt;
     };
     this.withdrawal = function(withdraw_amnt) {
-      this.totalBal = this.totalBal - withdraw_amnt;
+      return this.totalBal = this.totalBal - withdraw_amnt;
     };
   }
+  //make bank accounts, one for checking, one for savings
 var checkingAccnt = new Accnt();
 var savingsAccnt = new Accnt();
 
 //event handlers
   //checking account
-  //on deposit click, deposit
+    //on deposit click, deposit
 checkDBtn.click(function() {
     checkingAccnt.deposit(parseInt(checkingBox.val()));
     checkingBox.val('');
-
+    checkBalanceDisplay.html('$' + checkingAccnt.totalBal);
     console.log(checkingAccnt.totalBal);
 });
-  //on withdraw click, withdraw
+    //on withdraw click, withdraw
 checkWBtn.click(function() {
-  checkingAccnt.withdrawal(parseInt(checkingBox.val()));
-  checkingBox.val('');
-  console.log(checkingAccnt.totalBal);
+  // Make sure the balance in an account can't go negative. If a user tries to withdraw more money than exists in the account, ignore the transaction.
+  var withdraw_amnt = parseInt(checkingBox.val());
+    // If a withdrawal can be covered by the balances in both accounts, take the balance of the account withdrawn from down to $0 and take the rest of the withdrawal from the other account.
+  if (withdraw_amnt >= checkingAccnt.totalBal &&
+    checkingAccnt.totalBal > 0 &&
+    savingsAccnt.totalBal > 0) {
+    checkingLeftOver = checkingAccnt.withdrawal(withdraw_amnt);
+    checkingAccnt.totalBal = 0;
+    checkingBox.val('');
+    checkBalanceDisplay.html('$' + checkingAccnt.totalBal);
+    savingsAccnt.withdrawal(Math.abs(checkingLeftOver));
+    savingsDisplay.html('$' + savingsAccnt.totalBal);
+
+  } else if (withdraw_amnt < checkingAccnt.totalBal) {
+    checkingAccnt.withdrawal(withdraw_amnt);
+    checkingBox.val('');
+    checkBalanceDisplay.html('$' + checkingAccnt.totalBal);
+    console.log(checkingAccnt.totalBal);
+  }
+  // If the withdrawal amount is more than the combined account balance, ignore it.
+  else {
+    console.log('overdraft warning! entered amount can\t be converd');
+  }
 })
   //savings account
+    //on click, deposit
+  savingsDBtn.click(function() {
+    savingsAccnt.deposit(parseInt(savingsBox.val()));
+    savingsBox.val('');
+    savingsDisplay.html('$' + savingsAccnt.totalBal);
+    console.log(savingsAccnt.totalBal);
+  })
+    //on click, withdraw
+  savingsWBtn.click(function() {
 
-//make a constructor function for both checking and svings accounts
+    var withdraw_amnt = parseInt(savingsBox.val());
 
+    if (withdraw_amnt >= savingsAccnt.totalBal &&
+      savingsAccnt.totalBal > 0 &&
+      checkingAccnt.totalBal > 0) {
+      savingsLeftOver = savingsAccnt.withdrawal(withdraw_amnt);
+      savingsAccnt.totalBal = 0;
+      savingsBox.val('');
+      savingsDisplay.html('$' + savingsAccnt.totalBal);
+      checkingAccnt.withdrawal(Math.abs(savingsLeftOver));
+      checkBalanceDisplay.html('$' + checkingAccnt.totalBal);
 
-// var checkingBalance = document.querySelector(".balance");
-// checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
-//
-// var checkingDeposit = function(amount){
-//   checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
-//
-// }
-//
-// function checkingWithdrawal(amount){
-//
-// }
-//
-// function savingsDeposit(amount){
-//
-// }
-//
-// function savingsWithdrawal(amount){
-//
-// }
-
-// an eventListerner for each button, each one a "click"
+    } else if (withdraw_amnt < savingsAccnt.totalBal) {
+      savingsAccnt.withdrawal(withdraw_amnt);
+      savingsBox.val('');
+      savingsDisplay.html('$' + savingsAccnt.totalBal);
+      console.log(savingsAccnt.totalBal);
+    } else {
+      console.log('overdraft warning! entered amount can\t be converd');
+    }
+  })
 
 
 
-
-// Make sure the balance in an account can't go negative. If a user tries to withdraw more money than exists in the account, ignore the transaction.
-// As a user, I want overdraft protection
-// What happens when the user wants to withdraw more money from the checking account than is in the account?
-// If a withdrawal can be covered by the balances in both accounts, take the balance of the account withdrawn from down to $0 and take the rest of the withdrawal from the other account.
-// If the withdrawal amount is more than the combined account balance, ignore it.
 // As a user, I want the color of my back account to reflect it's balance (there's a CSS class called .zero already written for this!)
 // Are there ways to refactor your code to make it DRYer or more Object-Oriented?
