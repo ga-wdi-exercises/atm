1dc774fdc089f90ac99de245c366a41037484d7e
Jeffrey Eaton <jeffreyeaton.dc@gmail.com>
2016-03-02 09:19:28 -0500

    ckecking account works

--- atm.js
+++ atm.js
@@ -1,17 +1,23 @@
 $(document).ready(function() {
 
 var checkingBalance = $("#checkingBalance");
+var checkingInput = $("#checkingInput");
+var newTotal = 0;
 
 var checkingDeposit = function(amount){
-  var checkingInput = parseInt($("#checkingInput").val());
-  var newTotal = checkingBalance + checkingInput;
-  console.log(checkingBalance);
-  checkingBalance.html("$" + checkingInput);
-
+  amount.preventDefault();
+  var checkgingInputValue = parseInt(checkingInput.val());
+  newTotal = newTotal + checkgingInputValue;
+  checkingBalance.html("$" + (newTotal));
+  checkingInput.val("");
 };
 
 function checkingWithdrawal(amount){
-
+  amount.preventDefault();
+  var checkingInputValue = parseInt(checkingInput.val());
+  newTotal = newTotal - checkingInputValue;
+  checkingBalance.html("$" + (newTotal));
+  checkingInput.val("");
 }
 
 function savingsDeposit(amount){
@@ -21,7 +27,6 @@ function savingsDeposit(amount){
 function savingsWithdrawal(amount){
 
 }
-
 
 $("#checkingDepositButton").on("click", checkingDeposit);
 $("#checkingWithdrawalButton").on("click", checkingWithdrawal);
