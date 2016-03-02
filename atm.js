e4e3bf6fc64a79966574bf3007767d975cbd896f
Bipin Neupane <icomp2@hotmail.com>
2016-03-01 19:25:48 -0500

    been trying to solve...

--- atm.js
+++ atm.js
@@ -1,21 +1,36 @@
-var checkingBalance = document.querySelector("div#checking_balance");
-checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+$(document).ready(function(){
+  // console.log(jQuery);
+  $("#depositCheckingButton").click(function(){
+    // console.log("clicked");
+    $("#checkingDepotext").val().append(".checkingBalance");
+  });
+});
 
-var checkingDeposit = function(amount){
-  checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
 
-}
 
-function checkingWithdrawal(amount){
 
-}
 
-function savingsDeposit(amount){
+//given
 
-}
 
-function savingsWithdrawal(amount){
-
-}
-
-// an eventListerner for each button, each one a "click"
+// var checkingBalance = document.querySelector("div#checking_balance");
+// checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+//
+// var checkingDeposit = function(amount){
+//   checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+//
+// };
+//
+// function checkingWithdrawal(amount){
+//
+// }
+//
+// function savingsDeposit(amount){
+//
+// }
+//
+// function savingsWithdrawal(amount){
+//
+// }
+//
+// // an eventListerner for each button, each one a "click"
