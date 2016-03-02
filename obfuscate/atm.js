4206d91024ac5ec99146f61ca0c413957fb1b45f
YOUR FULL NAME <farishta.haider@gmail.com>
2016-03-01 19:07:04 -0500

    Additions

--- obfuscate/atm.js
+++ obfuscate/atm.js
@@ -1,17 +1,43 @@
-var checking
-var savings
+var checking =
+var savings =
 
 
-function atm (){
+function ATMTrans (){
   this.entries = [];
   this.els = {
     form: $("entry")
     entries: $("entries")
-    total: $("newtransaction")
+    // deposit: $("")
+    // withdrawl: $("")
+    total: $("amount")
   }
 }
 
 
 
 
- 
+ATMTrans.prototype.calculateTotal = function(){
+  var checkingTotal =0;
+  for(var i = 0; i <this.entries.length; i++){
+    total += this.entries[i];
+  }
+  return total;
+}
+
+var newDeposit = new ATMTrans();
+var newWithdrawl = new ATMTrans();
+
+newDeposit.els.form.on("submit", function(event){
+event.preventDefault();
+
+
+newWithdrawl.els.form.on("submit", function(event){
+event.preventDefault();
+
+var userInput = parseFloat(newDeposit.els.input.val());
+parseFloat(newWithdrawl.els.input.val())
+
+//
+// add entry: deposit money into checking
+// see new total
+// add new entry: withdrawl money from checking
