e7362d7a9bfc48310897ea7a9fcc1448f1e16d25
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-03-02 18:12:06 -0500

    savings account psuedocode done

--- atm.js
+++ atm.js
@@ -33,25 +33,34 @@
   //function of preventDefault with event so that the page doesn't reload
 
   //two divs created with accounts as classes created as classes
-    //varCheckingAccountDiv
+    //varCheckingAccountDiv = #account1
       //var startingBalance is zero with pink background
-    //var savingsAccountDiv
+    //var savingsAccountDiv = #account2
       //startingBalance is zero with pink background
 
   //user enter amount into either checking #account1 or savings #account2 <div>
+    //if amount entered into account1
+      //checkingAcountDiv-user inputs num in input of #checking_amount placeholder
+        //enter in a $ amount to the input <div> of
+        //user .on("click", function) "button" with value "deposit" or "withdraw"
+        //button value deposit has id #checking_deposit
+        //button value withdraw has id #checking_withdraw
+          //if #checking_deposit button clicked, function
+            //var checkingTotal = checkingBalance.value + checking_amount.value
+          //else if #checking_withdraw button clicked, function
+            //var checkingTotal = checkingBalance.value - checking_withdraw.value
+    //if amount entered into account2
+      //savingsAccountDiv-user inputs num in input of #savings_amount placeholder
+        //enter in a $ amount to the input <div> in #account2
+        //user .on("click", function) "button" with value "deposit" or "withdraw"
+        //button value deposit has id #savings_deposit
+        //button value withdraw has id #savings_withdraw
+          //if #savings_deposit button clicked, function
+            //var savingsTotal = savingsBalance.value + savings_amount.value
+          //else if #savings_withdraw button clicked, function
+            //var savingsTotal = savingsBalance.value - savings_withdraw.value
+            
 
-    //checkingAcountDiv-user inputs num in input of #checking_amount placeholder
-      //enter in a $ amount to the input <div>.
-      //user .on("click", function) "button" with value "deposit" or "withdraw"
-      //button value deposit has id #checking_deposit
-      //button value withdraw has id #checking_withdraw
-        //if #checking_deposit button clicked, function
-          //var checkingTotal = checkingBalance.value + checking_amount.value
-        //else if #checking_withdraw button clicked, function
-          //var checkingTotal = checkingBalance.value - checking_withdraw.value
-          
 
-    //var withdraw button
-      //amount entered
 //var savings balance starts at zero with pink background
-//var checking balance starts at zero with pink background
+//var savings balance starts at zero with pink background
