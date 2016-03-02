e60d47784d6429b89c419c34d8d943b91714e604
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-03-02 18:05:56 -0500

    2nd commit with deposit pseudocode

--- atm.js
+++ atm.js
@@ -27,3 +27,31 @@
 //
 // // an eventListerner for each button, each one a "click"
 //
+
+//ATM PSEUDOCODE//
+// load page with the input places and buttons with $0 balance in both accounts
+  //function of preventDefault with event so that the page doesn't reload
+
+  //two divs created with accounts as classes created as classes
+    //varCheckingAccountDiv
+      //var startingBalance is zero with pink background
+    //var savingsAccountDiv
+      //startingBalance is zero with pink background
+
+  //user enter amount into either checking #account1 or savings #account2 <div>
+
+    //checkingAcountDiv-user inputs num in input of #checking_amount placeholder
+      //enter in a $ amount to the input <div>.
+      //user .on("click", function) "button" with value "deposit" or "withdraw"
+      //button value deposit has id #checking_deposit
+      //button value withdraw has id #checking_withdraw
+        //if #checking_deposit button clicked, function
+          //var checkingTotal = checkingBalance.value + checking_amount.value
+        //else if #checking_withdraw button clicked, function
+          //var checkingTotal = checkingBalance.value - checking_withdraw.value
+          
+
+    //var withdraw button
+      //amount entered
+//var savings balance starts at zero with pink background
+//var checking balance starts at zero with pink background
