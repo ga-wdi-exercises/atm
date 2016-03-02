47bfcc5f6c107027b48ed856b52370d821f8dc2f
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-03-02 13:49:51 -0500

    initial commit

--- atm.js
+++ atm.js
@@ -1,6 +1,8 @@
-var checkingBalance = document.querySelector("div#checking_balance");
-checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+// var checkingBalance = document.querySelector("div#checking_balance");
+// checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+var checkingBalance = $('#checkingBalance')
 
+//talking about a deposit
 var checkingDeposit = function(amount){
   checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
 
@@ -19,3 +21,18 @@ function savingsWithdrawal(amount){
 }
 
 // an eventListerner for each button, each one a "click"
+
+
+//
+
+the accounts of the atm
+load the page
+
+  var  checkingAccount = 0
+  var  savingsAccount = 0
+  var checkingBalance;
+  var savingsBalance;
+  }
+//
+
+//press deposit or withdraw and money goes up or down
