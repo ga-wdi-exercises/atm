49422534964e868f01ae6570ef3d33e04c9a97b2
Charles <charlesthwang@gmail.com>
2016-03-02 17:49:26 -0500

    initial commit

--- atm.js
+++ atm.js
@@ -1,21 +1,64 @@
-var checkingBalance = document.querySelector("div#checking_balance");
-checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+// var checkingBalance = document.querySelector("div#checking_balance");
+// checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+//
+// var checkingDeposit = function(amount){
+//   checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+//
+// }
+//
+// function checkingWithdrawal(amount){
+//
+// }
+//
+// function savingsDeposit(amount){
+//
+// }
+//
+// function savingsWithdrawal(amount){
+//
+// }
+//
+// // an eventListerner for each button, each one a "click"
 
-var checkingDeposit = function(amount){
-  checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+$(document).ready(function() {
 
+function checking() {
+  this.entries = [];
+  this.els = {
+    // input for deposit amount
+    input: $("#checkingAmt"),
+    // checking deposit button
+    checkingDeposit: $("#checkingDeposit"),
+    // checking withdraw button
+    checkingWithdrawal: $("#checkingWithdrawal"),
+  };
 }
 
-function checkingWithdrawal(amount){
 
-}
 
-function savingsDeposit(amount){
 
-}
 
-function savingsWithdrawal(amount){
 
-}
 
-// an eventListerner for each button, each one a "click"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
