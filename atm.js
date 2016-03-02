705e9665a2f97dee41ea57e5587c4527a60ab15f
Jeffrey Eaton <jeffreyeaton.dc@gmail.com>
2016-03-02 09:29:58 -0500

    savings works

--- atm.js
+++ atm.js
@@ -2,30 +2,41 @@ $(document).ready(function() {
 
 var checkingBalance = $("#checkingBalance");
 var checkingInput = $("#checkingInput");
-var newTotal = 0;
+var savingsBalance = $("#savingsBalance");
+var savingsInput = $("#savingsInput");
+var newCheckingTotal = 0;
+var newSavingsTotal = 0;
 
 var checkingDeposit = function(amount){
   amount.preventDefault();
-  var checkgingInputValue = parseInt(checkingInput.val());
-  newTotal = newTotal + checkgingInputValue;
-  checkingBalance.html("$" + (newTotal));
+  var checkingInputValue = parseInt(checkingInput.val());
+  newCheckingTotal = newCheckingTotal + checkingInputValue;
+  checkingBalance.html("$" + (newCheckingTotal));
   checkingInput.val("");
 };
 
 function checkingWithdrawal(amount){
   amount.preventDefault();
   var checkingInputValue = parseInt(checkingInput.val());
-  newTotal = newTotal - checkingInputValue;
-  checkingBalance.html("$" + (newTotal));
+  newCheckingTotal = newCheckingTotal - checkingInputValue;
+  checkingBalance.html("$" + (newCheckingTotal));
   checkingInput.val("");
 }
 
 function savingsDeposit(amount){
-
+  amount.preventDefault();
+  var savingsInputValue = parseInt(savingsInput.val());
+  newSavingsTotal = newSavingsTotal + savingsInputValue;
+  savingsBalance.html("$" + (newSavingsTotal));
+  savingsInput.val("");
 }
 
 function savingsWithdrawal(amount){
-
+  amount.preventDefault();
+  var savingsInputValue = parseInt(savingsInput.val());
+  newSavingsTotal = newSavingsTotal - savingsInputValue;
+  savingsBalance.html("$" + (newSavingsTotal));
+  savingsInput.val("");
 }
 
 $("#checkingDepositButton").on("click", checkingDeposit);
