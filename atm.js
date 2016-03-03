2556a13ec44c5629f7fe9409c86e788f20dc6698
Anissa Jones <anissajones10@gmail.com>
2016-03-02 19:25:14 -0500

    added withdrawl function and click event.

--- atm.js
+++ atm.js
@@ -4,21 +4,42 @@ $(document).ready(function(){
     //get innerhtml from balance div
 
 
-
+    //gets the jquery deposit button element
     var checkingDepositBtn = $("#checkingDepositBtn");
 
-    var checkingDisplay = $("#checkingAccount >.balance")
+    var checkingWithdrawBtn = $("#checkingWithdrawBtn");
 
+    //getting the jquery checking balance element.
+    var checkingDisplay = $("#checkingAccount >.balance");
+
+    //getting the current value of the balance
     var checkingAmount = parseInt(checkingDisplay.html().replace("$", ""));
 
+    //create function to call in click
     function addMoney(){
         //grab userInput
         var userInput = parseInt($("#checkingField").val());
-        //add checkingAmount
+        //add input to checkingAmount
         checkingAmount += userInput;
+        //updating the display
         checkingDisplay.html("$" + checkingAmount);
+        //clear value in input box
+        $("#checkingField").val("");
     }
 
-    checkingDepositBtn.on("click", addMoney)
+    function subtractMoney(){
+        //grab userInput
+        var userInput = parseInt($("#checkingField").val());
+        //add input to checkingAmount
+        checkingAmount -= userInput;
+        //updating the display
+        checkingDisplay.html("$" + checkingAmount);
+        //clear value in input box
+        $("#checkingField").val("");
+    }
 
+
+    checkingDepositBtn.on("click", addMoney);
+
+    checkingWithdrawBtn.on("click", subtractMoney);
 });
