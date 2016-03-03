45cbf97b310ebb257d5d09c09ab5cd923943afd7
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-03-03 01:57:24 -0500

    nothing works yet - select is ruining it all

--- atm.js
+++ atm.js
@@ -1,6 +1,29 @@
-$("input").eq(0).addClass("checking_input");
-$("input").eq(3).addClass("savings_input");
+var checkingUserInput = ($("body > div:nth-child(2) > input[type="text"]:nth-child(3)").val());
+var checkingDepositButton = ($("body > div:nth-child(2) > input[type="button"]:nth-child(4)").val());
+var checkingWithdrawlButton = ($("body > div:nth-child(2) > input[type="button"]:nth-child(5)").val());
+var checkingBalance = ($("body > div:nth-child(2) > div").val());
 
+// $("input").eq(0).addClass("checking_userInput");
+// $("input").eq(1).addClass("checking_deposit_button")
+// $("input").eq(2).addClass("checking_withdrawal_button")
+// $("input").eq(3).addClass("savings_userInput");
+// $("input").eq(4).addClass("checking_deposit_button")
+// $("input").eq(5).addClass("checking_withdrawal_button")
+
+var checkingTotal = 0
+
+$(checkingDepositButton).click(function(){
+checkingTotal += parseFloat($(checkingUserInput).val());
+$(checkingBalance).html(checkingTotal)
+console.log($(checkingUserInput).val());
+})
+
+
+
+
+//
+//
+//
 function Account( type, startingBal ){
   this.type = type;
   this.startingBal = startingBal;
@@ -8,26 +31,23 @@ function Account( type, startingBal ){
   this.withdrawals = [];
   this.depositMoney = function(input){
     this.deposits.push(input);
+  }
   this.withdrawMoney = function(input){
-    this.withdrawals = [];
+    this.withdrawals.push(input);
   }
-  }
-}
-// Account.prototype.depositMoney = function(input){
-//   this.deposits.push(input);
-//   return this;
-// }
-// Account.prototype.withdrawMoney = function(input){
-//   this.withdrawal.push(input);
-//   return this;
-// }
+};
 console.dir(Account);
 // --Checking----------------------
 var Checking = new Account("Checking","0");
-// Checking.prototype.checkingAmount =
-console.dir(Checking)
+
+Checking.prototype.deposit = function(){
+  
+}
+
+
+// console.dir(Checking)
 
 
 //--Account--------------------------
 var Savings = new Account("Savings","0");
-console.dir(Savings)
+// console.dir(Savings)
