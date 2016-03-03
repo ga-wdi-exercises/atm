b626beb78e28db2de983de2f09b5e68710f9eb3f
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-03-03 10:00:12 -0500

    after homework review - see notes and dead code

--- atm.js
+++ atm.js
@@ -1,53 +1,71 @@
-var checkingUserInput = ($("body > div:nth-child(2) > input[type="text"]:nth-child(3)").val());
-var checkingDepositButton = ($("body > div:nth-child(2) > input[type="button"]:nth-child(4)").val());
-var checkingWithdrawlButton = ($("body > div:nth-child(2) > input[type="button"]:nth-child(5)").val());
-var checkingBalance = ($("body > div:nth-child(2) > div").val());
+$(document).ready(function(){
+  var checking = new Account ($("#checking"));
+  var savings = new Account($("#savings"))
+});
 
-// $("input").eq(0).addClass("checking_userInput");
-// $("input").eq(1).addClass("checking_deposit_button")
-// $("input").eq(2).addClass("checking_withdrawal_button")
-// $("input").eq(3).addClass("savings_userInput");
-// $("input").eq(4).addClass("checking_deposit_button")
-// $("input").eq(5).addClass("checking_withdrawal_button")
-
-var checkingTotal = 0
-
-$(checkingDepositButton).click(function(){
-checkingTotal += parseFloat($(checkingUserInput).val());
-$(checkingBalance).html(checkingTotal)
-console.log($(checkingUserInput).val());
-})
-
-
-
-
-//
-//
-//
-function Account( type, startingBal ){
-  this.type = type;
-  this.startingBal = startingBal;
-  this.deposits = [];
-  this.withdrawals = [];
-  this.depositMoney = function(input){
-    this.deposits.push(input);
+function Account(element){
+  var account =  this;
+  account.element = element;
+  account.balance = 0;
+  account.elements = {
+    deposit: account.element.find(".deposit"),
+    withdraw: account.element.find(".withdraw"),
+    balance: account.element.find(".balance"),
+    input: account.element.find(".input")
   }
-  this.withdrawMoney = function(input){
-    this.withdrawals.push(input);
-  }
+  account.elements.deposit.click(function(){
+    var input = parseInt(account.elements.input.val());
+    account.balance += input;
+    account.elements.balance.html("$" + account.balance);
+  })
+  account.elements.withdraw.click(function(){
+    var input = parseInt(account.elements.input.val());
+    account.balance -= input;
+    account.elements.balance.html("$" + account.balance);
+  })
 };
-console.dir(Account);
-// --Checking----------------------
-var Checking = new Account("Checking","0");
-
-Checking.prototype.deposit = function(){
-  
-}
 
 
-// console.dir(Checking)
 
-
-//--Account--------------------------
-var Savings = new Account("Savings","0");
-// console.dir(Savings)
+// $("#checking .deposit").click(function(){
+//   var userInput = $("#checking .input").val());
+//   $("#checking .balance").html("<h1>userInput</h1>")
+//
+// }
+//
+//
+// var checkingTotal = 0
+//
+// $(checkingDepositButton).click(function(){
+// checkingTotal += parseFloat($(checkingUserInput).val());
+// $(checkingBalance).html(checkingTotal)
+// console.log($(checkingUserInput).val());
+// })
+//
+// function Account( type, startingBal ){
+//   this.type = type;
+//   this.startingBal = startingBal;
+//   this.deposits = [];
+//   this.withdrawals = [];
+//   this.depositMoney = function(input){
+//     this.deposits.push(input);
+//   }
+//   this.withdrawMoney = function(input){
+//     this.withdrawals.push(input);
+//   }
+// };
+// console.dir(Account);
+// // --Checking----------------------
+// var Checking = new Account("Checking","0");
+//
+// Checking.prototype.deposit = function(){
+//
+// }
+//
+//
+// // console.dir(Checking)
+//
+//
+// //--Account--------------------------
+// var Savings = new Account("Savings","0");
+// // console.dir(Savings)
