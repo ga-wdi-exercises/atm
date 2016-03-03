c6decd6dde274f69005ff13acc33fd8ce4bd7b7f
Hilary Masland <hilarymasland@gmail.com>
2016-03-03 07:43:04 -0500

    Pseudo coding / created variables

--- atm.js
+++ atm.js
@@ -6,38 +6,25 @@ var checkingDeposit = function(amount){
 
 };
 
-var checking = [{
-  var account = $("#account");
-  var balance = $("balance");
-  var text = $(".text");
+var checking = {
+  var account: $("#account");
+  var balance: $(".balance");
+  var text: $(".text");
 }];
-//var form = (deposit, withdraw);{
-  //this.deposit= deposit;
-  //this.withdraw= withdraw;
-//}
 
-//withdraw money out of checking
+
+
 function checkingWithdrawal(amount){
     event.preventDefault();
     checkingBalance.on("click", val(amount));
 
 }
 
-// function savingsDeposit(amount){
-//     event.preventDefault();
-//
-// }
-//
-// function savingsWithdrawal(amount){
-//
-// }
+function savingsDeposit(amount){
+    event.preventDefault();
 
-// an eventListerner for each button, each one a "click"
-//As a user, I want to deposit money into one of the bank accounts
+}
 
+function savingsWithdrawal(amount){
 
-
-//As a user, I want to withdraw money from one of the bank accounts
-
-
-//Make sure the balance in an account can't go negative. If a user tries to withdraw more money than exists in the account, ignore the transaction.
+}
---
+++ psuedo.js
@@ -0,0 +1,13 @@
+// <div class="account">
+//   <h2>Checking</h2>
+//   <div class="balance">$0</div>
+//   <input type="text" placeholder="enter an amount" />
+//   <input type="button" value="Deposit" />
+//   <input type="button" value="Withdraw" />
+// </div>
+
+//create a checking account function
+  //create a variable "account" that will hold all of the other functions
+  //Within the account variable, we need a variable balance
+    //we need a form field to enter the "amount"
+    //after the user enters the amount: when they click the deposit button, the information put in the form needs to be displayed
