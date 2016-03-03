24072c0bd97fabb449e9056795d5dbf3f8292c5f
Caitlin Daitch <caitlin.daitch@gmail.com>
2016-03-02 21:43:38 -0500

    Logging overage, need to fix so that it removes from the other account

--- atm.js
+++ atm.js
@@ -2,6 +2,7 @@ var atm = {
   currentInput: 0,
   checkingBalance: 0,
   savingsBalance: 0,
+  overage: 0,
   buttonInitialize: function() {
     $(".checking-deposit").click(this.checkingDeposit);
     $(".checking-withdrawal").click(this.checkingWithdrawal);
@@ -34,7 +35,24 @@ var atm = {
 
     self.checkingBalance -= self.currentInput;
 
+    if (self.checkingBalance <= 0) {
+      $("#checking_balance").html("$0").css("background-color", "red");
+
+      self.overage = self.checkingBalance - self.currentInput;
+
+      self.checkingOverage();
+
+      return;
+    };
+
     $("#checking_balance").html("$" + self.checkingBalance);
+  },
+  checkingOverage: function() {
+    var self = atm;
+
+    self.savingsBalance += self.overage;
+
+    $("#savings_balance").html("$" + self.savingsBalance);
   },
   savingsDeposit: function() {
     var self = atm;
@@ -52,8 +70,27 @@ var atm = {
 
     self.savingsBalance -= self.currentInput;
 
+    if (self.savingsBalance <= 0) {
+      $("#savings_balance").html("$0").css("background-color", "red");
+
+      self.overage = self.savingsBalance - self.currentInput;
+
+      self.savingsOverage();
+
+      return;
+    };
+
     $("#savings_balance").html("$" + self.savingsBalance);
-  }
+  },
+  savingsOverage: function() {
+    var self = atm;
+
+    self.checkingBalance += self.overage;
+
+    console.log(self.overage);
+
+    $("#checking_balance").html("$" + self.checkingBalance);
+  },
 }
 
 atm.buttonInitialize();
