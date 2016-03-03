4c4580c6df5affe2870472647a55bde68151ed94
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-03-02 23:05:30 -0500

    begin accounts as constructors - working

--- atm.js
+++ atm.js
@@ -2,23 +2,33 @@ $(document).ready(function(){
   $("input").eq(0).addClass("checking_input");
   $("input").eq(3).addClass("savings_input");
 
-function Account(type,min){
+function Account(type, min){
   this.type = type;
-  this.min = 0;
-  // this.input = input;
-  this.balance = balance;
-  this.deposit = function(){
-    parseInt($(".checking"))
-  };
-  this.withdrawal = function() {
-  }
-};
+  this.min = min;
+}
+console.dir(Account)
 
+var CheckingAccount = new Account("checking","0");
+console.dir(CheckingAccount)
 
-var Checking = new Account("checking",0)
-var Savings = new Account("savings",0)
-
+var SavingsAccount = new Account("savings","0");
+console.dir(SavingsAccount)
+// Checking.prototype.deposit =function(){
+//   return this.type;
+// };
+// Checking.prototype.balance = function(){
+//   $(".checking_input")
+// };
+// console.dir(Checking);
+// // --------savings copy --------------
+// var Savings = new Account("savings",0)
+// console.dir(Savings);
+//
+//
+//
+//
+//
 });
-
+//
 
 //console.dir(account (or checking etc. ))
