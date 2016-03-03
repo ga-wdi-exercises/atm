32066e9c3bbc265b24efedc79d41c30939632ebb
Caitlin Daitch <caitlin.daitch@gmail.com>
2016-03-02 21:23:50 -0500

    All checking and savings deposit working

--- atm.js
+++ atm.js
@@ -1,14 +1,22 @@
 var atm = {
   currentInput: 0,
   checkingBalance: 0,
+  savingsBalance: 0,
   buttonInitialize: function() {
     $(".checking-deposit").click(this.checkingDeposit);
-    // $(".checking-withdrawal").click(this.checkingWithdrawal);
+    $(".checking-withdrawal").click(this.checkingWithdrawal);
+    $(".savings-deposit").click(this.savingsDeposit);
+    //$(".savings-withdrawal").click(this.savingsWithdrawal);
   },
   checkingInput: function() {
     var self = atm;
 
-    self.currentInput = parseInt($(".checking").val())
+    self.currentInput = parseInt($(".checking").val());
+  },
+  savingsInput: function() {
+    var self = atm;
+
+    self.currentInput = parseInt($(".savings").val());
   },
   checkingDeposit: function() {
     var self = atm;
@@ -17,9 +25,25 @@ var atm = {
 
     self.checkingBalance += self.currentInput;
 
-    console.log(self.checkingBalance);
+    $("#checking_balance").html("$" + self.checkingBalance);
+  },
+  checkingWithdrawal: function() {
+    var self = atm;
+
+    self.checkingInput();
+
+    self.checkingBalance -= self.currentInput;
 
     $("#checking_balance").html("$" + self.checkingBalance);
+  },
+  savingsDeposit: function() {
+    var self = atm;
+
+    self.savingsInput();
+
+    self.savingsBalance += self.currentInput;
+
+    $("#savings_balance").html("$" + self.savingsBalance);
   }
 
 }
