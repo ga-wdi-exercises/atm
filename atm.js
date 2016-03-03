11bad93b3660aaca0a0cb576aec626458c59bf03
Woo Yun <hh747hh@gmail.com>
2016-03-03 08:57:21 -0500

    start from beginning

--- atm.js
+++ atm.js
@@ -1,22 +1,22 @@
-var checkingBalance = document.querySelector("div#checking_balance");
-checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
-
-var checkingDeposit = function(amount){
-  checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
-
-}
-
-function checkingWithdrawal(amount){
-
-}
-
-function savingsDeposit(amount){
-
-}
-
-function savingsWithdrawal(amount){
-
-}
+// var checkingBalance = document.querySelector("div#checking_balance");
+// checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+//
+// var checkingDeposit = function(amount){
+//   checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+//
+// }
+//
+// function checkingWithdrawal(amount){
+//
+// }
+//
+// function savingsDeposit(amount){
+//
+// }
+//
+// function savingsWithdrawal(amount){
+//
+// }
 
 // an eventListerner for each button, each one a "click"
 
@@ -27,22 +27,28 @@ var savingsAmount = $("savingsAmount");
 
 
 
-$("checkingDeposit").on("click", function(){
-checkingBalance.text("$"+ (parseInt(checkingBalance.html().replace("$","")) +parseInt(checkingAmount.val())));
-checkingAmount.val("");
-});
+// $("checkingDeposit").on("click", function(){
+// checkingBalance.text("$"+ (parseInt(checkingBalance.html().replace("$","")) +parseInt(checkingAmount.val())));
+// checkingAmount.val("");
+// });
+//
+// $("withDrawChecking").on("click", function(){
+//   checkingBalance.text("$" + (parseInt(checkingBalance.html().replace(
+//     replace("$", "")) - parseInt(checkingEntry.val())));
+//  +checkingEntry.val("");
+// if (checkingEntry.val() > parseInt(checkingBalance.html().replace("$",""))-parseInt(checkingAmount.val()))
+//   ))};
 
-$("withDrawChecking").on("click", function(){
-  checkingBalance.text("$" + (parseInt(checkingBalance.html().replace(
-    replace("$", "")) - parseInt(checkingEntry.val())));
- +checkingEntry.val("");
-
-  ))};
-
-
-)
-
-
-
-
+$(checkingDeposit).click(function(){
+  checkingTotal += parseFloat(checkingInput.val());
+  checkingBalance.html();
 })
+$(checkingWithdraw).click(function() {
+if (checkingBalance.html() < checkingAmount.val() ) {
+  checkingBalance.addClass("zero");
+else {
+  checkingTotal -= parseFloat(checkingInput.val());
+  checkingBalance.html();
+}
+}
+});
