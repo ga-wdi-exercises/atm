97befa1cc909c9bc15f0af264307bbad13bf8fe8
Charles <charlesthwang@gmail.com>
2016-03-02 19:02:20 -0500

    functional checking deposit button

--- atm.js
+++ atm.js
@@ -25,6 +25,8 @@ $(document).ready(function() {
 function checking() {
   this.entries = [];
   this.els = {
+    // checking value
+    value: $("#checkingAcct"),
     // input for deposit amount
     input: $("#checkingAmt"),
     // checking deposit button
@@ -34,7 +36,25 @@ function checking() {
   };
 }
 
+checking.prototype.calculateTotal = function(entry) {
 
+};
+
+
+//creating a new checking function based on checking constructor function
+var myChecking = new checking();
+myChecking.els.checkingDeposit.click( function(event) {
+  event.preventDefault();
+  // get user input
+  var userInput = parseInt(myChecking.els.input.val());
+  console.log(userInput);
+  // add the inputs to the array an entries
+  myChecking.entries.push(userInput);
+  // displays user input in checking box
+  myChecking.els.value.html(userInput);
+
+
+});
 
 
 
