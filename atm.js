28ad669d678dcc5d8f919d4e2251d35b238bfa7c
BrittanyBranson <brittanydibble@Brittanys-MacBook-Pro.local>
2016-03-02 22:44:20 -0500

    initial commit

--- atm.js
+++ atm.js
@@ -1,21 +1,57 @@
-var checkingBalance = document.querySelector("div#checking_balance");
-checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
-
-var checkingDeposit = function(amount){
-  checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
-
+//function gaATM with OOP
+function gaATM() {
+  this.balance = 0;
+  this.checking = {
+    input: $("#newChecking"),
+    total: 0
+  };
+  this.savings = {
+    input: $("#newSavings"),
+    total: 0
+  };
 }
 
-function checkingWithdrawal(amount){
+gaATM.prototype.balance = function() {
+  this.gaBalance = this.checking.total + this.savings.total;
+  var total = this.checking.total + this.savings.total;
+  return total;
+};
 
+var bank = new gaATM();
+var checkingBalance = $("#checkingBalance").addClass("empty");
+var savingsBalance = $("#savingsBalance").addClass("empty");
+
+//NEEDED EVENT LISTENERS
+
+//checking deposit
+//the input is depositCash, and we are creating an on click function event
+$("input.depositCash").on("click", function(event) {
+//prevent default behavior
+  event.preventDefault();
+//ensures user input is an integer
+  var input = parseInt(bank.checking.input.val());
+  checkingDeposit(input);
+//clear the input by utilizing an empty string
+  bank.checking.input.val("");
 }
-
-function savingsDeposit(amount){
-
+};
+//checking withdrawl
+//the input is withdrawCash, and we are creating an on click function
+$("input.withdrawCash").on("click", function(event) {
+//prevent the default behavior
+  event.preventDefault();
+//ensures the user input is an integer
+  var input = parseInt(bank.checking.input.val());
+  checkingWithdraw(input);
+//clear the input by utilizing an empty string
+  bank.checking.input.val("");
 }
+};
+//savings deposit
+//savings withdrawl
 
-function savingsWithdrawal(amount){
-
-}
-
-// an eventListerner for each button, each one a "click"
+//USER INPUT
+//adds user input to checking total
+//subtracts user input from checking total
+//adds user input to savings total
+//subtracts user input from savings total
