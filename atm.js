160f4eda898f4fbeeb6f9893b3a902746416e9ab
Caitlin Daitch <caitlin.daitch@gmail.com>
2016-03-02 20:26:33 -0500

    Initial commit

--- atm.js
+++ atm.js
@@ -2,7 +2,8 @@ var checkingBalance = document.querySelector("div#checking_balance");
 checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
 
 var checkingDeposit = function(amount){
-  checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+  // checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+  console.log(checkingBalance);
 
 }
 
@@ -19,3 +20,7 @@ function savingsWithdrawal(amount){
 }
 
 // an eventListerner for each button, each one a "click"
+$(".checking-deposit").click(checkingDeposit);
+$(".checking-withdrawal").click(checkingWithdrawal);
+$(".savings-deposit").click(savingsDeposit);
+$(".savings-withdrawal").click(savingsWithdrawal);
