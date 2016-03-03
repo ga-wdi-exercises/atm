0c3f039b181b81fa6712828601c15e6e85ca06e7
Bipin Neupane <icomp2@hotmail.com>
2016-03-02 19:48:15 -0500

    can deposit money, can withdraw money, red background there, does not let withdraw money if you dont have it, dollar sign not available, overdraft protection not available.

--- atm.js
+++ atm.js
@@ -1,36 +1,57 @@
-$(document).ready(function(){
-  // console.log(jQuery);
-  $("#depositCheckingButton").click(function(){
-    // console.log("clicked");
-    $("#checkingDepotext").val().append(".checkingBalance");
-  });
+//checking
+var checkingDepoButt = $("#checking_deposit")
+var checkingWithdrawButt = $("#checking_withdraw")
+var checkingInput = $("#checking_amount")
+var checkingBalance = $("#checking_balance")
+var checkingTotal = 0
+//savings
+var savingsDepoButton = $("#savings_deposit")
+var savingsWithdrawButt = $("#savings_withdraw")
+var savingsInput = $("#savings_amount")
+var savingsBalance = $("#savings_balance")
+var savingsTotal = 0
+
+
+// enter number in the text field
+//make the deposit button work
+//transfer number from field to the balance area.
+checkingDepoButt.click(function(){
+  checkingTotal += parseFloat(checkingInput.val());
+  checkingBalance.html(checkingTotal); // this line is putting in the checkingtotal value as an innerHTML for checkingBalance
 });
 
+//withdraw amount from the checking account
+checkingWithdrawButt.click(function(){
 
+  // if the user tries to make a withdrawl greater than their balance
+  if (checkingBalance.html() < checkingInput.val() ) {
+    checkingBalance.addClass("zero");
+    alert("$$Ain't got no monay$$")
+  }
+  // otherwise, if the balance is greater than the input, let them withdraw
+  else {
+    // this code does not execture if the blance is less than input
+    checkingTotal -= parseFloat(checkingInput.val());
+    checkingBalance.html(checkingTotal);
+  }
 
+});
 
+savingsDepoButton.click(function(){
+  savingsTotal += parseFloat(savingsInput.val());
+  savingsBalance.html(savingsTotal);
+});
 
-//given
-
-
-// var checkingBalance = document.querySelector("div#checking_balance");
-// checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
-//
-// var checkingDeposit = function(amount){
-//   checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
-//
-// };
-//
-// function checkingWithdrawal(amount){
-//
-// }
-//
-// function savingsDeposit(amount){
-//
-// }
-//
-// function savingsWithdrawal(amount){
-//
-// }
-//
-// // an eventListerner for each button, each one a "click"
+//withdraw amount from the savings account
+savingsWithdrawButt.click(function(){
+  //this is comparing the amount between balance and input
+  if (savingsBalance.html() < savingsInput.val() ) {
+    savingsBalance.addClass("zero");
+    alert("$$Ain't got no monay$$");
+  }
+  
+  else
+  //this is withdrawing amount
+  savingsTotal -= parseFloat(savingsInput.val());
+  savingsBalance.html(savingsTotal);
+});
