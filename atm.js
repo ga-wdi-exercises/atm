0f8770e5c5f38eafaa1a7b0ad1f04daa61677df2
James <james@team-wombat.com>
2016-06-02 09:33:07 -0400

    Transitioned draw/depo/bal functions to general variables

--- atm.js
+++ atm.js
@@ -2,30 +2,61 @@ var checkBal = 0;
 var savBal = 0;
 var checkDepo = 0;
 var checkDraw = 0;
-var depoType;
-var drawType;
+var txType;
+var txType;
+var txAmt = 0;
 
 $(document).on("ready", function() {
 
-$("#checking").find(".deposit").on("click", function(){
-    checkDepo = parseInt($("#checking").find(".input").val());
-    checkBal = parseInt(checkBal + checkDepo);
-    $("#checking").find(".balance").text(checkBal);
-    checkDepo = null;
-    $("#checking").find(".input").val("");
-});
+// $("#checking").find(".deposit").on("click", function(){
+//     checkDepo = parseInt($("#checking").find(".input").val());
+//     checkBal = parseInt(checkBal + checkDepo);
+//     $("#checking").find(".balance").text(checkBal);
+//     // checkDepo = null;
+//     $("#checking").find(".input").val("");
+// });
 
-$("#checking").find(".withdraw").on("click", function(){
-    checkDraw = parseInt($("#checking").find(".input").val());
-    checkBal = parseInt(checkBal - checkDraw);
-    $("#checking").find(".balance").text(checkBal);
-    checkDraw = null;
-    $("#checking").find(".input").val("");
-});
+// $("#checking").find(".withdraw").on("click", function(){
+//     checkDraw = parseInt($("#checking").find(".input").val());
+//     checkBal = parseInt(checkBal - checkDraw);
+//     $("#checking").find(".balance").text(checkBal);
+//     checkDraw = null;
+//     $("#checking").find(".input").val("");
+// });
+
+// $(":button").on("click", function() {
+//     txType = $(this).val();
+//     txAcct = $(this).parent($(".class")).attr('id');
+// })
 
 $(":button").on("click", function() {
     txType = $(this).val();
     txAcct = $(this).parent($(".class")).attr('id');
+    if (txAcct == "checking") {
+        txAmt = parseInt($("#checking").find(".input").val());
+        if (txType == "Deposit") {
+            checkBal = parseInt(checkBal + txAmt);
+            $("#checking").find(".balance").text(checkBal);
+        }
+        else {
+            checkBal = parseInt(checkBal - txAmt);
+            $("#checking").find(".balance").text(checkBal);
+        }
+        $("#checking").find(".input").val("");
+    }
+    else {
+        txAmt = parseInt($("#savings").find(".input").val());
+        if (txType == "Deposit") {
+            savBal = parseInt(savBal + txAmt);
+            $("#savings").find(".balance").text(savBal);
+        }
+        else {
+            savBal = parseInt(savBal - txAmt);
+            $("#savings").find(".balance").text(savBal);
+        }
+        $("#savings").find(".input").val("");
+    }
+    // txAmt = $(this).prevAll($(".input")).val();
 })
 
 });
