38df000c4a0f4ed374107bff538dbd5e53b2c5a1
Adina Pollack <adinap16@gmail.com>
2016-06-02 12:25:48 -0400

    Added color change to checking and replicated code for savings

--- atm.js
+++ atm.js
@@ -1,25 +1,87 @@
 $(document).ready(function(){
- // var button = $("#checking").children().find(".deposit")
- //
- // function buttonClick(){
- //   button.on("click", console.log("hello"))
-var checkingInput = $("#checking .input").val()
-var checkingTotal = 0
 
+var checkingInput = $("#checking .input").val()
+var savingsInput = $("#savings .input").val()
+var checkingTotal = 0
+var savingsTotal = 0
+
+function checkingColor(){
+  if (checkingTotal == 0){
+    $("#checking .balance").addClass("zero");
+  }
+  else{
+    $("#checking .balance").removeClass("zero");
+    console.log("work bitch");
+  }
+}
+function savingsColor(){
+  if (savingsTotal == 0){
+    $("#savings .balance").addClass("zero");
+  }
+  else{
+    $("#savings .balance").removeClass("zero");
+  }
+}
+
+checkingColor();
+savingsColor();
 
 $("#checking .deposit").on("click", function(){
   var checkingInput = $("#checking .input").val()
-
-  checkingInput = parseInt(checkingInput);
-
-  checkingTotal += checkingInput;
-
-  checkingTotal = String(checkingTotal);
-
-  $("#checking .balance").text("$" + checkingTotal);
-
+  if (checkingInput <= 0){
+    alert("Your desposit must be greater than zero.")
+  }
+  else {
+    checkingInput = parseInt(checkingInput);
+    checkingTotal += checkingInput;
+    $("#checking .balance").html('$' + checkingTotal);
+    checkingColor()
+  }
 });
 
+$("#savings .deposit").on("click", function(){
+  var savingsInput = $("#savings .input").val()
+  if (savingsInput <= 0){
+    alert("Your desposit must be greater than zero.")
+  }
+  else {
+    savingsInput = parseInt(savingsInput);
+    savingsTotal += savingsInput;
+    $("#savings .balance").html('$' + savingsTotal);
+    savingsColor()
+  }
+});
 
+$("#checking .withdraw").on("click", function(){
+  var checkingWithdraw = $("#checking .input").val();
+
+    checkingWithdraw = parseInt(checkingWithdraw);
+  if (checkingWithdraw <= checkingTotal){
+    checkingTotal -= checkingWithdraw;
+
+    $("#checking .balance").html('$' + checkingTotal);
+    checkingColor();
+
+  }
+  else {
+    alert("Account balance is too low for withdrawl.")
+  }
+});
+
+$("#savings .withdraw").on("click", function(){
+  var savingsWithdraw = $("#savings .input").val();
+
+    savingsWithdraw = parseInt(savingsWithdraw);
+  if (savingsWithdraw <= savingsTotal){
+    savingsTotal -= savingsWithdraw;
+
+    $("#savings .balance").html('$' + savingsTotal);
+    savingsColor();
+
+  }
+  else {
+    alert("Account balance is too low for withdrawl.")
+  }
+});
 
 });
