42f80754ab6900c70396e0b227034bd3db8829e7
Kristin Pasive <kristin.pasive@gmail.com>
2016-06-02 11:02:01 -0400

    Balance adds up

--- atm.js
+++ atm.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
-
+  var moneyDeposit = $("#checking").find(".deposit")
 $(".deposit").on("click", function(){
-  var moneyInput = $(".input").val();
-  var balanceChange = $(".balance");
-  balanceChange.html("$" + moneyInput);
+  var moneyInput = parseInt($(".input").val());
+  var balanceInput = parseInt($("#balanceChecking").html().replace("$", ""));
+  var balanceChange = $("#balanceChecking");
+  var totalBalance = moneyInput + balanceInput;
+  balanceChange.html("$" + totalBalance);
 });
 
 });
