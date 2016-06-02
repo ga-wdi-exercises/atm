1f607c6aebaf09cde8ed7d9584e51e1770baab4d
Chris Gardner <ctgardner89@gmail.com>
2016-06-02 09:56:56 -0400

    init'd some variables

--- atm.js
+++ atm.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+var savings = {
+  balance: $("#savings .balance"),
+  input: $("#savings .input"),
+  draw: $("#savings .withdraw"),
+  depo: $("#savings .deposit")
+};
+
+var checking = {
+  balance: $("#checking .balance"),
+  input: $("#checking .input"),
+  draw: $("#checking .withdraw"),
+  depo: $("#checking .deposit")
+}
+
+
+
 });
