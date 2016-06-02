5ce6c7022df7fd86534e0e50f5b8368e4b1a50e0
Thaisun Green <Tlgreen16@gmail.COM>
2016-06-02 10:21:19 -0400

    added functionality for withdraw and depsoit button

--- atm.js
+++ atm.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
+  console.log("!!!!!!!!!!!!");
+  var checking = $("#checking .balance").text();
+  var savings = $("#savings .balance").text();
+  console.log("Checking Balance " + checking);
+  console.log("Savings Balance " + savings);
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+
 });
+$(".deposit").on("click",function(evt){
+  evt.preventDefault();
+  console.log("deposit button clicked")
+})
+
+$(".withdraw").on("click",function(evt){
+  evt.preventDefault()
+  console.log("withdraw button clicked")
+  $(this).css("background-color","red")
+})
