05c321a52c4b7d23cb0076a04c1d2ca541105b8a
David <dcordz@gmail.com>
2016-06-02 09:36:13 -0400

    prevented withdrawals greater than current checking

--- atm.js
+++ atm.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   var checkingAmt = 0;
-  var checkingDeposit = $("#checking .input").val();
   $("#checking .deposit").on("click", function() {
     //checkingAmt += amount from input field
+    var checkingDeposit = $("#checking .input").val();
     console.log("Checking Deposit Button Clicked");
     checkingAmt = parseInt(checkingAmt);
     checkingDeposit = parseInt(checkingDeposit);
@@ -11,13 +11,18 @@ $(document).ready(function(){
     $("#checking .balance").text("$"+checkingAmt);
   });
 
-  var checkingWithdraw = $("#checking .input").val();
   $("#checking .withdraw").on("click", function() {
     console.log("Checking Withdraw Button Clicked");
-    checkingAmt = parseInt(checkingAmt);
-    checkingWithdraw = parseInt(checkingWithdraw);
-    checkingAmt -= checkingWithdraw;
-    checkingAmt = String(checkingAmt);
-    $("#checking .balance").text("$"+checkingAmt);
+    var checkingWithdraw = $("#checking .input").val();
+    if (checkingWithdraw > checkingAmt) {
+      console.log("Sorry you have do have not enough in your checking account to withdraw that much.");
+    }
+    else {
+      checkingAmt = parseInt(checkingAmt);
+      checkingWithdraw = parseInt(checkingWithdraw);
+      checkingAmt -= checkingWithdraw;
+      checkingAmt = String(checkingAmt);
+      $("#checking .balance").text("$"+checkingAmt);
+    }
   });
 });
