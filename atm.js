3a763ed68fee6468fbed68ca47f3d4c9fd01acd0
Chris Gardner <ctgardner89@gmail.com>
2016-06-02 16:54:27 -0400

    finished the proj

--- atm.js
+++ atm.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-
 var savings = {
   balance: $("#savings .balance"),
   putted: $("#savings .input"),
@@ -9,6 +8,13 @@ var savings = {
       savings.balVal = savings.balVal - parseInt(savings.putted.val());
       savings.balance.html("$" + savings.balVal.toString());
       savings.putted.val("");
+
+      if(savings.balVal == 0){
+        checking.balance.addClass("zero");
+      }else{
+        checking.balance.removeClass("zero")
+      }
+
     }else{
       alert("Not enough BREAD!");
     }
@@ -17,60 +23,53 @@ var savings = {
     savings.balVal = savings.balVal + parseInt(savings.putted.val());
     savings.balance.html("$" + savings.balVal.toString());
     savings.putted.val("");
+    if(savings.balVal == 0){
+      checking.balance.addClass("zero");
+    }else{
+      checking.balance.removeClass("zero")
+    }
   }),
-  balVal: parseInt($("#savings .balance").html().replace("$",""))
+  balVal: parseInt($("#savings .balance").html().replace("$","")),
 };
 
 var checking = {
   balance: $("#checking .balance"),
-
   putted: $("#checking .input"),
-
-
   draw: $("#checking .withdraw").click(function(){
     if( ( checking.balVal - parseInt(checking.putted.val()) ) >= 0){
       checking.balVal = checking.balVal - parseInt(checking.putted.val());
-      checking.balance.html("$" +  checking.balVal.toString())
+      checking.balance.html("$" +  checking.balVal.toString());
+
     }else if( (checking.balVal + savings.balVal) > parseInt(checking.putted.val()) ){
        var over = parseInt(checking.putted.val()) - checking.balVal;
-       /*savings.balVal = savings.balVal - over ;
-       checking.balVal = bank.moneyExist - bank.moneyExist;*/
-       savings.balance.html("$" + ((checking.balVal + savings.balVal) - parseInt(checking.putted.val())).toString() );
+       savings.balVal = savings.balVal - over;
+       checking.balVal = 0;
+       savings.balance.html("$" + savings.balVal.toString());
        checking.balance.html("$" + checking.balVal.toString());
+
     }else{
       alert("Not enough BREAD!");
     }
        checking.putted.val("");
+       if(checking.balVal == 0){
+         checking.balance.addClass("zero");
+       }else{
+         checking.balance.removeClass("zero");
+       }
   }),
   depo: $("#checking .deposit").click(function(){
     checking.balVal = checking.balVal + parseInt(checking.putted.val());
     checking.balance.html("$" + checking.balVal.toString());
     checking.putted.val("");
+
+    if(checking.balVal == 0){
+      checking.balance.addClass("zero");
+    }else{
+      checking.balance.removeClass("zero")
+    }
   }),
 
-  balVal : parseInt($("#checking .balance").html().replace("$",""))
+  balVal : parseInt($("#checking .balance").html().replace("$","")),
 };
-
-var bank = {
-
-  moneyExist: (checking.balVal + savings.balVal),
-
-};
-
-$("*").hover(function(){
-
-if ( checking.balVal === 0){
-  checking.balance.addClass("zero");
-}else if( savings.balVal === 0){
-  savings.balance.addClass("zero");
-}else{
-  savings.balance.removeClass("zero");
-  checking.balance.removeClass("zero");
-}
-
-});
-
-
-
 
 });
