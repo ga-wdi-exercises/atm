219c210407a772a3ccad9e6866cbc67e4e93eaa8
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2016-06-02 12:16:51 -0400

    more madness with jquery

--- atm.js
+++ atm.js
@@ -8,7 +8,7 @@ var updatedBalance = ("$") + (balance + deposit)
 $("#checking .balance").text(updatedBalance)
 })
 
-// I keep getting NaN for the parseInt here in "savings", despite .input always being a number. I don't understand why it is doing this here and not on the "checking".
+// I keep getting NaN for the parseInt here in "savings", despite .input always being a number. I don't understand why it is doing this here and not on the "checking". Do I need to further define .input? Does this have to do with how it's arranged? 
 $("#savings .deposit").click(function(){
 var deposit = parseInt($(".input").val())
 var balance = parseInt($("#savings .balance").text().replace("$", ""))
