1618256296801e272c47f0f0497f632d4bc2e8fd
Sean Tay <seantay@Seans-MacBook-Pro-2.local>
2016-06-02 11:43:09 -0400

    bonus completed

--- atm.js
+++ atm.js
@@ -2,45 +2,6 @@
 var CcurrentBalance = 0;
 var ScurrentBalance = 0;
 
-//checking deposit button function
-$('#checking input.deposit').on('click', function(){
-        //retreiving deposit value
-        var depositAmt = $('#checking input.input').val();
-        console.log(depositAmt);
-
-        //changing deposit to Number
-        depositAmt = parseInt(depositAmt);
-        console.log(depositAmt);
-
-        //adding to the current balance
-        CcurrentBalance = CcurrentBalance + depositAmt;
-        console.log(CcurrentBalance);
-
-        //display the currentBalance
-        $('#checking div.balance').html(CcurrentBalance);
-})
-
-
-//checking withdraw button function
-$('#checking input.withdraw').on('click', function(){
-        //retreiving deposit value
-        var withdrawAmt = $('#checking input.input').val();
-        console.log(withdrawAmt);
-
-        //changing deposit to Number
-        withdrawAmt = parseInt(withdrawAmt);
-        console.log(withdrawAmt);
-
-        //adding to the current balance
-        CcurrentBalance = CcurrentBalance - withdrawAmt;
-        console.log(CcurrentBalance);
-
-        //display the currentBalance
-        $('#checking div.balance').html(CcurrentBalance);
-})
-
-
-
 //saving deposit button function
 $('#savings input.deposit').on('click', function(){
         //retreiving deposit value
@@ -76,4 +37,60 @@ $('#savings input.withdraw').on('click', function(){
 
         //display the currentBalance
         $('#savings div.balance').html(ScurrentBalance);
+})
+
+
+//checking deposit button function
+$('#checking input.deposit').on('click', function(){
+        //retreiving deposit value
+        var depositAmt = $('#checking input.input').val();
+        console.log(depositAmt);
+
+        //changing deposit to Number
+        depositAmt = parseInt(depositAmt);
+        console.log(depositAmt);
+
+        //adding to the current balance
+        CcurrentBalance = CcurrentBalance + depositAmt;
+        console.log(CcurrentBalance);
+
+        //display the currentBalance
+        $('#checking div.balance').html(CcurrentBalance);
+})
+
+
+//checking withdraw button function
+$('#checking input.withdraw').on('click', function(){
+        //retreiving deposit value
+        var withdrawAmt = $('#checking input.input').val();
+        // console.log(withdrawAmt);
+
+        //changing deposit to Number
+        withdrawAmt = parseInt(withdrawAmt);
+        // console.log(withdrawAmt);
+
+        var totalBalance = CcurrentBalance + ScurrentBalance;
+        console.log(totalBalance);
+
+        if (CcurrentBalance > withdrawAmt) {
+        //adding to the current balance
+          CcurrentBalance = CcurrentBalance - withdrawAmt;
+        // console.log(CcurrentBalance);
+        }
+        //overdraftprotection
+        else if (CcurrentBalance < withdrawAmt) {
+              if (withdrawAmt > totalBalance){
+                alert("you're broke!")
+              } else {
+              ScurrentBalance = ScurrentBalance - (withdrawAmt - CcurrentBalance);
+              CcurrentBalance = 0;
+          }
+        }
+
+
+
+        //display the currentBalance
+        $('#checking div.balance').html(CcurrentBalance);
+        $('#savings div.balance').html(ScurrentBalance);
+
 })
