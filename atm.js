e5484a247d8c672d4ae546b45492921589364f62
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2016-06-02 11:09:18 -0400

    changed js file location

--- atm.js
+++ atm.js
@@ -1,19 +1,17 @@
 $(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-//not anymore!
-});
+})
+
 $("#checking .deposit").click(function(){
-var deposit = parseInt($(".input").val(), 0)
-var balance = parseInt($("#checking .balance").text().replace("$", ""))
-var newBalance = "$" + (balance + deposit)
+var deposit = parseInt($(".input").val());
+var balance = parseInt($("#checking .balance").text().replace("$", ""));
+var newBalance = "$" + (balance + deposit);
 $("#checking .balance").text(newBalance)
 })
 
 
-
 $("#savings .deposit").click(function(){
-var deposit = parseInt($(".input").val(), 0)
-var balance = parseInt($("#savings .balance").text().replace("$", ""))
-var newBalance = "$" + (balance + deposit)
+var deposit = parseInt($(".input").val());
+var balance = parseInt($("#savings .balance").text().replace("$", ""));
+var newBalance = "$" + (balance + deposit);
 $("#savings .balance").text(newBalance)
-});
+})
