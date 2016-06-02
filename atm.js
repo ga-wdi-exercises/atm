4ca4562bf1f06456cb08745b0d099dc5776e9da7
Matthew Scott <matthewruelscott@gmail.com>
2016-06-02 09:53:38 -0400

    Gets user input

--- atm.js
+++ atm.js
@@ -1,2 +1,7 @@
 $(document).ready(function(){
+  var moneyDeposit = $(".deposit")
+  moneyDeposit.on("click", function(){
+    var moneyInput = $(".input").val();
+    console.log(moneyInput);
+  })
 });
