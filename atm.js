d6e0dd82a6367232c3ba21b6c1ab89f1bc96d697
Kevin Golden <kwg@cs.net>
2016-06-02 09:18:20 -0400

    checking deposit click working

--- atm.js
+++ atm.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-  $("body").css("background-color", "red");
+  // $("body").css("background-color", "red");
+
+//   Add a click listener to the checking account's "Deposit" button
+// When you click the button it should console.log("hello")
+$("#checking .deposit").on("click", function(){
+    console.log("clicked checking deposit");
+});
 });
