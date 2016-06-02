f16c9840e477db547929e64c4f0a1f50d9025851
Andrew Young <prince.of.utah@gmail.com>
2016-06-02 15:18:24 -0400

    jquery stuff added-forgot to commit all day

--- atm.js
+++ atm.js
@@ -1,3 +1,43 @@
 $(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-});
+  $("#checkingDeposit").on("click", deposit);
+  $("#savingsDeposit").on("click", deposit);
+  $("#checkingWithdraw").on("click", withdraw);
+  $("#savingsWithdraw").on("click", withdraw);
+
+  $("#checkingDeposit").on("click", function(){
+    var checkingDeposit = parseInt($("#checkingDeposit").val());
+    var checkingWithdraw = parseInt($("#checkingWithdraw").val());
+    var checkingInput = parseInt($("#checkingInput").val());
+
+    function deposit(){
+      var prevBalance = $(this).siblings("div.balance");
+      var result = prevBalance.text();
+      var currentBalance = parseInt(result.replace("$", ""));
+      var newValue = parseInt($(this).siblings("input.moneyInTheBank").val());
+
+      $(prevBalance).text(function(){
+        var total = newEntry + currentBalance;
+        return "$" + total;
+      });
+    }
+
+    var balanceSum;
+    function getSumBalances() {
+      var balanceChecking = $("#balanceChecking").text();
+      var checkingBalance = parseInt(cBalance.replace("$", ""));
+      var balanceSavings = $("#balanceSavings").text();
+      var savingsBalance = parseInt(sBalance.replace("$", ""));
+      balanceSum = savingsBalance + checkingBalance;
+      return balanceSum;
+    }
+    function withdrawlMoney(){
+      getSumBalance();
+      var prevBalance = $(this).siblings("div.balance");
+      var result = prevBalance.text();
+      var currentBalance = parseInt(result.replace("$", ""));
+      var newWithdrawl = parseInt($(this).siblings("input.moneyInTheBank").val());
+    }
+    $(prevBalance).text(function(){
+      var total = currentBalance - newWithdrawl;
+
+    }
