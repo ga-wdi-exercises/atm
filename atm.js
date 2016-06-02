7fdf0a08be50aea7871de653f0ec5436211e8f64
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2016-06-02 10:47:48 -0400

    more jquery funsies

--- atm.js
+++ atm.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+//not anymore!
 });
+$("#checking .deposit").click(function(){
+var deposit = parseInt($(".input").val(), 0)
+var balance = parseInt($("#checking .balance").text().replace("$", ""))
+var newBalance = "$" + (balance + deposit)
+$("#checking .balance").text(newBalance)
+})
+
+
+
 $("#savings .deposit").click(function(){
-  console.log("hello")
+var deposit = parseInt($(".input").val(), 0)
+var balance = parseInt($("#savings .balance").text().replace("$", ""))
+var newBalance = "$" + (balance + deposit)
+$("#savings .balance").text(newBalance)
 });
