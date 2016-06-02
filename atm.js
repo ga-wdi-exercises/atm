098ed92fce280474d2746196c11f1210cfbe3c7c
Michael Holzheimer <michael.r.holzheimer@gmail.com>
2016-06-02 16:03:09 -0400

    everything but css working, no bonus yet

--- atm.js
+++ atm.js
@@ -1,3 +1,5 @@
+//fix css update
+
 $( document ).ready(function() {
   console.log( "document loaded")
 
@@ -5,28 +7,27 @@ $( document ).ready(function() {
   var depositb = document.getElementById('depositb')
   var withdraw = document.getElementById('withdraw')
   var withdrawb = document.getElementById('withdrawb')
-  // var checking_balance = document.getElementById('#checking_balance')
   var checking_balance = Number(0)
   var savings_balance = Number(0)
-
-
-
 
 
   deposit.addEventListener('click', ca_chang)
   function ca_chang (){
     var add = document.getElementById('add').value;
     checking_balance += Number(add)
-    console.log("checking:" + checking_balance)
+    console.log("checking:" + checking_balance);
     $("#checking_balance").text("$" + checking_balance);
   }
 
   withdraw.addEventListener('click', take)
   function take (){
     var add = document.getElementById('add').value;
-    checking_balance -= Number(add)
-    console.log("checking:" + checking_balance)
-    $("#checking_balance").text("$" + checking_balance);
+    if (checking_balance - Number(add) >= 0) {
+      checking_balance -= Number(add)
+      console.log("checking:" + checking_balance);
+      $("#checking_balance").text("$" + checking_balance);
+    }
+    else $("#checking_balance").css(".zero");
   }
 
   depositb.addEventListener('click', ca_changb)
@@ -40,23 +41,13 @@ $( document ).ready(function() {
   withdrawb.addEventListener('click', takeb)
   function takeb (){
     var addb = document.getElementById('addb').value;
-    savings_balance -= Number(addb)
-    console.log('savings:' + savings_balance)
-    $("#savings_balance").text("$" + savings_balance);
+    if (savings_balance - Number(addb) >=0) {
+      savings_balance -= Number(addb)
+      console.log('savings:' + savings_balance)
+      $("#savings_balance").text("$" + savings_balance);
+    }
+    // else $("#checking_balance").css(".zero");
   }
-
-//displaying values in main block
-
-
-
-  // var b1 = 0
-  // function account_update (){
-  //   b1 += 1;
-  //   console.log(b1);
-  // }
-
-
-
 
 
 });
