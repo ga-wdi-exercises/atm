dab9ed78ab659ad75039bdd9098a29cd5a7f4641
David <dcordz@gmail.com>
2016-06-02 09:30:27 -0400

    intial commit, checking deposit and withdraw buttons work

--- atm.js
+++ atm.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+  var checkingAmt = 0;
+  var checkingDeposit = $("#checking .input").val();
+  $("#checking .deposit").on("click", function() {
+    //checkingAmt += amount from input field
+    console.log("Checking Deposit Button Clicked");
+    checkingAmt = parseInt(checkingAmt);
+    checkingDeposit = parseInt(checkingDeposit);
+    checkingAmt += checkingDeposit;
+    checkingAmt = String(checkingAmt);
+    $("#checking .balance").text("$"+checkingAmt);
+  });
+
+  var checkingWithdraw = $("#checking .input").val();
+  $("#checking .withdraw").on("click", function() {
+    console.log("Checking Withdraw Button Clicked");
+    checkingAmt = parseInt(checkingAmt);
+    checkingWithdraw = parseInt(checkingWithdraw);
+    checkingAmt -= checkingWithdraw;
+    checkingAmt = String(checkingAmt);
+    $("#checking .balance").text("$"+checkingAmt);
+  });
 });
