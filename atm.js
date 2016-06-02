45585ee9e83ffe9c17b806b5c408894da56888f3
Michael Holzheimer <michael.r.holzheimer@gmail.com>
2016-06-02 10:36:52 -0400

    basic balance addition working

--- atm.js
+++ atm.js
@@ -1,3 +1,27 @@
-$(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-});
+var balance2 = 0
+
+document.addEventListener("click", function1)
+
+function function1 (){
+  balance2 = balance2 + 1
+  console.log(balance2)
+}
+
+
+
+// $(document).ready(function(){
+// });
+
+// var check = document.getElementById(".header");
+// var save = document.getElementById("savings account");
+
+// check.background-color input.value = "blue";
+
+
+// form.addEventListener("submit", function(){
+//   ID="savings"".account" = input.value;
+//   box = clear
+// })
+
+
+// on("click", "background-color:red")
