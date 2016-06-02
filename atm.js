9b620f222f7315c59a921c1b67a116dd89574194
James <james@team-wombat.com>
2016-06-01 20:39:34 -0400

    Finished deposits, withdrawals and balance display for checking account

--- atm.js
+++ atm.js
@@ -1,3 +1,24 @@
-$(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+var checkBal = 0;
+var savBal = 0;
+var checkDepo = 0;
+var checkDraw = 0;
+
+$(document).on("ready", function() {
+
+$("#checking").find(".deposit").on("click", function(){
+    checkDepo = parseInt($("#checking").find(".input").val());
+    checkBal = parseInt(checkBal + checkDepo);
+    $("#checking").find(".balance").text(checkBal);
+    checkDepo = null;
+    $("#checking").find(".input").val("");
+});
+
+$("#checking").find(".withdraw").on("click", function(){
+    checkDraw = parseInt($("#checking").find(".input").val());
+    checkBal = parseInt(checkBal - checkDraw);
+    $("#checking").find(".balance").text(checkBal);
+    checkDraw = null;
+    $("#checking").find(".input").val("");
+});
+
 });
