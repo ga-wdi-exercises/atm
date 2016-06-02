cd6a16f791e02f47044bd83973d48522450aeda2
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2016-06-02 12:32:53 -0400

    fixed input

--- atm.js
+++ atm.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 })
 
 $("#checking .deposit").click(function(){
-  var deposit = parseInt($(".input").val())
+  var deposit = parseInt($("#checking .input").val())
   var balance = parseInt($("#checking .balance").text().replace("$", ""))
   var updatedBalance = ("$") + (balance + deposit)
   $("#checking .balance").text(updatedBalance)
@@ -10,7 +10,7 @@ $("#checking .deposit").click(function(){
 
 // I keep getting NaN for the parseInt here in "savings", despite .input always being a number. I don't understand why it is doing this here and not on the "checking". Do I need to further define .input? Does this have to do with how it's arranged?
 $("#savings .deposit").click(function(){
-  var deposit = parseInt($(".input").val())
+  var deposit = parseInt($("#savings .input").val())
   var balance = parseInt($("#savings .balance").text().replace("$", ""))
   var updatedBalance = ("$") + (balance + deposit)
   $("#savings .balance").text(updatedBalance)
