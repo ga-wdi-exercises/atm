aab2084e45ad7d0512bc0b83ba9fe4e69158915f
Julia Schultz <julia.schultz91@gmail.com>
2016-06-02 10:54:05 -0400

    checking and savings working

--- atm.js
+++ atm.js
@@ -1,14 +1,11 @@
 
-
-
-
 $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
 
 var checkingInput; //#checking .input
 var savingsInput; //#savings .input
 var checkingBalance=0; //#checking .balance
-var savingBalance=0;
+var savingsBalance=0;
 var checkingInput = $("#checking .input").val();
 
 
@@ -16,52 +13,80 @@ var checkingInput = $("#checking .input").val();
 
 $("#checking .deposit").on("click", function(){
 
-  var checkingInput = $("#checking .input").val();
+    var checkingInput = $("#checking .input").val();
 
-    if (checkingInput <=0) {
-      alert("Please enter an amount greater than 0");
-      }
+      if (checkingInput <=0) {
+        alert("Please enter an amount greater than 0");
+        }
 
-    else {
-  // console.log(checkingInput);
-  checkingInput = parseInt(checkingInput);
-  // console.log((checkingInput+10)+" is the user input plus 10")
-  checkingBalance=checkingBalance+checkingInput
-  $("#checking .balance").html("$"+checkingBalance);
-  console.log("the new balance is $"+checkingBalance);
-  }
+      else {
+    // console.log(checkingInput);
+    checkingInput = parseInt(checkingInput);
+    // console.log((checkingInput+10)+" is the user input plus 10")
+    checkingBalance=checkingBalance+checkingInput
+    $("#checking .balance").html("$"+checkingBalance);
+    console.log("the new balance is $"+checkingBalance);
+    }
 
   })
 
 
 $("#checking .withdraw").on("click", function(){
 
-  var checkingInput = $("#checking .input").val();
+    var checkingInput = $("#checking .input").val();
 
-    if (checkingInput <=0) {
-      alert("Please enter an amount greater than 0");
+      if (checkingInput <=0) {
+        alert("Please enter an amount greater than 0");
+        }
+
+      else {
+
+      checkingInput = parseInt(checkingInput);
+      // console.log((checkingInput+10)+" is the user input plus 10")
+      checkingBalance=checkingBalance-checkingInput
+      $("#checking .balance").html("$"+checkingBalance);
+      console.log("the new balance is $"+checkingBalance);
       }
-
-    else {
-  
-    checkingInput = parseInt(checkingInput);
-    // console.log((checkingInput+10)+" is the user input plus 10")
-    checkingBalance=checkingBalance-checkingInput
-    $("#checking .balance").html("$"+checkingBalance);
-    console.log("the new balance is $"+checkingBalance);
-    }
 
     })
 
+    $("#savings .deposit").on("click", function(){
+
+        var savingsInput = $("#savings .input").val();
+
+          if (savingsInput <=0) {
+            alert("Please enter an amount greater than 0");
+            }
+
+          else {
+        // console.log(checkingInput);
+        savingsInput = parseInt(savingsInput);
+        // console.log((checkingInput+10)+" is the user input plus 10")
+        savingsBalance=savingsBalance+savingsInput
+        $("#savings .balance").html("$"+savingsBalance);
+        console.log("the new balance is $"+savingsBalance);
+        }
+
+      })
 
 
-//
-// On clicking "Deposit", it should update the "balance" with the user input
-//  - Just make the user input show up. Don't worry about actually keeping track of a balance yet.
-// - On "Deposit", it should get the current "balance"
-//  - How can you get the content of the "balance" element?
-//  - The content has a `$` at the beginning of it, so Javascript will read it as text rather than as a number. How can you convert this text into a number?
-//
+    $("#savings .withdraw").on("click", function(){
 
+        var savingsInput = $("#savings .input").val();
+
+          if (savingsInput <=0) {
+            alert("Please enter an amount greater than 0");
+            }
+
+          else {
+
+          savingsInput = parseInt(savingsInput);
+          // console.log((checkingInput+10)+" is the user input plus 10")
+          savingsBalance=savingsBalance-savingsInput
+          $("#savings .balance").html("$"+savingsBalance);
+          console.log("the new balance is $"+savingsBalance);
+          }
+
+        })
 
 });
