d98ea2ab0bba81ec23c007ce652044564132d623
Anh Truong <anh.truong.07@cnu.edu>
2016-06-02 11:37:13 -0400

    background changes to red when balance is 0

--- atm.js
+++ atm.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     var checkingBal = 0;
     var savingsBal = 0;
 
-    //Checking
+//Checking
     //deposit
     $("#checking .deposit").on("click", function(){
         //amount in box, click deposit-adds val
@@ -27,32 +27,41 @@ $(document).ready(function(){
         else{
             checkingBal = checkingBal - parseInt(checkingInput); //subtracting the value from checkingBal
             $("#checking .balance").text(checkingBal);
+            if (checkingBal === 0){
+                    $("#checking").css("background", "red");
+                }
         }
     });
-        //savings
-        //deposit
-        $("#savings .deposit").on("click", function(){
-            //amount in box, click deposit-adds val
-            console.log(savingsBal);
 
-            var savingsInput = $("#savings .input").val();
-            savingsBal = parseInt(savingsInput) + savingsBal; //adding the value to the balance after parsing the string in savingsInput to a var
+
+//savings
+    //deposit
+    $("#savings .deposit").on("click", function(){
+        //amount in box, click deposit-adds val
+        console.log(savingsBal);
+
+        var savingsInput = $("#savings .input").val();
+        savingsBal = parseInt(savingsInput) + savingsBal; //adding the value to the balance after parsing the string in savingsInput to a var
+        $("#savings .balance").text(savingsBal);
+    });
+
+    //withdraw
+    $("#savings .withdraw").on("click", function(){
+        //amount in box withdraw-subtract val
+        console.log(savingsBal);
+
+        var savingsInput = $("#savings .input").val();
+        if (savingsInput > savingsBal){
+            console.log("not enough money in savings balance")
+        }
+        else{
+            savingsBal = savingsBal - parseInt(savingsInput); //subtracting the value from savingsBal
             $("#savings .balance").text(savingsBal);
-        });
+            if (savingsBal === 0){
+                    $("#savings").css("background", "red");
+                }
+        }
 
-        //withdraw
-        $("#savings .withdraw").on("click", function(){
-            //amount in box withdraw-subtract val
-            console.log(savingsBal);
-
-            var savingsInput = $("#savings .input").val();
-            if (savingsInput > savingsBal){
-                console.log("not enough money in savings balance")
-            }
-            else{
-                savingsBal = savingsBal - parseInt(savingsInput); //subtracting the value from savingsBal
-                $("#savings .balance").text(savingsBal);
-            }
     });
 
 }); //overall load function close (this is the last bracket)
