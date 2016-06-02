e4fbe934419af49b09107c7727176747dc2f0074
Matthew Scott <matthewruelscott@gmail.com>
2016-06-02 11:16:16 -0400

    Checking withdrawl works

--- atm.js
+++ atm.js
@@ -7,4 +7,12 @@ $(document).ready(function(){
     var balanceChange = $("#balanceCheck");
     balanceChange.html("$"+totalBalance);
   })
+  var moneyWithdraw = $("#checking").find(".withdraw")
+  moneyWithdraw.on("click", function(){
+    var currentBalance = parseInt($("#balanceCheck").html().replace("$", ""));
+    var moneyInput = parseInt($(".input").val());
+    var totalBalance = currentBalance - moneyInput;
+    var balanceChange = $("#balanceCheck");
+    balanceChange.html("$"+totalBalance);
+  })
 });
