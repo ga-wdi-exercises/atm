384c95470727e750fca95b2d808f72cbf5b9fa34
Sean Tay <seantay@Seans-MacBook-Pro-2.local>
2016-06-02 10:53:55 -0400

    checking deposit complete

--- atm.js
+++ atm.js
@@ -1,4 +1,20 @@
+//initial balance
+var currentBalance = 0;
+
+//checking button function
 $('#checking input.deposit').on('click', function(){
+  //retreiving deposit value
   var depositAmt = $('#checking input.input').val();
   console.log(depositAmt);
+
+  //changing deposit to Number
+  depositAmt = parseInt(depositAmt);
+  console.log(depositAmt);
+
+  //adding to the current balance
+  currentBalance = currentBalance + depositAmt;
+  console.log(currentBalance);
+
+  //display the currentBalance
+  $('#checking div.balance').html(currentBalance);
 })
