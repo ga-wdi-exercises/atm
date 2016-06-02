097d0b85bdae10f1b029a6f37209002c2232d29f
Julia Najafi <Julia.Najafi@gmail.com>
2016-06-02 09:40:16 -0400

    Checking Account button deposits user input into balance

--- atm.js
+++ atm.js
@@ -1,8 +1,22 @@
 $(document).ready(function(){
 
-  
+  // checking account
 
 
+
+  //this var is a link to the checking deposit button
+  var checkingDepositButton = $("#checking .deposit");
+
+  //if click on checking account deposit button, then
+  checkingDepositButton.on("click", checkingGetUserInput);
+
+  //this var links to the input value from user under checking account
+  var checkingInputValue = $("#checking .input");
+
+  // this function gets user input for despoit box and inserts it into checking tally
+  function checkingGetUserInput() {
+    $("#checking .balance").text("$" + checkingInputValue.val());
+  }
 
 
 
