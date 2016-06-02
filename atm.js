3572a39f72c908906e502b0c6b352b8b7345a36a
Julia Najafi <Julia.Najafi@gmail.com>
2016-06-02 11:01:24 -0400

    despoit button for checking account updates total checking Balance .

--- atm.js
+++ atm.js
@@ -2,21 +2,26 @@ $(document).ready(function(){
 
   // checking account
 
-
-
   //this var is a link to the checking deposit button
   var checkingDepositButton = $("#checking .deposit");
 
-  //if click on checking account deposit button, then
-  checkingDepositButton.on("click", checkingGetUserInput);
+  // the value of total checking balance
+  var checkingAccountTotal = 0;
+
 
   //this var links to the input value from user under checking account
-  var checkingInputValue = $("#checking .input");
 
-  // this function gets user input for despoit box and inserts it into checking tally
-  function checkingGetUserInput() {
-    $("#checking .balance").text("$" + checkingInputValue.val());
-  }
+  //if click on checking account deposit button, then inserts into checking balance
+  checkingDepositButton.on("click", function(){
+    var checkingInputValue = $("#checking .input");
+    var numCheckingInputValue = parseInt(checkingInputValue.val());
+    checkingAccountTotal = parseInt(checkingAccountTotal);
+    checkingAccountTotal = checkingAccountTotal + numCheckingInputValue;
+    checkingAccountTotal = String(checkingAccountTotal);
+    $("#checking .balance").html("$" + checkingAccountTotal);
+  });
+
+
 
 
 
