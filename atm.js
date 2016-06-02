13e5b4bc2dd42867a3b124383df8c701c1f13a24
Mara Sanders <sandersmara@gmail.com>
2016-06-02 16:25:41 -0400

    Consistent tags cleaned up working version

--- atm.js
+++ atm.js
@@ -1,93 +1,60 @@
-
-
-var balanceInt = parseInt (0);
-var breakYes = "no"
 var depositInt = parseInt (0)
 var savingsBal = parseInt (0)
+var checkingBal = parseInt (0)
 
-function changeClass(balanceInt){
+function changeClass(checkingBal){
    $("#checking .balance").removeClass("zero");
-   if (balanceInt === 0) {
+   if (checkingBal === 0) {
     $("#checking .balance").addClass("zero");
    }
 };
 
-function changeSClass(balanceInt){
+function changeSClass(savingsBal){
    $("#savings .balance").removeClass("zero");
-   if (balanceInt === 0) {
+   if (savingsBal === 0) {
     $("#savings .balance").addClass("zero");
    }
 };
 
-function validateNum(depositInt,breakYes){
-  breakYes = "no"
-  if (!depositInt) {
-    alert("Invalid Amount Entered. Please try again.");
-    breakYes = "yes"
-  }
-  return breakYes;
-}
-
-
 $(document).ready(function(){
-  $("body").css("background-color","red");
   $("#checking .deposit").on("click",function(){
      var deposit = $("#checking .input").val();
      depositInt = parseInt(deposit);
-/*     validateNum(depositInt,breakYes)
-     if (breakYes = "yes"){
-        console.log(breakYes)
-        break - break out of this and start again
-    } */
-     balanceInt = balanceInt + depositInt
-     $("#checking .balance").html("$"+balanceInt);
-     changeClass(balanceInt)
-     console.log (balanceInt)
-     console.log (depositInt)
+     checkingBal = checkingBal + depositInt
+     $("#checking .balance").html("$"+checkingBal);
+     changeClass(checkingBal)
   })
 
   $("#checking .withdraw").on("click",function(){
-    var withdrawl = $("#checking .input").val();
-        var withdrawlInt = parseInt(withdrawl);
-        balanceInt = balanceInt - withdrawlInt
-        if (balanceInt < 0){
-          balanceInt = balanceInt + withdrawlInt
-          alert("Invalid Withdrawl Balance Fell Below Zero");
-         }
-       $("#checking .balance").html("$"+balanceInt);
-       changeClass(balanceInt)
-       console.log (balanceInt)
-       console.log (withdrawlInt)
-   });
+    var deposit = $("#checking .input").val();
+    var depositInt = parseInt(deposit);
+    checkingBal = checkingBal - depositInt
+    if (checkingBal < 0){
+      checkingBal = checkingBal + depositInt
+      alert("Invalid Withdrawl Balance Fell Below Zero");
+    }
+    $("#checking .balance").html("$"+checkingBal);
+    changeClass(checkingBal)
+  });
 
    $("#savings .deposit").on("click",function(){
       var deposit = $("#savings .input").val();
       depositInt = parseInt(deposit);
- /*     validateNum(depositInt,breakYes)
-      if (breakYes = "yes"){
-         console.log(breakYes)
-         break - break out of this and start again
-     } */
       savingsBal = savingsBal + depositInt
       $("#savings .balance").html("$"+savingsBal)
       changeSClass(savingsBal)
-      console.log (balanceInt)
-      console.log (savingsBal)
    })
 
 
-      $("#savings .withdraw").on("click",function(){
-         var deposit = $("#savings .input").val();
-         depositInt = parseInt(deposit);
-    /*     validateNum(depositInt,breakYes)
-         if (breakYes = "yes"){
-            console.log(breakYes)
-            break - break out of this and start again
-        } */
-         savingsBal = savingsBal - depositInt
-         $("#savings .balance").html("$"+savingsBal)
-         changeSClass(savingsBal)
-         console.log (balanceInt)
-         console.log (savingsBal)
-      })
-   });
+   $("#savings .withdraw").on("click",function(){
+     var deposit = $("#savings .input").val();
+     depositInt = parseInt(deposit);
+     savingsBal = savingsBal - depositInt
+     if (savingsBal < 0){
+       savingsBal = savingsBal + depositInt
+       alert("Invalid Withdrawl Balance Fell Below Zero");
+     }
+     $("#savings .balance").html("$"+savingsBal)
+     changeSClass(savingsBal)
+   })
+ });
