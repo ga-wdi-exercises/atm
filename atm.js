a5768757b75e44a5bde50c90a40e509a62f08596
Thaisun Green <Tlgreen16@gmail.COM>
2016-06-02 14:15:36 -0400

    Checking and Savings withdraw ,deposit working

--- atm.js
+++ atm.js
@@ -1,19 +1,57 @@
 $(document).ready(function(){
-  console.log("!!!!!!!!!!!!");
-  var checking = $("#checking .balance").text();
-  var savings = $("#savings .balance").text();
   console.log("Checking Balance " + checking);
   console.log("Savings Balance " + savings);
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-
-});
-$(".deposit").on("click",function(evt){
-  evt.preventDefault();
-  console.log("deposit button clicked")
+// -------------Savings Account-------------------------
+  var savings = $("#savings .balance")
+  $("#savings .deposit").on("click", function(){
+  var deposit = $("#savings .input").val();
+  var balance = savings.text();
+  balance = parseInt(balance.replace("$", ""))
+  console.log("Deposit amount: " + deposit);
+  var deposit = parseInt(deposit);
+  var total = deposit + balance;
+  console.log("total: "+total)
+  $("#savings .balance").html(total);
 })
+// |-------------------Savings Account withdraw -------|
+  var savings = $("#savings .balance")
+$("#savings .withdraw").on("click", function(){
+  var withdraw = $("#savings .input").val();
+  var balance = savings.text();
+  balance = parseInt(balance.replace("$", ""))
+  console.log("withdraw amount: " + withdraw);
+  var withdraw = parseInt(withdraw);
+  var total = balance - withdraw ;
+  console.log("total: "+total)
+  $("#savings .balance").html(total);
+})
+// |------------------------------------------------------|
+// -------------checking Account-------------------------|
+  var checking = $("#checking .balance")
+  $("#checking .deposit").on("click", function(){
+  var deposit = $("#checking .input").val();
+  var balance = checking.text();
+  balance = parseInt(balance.replace("$", ""))
+  console.log("Deposit amount: " + deposit);
+  var deposit = parseInt(deposit);
+  var total = deposit + balance;
+  console.log("total: "+total)
+  $("#checking .balance").html(total);
+})
+// |-------------------checking Account withdraw -------|
+  var savings = $("#checking .balance")
+$("#checking .withdraw").on("click", function(){
+  var withdraw = $("#checking .input").val();
+  var balance = checking.text();
+  balance = parseInt(balance.replace("$", ""))
+  console.log("withdraw amount: " + withdraw);
+  var withdraw = parseInt(withdraw);
+  var total = balance - withdraw ;
+  console.log("total: "+total)
+  $("#checking .balance").html(total);
+})
+// |------------------------------------------------------|
 
-$(".withdraw").on("click",function(evt){
-  evt.preventDefault()
-  console.log("withdraw button clicked")
-  $(this).css("background-color","red")
+
+
 })
