9ce20f6dd90f072f6557d5a5a37ef5ad3ad263b7
Matthew Scott <matthewruelscott@gmail.com>
2016-06-02 12:43:31 -0400

    Adds savings account and color indication

--- atm.js
+++ atm.js
@@ -1,18 +1,59 @@
 $(document).ready(function(){
+  checkZero($("#balanceCheck"));
+  checkZero($("#balanceSave"));
+  //Checking Account Deposit
   var moneyDeposit = $("#checking").find(".deposit")
   moneyDeposit.on("click", function(){
     var currentBalance = parseInt($("#balanceCheck").html().replace("$", ""));
-    var moneyInput = parseInt($(".input").val());
+    var moneyInput = parseInt($("#checkInput").val());
     var totalBalance = moneyInput + currentBalance;
     var balanceChange = $("#balanceCheck");
     balanceChange.html("$"+totalBalance);
+    checkZero($("#balanceCheck"));
   })
+  //Checking Account Withdraw
   var moneyWithdraw = $("#checking").find(".withdraw")
   moneyWithdraw.on("click", function(){
     var currentBalance = parseInt($("#balanceCheck").html().replace("$", ""));
-    var moneyInput = parseInt($(".input").val());
+    var moneyInput = parseInt($("#checkInput").val());
     var totalBalance = currentBalance - moneyInput;
     var balanceChange = $("#balanceCheck");
+    balanceChange.html("$"+totalBalance)
+    checkZero($("#balanceCheck"));
+    if(currentBalance == 0){
+      balanceChange.html("$0");
+      checkZero($("#balanceCheck"));
+    };
+  })
+  //Savings Account Desposit
+  var moneydepositSave = $("#savings").find(".deposit")
+  moneydepositSave.on("click", function(){
+    var currentBalance = parseInt($("#balanceSave").html().replace("$", ""));
+    var moneyInput = parseInt($("#saveInput").val());
+    var totalBalance = moneyInput + currentBalance;
+    var balanceChange = $("#balanceSave");
     balanceChange.html("$"+totalBalance);
+    checkZero($("#balanceSave"));
+  })
+  //Savings Account Withdraw
+  var moneywithdrawSave = $("#savings").find(".withdraw")
+  moneywithdrawSave.on("click", function(){
+    var currentBalance = parseInt($("#balanceSave").html().replace("$", ""));
+    var moneyInput = parseInt($("#saveInput").val());
+    var totalBalance = currentBalance - moneyInput;
+    var balanceChange = $("#balanceSave");
+    balanceChange.html("$"+totalBalance);
+    checkZero($("#balanceSave"));
+    if(currentBalance == 0){
+      balanceChange.html("$0")
+      checkZero($("#balanceSave"));
+    }
   })
 });
+function checkZero(accountDiv){
+  if (accountDiv.html() === "$0") {
+    accountDiv.addClass("zero")
+  } else {
+    accountDiv.removeClass("zero")
+  }
+}
