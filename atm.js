177fc6ebb4ad8f377966a441e3a2190963301895
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2016-06-02 12:29:33 -0400

    auto-indented, not responsible for lack of indentation now

--- atm.js
+++ atm.js
@@ -2,16 +2,16 @@ $(document).ready(function(){
 })
 
 $("#checking .deposit").click(function(){
-var deposit = parseInt($(".input").val())
-var balance = parseInt($("#checking .balance").text().replace("$", ""))
-var updatedBalance = ("$") + (balance + deposit)
-$("#checking .balance").text(updatedBalance)
+  var deposit = parseInt($(".input").val())
+  var balance = parseInt($("#checking .balance").text().replace("$", ""))
+  var updatedBalance = ("$") + (balance + deposit)
+  $("#checking .balance").text(updatedBalance)
 })
 
-// I keep getting NaN for the parseInt here in "savings", despite .input always being a number. I don't understand why it is doing this here and not on the "checking". Do I need to further define .input? Does this have to do with how it's arranged? 
+// I keep getting NaN for the parseInt here in "savings", despite .input always being a number. I don't understand why it is doing this here and not on the "checking". Do I need to further define .input? Does this have to do with how it's arranged?
 $("#savings .deposit").click(function(){
-var deposit = parseInt($(".input").val())
-var balance = parseInt($("#savings .balance").text().replace("$", ""))
-var updatedBalance = ("$") + (balance + deposit)
-$("#savings .balance").text(updatedBalance)
+  var deposit = parseInt($(".input").val())
+  var balance = parseInt($("#savings .balance").text().replace("$", ""))
+  var updatedBalance = ("$") + (balance + deposit)
+  $("#savings .balance").text(updatedBalance)
 })
