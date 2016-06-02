50c24ae5517804cb3099dbc3bb823019ce954d4f
Kevin Golden <kwg@cs.net>
2016-06-02 11:59:05 -0400

    withdrawing works

--- atm.js
+++ atm.js
@@ -1,15 +1,28 @@
 $(document).ready(function(){
-//   On "Deposit", it should get the current "balance"
-// How can you get the content of the "balance" element?
-// The content has a $ at the beginning of it, so Javascript will read it as text
-// rather than as a number. How can you convert this text into a number?
 
-$("#checking .deposit").on("click", function(){
+  $("#checking .deposit").on("click", function(){
     var amtInput = $("input").val();
-    // $("#checking .balance").html("$" + amtInput);
-    var ckBalTmp = $("#checking .balance").html();
-    var ckBal = parseInt(ckBalTmp.substring(1));
-    console.log("Balance is: " + ckBal);
-    // console.log("Balance type is:" + ckBalType);
+    ckDeposit(amtInput);
+  });
+  $("#checking .withdraw").on("click", function(){
+    var amtInput = $("input").val();
+    ckWithdraw(amtInput);
+  });
 });
-});
+
+function ckDeposit(inAmtTmp) {
+  inAmt = parseInt(inAmtTmp,10);
+  var ckBalTmp = $("#checking .balance").html();
+  var ckBal = parseInt(ckBalTmp.substring(1),10);
+  ckBal += inAmt;
+  $("#checking .balance").html("$" + ckBal.toString());
+};
+
+function ckWithdraw(inAmtTmp) {
+  inAmt = parseInt(inAmtTmp,10);
+  // var ckBalTmp = $("#checking .balance").html();
+  // var ckBal = parseInt(ckBalTmp.substring(1),10);
+  var ckBal = parseInt($("#checking .balance").html().substring(1),10);
+  ckBal -= inAmt;
+  $("#checking .balance").html("$" + ckBal.toString());
+};
