824fd2edae47a328e741eefc5a94e39040307372
Arthur Adams <arthuradams@gmail.com>
2016-06-03 08:56:48 -0400

    added finctionality to HTML

--- atm.js
+++ atm.js
@@ -20,7 +20,7 @@ function overdraftWithdrawal(currentBalance, withdrawalAmount, backupBalance) {
 
   // Attempt to perform a normal withdrawal.
   var newBalance = withdrawal(currentBalance, withdrawalAmount);
-
+  console.log(currentBalance, withdrawalAmount, backupBalance);
   // If the normal withdrawal was successful, we don't need to worry about
   // overdraft protection. Just return the result of a normal withdrawal and the
   // unchanged backup account balance.
@@ -39,7 +39,40 @@ function overdraftWithdrawal(currentBalance, withdrawalAmount, backupBalance) {
 }
 
 $(document).ready(function(){
+  // checking deposit
+  $("#checkingdeposit").on("click", function(){
+    var cdeposit = parseInt($("#checkinginput").val());
+    var cbalance = parseInt($("#checkingbalance").text().replace("$", ""));
+    var cnewBalance = deposit(cbalance,cdeposit);
+    $("#checkingbalance").text("$" + cnewBalance);
+  })
 
+  // checking withdrawal /w overdraft protection
+  $("#checkingwithdrawal").on("click", function(){
+    var withdraw = parseInt($("#checkinginput").val());
+    var checkingBalance = parseInt($("#checkingbalance").text().replace("$", ""));
+    var savingsBalance = parseInt($("#savingsbalance").text().replace("$", ""));
+    [checkingBalance,savingsBalance] =
+      overdraftWithdrawal(checkingBalance, withdraw, savingsBalance);
+    $("#checkingbalance").text("$" + checkingBalance);
+    $("#savingsbalance").text("$" + savingsBalance);
+  })
+  // savings deposit
+  $("#savingsdeposit").on("click", function(){
+    var sdeposit = parseInt($("#savingsinput").val());
+    var sbalance = parseInt($("#savingsbalance").text().replace("$", ""));
+    var snewBalance = deposit(sbalance,sdeposit);
+    $("#savingsbalance").text("$" + snewBalance);
+  })
 
-}
-);
+  // savings withdraw with overdraft protection
+  $("#savingsWithdraw").on("click", function(){
+    var withdraw = parseInt($("#savingsInput").val());
+    var checkingBalance = parseInt($("#checkingbalance").text().replace("$", ""));
+    var savingsBalance = parseInt($("#savingsbalance").text().replace("$", ""));
+    [savingsBalance,checkingBalance] =
+      overdraftWithdrawal(savingsBalance, withdraw, checkingBalance);
+    $("#checkingbalance").text("$" + checkingBalance);
+    $("#savingsbalance").text("$" + savingsBalance);
+  })
+})
