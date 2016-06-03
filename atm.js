0120faa082abd2a9e38406e9253461ed2af5a0d4
Arthur Adams <arthuradams@gmail.com>
2016-06-02 21:28:37 -0400

    added function to withdraw funds. no overdrasft protection yet.

--- atm.js
+++ atm.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-});
+
+  function withdrawal(currentBalance, withdrawalAmount) {
+
+    // Determine what the balance will be if the withdrawal completes
+    var newBalance = currentBalance - withdrawalAmount;
+
+    // If there would be positive or zero, return the new balance
+    if (newBalance >= 0) {
+      return newBalance;
+    }
+    // If the balance would be negative, refuse the transaction
+    // and return the unchanged balance
+    else {
+      return currentBalance;
+    }
+
+  }
+
+
+);
