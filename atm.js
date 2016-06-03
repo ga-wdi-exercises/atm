24b81c18ee0e093d5a62135f0a76aad6fa4987df
Arthur Adams <arthuradams@gmail.com>
2016-06-03 06:14:45 -0400

    added overdraft protection withdrawal, standard withdrawal will go to neg bslance

--- atm.js
+++ atm.js
@@ -1,17 +1,8 @@
 function withdrawal(currentBalance, withdrawalAmount) {
 
   // Determine what the balance will be if the withdrawal completes
-  var newBalance = currentBalance - withdrawalAmount;
+  return currentBalance - withdrawalAmount;
 
-  // If there would be positive or zero, return the new balance
-  if (newBalance >= 0) {
-    return newBalance;
-  }
-  // If the balance would be negative, refuse the transaction
-  // and return the unchanged balance
-  else {
-    return currentBalance;
-  }
 
 }
 
@@ -20,6 +11,30 @@ function deposit(currentBalance, depositAmount) {
   // For the scope of this project, it's sim\ple addition.
 
   return currentBalance + depositAmount;
+
+}
+
+function overdraftWithdrawal(currentBalance, withdrawalAmount, backupBalance) {
+  // Execute a withdrawal with overdraft protection.
+  // This will return an array of two values,
+
+  // Attempt to perform a normal withdrawal.
+  var newBalance = withdrawal(currentBalance, withdrawalAmount);
+
+  // If the normal withdrawal was successful, we don't need to worry about
+  // overdraft protection. Just return the result of a normal withdrawal and the
+  // unchanged backup account balance.
+  if ( newBalance > 0 ) {
+    return [newBalance, backupBalance];
+  } else {
+    newBalance = 0;
+    withdrawalAmount -= currentBalance;
+    if (backupBalance >= withdrawalAmount) {
+      return [0, backupBalance - withdrawalAmount];
+    } else {
+      return [currentBalance,backupBalance];
+    }
+  }
 
 }
 
