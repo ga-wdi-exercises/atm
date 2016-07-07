47d20378a75a9101510455b76cbcaa6a431e01c4
Christopher Phillips <cphillips918@gmail.com>
2016-07-07 10:10:35 -0400

    deposit works and withdraw conditional is in effect - next step update withdraw amount

--- atm.js
+++ atm.js
@@ -1,12 +1,22 @@
 $(document).ready(function() {
     $(".deposit:first").on('click', function(event) {
         event.preventDefault();
-        console.log('I Work')
+        var depositAmount = parseFloat($('.input:first').val()).toFixed(2)
+        if(!isNaN(depositAmount)){
+            var currentBalance = parseFloat($('.balance:first').html().split('$')[1])
+            $('.balance:first').html('$' + (parseFloat(depositAmount)+currentBalance).toFixed(2));
+            $('#entry')[0].reset();
+        }
     });
 
+    $(".withdraw:first").on('click', function(event) {
+        event.preventDefault();
+        var withdrawAmount = parseFloat($('.input:first').val()).toFixed(2)
+        var currentBalance = parseFloat($('.balance:first').html().split('$')[1])
+        if(!isNaN(withdrawAmount) && currentBalance > 0){
 
-
-
+        }
+    });
 });
 
 //Pseudocode
