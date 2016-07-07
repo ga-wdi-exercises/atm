97019885d3bb7fc20c1c2d768f2b5b721dcf1c6d
Sarah K Shultis <skshultis@Sks-MacBook-Air.local>
2016-07-07 15:20:39 -0400

    numeric inputs working but only on first press of button, then NaN

--- automatedteller.js
+++ automatedteller.js
@@ -1,27 +1,64 @@
 $(document).ready(function(){
   console.log("Your Mom");
+//
+// // change body to red on mouseover:
+//
+// $("body").on("mouseover", bodyRed);
+//
+//   function bodyRed() {
+//
+//     $("body").css("background", "red");
+//   }
+//
+// // log hello in console on click of checking deposit button:
+//
+// $("#checking .deposit").on("click", hello);
+//
+//   function hello()  {
+//     console.log("Hello.");
+//   }
+//
+// $("#savings .deposit").on("click", function()  {
+//
+//     var deposit = $("#savings .input").val();
+//      var floatDeposit = parseFloat(deposit);
+//
+//     console.log(deposit);
+//
+// });
 
-$("body").on("mouseover", bodyRed);
+$("#checking .deposit").on("click", function(e)  {
+    e.preventDefault();
+    var userDeposit = $("#checking .input").val();
+    var balanceAmount = $("#checking .balance").html();
+    var nUserDeposit = parseFloat(userDeposit);
+    var nBalanceAmount = parseFloat(balanceAmount.split("$")[1]);
 
-  function bodyRed() {
+    console.log(nUserDeposit);
+    console.log(nBalanceAmount);
 
-    $("body").css("background", "red");
-  }
+    var totalBalance = nUserDeposit + nBalanceAmount;
+    console.log(totalBalance);
+    $("#checking .balance").html(totalBalance.toFixed(2));
+});
 
-$("#checking .deposit").on("click", hello);
+$("#savings .deposit").on("click", function(e)  {
+  e.preventDefault();
+  var userDeposit = $("#savings .input").val();
+  var balanceAmount = $("#savings .balance").html();
+  var nUserDeposit = parseFloat(userDeposit);
+  var nBalanceAmount = parseFloat(balanceAmount.split("$")[1]);
 
-  function hello()  {
-    console.log("Hello.");
-  }
+  console.log(nUserDeposit);
+  console.log(nBalanceAmount);
 
-$("#savings .deposit").on("click", function()  {
-
-    var deposit = $("#savings .input").val();
-     var floatDeposit = parseFloat(deposit);
-
-    console.log(deposit);
+  var totalBalance = nUserDeposit + nBalanceAmount;
+  console.log(totalBalance);
+  $("#savings .balance").html(totalBalance.toFixed(2));
 
 });
+
+$("#checking .withdraw")
 
 
 
