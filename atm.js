381c72087b0d7b0710fc2766ee0adabf439c6fd5
Tyler Rosenberg <tyryanr@gmail.com>
2016-07-07 15:37:24 -0400

    all functionings working

--- atm.js
+++ atm.js
@@ -1,7 +1,17 @@
+/*********
+Pseudo Code At Bottom
+**********/
+
 $(document).ready(function(){
 
-/* CHECKINGS */
+/*******
+CHECKINGS
+********/
+
   var checkingTotal = 0;
+  if (checkingTotal === 0) {
+    $("#checking .balance").css("background", "rgb(232, 63, 63)")
+  }
 
   $("#checking .deposit").on("click", function (evt) {
     evt.preventDefault();
@@ -42,7 +52,7 @@ $(document).ready(function(){
       setTimeout(reset, 2000);
     } else if (backToNumber === checkingTotal) {
       checkingTotal = 0;
-      $("#checking .balance").html(checkingTotal);
+      $("#checking .balance").html("$0");
       $("#checking .balance").css("background", "rgb(232, 63, 63)");
     } else {
       checkingTotal -= backToNumber;
@@ -50,11 +60,90 @@ $(document).ready(function(){
       $("#checking .balance").html(finalNumber);
     }
 
+
+    //setTimeout function in order to re-display total value after withdraw has been rejected
     function reset() {
-      $("#checking .balance").css("background", "#E3E3E3");
-      $("#checking .balance").html(checkingTotal);
+      $("#checking .balance").html("$" + parseFloat(checkingTotal.toFixed(2)));
+      if (checkingTotal === 0) {
+        $("#checking .balance").css("background", "rgb(232, 63, 63)");
+      } else {
+        $("#checking .balance").css("background", "#E3E3E3");
+      }
     };
   }
+
+/*******
+SAVINGS
+********/
+
+var savingsTotal = 0;
+if (savingsTotal === 0) {
+  $("#savings .balance").css("background", "rgb(232, 63, 63)")
+}
+
+$("#savings .deposit").on("click", function (evt) {
+  evt.preventDefault();
+  var depositAmount = $("#savings .input").val()
+  $("#savings .balance").css("background", "#E3E3E3");
+  savingsDepositFunction(depositAmount);
+  $(".input").val("");
+});
+
+$("#savings .withdraw").on("click", function (evt) {
+  evt.preventDefault();
+  var withdrawAmount = $("#savings .input").val()
+  if (withdrawAmount == "") {
+    $("#savings .balance").html("<p class='smallText'>Please enter an amount</p>");
+    $("#savings .balance").css("background", "rgb(232, 63, 63)");
+  } else {
+  savingsWithdrawFunction(withdrawAmount);
+  $(".input").val("");
+}
+});
+
+function savingsDepositFunction(depositAmount) {
+  var toNumber = parseFloat(depositAmount);
+  var toTwoDecimalPlaces = toNumber.toFixed(2);
+  var backToNumber = parseFloat(toTwoDecimalPlaces);
+  savingsTotal += backToNumber;
+  var finalNumber = "$" + parseFloat(savingsTotal.toFixed(2));
+  $("#savings .balance").html(finalNumber);
+}
+
+function savingsWithdrawFunction(withdrawAmount) {
+  var toNumber = parseFloat(withdrawAmount);
+  var toTwoDecimalPlaces = toNumber.toFixed(2);
+  var backToNumber = parseFloat(toTwoDecimalPlaces);
+  if (backToNumber > savingsTotal) {
+    $("#savings .balance").html("<p class='smallText'>Not Enough Funds</p>");
+    $("#savings .balance").css("background", "rgb(232, 63, 63)");
+    setTimeout(savingsReset, 2000);
+  } else if (backToNumber === savingsTotal) {
+    savingsTotal = 0;
+    $("#savings .balance").html("$0");
+    $("#savings .balance").css("background", "rgb(232, 63, 63)");
+  } else {
+    savingsTotal -= backToNumber;
+    var finalNumber = "$" + parseFloat(savingsTotal.toFixed(2));
+    $("#savings .balance").html(finalNumber);
+  }
+
+  //setTimeout function in order to re-display total value after withdraw has been rejected
+  function savingsReset() {
+    $("#savings .balance").html("$" + parseFloat(savingsTotal.toFixed(2)));
+    if (savingsTotal === 0) {
+      $("#savings .balance").css("background", "rgb(232, 63, 63)");
+    } else {
+      $("#savings .balance").css("background", "#E3E3E3");
+    }
+  };
+}
+
+
+
+
+
+
 
 /*********
 Pseudo Code
