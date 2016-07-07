004868f72dc4ca8a9676a789812c8b8cca37ef56
johnestes4 <johnestes4@gmail.com>
2016-07-07 12:20:09 -0400

    Moved deposit/withdraw code into discrete functions

--- atm.js
+++ atm.js
@@ -4,75 +4,122 @@ $(document).ready(function(){
   // console.log("JQuery!");
   // $('body').css('background', 'red');
 
+  //create balance variables to hold the balances of checking and savings
   var checkingBalance = parseFloat($('#checking').children('.balance').html().split('$')[1]);
   var savingsBalance = parseFloat($('#checking').children('.balance').html().split('$')[1]);
 
-  if (checkingBalance == 0) {
-    console.log('zero!');
-    $('#checking').addClass("zero");
-  }
-  if (savingsBalance == 0) {
-    console.log('zero!');
-    $('#savings').addClass("zero");
-  }
+  //a variable that holds the total balance of both, that will come into play with overdrafting
+  var totalBalance = 0;
 
-  $('#checking').children('.deposit').click(function() {
-    var depositAmount = parseFloat($('#checking').children('input').val());
-    if (isNaN(depositAmount) == false) {
-      checkingBalance = parseFloat(checkingBalance + depositAmount);
-      console.log(checkingBalance);
-      $('#checking').children('.balance').text('$' + checkingBalance.toFixed(2));
-      if (checkingBalance == 0) {
-        console.log('zero!');
-        $('#checking').addClass("zero");
+  //initialize and run the function that activates the zero class when balance is 0, this should always run the first time since the balances begin at 0
+  function checkIfZero() {
+    if (checkingBalance == 0) {
+    $('#checking').addClass("zero");
+    }
+    else {
+      $('#checking').removeClass('zero');
+    }
+    if (savingsBalance == 0) {
+      $('#savings').addClass("zero");
+    }
+    else {
+      $('#savings').removeClass('zero');
+    }
+  }
+  checkIfZero();
+
+  //The function to deposit money. It executes in two slightly different ways depending on which account it's working with, largely just in placement of certain variables.
+  function deposit(account) {
+      if (account == 'checking') {
+        //pull the amount to be deposited from the input field
+        var depositAmount = parseFloat($('#checking').children('input').val());
+        //if what was inputted isn't a number, it doesn't run past this point
+        if (isNaN(depositAmount) == false) {
+          //update checking and total balances
+          checkingBalance = parseFloat(checkingBalance + depositAmount);
+          totalBalance = parseFloat(checkingBalance + savingsBalance);
+          //push the new checking balance to the html
+          $('#checking').children('.balance').text('$' + checkingBalance.toFixed(2));
+          //run checkIfZero to see if the .zero class should be added or removed
+          checkIfZero();
+          //clear the input field
+          $('#savings').children('.input').val("");
+        }
       }
-      else {
-        $('#checking').removeClass('zero');
+      //doess essentially the same thing as the previous one, just to the savings side instead
+      else if (account == 'savings') {
+        var depositAmount = parseFloat($('#savings').children('input').val());
+        if (isNaN(depositAmount) == false) {
+          savingsBalance = parseFloat(savingsBalance + depositAmount);
+          totalBalance = parseFloat(checkingBalance + savingsBalance);
+          $('#savings').children('.balance').text('$' + savingsBalance.toFixed(2));
+          checkIfZero();
+          $('#savings').children('.input').val("");
       }
     }
+  }
+
+  //The function to withdraw money. It also includes functionality for overdrafting.
+  function withdraw(account) {
+    if (account == 'checking') {
+      var withdrawAmount = parseFloat($('#checking').children('input').val());
+      //Checks if withdrawAmount is a number, as well as if it is less than or equal to the balance. If not, it goes to the overdraft.
+      if (withdrawAmount <= checkingBalance && isNaN(withdrawAmount) == false) {
+        checkingBalance = parseFloat(checkingBalance - withdrawAmount);
+        totalBalance = parseFloat(checkingBalance + savingsBalance);
+        $('#checking').children('.balance').text('$' + checkingBalance.toFixed(2));
+        checkIfZero();
+        $('#checking').children('.input').val("");
+      }
+      //The overdrafting functionality, which only executes if the withdrawAmount is greater than the checkingBalance and less than the totalBalance.
+      else if (withdrawAmount > checkingBalance && withdrawAmount <= totalBalance && isNaN(withdrawAmount) == false) {
+        //A variable of how much needs to be overdrafted - how much larger the withdrawal amount is than the checking.
+        var overdraft = parseFloat(withdrawAmount - checkingBalance);
+        //Since the entirety of checking is being withdrawn, it can just be set to zero.
+        checkingBalance = 0;
+        //Set the new savings balance, of the initial balance minus the overdraft.
+        savingsBalance = parseFloat(savingsBalance - overdraft);
+        totalBalance = parseFloat(checkingBalance + savingsBalance);
+        $('#checking').children('.balance').text('$' + checkingBalance.toFixed(2));
+        $('#savings').children('.balance').text('$' + savingsBalance.toFixed(2));
+        checkIfZero();
+        $('#checking').children('.input').val("");
+      }
+    }
+    //Again, largely the same as the functionality for checking, just with the checking and savings variables swapped.
+    if (account == 'savings') {
+      var withdrawAmount = parseFloat($('#savings').children('input').val());
+      if (withdrawAmount <= savingsBalance && isNaN(withdrawAmount) == false) {
+        savingsBalance = parseFloat(savingsBalance - withdrawAmount);
+        totalBalance = parseFloat(checkingBalance + savingsBalance);
+        $('#savings').children('.balance').text('$' + savingsBalance.toFixed(2));
+        checkIfZero();
+        $('#savings').children('.input').val("");
+      }
+      else if (withdrawAmount > savingsBalance && withdrawAmount <= totalBalance  && isNaN(withdrawAmount) == false) {
+        var overdraft = parseFloat(withdrawAmount - savingsBalance);
+        savingsBalance = 0;
+        checkingBalance = parseFloat(checkingBalance - overdraft);
+        totalBalance = parseFloat(checkingBalance + savingsBalance);
+        $('#checking').children('.balance').text('$' + checkingBalance.toFixed(2));
+        $('#savings').children('.balance').text('$' + savingsBalance.toFixed(2));
+        checkIfZero();
+        $('#savings').children('.input').val("");
+      }
+    }
+  }
+
+  //Attach the deposit buttons with the proper Account values to the corresponding buttons.
+  $('#checking').children('.deposit').click(function() {
+    deposit('checking');
   });
   $('#checking').children('.withdraw').click(function() {
-    var withdrawAmount = parseFloat($('#checking').children('input').val());
-    if (withdrawAmount <= checkingBalance && isNaN(withdrawAmount) == false) {
-      checkingBalance = parseFloat(checkingBalance - withdrawAmount);
-      console.log(checkingBalance);
-      $('#checking').children('.balance').text('$' + checkingBalance.toFixed(2));
-      if (checkingBalance == 0) {
-        console.log('zero!');
-        $('#checking').addClass("zero");
-      }
-      else {
-        $('#checking').removeClass('zero');
-      }
-    }
+    withdraw('checking');
   });
   $('#savings').children('.deposit').click(function() {
-    var depositAmount = parseFloat($('#savings').children('input').val());
-    if (isNaN(depositAmount) == false) {
-      savingsBalance = parseFloat(savingsBalance + depositAmount);
-      $('#savings').children('.balance').text('$' + savingsBalance.toFixed(2));
-      if (savingsBalance == 0) {
-        console.log('zero!');
-        $('#savings').addClass("zero");
-      }
-      else {
-        $('#savings').removeClass('zero');
-      }
-    }
+    deposit('savings');
   });
   $('#savings').children('.withdraw').click(function() {
-    var withdrawAmount = parseFloat($('#savings').children('input').val());
-    if (withdrawAmount <= savingsBalance && isNaN(withdrawAmount) == false) {
-      savingsBalance = parseFloat(savingsBalance - withdrawAmount);
-      $('#savings').children('.balance').text('$' + savingsBalance.toFixed(2));
-      if (savingsBalance == 0) {
-        console.log('zero!');
-        $('#savings').addClass("zero");
-      }
-      else {
-        $('#savings').removeClass('zero');
-      }
-    }
+    deposit('savings');
   });
-
 });
