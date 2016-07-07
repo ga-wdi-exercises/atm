37ff317a847cc67fedff6628a61f152560d852bc
Taylor Edwards <tedwards1884@gmail.com>
2016-07-07 10:28:59 -0400

    initial commit

--- atm.js
+++ atm.js
@@ -1,3 +1,38 @@
+//   make sure document is ready
+//   make sure inputs are ready
+        // checking: input, deposit, withdraw
+        // savings: input, deposit, withdraw
+//   get/display checking balance
+//   get/display savings balance
+//
+// for the checking account:
+//   make sure input field is clear and ready for user input
+//   get and store value from input field
+//     make sure input is float w/ two decimals
+//   add input to balance and set if user clicks "deposit"
+//   subtract and set if user clicks "withdraw"
+//     if withdrawal request is greater than available balance, don't withdraw
+//   if the account balance is now 0, change the background to red
+//   after the transaction is complete, clear the input field, display the new balance and await user input
+
+// for the savings account:
+//   make sure input field is clear and ready for user input
+//   get and store value from input field
+//     make sure input is float w/ two decimals
+//   add input to balance and set if user clicks "deposit"
+//   subtract and set if user clicks "withdraw"
+//     if withdrawal request is greater than available balance, don't withdraw
+//   if the account balance is now 0, change the background to red
+//   after the transaction is complete, clear the input field, display the new balance and await user input
+
+// BONUS:
+// if checking account balance is less than withdrawal request...
+//   get sum of both account balances
+//   if the sum of both account balances is greater than withdrawal request...
+//     withdraw all money from checking and (withdrawal - checking balance) from savings
+// ... and vice versa
+
+
 $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
 });
