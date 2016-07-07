a3684df0f244dab9f3d9fa4e474fdca24c188b31
johnestes4 <johnestes4@gmail.com>
2016-07-07 10:27:03 -0400

    Added pseudocode, made variables for balance values

--- atm.js
+++ atm.js
@@ -1,19 +1,30 @@
 $(document).ready(function(){
 
+  // basic jquery testing
   // console.log("JQuery!");
   // $('body').css('background', 'red');
 
+  var checkingBalance = parseFloat($('#checking').children('.balance').html().split('$')[1]);
+  console.log(checkingBalance);
+  var savingsBalance = parseFloat($('#checking').children('.balance').html().split('$')[1]);
+  console.log(savingsBalance);
+
+
   $('#checking').children('.deposit').click(function() {
-    console.log('checking deposit!');
+    var depositAmount = parseFloat($('#checking').children('input').val());
+    console.log(depositAmount);
   });
   $('#checking').children('.withdraw').click(function() {
-    console.log('checking withdraw!');
+    var depositAmount = parseFloat($('#checking').children('input').val());
+    console.log(depositAmount);
   });
   $('#savings').children('.deposit').click(function() {
-    console.log('savings deposit!');
+    var depositAmount = parseFloat($('#checking').children('input').val());
+    console.log(depositAmount);
   });
   $('#savings').children('.withdraw').click(function() {
-    console.log('savings withdraw!');
+    var depositAmount = parseFloat($('#checking').children('input').val());
+    console.log(depositAmount);
   });
 
 });
