2899e81c1757df0ce64d93416d77b2884a69a40d
Megan Coyle <meg@mcoyle.com>
2016-07-07 10:18:46 -0400

    Add pseudocode

--- atm.js
+++ atm.js
@@ -1,5 +1,27 @@
 $(document).ready(function(){
 
+// define savings account
+// define checking account
 
+// define value of user input
+  //only accept numeric value
+
+// on click of deposit, user input added to total value of specific account
+   // if amount is > 0, remove zero class
+   // clear input field after amount is added
+
+// on click of withdraw, user input subtracted from total value of specific account
+  // if total amount = 0, apply zero class
+  // if amount to withdraw > total funds, ignore transaction
+  // clear input field after amount is withdrawn
+
+
+//Bonus
+
+// if total to withdraw is equal to total in both accounts
+// have current account total = 0
+// subtract remaining amount from other account
+
+// if total to withdraw > than amounts in both accounts, display error
 
 });
