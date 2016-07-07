edf4b99ac85ed894956cff5e9ab1912b27e8755c
Rasheeda Alexander <sheedabeeda@gmail.com>
2016-07-07 10:17:00 -0400

    Changed background color to red and added jQuery script file. Also moved atm.js to bottom of body tag

--- atm.js
+++ atm.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+//User input a dollar amount to deposit money to checking
+$('body').css("background", "red");
+  //update amount
+//input a dollar amound to withdraw money for checking
+  //update amount
+//if User withdraw amount > account balance
+  //reject transaction
+//color of bank account green for positive value
+  //red for 0 (or less for bonus)
+
+  //User input a dollar amount to deposit money to savings
+    //update amount
+  //input a dollar amound to withdraw money to savings
+    //update amount
+  //if User withdraw amount > account balance
+    //reject transaction
+  //color of bank account green for positive value
+    //red for 0 (or less for bonus)
 });
