ff661ebf28edb58ed95247b3ec7d2fdbf4d88d28
Rasheeda Alexander <sheedabeeda@gmail.com>
2016-07-07 14:47:24 -0400

    Updated total but it concatenates the numbers rather than adding them together

--- atm.js
+++ atm.js
@@ -1,20 +1,35 @@
 $(document).ready(function(){
-//User input a dollar amount to deposit money to checking
-$('body').css("background", "red");
-  //update amount
-//input a dollar amound to withdraw money for checking
-  //update amount
-//if User withdraw amount > account balance
-  //reject transaction
-//color of bank account green for positive value
-  //red for 0 (or less for bonus)
+  //Turn background red
+  $('body').css("background", "red");
+  //Add a click listener to the **checking account's** "Deposit" button
+  console.log("hola");
+  //User input a dollar amount to deposit money to checking
+  var deposit = $('.deposit');
+  $(deposit).on("click", function(evt) {
+    //Prevent default of submit button from refreshing page
+    evt.preventDefault();
 
-  //User input a dollar amount to deposit money to savings
+    console.log("Deposit button pressed");
+
+  });
+  //Make sure input field is clear
     //update amount
-  //input a dollar amound to withdraw money to savings
+    //use 2 decimals (parseFloat())
+  //input a dollar amound to withdraw money for checking
     //update amount
   //if User withdraw amount > account balance
     //reject transaction
   //color of bank account green for positive value
     //red for 0 (or less for bonus)
+
+  //User input a dollar amount to deposit money to savings
+  //Make sure input field is clear
+  //update amount
+  //use 2 decimals (parseFloat())
+  //input a dollar amound to withdraw money to savings
+  //update amount
+  //if User withdraw amount > account balance
+  //reject transaction
+  //color of bank account green for positive value
+  //red for 0 (or less for bonus)
 });
