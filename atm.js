33eff25770e7c80b064586c5c7f7d025ff094e5c
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2016-07-07 10:01:34 -0400

    initial psuedocode

--- atm.js
+++ atm.js
@@ -1,5 +1,11 @@
+//Psuedocode -
+//When user types in amount to deposit in either checking or savings, clicks 'Deposit' and amount will show up in text display box.
+//Each additional deposits will add amount into 'balance' or total for each savings and checking
+//Conversely, When the user enters an amount and clicks 'Withdraw', the specified amount will be subtracted from the 'balance' or total.
+ 
+
 $(document).ready(function(){
 
+console.log("Ready!")
 
-  
 })
