cd364620ea0e238736e602d3a13b0503bb65e540
Ahmed Elkheshin <ahmed.elkheshin@gmail.com>
2016-07-07 10:44:44 -0400

    Changed background color to red

--- atm.js
+++ atm.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+  console.log("hello")
+$("body").css("background", "red");
 });
+
+
+//If Checking is clicked, provide user with list of options to choose from, ie. "Withdrawl", "Deposit", and "Balance Inquiry"
+    //If Withdrawl is clicked, prompt user to enter amount desired.
+    //If amount in balance <=100, change background color red
+    //If amount in balance >=100, change background color green
+        //If amount in balance >= amount desired, subtract desired amount from checking balance and withdraw money
+        //Else If amount in balance is < amount desired, send alert ("insufficient funds")
+    //If Desposit is clicked, send alert ("ready for cash to be deposited")
+        //Once fully deposited, provide updated account balance
+    //If Balance Inquiry is clicked, provide user with total balance information.
+
+//If Savings is clicked, provide same list above
