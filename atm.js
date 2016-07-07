8fa9b65e779141aa7482a4efd5be5dd041a24b94
johnestes4 <johnestes4@gmail.com>
2016-07-07 09:46:27 -0400

    Added basic test listeners to the buttons

--- atm.js
+++ atm.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
 
-  console.log("JQuery!");
-  $('body').css('background', 'red');
+  // console.log("JQuery!");
+  // $('body').css('background', 'red');
+
+  $('#checking').children('.deposit').click(function() {
+    console.log('checking deposit!');
+  });
+  $('#checking').children('.withdraw').click(function() {
+    console.log('checking withdraw!');
+  });
+  $('#savings').children('.deposit').click(function() {
+    console.log('savings deposit!');
+  });
+  $('#savings').children('.withdraw').click(function() {
+    console.log('savings withdraw!');
+  });
 
 });
