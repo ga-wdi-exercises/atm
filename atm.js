5a09f8983c68703bb7359d43cf1add63c864d766
Ahmed Elkheshin <ahmed.elkheshin@gmail.com>
2016-07-07 16:26:13 -0400

    Added withdraw functionality, color coding for -bash balance and savings with similar functionalities to checking

--- atm.js
+++ atm.js
@@ -1,22 +1,78 @@
 var checkingInput;
-var checkingBalance;
+var checkingBalance ;
 var currentBalance;
 $(document).ready(function(){
   checkingInput = $("#checking > .input");
   checkingBalance = $("#checking > .balance");
   currentBalance = $("checkingBalance" + "checkingInput")
   console.log("hello");
-  // $("body").css("background", "red");
   $(".deposit:first").on("click", function(evt){
     evt.preventDefault();
-    var currentChecking = parseFloat($('.balance:first').html().split('$')[1])
-    $(".balance:first").html("$" + (parseFloat(checkingInput.val())+currentChecking).toFixed(2));
+    var currentChecking = parseFloat($('.balance:eq(0)').html().split('$')[1])
 
-    // $(checkingInput).on("click", function(){
+    $(".balance:eq(0)").html("$" + (parseFloat(checkingInput.val())+currentChecking).toFixed(2));
+    currentChecking = parseFloat($('.balance:eq(0)').html().split('$')[1])
+
+    console.log(currentChecking)
+    if(currentChecking == 0) {
+      $("#checking").addClass("zero")
+    } else {
+      $("#checking").removeClass("zero")
+    };
   });
 
+  $(".withdraw:first").on("click", function(evt){
+    evt.preventDefault();
+    var checkingIn = checkingInput.val()
+    console.log(checkingIn)
+    var currentChecking = parseFloat($('.balance:eq(0)').html().split('$')[1])
+    if (checkingIn <= currentChecking) {
+      $(".balance:eq(0)").html("$" + (parseFloat(currentChecking - checkingInput.val())).toFixed(2));
+    }
+    currentChecking = parseFloat($('.balance:eq(0)').html().split('$')[1])
 
+    if(currentChecking == 0) {
+      $("#checking").addClass("zero")
+    } else {
+      $("#checking").removeClass("zero")
+    };
+  });
+  console.log("hekeleele")
 
+  var savingsInput = $("#savings > .input");
+  var savingsBalance = $("#savings > .balance");
+  var currentSavingsBalance = $("savingsBalance" + "savingsInput")
+  $(".deposit:eq(1)").on("click", function(evt){
+    evt.preventDefault();
+    var currentSavings = parseFloat($('.balance:eq(1)').html().split('$')[1])
+    $(".balance:eq(1)").html("$" + (parseFloat(savingsInput.val())+currentSavings).toFixed(2));
+    currentSavings = parseFloat($('.balance:eq(1)').html().split('$')[1])
+
+    console.log(currentSavings)
+    if(currentSavings == 0) {
+      $("#savings").addClass("zero")
+    } else {
+      $("#savings").removeClass("zero")
+    };
+  });
+
+  $(".withdraw:eq(1)").on("click", function(evt){
+    evt.preventDefault();
+
+    var savingsIn = savingsInput.val()
+    console.log(savingsIn)
+    var currentSavings = parseFloat($('.balance:eq(1)').html().split('$')[1])
+    if (savingsIn <= currentSavings) {
+      $(".balance:eq(1)").html("$" + (parseFloat(currentSavings - savingsInput.val())).toFixed(2));
+    }
+    currentSavings = parseFloat($('.balance:eq(0)').html().split('$')[1])
+
+    if(currentSavings == 0) {
+      $("#savings").addClass("zero")
+    } else {
+      $("#savings").removeClass("zero")
+    };
+  });
 });
 
 //If Checking is clicked, provide user with list of options to choose from, ie. "Withdrawl", "Deposit", and "Balance Inquiry"
