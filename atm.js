8532ef0c3ccce3a8d208bf80ca04730efaa0b463
johnestes4 <johnestes4@gmail.com>
2016-07-07 10:56:07 -0400

    Added deposit/withdraw functionality, if statements to activate .zero class

--- atm.js
+++ atm.js
@@ -5,26 +5,74 @@ $(document).ready(function(){
   // $('body').css('background', 'red');
 
   var checkingBalance = parseFloat($('#checking').children('.balance').html().split('$')[1]);
-  console.log(checkingBalance);
   var savingsBalance = parseFloat($('#checking').children('.balance').html().split('$')[1]);
-  console.log(savingsBalance);
 
+  if (checkingBalance == 0) {
+    console.log('zero!');
+    $('#checking').addClass("zero");
+  }
+  if (savingsBalance == 0) {
+    console.log('zero!');
+    $('#savings').addClass("zero");
+  }
 
   $('#checking').children('.deposit').click(function() {
     var depositAmount = parseFloat($('#checking').children('input').val());
-    console.log(depositAmount);
+    if (isNaN(depositAmount) == false) {
+      checkingBalance = parseFloat(checkingBalance + depositAmount);
+      console.log(checkingBalance);
+      $('#checking').children('.balance').text('$' + checkingBalance.toFixed(2));
+      if (checkingBalance == 0) {
+        console.log('zero!');
+        $('#checking').addClass("zero");
+      }
+      else {
+        $('#checking').removeClass('zero');
+      }
+    }
   });
   $('#checking').children('.withdraw').click(function() {
-    var depositAmount = parseFloat($('#checking').children('input').val());
-    console.log(depositAmount);
+    var withdrawAmount = parseFloat($('#checking').children('input').val());
+    if (withdrawAmount <= checkingBalance && isNaN(withdrawAmount) == false) {
+      checkingBalance = parseFloat(checkingBalance - withdrawAmount);
+      console.log(checkingBalance);
+      $('#checking').children('.balance').text('$' + checkingBalance.toFixed(2));
+      if (checkingBalance == 0) {
+        console.log('zero!');
+        $('#checking').addClass("zero");
+      }
+      else {
+        $('#checking').removeClass('zero');
+      }
+    }
   });
   $('#savings').children('.deposit').click(function() {
-    var depositAmount = parseFloat($('#checking').children('input').val());
-    console.log(depositAmount);
+    var depositAmount = parseFloat($('#savings').children('input').val());
+    if (isNaN(depositAmount) == false) {
+      savingsBalance = parseFloat(savingsBalance + depositAmount);
+      $('#savings').children('.balance').text('$' + savingsBalance.toFixed(2));
+      if (savingsBalance == 0) {
+        console.log('zero!');
+        $('#savings').addClass("zero");
+      }
+      else {
+        $('#savings').removeClass('zero');
+      }
+    }
   });
   $('#savings').children('.withdraw').click(function() {
-    var depositAmount = parseFloat($('#checking').children('input').val());
-    console.log(depositAmount);
+    var withdrawAmount = parseFloat($('#savings').children('input').val());
+    if (withdrawAmount <= savingsBalance && isNaN(withdrawAmount) == false) {
+      savingsBalance = parseFloat(savingsBalance - withdrawAmount);
+      $('#savings').children('.balance').text('$' + savingsBalance.toFixed(2));
+      if (savingsBalance == 0) {
+        console.log('zero!');
+        $('#savings').addClass("zero");
+      }
+      else {
+        $('#savings').removeClass('zero');
+      }
+    }
   });
 
 });
