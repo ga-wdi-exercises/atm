64ef51bafd1a31a0987eb5e90bbd03266b95454c
Ben Brizzi <brizzitime@gmail.com>
2016-07-07 13:00:47 -0400

    deposit and withdraw both work for checking and savings, text turns red if illegal withdraw occurs.

--- atm.js
+++ atm.js
@@ -2,22 +2,22 @@
 select user input
 convert input to float with 2 decimal places
 if (withdraw button pressed){
-  subtract input from current balance
+subtract input from current balance
 }
 if (deposit button pressed){
-  add input to current balance
+add input to current balance
 }
 if (balance < 0){
-  do not update balance on dom
-  reset balance to previous value
-  display error message within input field
+do not update balance on dom
+reset balance to previous value
+display error message within input field
 }
 else if (balance == 0){
-  use css to change box background to red
+use css to change box background to red
 }
 else{
-  update account balance on DOM
-  bank color should be green
+update account balance on DOM
+bank color should be green
 }
 clear specific input field
 */
@@ -26,6 +26,8 @@ $(document).ready(function(){
 
   $("#checking .deposit").on("click", function(evt){
     evt.preventDefault();
+    $("#checking .input").css("color", "black");
+    $("#checking .input").select();
     var displayedBalance = $("#checking .balance").html();
     var totalBalance = parseFloat(displayedBalance.split("$")[1]);
     console.log(totalBalance);
@@ -36,4 +38,67 @@ $(document).ready(function(){
 
   })
 
+  $("#checking .withdraw").on("click", function(evt){
+    evt.preventDefault();
+    $("#checking .input").select();
+    var displayedBalance = $("#checking .balance").html();
+    var totalBalance = parseFloat(displayedBalance.split("$")[1]);
+    console.log(totalBalance);
+    var amount = $("#checking .input").val();
+    var floatAmount = parseFloat(amount).toFixed(2);
+    var balance = totalBalance - parseFloat(floatAmount);
+    if (balance < 0){
+      balance = totalBalance;
+      $("#checking .input").css("color", "red");
+      $("#checking .input").val("DONT DO IT");
+      $("#checking .input").select();
+      // $("checking .input").on("keyup", function(evt){
+      //   evt.preventDefault();
+      //   console.log("key")
+      // })
+    }
+    else{
+      $("#checking .balance").html("$" + balance);
+      $("#checking .input").css("color", "black");
+    }
+  })
+
+  $("#savings .deposit").on("click", function(evt){
+    evt.preventDefault();
+    $("#savings .input").css("color", "black");
+    $("#savings .input").select();
+    var displayedBalance = $("#savings .balance").html();
+    var totalBalance = parseFloat(displayedBalance.split("$")[1]);
+    console.log(totalBalance);
+    var amount = $("#savings .input").val();
+    var floatAmount = parseFloat(amount).toFixed(2);
+    var balance = parseFloat(floatAmount) + totalBalance;
+    $("#savings .balance").html("$" + balance);
+
+  })
+
+  $("#savings .withdraw").on("click", function(evt){
+    evt.preventDefault();
+    $("#savings .input").select();
+    var displayedBalance = $("#savings .balance").html();
+    var totalBalance = parseFloat(displayedBalance.split("$")[1]);
+    console.log(totalBalance);
+    var amount = $("#savings .input").val();
+    var floatAmount = parseFloat(amount).toFixed(2);
+    var balance = totalBalance - parseFloat(floatAmount);
+    if (balance < 0){
+      balance = totalBalance;
+      $("#savings .input").css("color", "red");
+      $("#savings .input").val("DONT DO IT");
+      $("#savings .input").select();
+      // $("checking .input").on("keyup", function(evt){
+      //   evt.preventDefault();
+      //   console.log("key")
+      // })
+    }
+    else{
+      $("#savings .balance").html("$" + balance);
+      $("#savings .input").css("color", "black");
+    }
+  });
 });
