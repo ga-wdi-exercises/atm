129c0094f5f9046065a368cbf817b2e6738f6f47
Chase <chasefreeman13@gmail.com>
2016-07-07 13:27:32 -0400

    all basic functions work

--- atm.js
+++ atm.js
@@ -1,25 +1,85 @@
 $("document").ready(function(){
 console.log("hello");
 
+//*******Checking Deposit START*********
 $("#checking > .deposit").on("click", function(evt){
     // prevent default action
     evt.preventDefault();
     // get user input && convert to float with 2 decimal placess
     var newInput = $("#checking > .input").val()
-    var floatNewInput = parseFloat(newInput).toFixed(2);
-    console.log(floatNewInput);
+    var floatnewInput = parseFloat(newInput).toFixed(2);
+    console.log(floatnewInput);
 
     // update total
       //  get current total
     var total = $("#checking > .balance").html()
-    var numericTotal = parseFloat(total.split("$")[1])
-    var actualTotal = parseFloat(floatNewInput) + numericTotal
-    $("#checking > .balance").html("$ " + actualTotal.toFixed(2));
+    var numerictotal = parseFloat(total.split("$")[1])
+    var actualtotal = parseFloat(floatnewInput) + numerictotal
+    $("#checking > .balance").html("$ " + actualtotal.toFixed(2));
     console.log()
-    $("input.input").val("")
+    $("#checking > .input").val("")
+    })
+//*********Checking Deposit END************
 
+//*********Savings Deposit START***********(change Variables)
+$("#savings > .deposit").on("click", function(evt){
+    // prevent default action
+    evt.preventDefault();
+    // get user input && convert to float with 2 decimal placess
+    var newInput = $("#savings > .input").val()
+    var floatnewInput = parseFloat(newInput).toFixed(2);
+    console.log(floatnewInput);
 
-  })
+    // update total
+      //  get current total
+    var total = $("#savings > .balance").html()
+    var numerictotal = parseFloat(total.split("$")[1])
+    var actualtotal = parseFloat(floatnewInput) + numerictotal
+    $("#savings > .balance").html("$ " + actualtotal.toFixed(2));
+    console.log()
+    $("#savings > .input").val("")
+    })
 
+//*********Savings Deposit END************
+
+//********** Checking Withdrawl BEGIN***********
+$("#checking > .withdraw").on("click", function(evt){
+    // prevent default action
+    evt.preventDefault();
+    // get user input && convert to float with 2 decimal placess
+    var newInput = $("#checking > .input").val()
+    var floatnewInput = parseFloat(newInput).toFixed(2);
+    console.log(floatnewInput);
+
+    // update total
+      //  get current total
+    var total = $("#checking > .balance").html()
+    var numerictotal = parseFloat(total.split("$")[1])
+    var actualtotal = numerictotal - parseFloat(floatnewInput)
+    $("#checking > .balance").html("$ " + actualtotal.toFixed(2));
+    console.log()
+    $("#checking > .input").val("")
+    })
+//**********Checking Withdrawl END*************
+
+//**********Savings Withdrawl BEGIN*************
+$("#savings > .withdraw").on("click", function(evt){
+    // prevent default action
+    evt.preventDefault();
+    // get user input && convert to float with 2 decimal placess
+    var newInput = $("#savings > .input").val()
+    var floatnewInput = parseFloat(newInput).toFixed(2);
+    console.log(floatnewInput);
+
+    // update total
+      //  get current total
+    var total = $("#savings > .balance").html()
+    var numerictotal = parseFloat(total.split("$")[1])
+    var actualtotal = numerictotal - parseFloat(floatnewInput)
+    $("#savings > .balance").html("$ " + actualtotal.toFixed(2));
+    console.log()
+    $("#savings > .input").val("")
+    })
+//**********Savings Withdrawl END****************
 
 });
