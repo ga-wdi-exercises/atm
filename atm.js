8661be69cd0268c381e5e13fdd87c4e65314e111
Chase <chasefreeman13@gmail.com>
2016-07-07 11:27:06 -0400

    Got Checking input to work

--- atm.js
+++ atm.js
@@ -1,3 +1,25 @@
-$(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+$("document").ready(function(){
+console.log("hello");
+
+$("#checking > .deposit").on("click", function(evt){
+    // prevent default action
+    evt.preventDefault();
+    // get user input && convert to float with 2 decimal placess
+    var newInput = $("#checking > .input").val()
+    var floatNewInput = parseFloat(newInput).toFixed(2);
+    console.log(floatNewInput);
+
+    // update total
+      //  get current total
+    var total = $("#checking > .balance").html()
+    var numericTotal = parseFloat(total.split("$")[1])
+    var actualTotal = parseFloat(floatNewInput) + numericTotal
+    $("#checking > .balance").html("$ " + actualTotal.toFixed(2));
+    console.log()
+    $("input.input").val("")
+
+
+  })
+
+
 });
