439049d9f83bf4fb8ac2db4d5d2413fd10c7daf2
Christopher Phillips <cphillips918@gmail.com>
2016-07-07 12:26:54 -0400

    it works with the bonus!!

--- atm.js
+++ atm.js
@@ -2,18 +2,19 @@ function addThis(selector){
     $(selector).addClass(
         function(index,currentClass){
             var addedClass;
-            if(parseFloat($('.balance:first').html().split('$')[1])==0){
+            if(parseFloat($(selector).html().split('$')[1])==0){
                 addedClass = "zero";
             }
             return addedClass;
         }
     )
 }
+
 function removeThis(selector){
     $(selector).removeClass(
         function(index,currentClass){
             var removedClass;
-            if(parseFloat($('.balance:first').html().split('$')[1])!==0){
+            if(parseFloat($(selector).html().split('$')[1])!==0){
                 removedClass = "zero";
             }
             return removedClass;
@@ -21,11 +22,14 @@ function removeThis(selector){
     )
 }
 $(document).ready(function() {
+    //implements the zero class on start
     addThis(".balance");
+
     //prevent enter from reloading page
     $('form').submit(function(e) {
         e.preventDefault();
     });
+
     //deposit function checking
     $(".deposit:first").on('click', function(event) {
         event.preventDefault();
@@ -38,18 +42,31 @@ $(document).ready(function() {
             $('.entry:first')[0].reset();
         }
     });
+
     //withdraw function checking
     $(".withdraw:first").on('click', function(event) {
         event.preventDefault();
         var withdrawAmount = parseFloat($('.input:first').val()).toFixed(2)
         var currentBalance = parseFloat($('.balance:first').html().split('$')[1])
-        if(!isNaN(withdrawAmount) && currentBalance != 0){
+        var difference = currentBalance - parseFloat(withdrawAmount)
+        if(!isNaN(withdrawAmount) && difference >= 0){
             $('.balance:first').html('$' + (currentBalance - parseFloat(withdrawAmount)).toFixed(2));
             addThis(".balance:first");
             removeThis(".balance:first");
             $('.entry:first')[0].reset();
+            //overdraft protection logic
+        } else if (!isNaN(withdrawAmount) && difference < 0) {
+            if(parseFloat($('.balance:eq(1)').html().split('$')[1])>0){
+                var protectionAmount = ((currentBalance - withdrawAmount) * -1).toFixed(2)
+                var savingsAmount = parseFloat($('.balance:eq(1)').html().split('$')[1])
+                $('.balance:eq(1)').html('$'+ (savingsAmount - parseFloat(protectionAmount)).toFixed(2))
+                $('.balance:first').html('$0.00')
+                addThis(".balance:first");
+                addThis(".balance:eq(1)");
+            }
         }
     });
+
     //deposit savings function
     $(".deposit:eq(1)").on('click', function(event) {
         event.preventDefault();
@@ -57,20 +74,20 @@ $(document).ready(function() {
         if(!isNaN(depositAmount)){
             var currentBalance = parseFloat($('.balance:eq(1)').html().split('$')[1])
             $('.balance:eq(1)').html('$' + (parseFloat(depositAmount)+currentBalance).toFixed(2));
+            removeThis('.balance:eq(1)')
             addThis('.balance:eq(1)');
-            removeThis('.balance:eq(1)');
             $('.entry:eq(1)')[0].reset();
         }
     });
+
     //withdraw savings function
     $(".withdraw:eq(1)").on('click', function(event) {
         event.preventDefault();
         var withdrawAmount = parseFloat($('.input:eq(1)').val()).toFixed(2)
         var currentBalance = parseFloat($('.balance:eq(1)').html().split('$')[1])
-        if(!isNaN(withdrawAmount) && currentBalance != 0){
+        var difference = currentBalance - parseFloat(withdrawAmount)
+        if(!isNaN(withdrawAmount) && difference >= 0){
             $('.balance:eq(1)').html('$' + (currentBalance - parseFloat(withdrawAmount)).toFixed(2));
-            addThis('.balance:eq(1)');
-            removeThis('.balance:eq(1)');
             $('.entry:eq(1)')[0].reset();
         }
     });
