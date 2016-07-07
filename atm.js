e4b80ade0ac6c1e53650aa94cae4719b2b21fb71
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2016-07-07 10:29:31 -0400

    final psuedocode

--- atm.js
+++ atm.js
@@ -1,13 +1,14 @@
-//Psuedocode -
+//Psuedocode
 // - When user types in amount to deposit in either checking or savings, and clicks 'Deposit', amount will show up in text display box.
-// - Each additional deposit will add amount into 'balance' or total for each savings and //checking.
-// - Conversely, When the user enters an amount and clicks 'Withdraw', the specified //amount will be subtracted from the 'balance' or total.
-// - The user can not withdraw more than the balance of each account ((wishlist: possible //overdraft protection, where if funds are available on either account to cover the full //withdrawal of a potential 'overdraft')).
-//When either account balance reaches 0, red color will show in background of text/balance //display.
+// - Each additional deposit will add amount into 'balance' or total for each savings and checking.
+// - Conversely, When the user enters an amount and clicks 'Withdraw', the specified amount will be subtracted from the 'balance' or total.
+// - If possible, convert user input to two decimal places to calculate proper currency value and display.
+// - The user cannot withdraw more than the balance of each account ((wishlist: possible //overdraft protection, where if funds are available on either account to cover the full withdrawal of a potential 'overdraft', the needed amount is withdrawn)).
+// - When either account balance reaches 0, red warning color will show in background of text/balance display.
 
 $(document).ready(function(){
 
-console.log("Ready!");
+console.log("I'm So Ready!");
 
 
 
