cf5168287493ec6ad978f1235321e9d0d9252af8
Evan SandersonE <evan.j.sanderson@gmail.coM>
2016-07-07 11:33:00 -0400

    completed first deposit button

--- atm.js
+++ atm.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+  console.log("JS is working");
+
+  // var depositChecking;
+  // var withdrawChecking;
+  // $("#checking .input").on("change")
+  // depositChecking = $(".input").val();
+  // console.log(depositChecking);
+  // console.log(typeof(depositChecking));
+
+  $("#checking .deposit").on("click", function(e){
+      console.log("button working");
+      e.preventDefault();
+      var userAmount = $("#checking .input").val();
+      var userAmountN = parseFloat(userAmount);
+      var balance = $("#checking .balance").html();
+      var nBalance = parseFloat(balance.split("$")[1])
+      var totalBalance = nBalance + userAmountN;
+      $("#checking .balance").html("$" + totalBalance.toFixed(2));
+
+      $("#checking .input").val("");
+
+  })
+
+
+
 });
