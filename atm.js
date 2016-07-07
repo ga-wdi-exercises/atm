8cef5a681aac79e883d981b5e7666f88821c62fa
Marc Fournier <marc.fournier91@gmail.com>
2016-07-07 11:05:36 -0400

    Added pseudo code for checking

--- atm.js
+++ atm.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-  $("body").css("background", "red");
+  //listen for click event on checking account deposit button
+    //get user input from field
+    //Add user input to balance
+    //If checking has a class of zero and balance != 0 remove class zero
+    //clear field
+
+    //listen for click even on cheking account withdrawl button
+      //get user input from field
+      //if user input is not greater than balance, subtract user input from balance
+      //If checking does not have a class of 0 and balance == 0 add class zero
+
+
+
   console.log("hi");
 });
