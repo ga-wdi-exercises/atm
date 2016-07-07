2a41b8771f28e4ed7120c2730db0f9213664e0bc
Ben Brizzi <brizzitime@gmail.com>
2016-07-07 10:52:28 -0400

    jquery is working, the background runs red with the blood of my atms enemies

--- atm.js
+++ atm.js
@@ -1,3 +1,28 @@
+/*
+select user input
+convert input to float with 2 decimal places
+if (withdraw button pressed){
+  subtract input from current balance
+}
+if (deposit button pressed){
+  add input to current balance
+}
+if (balance < 0){
+  do not update balance on dom
+  reset balance to previous value
+  display error message within input field
+}
+else if (balance == 0){
+  use css to change box background to red
+}
+else{
+  update account balance on DOM
+  bank color should be green
+}
+clear specific input field
+*/
 $(document).ready(function(){
+  $("body").css("background", "red");
+
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
 });
