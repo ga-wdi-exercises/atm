f9dca7aee3572a12fe97783e12c8bc8043e9d5b1
Rasheeda Alexander <sheedabeeda@gmail.com>
2016-07-08 08:57:18 -0400

    Added withdraw variables for checking and savings account

--- atm.js
+++ atm.js
@@ -1,26 +1,65 @@
+//Checking variable
+var checkingInput = $('#checking .input');
+var checkingBalance = $('#checking .balance');
+var checkingDeposit = $('#checking .deposit');;
+var checkingWithdraw = ('#checking .withdraw');
+
+//Savings variable
+var savingsInput = $('#savings .input');
+var savingsBalance = $('#savings .balance');
+var savingsDeposit = $('#savings .deposit');
+var savingsWithdraw = $('#savings .withdraw');
+
 $(document).ready(function(){
   //Turn background red
-  $('body').css("background", "red");
+  //$('body').css("background", "red");
   //Add a click listener to the **checking account's** "Deposit" button
   console.log("hola");
   //User input a dollar amount to deposit money to checking
-  var deposit = $('.deposit');
-  $(deposit).on("click", function(evt) {
+
+  //Checking deposit button adds user input
+  $(checkingDeposit).on("click", function(evt) {
     //Prevent default of submit button from refreshing page
     evt.preventDefault();
+    console.log("Checking deposit button pressed");
 
-    console.log("Deposit button pressed");
+    currentChecking = parseFloat($(checkingBalance).html().split('$')[1]);
+    console.log("Current Checking Amount is a type of: " + typeof(currentChecking));
+
+    //update amount
+    checkingInputVal = parseFloat(checkingInput.val());
+    console.log("Checking Input Value is a type of: " + typeof checkingInputVal);
+    $('#checking .balance').html("$" + (checkingInputVal + currentChecking));
+  });
+
+  $(checkingWithdraw).on("click", function(evt) {
+    //Prevent default of submit button from refreshing page
+    evt.preventDefault();
+    console.log("Checking withdraw button pressed");
+
+    currentChecking = parseFloat($(checkingBalance).html().split('$')[1]);
+    console.log("Current Checking Amount is a type of: " + typeof(currentChecking));
+
+    //update amount
+    checkingInputVal = parseFloat(checkingInput.val());
+    console.log("Checking Input Value is a type of: " + typeof checkingInputVal);
+
+    if(checkingBalance > 0) {
+      $('#checking .balance').html("$" + (currentChecking - checkingInputVal));
+    }
+    else {
+      $('.balance').css("background", "red");
+    }
 
   });
-  //Make sure input field is clear
-    //update amount
-    //use 2 decimals (parseFloat())
+  //Checking Withdraw button adds user input
+
   //input a dollar amound to withdraw money for checking
-    //update amount
+  //update amount
   //if User withdraw amount > account balance
-    //reject transaction
+  //reject transaction
   //color of bank account green for positive value
-    //red for 0 (or less for bonus)
+  //red for 0 (or less for bonus)
 
   //User input a dollar amount to deposit money to savings
   //Make sure input field is clear
