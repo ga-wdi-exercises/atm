6ca12f5f098d001e7619570b9e416ea65b14dcfe
Van Phan <VanPhan514@gmail.com>
2016-08-01 21:04:47 -0400

    Created solution for ATM

--- atm.js
+++ atm.js
@@ -1,3 +1,33 @@
 $(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+	$("#checking .deposit").on("click", function() {
+		logic("checking", 1)
+	})
+	$("#checking .withdraw").on("click", function() {
+		logic("checking", 2)
+	})
+	$("#savings .deposit").on("click", function() {
+		logic("savings", 1)
+	})
+	$("#savings .withdraw").on("click", function() {
+		logic("savings", 2)
+	})
 });
+
+function logic(acc, action) {
+	if (action == 1) {
+		if (isNaN(parseInt($("#" + acc + " .input")[0].value))) {
+			alert("Enter a valid amount into " + acc);
+		}
+		else {
+			$("#" + acc + " .balance")[0].innerHTML = "$" + (parseInt(($("#" + acc + " .balance")[0].innerHTML).split("$")[1]) + parseInt($("#" + acc + " .input")[0].value));
+		}
+	}
+	else {
+		if (isNaN(parseInt($("#" + acc + " .input")[0].value))) {
+			alert("Enter a valid amount into " + acc);
+		}
+		else {
+			$("#" + acc + " .balance")[0].innerHTML = "$" + (parseInt(($("#" + acc + " .balance")[0].innerHTML).split("$")[1]) - parseInt($("#" + acc + " .input")[0].value));
+		}
+	}
+}
