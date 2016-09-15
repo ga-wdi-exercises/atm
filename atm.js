fab5ac7ffabb872d4b1f3514312666daed762ff3
Doray Gisele Owens <doray.owens@gmail.com>
2016-09-15 16:01:26 -0400

    jQuery is working

--- atm.js
+++ atm.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
 });
+//variables I'll need: balance, amountWithdraw, amountDeposit,
+
+//When I click on Deposit, add value to checking total.
+
+//When I click Withdraw, subtract value from checking total.
+
+//User can not withdraw more than available balance.
+
+//If user tries to withdraw more than available balance, ignore the transaction.
