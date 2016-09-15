62bb33a5e6255069d9683061e4a3f0e978afe6c8
Mike Flores <michaelprflores@gmail.com>
2016-09-15 15:22:11 -0400

    Wrote pseudocode steps

--- atm.js
+++ atm.js
@@ -1,3 +1,13 @@
+/* This ATM allows a user to deposit and withdraw money
+ * - user chooses to deposit into Checking or Savings by clicking into input
+ * field. Event is triggered!!!
+ * - user can now set the dollar amount by typing in a number value.
+ * - user can deposit or withdraw
+ * - the balance number window reflects the new value resulting in the user's action
+ * - addition operator
+ */
+
+
 $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
 });
