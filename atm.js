5358c768c88843dbf849c041872fb559971c7a9e
manish pradhan <pradhanmanish33@gmail.com>
2016-09-15 14:22:49 -0400

    initial commit

--- atm.js
+++ atm.js
@@ -1,3 +1,8 @@
+if (jQuery) {
+     // jQuery is loaded
+    $("body").css("background", "red");
+  };
+
 $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
 });
