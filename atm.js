e1fd19dc9fde9cfe5ccd073171f49f6e7522d604
Kevin Mahoney <kjmahoney13@gmail.com>
2016-09-15 15:43:58 -0400

    included totalAmount functionality

--- atm.js
+++ atm.js
@@ -1,10 +1,27 @@
-function deposit() {
-  var depValue = $('.input').val()
-  $('.balance').text(depValue)
+var checkingBalance = $('#checking').children().eq(1)
+var checkingInput = $('#checking').children().eq(2)
+var checkingDepositButton = $('#checking').children().eq(3)
+var checkingWithdrawButton = $('#checking').children().eq(4)
 
+var savingBalance = $('#savings').children().eq(1)
+var savingInput = $('#savings').children().eq(2)
+var savingDepositButton = $('#savings').children().eq(3)
+var savingWithdrawButton = $('#savings').children().eq(4)
+
+var convertedNumber = checkingBalance.text().replace("$", "");
+var totalCheckingValue = parseInt(convertedNumber);
+
+
+function depositChecking() {
+  var userDepositChecking = parseInt(checkingInput.val())
+  totalCheckingValue = totalCheckingValue + userDepositChecking
+  checkingBalance.text("$" + totalCheckingValue)
+  // $('.balance').text(userDepositChecking)
 }
 
-$('.deposit').on("click",deposit);
+$('.deposit').on("click",depositChecking);
+
+//change
 
 
 
@@ -22,10 +39,16 @@ $('.deposit').on("click",deposit);
 // Select checking or ssaving
 
 
+//Presented with total Amounts
+// Create variable totalAmounts
 
-// Enter the amount you would like to withdraw
+// Enter the amount you would like to deposit/withdraw
+////Amount is pulled from the box.
+////Function also updates totalAmounts
+
 // Confirm transaction
 // Amounts updated and presented
+
 
 // OTHER FEATURES
 //accounts change color when hit zero
