5795b055efd6db0903780dfd730b3a556c0b8dbc
Latoya Watson <latoyamwatson@gmail.com>
2016-09-15 16:39:01 -0400

    Checking is working, addition is working. Need to do withdraw

--- atm.js
+++ atm.js
@@ -1,3 +1,51 @@
-$(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-});
+// Four functions
+// Two Deposit Functions and
+// Two withdraw functions
+// Four event listener
+// Each Function will be an event listener
+
+// Variables
+var checkingAdd = $("#checking").children("input.deposit");
+var checkingSub = $("#checking").children("input.withdraw");
+var savingAdd = $("#savings").children("input.deposit");
+var savingSub = $("#savings").children("input.withdraw");
+
+var checkingBalance = $("#checking").children(".balance");
+var savingsBalance = $("#savings").children(".balance");
+
+var checkingInput = ($("#checking").children("input.input").val());
+var savingsInput = $("#savings").children("input.input").val();
+
+// Function Time
+
+var oldBalance = 0;
+
+$("#checking .deposit").on("click", handleChecking);
+
+function handleChecking() {
+  var checkingInput = parseInt($("#checking").children("input.input").val());
+  var newBalance = parseInt(checkingInput);
+  oldBalance = oldBalance + newBalance;
+  checkingBalance.text(oldBalance);
+}
+
+// console.log(CheckingInput);
+// }
+//
+// // Function Two
+//
+// checkingAdd.on("click", handleBalance);
+//
+// function handleBalance() {
+//   var balance = CheckingInput.val();
+//   CheckingBalance.text(balance);
+// }
+//
+// // function handleBalance() {
+// //   var CheckingBalance = parseInt($("#checking").children(".balance").val());
+// //   var NewAmount = CheckingBalance + CheckingInput;
+// //   console.log(NewAmount);
+// // }
+//
+//
+// // var NewAmount = CheckingBalance + CheckingInput;
