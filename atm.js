4681181ff06984b5b08692a4902b5057d9741642
Michael Lai <mike24567@gmail.com>
2016-09-15 15:52:37 -0400

    Tested jQuery is working, added click listener to button

--- atm.js
+++ atm.js
@@ -1,3 +1,18 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+// });
+
+$("#checking .deposit").on("click", function(){
+  $("body").css("background-color","red");
 });
+console.log("hello");
+//Checkings Account Flow
+// var balance = $("#checking.balance");
+//
+// function withdraw(amount)
+
+
+//Savings Account Flow
+// var balance = $("#savings.balance");
+//
+// function withdraw(amount)
