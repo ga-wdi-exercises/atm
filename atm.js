0d1ab13c1d3332d15b93c2429aec230c1fa0cf14
Tim Foley <thetimfoley@gmail.com>
2016-09-15 14:57:37 -0400

    added first event for Checking deposit

--- atm.js
+++ atm.js
@@ -1,7 +1,16 @@
 //withdrawing money
   // get user amount
+  var $depChecking = $('#checking > .deposit');
+  var $inputChecking = $('#checking > .input');
+
+  $depChecking.on('click', function(){
+    var deposit = parseInt($inputChecking.val());
+    console.log("Deposit clicked. Value = " + deposit);
+  })
   // if amount > amount in bank,
     // throw error
-  // else subtract amount from account 
+  // else subtract amount from account
 
 //adding money
+  // get user amount
+  // account balance += amount
