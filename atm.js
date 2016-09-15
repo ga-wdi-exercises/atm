2f76cf19f2614dfa3c0d47afe405e2a3b61900b1
Poh Chuan Kuan <pohckuan@yahoo.com>
2016-09-15 18:16:31 -0400

    done

--- atm.js
+++ atm.js
@@ -1,3 +1,69 @@
-$(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-});
+// $(document).ready(function(){
+// // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+// });
+
+//
+var checkBal = $("#checking").children("div.balance");
+var checkAdd = $("#checking").children("input.deposit");
+var checkSub = $("#checking").children("input.withdraw");
+var checkInput=$("#checking").children("input.input");
+
+var savBal = $("#savings").children("div.balance");
+var savAdd = $("#savings").children("input.deposit");
+var savSub = $("#savings").children("input.withdraw");
+var savInput = $("#savings").children("input.input");
+
+var balance = 0
+checkAdd.on("click",addAmount);
+function addAmount(){
+  var value = parseInt(checkInput.val());
+  balance = value + balance
+  checkBal.text(balance);
+}
+  checkSub.on("click",subtractAmount);
+  function subtractAmount(){
+    var value = parseInt(checkInput.val());
+    balance = balance - value
+    checkBal.text(balance);
+  }
+
+  var savingBalance = 0
+  savAdd.on("click",savAddAmount);
+  function savAddAmount(){
+    var value = parseInt(savInput.val());
+    savingBalance = value + savingBalance
+    savBal.text(savingBalance);
+  }
+    savSub.on("click",savSubtractAmount);
+    function savSubtractAmount(){
+      var value = parseInt(savInput.val());
+      savingBalance = savingBalance - value;
+      savBal.text(savingBalance);
+    }
+
+  // checkBal.text(value) + parseInt((checkInput.val()));
+  // console.log(value)
+  // value = parseInt(checkInput.val());
+  // checkBal.text(value + parseInt(checkBal.text()));
+  // // console.log(value)
+
+//
+// $(cehcking.balance).text("$"+ (input + balance))
+//   $(.balance).eturn balance = balance + "amount entered"
+//
+//   ;
+
+// 1. User chooses checking or savings account.
+// 2. Enter amount to deposit in input text placeholder (.input)<input value === something>
+// 3. click deposit button.
+//     $(".deposit").on("click", addAmount);
+//     function addAmount(){
+//
+//     }
+// 4. show balance in .balance
+// 5. Choose checking or savings account
+// 6. enter amount to withdraw in input text placeholder (.input)
+// 7. click withdraw button
+// $(".withdraw").on("click", subtractAmount);
+// function subtractAmount(){
+// 8. show remaining balance
