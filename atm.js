b048a22a2176172dd2e50e719a938181834f4fe2
Kevin Mahoney <kjmahoney13@gmail.com>
2016-09-15 14:20:43 -0400

    updated to capture user input

--- atm.js
+++ atm.js
@@ -1,3 +1,34 @@
-$(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-});
+function deposit() {
+  var depValue = $('.input').val()
+  $('.balance').text(depValue)
+
+}
+
+$('.deposit').on("click",deposit);
+
+
+
+// $(document).ready(function(){
+// // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+// });
+//
+
+
+//
+// //1. insert card
+// enter pin
+// Presented with total amounts
+// Prompted for checking or ssaving
+// Select checking or ssaving
+
+
+
+// Enter the amount you would like to withdraw
+// Confirm transaction
+// Amounts updated and presented
+
+// OTHER FEATURES
+//accounts change color when hit zero
+
+
+//account cannot be less than zero
