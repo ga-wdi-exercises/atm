57745adb92c110bb07d7a8a741b718d2451e6657
Michael Lai <mike24567@gmail.com>
2016-09-15 17:52:40 -0400

    Added checking deposit and withdraw

--- atm.js
+++ atm.js
@@ -1,5 +1,6 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+var checkBalance = $("#checking").children(".balance");
 var checkAmt = $("#checking").children("input.input");
 var checkDeposit = $("#checking").children("input.deposit");
 var checkWithdraw = $("#checking").children("input.withdraw");
@@ -7,17 +8,18 @@ var saveAmt = $("#savings").children("input.input");
 var saveDeposit = $("#savings").children("input.deposit");
 var saveWithdraw = $("#savings").children("input.withdraw");
 
-//Checkings Account Flow
-checkDeposit.on("click", function(){
-  checkAmt.val();
-  console.log(checkAmt.val());
-});
-
-
-//Savings Account Flow
-// var balance = $("#savings.balance");
-//
-// function withdraw(amount)
-
-
-});
+//Checkings Account Flow//
+//Deposit into Checking Account
+var balance = 0;
+checkDeposit.on("click", changeDepositAmt);
+function changeDepositAmt(){
+  var input = parseInt(checkAmt.val());
+  balance = balance + input;
+  checkBalance.text("$" + (balance));
+};
+checkWithdraw.on("click", changeWithdrawAmt);
+function changeWithdrawAmt(){
+  var input = parseInt(checkAmt.val());
+  balance = balance - input;
+  checkBalance.text("$" + (balance));
+};
