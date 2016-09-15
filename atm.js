a21d9d8151fdf24c23accab0d4d0d8a6a18b7e51
Khoi Le <khoileee89@gmail.com>
2016-09-15 17:56:00 -0400

    checking done

--- atm.js
+++ atm.js
@@ -1,3 +1,46 @@
-$(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-});
+var addCheck = $("#checking").children("input.deposit")
+var balanceText = $("#checking .balance")
+var input = $("#checking .input")
+var subtractCheck =$("#checking").children("input.withdraw")
+
+
+
+addCheck.on("click", deposit)
+var newBalance = 0
+function deposit(){
+var checkInput = input.val()
+var balance = parseInt(checkInput);
+ newBalance = balance + newBalance
+balanceText.text(newBalance)
+}
+
+subtractCheck.on("click", withdraw)
+function withdraw(){
+  var checkInput = input.val()
+  var balance = parseInt(checkInput)
+  newBalance = newBalance - balance;
+  balanceText.text(newBalance)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// balance.replace("$", " ")
+// parseInt(balance.replace("$", " "))
+//
+// ---
+// var balance = 0
+// var input = 25
+// $("#checking .balance").text("$" + (input+balance))
