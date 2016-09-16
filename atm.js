5ea24df210b520cddea801c8a3d31cdf0c70fe41
Chris Jaffier <chrisd@Chriss-MacBook-Pro.local>
2016-09-16 13:35:44 -0400

    Checking account add/subtract

--- atm.js
+++ atm.js
@@ -1,16 +1,35 @@
 $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
 });
+//var balance = 0
+//var deposit = 0
+//var withdraw = 0
+
+//$("div.balance").html(parseInt("0"));
+//$(".deposit").on("click", addMoney);
+
+//function addMoney (input) {
+//var input = parseInt(($(".input").val()));
+//balance = balance + input
+//$(".balance").html(balance);
+
+//}
 var balance = 0
 var deposit = 0
 var withdraw = 0
 
-$("div.balance").html(parseInt("0"));
-$(".deposit").on("click", addMoney);
+$(".balance").html(parseInt("0"));
+$(".deposit").on("click", addMoneyChecking);
+$(".withdraw").on("click", takeMoneyChecking);
 
-function addMoney (input) {
+function addMoneyChecking () {
 var input = parseInt(($(".input").val()));
 balance = balance + input
-$(".balance").html(balance);
+$("#checking .balance").html(balance);
+}
 
+function takeMoneyChecking () {
+  var input = parseInt(($(".input").val()));
+  balance = balance - input
+  $("#checking .balance").html(balance);
 }
