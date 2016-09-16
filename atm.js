6c9d9a626bce828a039cb15e4e3322633ace9927
Barrett Cope <barrettcope@gmail.com>
2016-09-16 10:41:50 -0400

    Somewhat functional

--- atm.js
+++ atm.js
@@ -1,6 +1,3 @@
-
-$(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
 var accounts = [
   // array of account objects
   {
@@ -13,7 +10,7 @@ var currentAccount = {
   name: "My Primary Checking Account",
   type: "checking",
   balance: 100,
-  overdraftAccount: undefined,
+  overdraftAccount: undefined
 };
 var overdraftAccount = {
   name: "Emergency Fund",
@@ -22,32 +19,117 @@ var overdraftAccount = {
 };
 var transferAccount;
 
-var $inputDiv = $("#input");
+var $activeDiv = $("#active")
+var $activeAccount = $("#active .account");
+
 var $optionsDiv = $("#options")
+var $deposit = $("#deposit");
+var $withdraw = $("#withdraw");
+var $transfer = $("#transfer");
+
+var $inputDiv = $("#input");
 var $inputField = $("#input-field");
 var $button = $("#button");
+var $cancel = $("#cancel");
 
-var inputValue = $inputField.val();
+var $accountsDiv = $("#accounts");
+var $otherAccountsDivs = $("#accounts .account");
+
+var inputValue;
+
+var getInputValue = function() {
+  inputValue = $inputField.val();
+}
+
+var getActiveAccount = function() {
+  $activeAccount = $("#active .account");
+}
+
+var getOtherAccounts = function() {
+  $otherAccountsDivs = $("#accounts .account");
+}
+
+var transactionType;
+
+function createAccountDiv(account) {
+  var newAccountDiv = $("<div class='account'></div>");
+  var newAccountUl = $("<ul></ul>");
+  var newAccountNameLi = $("<li class='accountName'></li>").html("Account Name: " + account.name);
+  var newAccountTypeLi = $("<li class='accountType'></li>").html("Type: " + account.type);
+  var newAccountBalanceLi = $("<li class='accountBalance'></li>").html("Balance: $" + account.balance);
+  newAccountUl.append(newAccountNameLi).append(newAccountTypeLi).append(newAccountBalanceLi);
+  newAccountDiv.append(newAccountUl);
+  return newAccountDiv;
+}
+
+function refreshCurrentAccount() {
+  getActiveAccount();
+  $activeAccount.remove();
+  $activeDiv.append(createAccountDiv(currentAccount));
+}
+
+function refreshOtherAccounts() {
+  getOtherAccounts();
+  $otherAccountsDivs.remove();
+  $accountsDiv.append(createAccountDiv(overdraftAccount));
+  // $accountsDiv.append(createAccountDiv(transferAccount));
+}
+
+function setTransactionType() {
+  transactionType = $(this).attr("id");
+  $button.html(transactionType);
+}
 
 var atm = {
   // methods
-  removeEventHandler: function(object) {
-    object.off();
+  tryTransaction: function() {
+    getInputValue();
+    var amount = atm.cleanInput(inputValue);
+    if (!atm.validateInput(amount)) {
+      return atm.invalidInput();
+    } else if (transactionType === undefined) {
+      return alert("transactionType is undefined");
+    } else if (transactionType === "deposit") {
+      if (atm.deposit(amount)) {
+        return atm.transactionSuccessful();
+      } else {
+        return atm.transactionFailed();
+      }
+    } else if (transactionType === "withdraw") {
+      if (atm.withdraw(amount)) {
+        return atm.transactionSuccessful();
+      } else {
+        return atm.transactionFailed();
+      }
+    } else if (transactionType === "transfer") {
+      // may or may not finish this later
+      return atm.transactionFailed();
+    } else {
+      alert("tryTransaction has failed by meeting none of the logical criteria");
+    }
   },
-  selectDeposit: function() {
-    // change event handler for input form and button
-    removeEventHandler($button);
-    $button.on("click",atm.deposit)
+
+
+
+  transactionSuccessful: function() {
+    refreshCurrentAccount();
+    refreshOtherAccounts();
+    atm.inputToOptions();
   },
-  selectWithdraw: function() {
-    // change event handler for input form and button
-    removeEventHandler($button);
-    $button.on("click",atm.withdraw)
+  transactionFailed: function() {
+    // this should be changed to be dynamic
+    alert("Insufficient funds in accounts.")
   },
-  selectTransfer: function() {
-    // change event handler for input form and button
-    removeEventHandler($button);
-    $button.on("click",atm.transfer)
+
+
+
+  optionsToInput: function() {
+    atm.hide($optionsDiv);
+    atm.reveal($inputDiv);
+  },
+  inputToOptions: function() {
+    atm.hide($inputDiv);
+    atm.reveal($optionsDiv);
   },
   reveal: function(object) {
     object.removeClass("hidden");
@@ -55,6 +137,14 @@ var atm = {
   hide: function(object) {
     object.addClass("hidden");
   },
+  // may not be using
+  removeEventHandler: function(object) {
+    object.off();
+  },
+
+
+
+  // secondary - use if time permits
   getAccount: function(arrayIndex) {
     return accounts[arrayIndex].pop();
   },
@@ -71,18 +161,17 @@ var atm = {
     } else {
       alert("An error has occurred cause your banker is a dumb dumb.");
     }
-  }
-  selectAccount: function(purpose,) {
-    // list accounts
-    if (purpose === "transfer") {
-      deselectAccount("transfer");
-      transferAccount = accounts[this.data-id].pop();
-    }
+  },
+  selectAccount: function(purpose) {
+    // get account from array and assign to variable
   },
   deselectAccount: function(purpose) {
     // put current account back into accounts array
-
   },
+
+
+
+  // input handling
   cleanInput: function(input) {
     // remove special characters: $ and ,
     var cleanedInput = input.replace("$","");
@@ -90,20 +179,28 @@ var atm = {
     return cleanedInput = parseInt(cleanedInput);
   },
   validateInput: function(input) {
-    return !isNaN(input) && input !== NULL;
+    return !isNaN(input);
   },
+  invalidInput: function() {
+    alert("The value '" + inputValue + "' is an invalid entry.");
+    $inputField.attr("value","0");
+  },
+
+
+
+  // math
   deposit: function(amount) {
     // add amount to currentAccount.balance
-    currentAccount.balance += amount;
+    return currentAccount.balance += amount;
     // may need to add in function calls to update displayed elements
   },
   withdraw: function(amount) {
-    if (getBalance(currentAccount) >= amount) {
+    if (atm.getBalance(currentAccount) >= amount) {
       currentAccount.balance -= amount;
       // may need to add function calls
       return true;
-    } else if (getBalance(currentAccount) + getBalance(overdraftAccount) >= amount) {
-      overdraw(amount - currentAccount.balance);
+    } else if (atm.getBalance(currentAccount) + atm.getBalance(overdraftAccount) >= amount) {
+      atm.overdraw(amount - currentAccount.balance);
       currentAccount.balance = 0;
       // may need to add function calls
       return true
@@ -111,21 +208,22 @@ var atm = {
       // may need to add function calls
       return false
     }
-    // subtract amount from currentAccount.balance
   },
-  transfer: function() {
-    if () {
-
-    }
-  }
   overdraw: function(amount) {
     // subtract amount from overdraftAccount.balance
     overdraftAccount.balance -= amount;
     // may need to add function calls
     return true;
   },
+  // secondary - do if time permits
+  transfer: function() {
+  },
+
+
+
+  // account info
   getBalance: function(account) {
-    if (account.balance === undefined || account.balance === NULL) {
+    if (account.balance === undefined || account.balance === "") {
       return 0;
     } else {
       return account.balance;
@@ -136,8 +234,16 @@ var atm = {
     return true
   },
   getAccountName: function(account) {
-
   }
-
 }
-});
+
+// add event listeners
+$deposit.on("click",setTransactionType);
+$withdraw.on("click",setTransactionType);
+$transfer.on("click",setTransactionType);
+$accountsDiv.on("click",atm.selectAccount);
+$button.on("click",atm.tryTransaction);
+$cancel.on("click",atm.inputToOptions);
+
+refreshCurrentAccount();
+refreshOtherAccounts();
