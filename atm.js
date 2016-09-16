7d589f017c697d45636b0164dd007cc6b6cec616
Kevin Mahoney <kjmahoney13@gmail.com>
2016-09-16 10:59:17 -0400

    updated to include overdraft functionality

--- atm.js
+++ atm.js
@@ -1,11 +1,11 @@
-var checking = {
+var checking = { //Checking account fields
   Balance: $('#checking').children().eq(1),
   Input: $('#checking').children().eq(2),
   DepositButton: $('#checking').children().eq(3),
   WithdrawButton: $('#checking').children().eq(4)
 }
 
-var savings = {
+var savings = { //Savings account fields
   Balance: $('#savings').children().eq(1),
   Input: $('#savings').children().eq(2),
   DepositButton: $('#savings').children().eq(3),
@@ -13,15 +13,15 @@ var savings = {
 }
 
 var convertedNumberChecking = checking.Balance.text().replace("$", ""); //Converts text input to integer for checking acct
-var totalCheckingValue = parseInt(convertedNumberChecking);
+var totalCheckingValue = parseInt(convertedNumberChecking); //Initiates total value of checking account
 
 var convertedNumberSavings = savings.Balance.text().replace("$", ""); //Converts text input to integer for savings acct
-var totalSavingsValue = parseInt(convertedNumberSavings);
+var totalSavingsValue = parseInt(convertedNumberSavings); //Initiates total value of savings account
 
-checkingBalanceCheck()
+checkingBalanceCheck() //checks if balance is zero, if so then the display will be red
 savingsBalanceCheck()
 
-function checkingBalanceCheck() { //Changes the checking module if total amount is $0 Toggle Class
+function checkingBalanceCheck() { //Changes the color of the account interface if total amount is $0
   if (totalCheckingValue === 0) {
     $('#checking').removeClass("account")
     $('#checking').addClass("zero")
@@ -31,7 +31,7 @@ function checkingBalanceCheck() { //Changes the checking module if total amount 
   }
 }
 
-function savingsBalanceCheck() { //Changes the checking module if total amount is $0
+function savingsBalanceCheck() { //Changes the color of the account interface if total amount is $0
   if (totalSavingsValue === 0) {
     $('#savings').removeClass("account")
     $('#savings').addClass("zero")
@@ -43,7 +43,7 @@ function savingsBalanceCheck() { //Changes the checking module if total amount i
 
 function depositChecking() {  //Deposit Button for Checking
   var userDepositChecking = parseInt(checking.Input.val())
-    if (isNaN(userDepositChecking)) {
+    if (isNaN(userDepositChecking)) { //prevents input of NaN
       alert("Deposit amount not accepted, please insert a number without special characters i.e (1000)");
     }else{
   totalCheckingValue = totalCheckingValue + userDepositChecking
@@ -51,29 +51,42 @@ function depositChecking() {  //Deposit Button for Checking
   checkingBalanceCheck()
 }
 }
-
-  checking.DepositButton.on("click",depositChecking);
+checking.DepositButton.on("click",depositChecking);
 
 function withdrawChecking() {  //Withdraw Button for Checking
   var userWithdrawChecking = parseInt(checking.Input.val())
-  if (isNaN(userWithdrawChecking)) {
+  if (isNaN(userWithdrawChecking)) { //prevents input of NaN
     alert("Withdraw amount not accepted, please insert a number without special characters i.e (1000)");
     }else{
-      if (userWithdrawChecking > totalCheckingValue) {
-        alert(" Insufficent funds in checking account")
-      }else{
-        totalCheckingValue = totalCheckingValue - userWithdrawChecking
+      if (userWithdrawChecking > totalCheckingValue) { //Checks for need to overdraft from savings
+
+        var overDraw = (userWithdrawChecking - totalCheckingValue)
+
+          if (overDraw > totalSavingsValue) {
+            alert("Cannot draw from savings account due to insufficient funds, you are broke")
+          }else {
+            alert("Overdraft warning: You are withdrawing $" + userWithdrawChecking + " However, there is only $" + totalCheckingValue +
+            " available in Checking Account. Drafting $" + overDraw + " from Savings to Checking")
+        totalSavingsValue = totalSavingsValue - overDraw
+        totalCheckingValue = totalCheckingValue + overDraw
+         savings.Balance.text("$" + totalSavingsValue)
+         savingsBalanceCheck()
+         checking.Balance.text("$" + totalCheckingValue)
+         checkingBalanceCheck()
+       }
+     }else {
+        totalCheckingValue = totalCheckingValue - userWithdrawChecking //Procedure if there are sufficient funds
         checking.Balance.text("$" + totalCheckingValue)
         checkingBalanceCheck()
     }
-}
+  }
 }
       checking.WithdrawButton.on("click",withdrawChecking);
 
 
-  function depositSavings() {  //Deposit Button for SAvings
+  function depositSavings() {  //Deposit Button for Savings
         var userDepositSavings = parseInt(savings.Input.val())
-        if (isNaN(userDepositSavings)) {
+        if (isNaN(userDepositSavings)) { //prevents input of NaN
           alert("Deposit amount not accepted, please insert a number without special characters i.e (1000)");
         }else{
           totalSavingsValue = totalSavingsValue + userDepositSavings
@@ -86,11 +99,11 @@ function withdrawChecking() {  //Withdraw Button for Checking
 
   function withdrawSavings() {  //Withdraw Button for Savings
         var userWithdrawSavings = parseInt(savings.Input.val())
-        if (isNaN(userWithdrawSavings)) {
+        if (isNaN(userWithdrawSavings)) { //prevents input of NaN
           alert("Deposit amount not accepted, please insert a number without special characters i.e (1000)");
         }else{
-          if (userWithdrawSavings > totalSavingsValue) {
-            alert(" Insufficent funds in savings account")
+          if (userWithdrawSavings > totalSavingsValue) { //prevents savings account from going negative
+            alert(" Insufficent funds in savings account. Overdraft from checking to savings is not permitted as per 12 CFR 205.17")
           }else{
             totalSavingsValue = totalSavingsValue - userWithdrawSavings
             savings.Balance.text("$" + totalSavingsValue)
@@ -101,14 +114,7 @@ function withdrawChecking() {  //Withdraw Button for Checking
           savings.WithdrawButton.on("click",withdrawSavings);
 
 
-
-
-
-//Prevent blank number NaN
-//Install bonus
-//Clean up converted number
-//Clean up color check
-
+//Fix issue with comma inputs
 
 // var userDepositChecking = checking.Input.val()
 // if (userDepositChecking === "") {
