ec29d800350808d6821dd895d35cfbb7abd749ab
Tim Foley <thetimfoley@gmail.com>
2016-09-16 09:03:40 -0400

    added function to format for USD

--- atm.js
+++ atm.js
@@ -1,6 +1,7 @@
 //TODO:
-  // [ ] combine event listeners
+  // [x] combine event listeners
   // [ ] add a validator
+  // [ ] handle zero balance
   // [ ] overdraft protection
 
 var bank = {
@@ -15,7 +16,7 @@ var $depositButtons = $('.deposit');
 var $withdrawButtons = $('.withdraw');
 
 $depositButtons.on('click', function(){
-  var amount = parseInt($(this).siblings('.input').val());
+  var amount = parseFloat($(this).siblings('.input').val());
   if ($(this).parent().is("#checking")) {
     deposit("checkingBalance", $checkingBalance, amount);
   } else {
@@ -24,7 +25,7 @@ $depositButtons.on('click', function(){
 })
 
 $withdrawButtons.on('click', function(){
-  var amount = parseInt($(this).siblings('.input').val());
+  var amount = parseFloat($(this).siblings('.input').val());
   if ($(this).parent().is("#checking")) {
     withdraw("checkingBalance", $checkingBalance, amount);
   } else {
@@ -35,10 +36,31 @@ $withdrawButtons.on('click', function(){
 //DEPOSIT and WITHDRAW functions
 function deposit(account, display, amount) {
   bank[account] += amount;
-  display.html('$' + bank[account]);
+  display.html(toUSD(bank[account]));
 }
 
 function withdraw(account, display, amount) {
   bank[account] -= amount;
-  display.html('$' + bank[account]);
+  display.html(toUSD(bank[account]));
+}
+
+function validate(input) {
+  input.replace()
+  if (isNaN(input)) {
+    alert("Please, numbers only. Don't bother with dollar signs or anything else.")
+  } else {
+    return input;
+  }
+}
+
+function toUSD(number) {
+    //found this function at the following link. It gives a nice walkthrough of what's going on as well:
+    // http://www.cssnewbie.com/javascript-currency-conversion-script/#.V9vsYZMrLUo
+    var number = number.toString(),
+    dollars = number.split('.')[0],
+    cents = (number.split('.')[1] || '') +'00';
+    dollars = dollars.split('').reverse().join('')
+        .replace(/(\d{3}(?!$))/g, '$1,')
+        .split('').reverse().join('');
+    return '$' + dollars + '.' + cents.slice(0, 2);
 }
