25736dcaa8eec1ec91b739309e9fabe490162226
Andrew Whitley <awhitley1233@gmail.com>
2016-09-16 12:08:28 -0400

    added function to check account balance; if 0, font color red/debugged overdraft protection trigger conditions

--- atm.js
+++ atm.js
@@ -17,34 +17,38 @@ var atm = {
     checkingBalance += eval(checkingInput.val());
     checkingDisplay.html('$'+checkingBalance);
     checkingInput.val('');
+    this.checkForZero(checkingBalance, checkingDisplay);
   },
   depositToSavings : function() {
     savingsBalance += eval(savingsInput.val());
     savingsDisplay.html('$'+savingsBalance);
     savingsInput.val('');
+    this.checkForZero(savingsBalance, savingsDisplay);
   },
   withdrawFromChecking : function() {
     var amount = eval(checkingInput.val());
-    if (checkingBalance - amount > 0) {
+    if (checkingBalance - amount >= 0) {
       checkingBalance -= amount;
       checkingDisplay.html('$'+checkingBalance);
-    } else if ((savingsBalance+checkingBalance) > 0) {
+    } else if ((savingsBalance+checkingBalance) - amount >= 0) {
       this.overdraftProtection("fromSavings", amount);
     } else {
       alert('There are insufficient funds in your account to process your request');
     };
+    this.checkForZero(checkingBalance, checkingDisplay);
     checkingInput.val('');
   },
   withdrawFromSavings : function() {
     var amount = eval(savingsInput.val());
-    if (savingsBalance - amount > 0) {
+    if (savingsBalance - amount >= 0) {
       savingsBalance -= amount;
       savingsDisplay.html('$'+savingsBalance);
-    } else if ((savingsBalance+checkingBalance) > 0) {
+    } else if ((savingsBalance+checkingBalance) - amount >= 0) {
         this.overdraftProtection("fromChecking", amount);
     } else {
       alert('There are insufficient funds in your account to process your request');
     };
+    this.checkForZero(savingsBalance, savingsDisplay);
     savingsInput.val('');
   },
   overdraftProtection : function(fromAccount, amount) {
@@ -53,17 +57,28 @@ var atm = {
         var overdraftAmount = (amount - savingsBalance);
         checkingBalance -= overdraftAmount;
         savingsBalance -= savingsBalance;
+        this.checkForZero(checkingBalance, checkingDisplay);
+        this.checkForZero(savingsBalance, savingsDisplay);
         alert('Overdraft Protection has transferred $'+overdraftAmount+' from your Checking Account to cover the withdrawal');
         break;
       case "fromSavings" :
         var overdraftAmount = (amount - checkingBalance);
         savingsBalance -= overdraftAmount;
         checkingBalance -= checkingBalance;
+        this.checkForZero(checkingBalance, checkingDisplay);
+        this.checkForZero(savingsBalance, savingsDisplay);
         alert('Overdraft Protection has transferred $'+overdraftAmount+' from your Savings Account to cover the withdrawal');
         break;
     };
     checkingDisplay.html('$'+checkingBalance);
     savingsDisplay.html('$'+savingsBalance);
+  },
+  checkForZero : function(accountBalance, accountDisplay) {
+    if (accountBalance == 0) {
+      accountDisplay.css('color','red');
+    } else {
+      accountDisplay.css('color','black');
+    }
   }
 };
 
