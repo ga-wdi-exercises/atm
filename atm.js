ef559c5971f1f14d44134d727c434f61af451e28
brown2206 <brown.angelo.jr@gmail.com>
2016-09-16 13:56:06 -0400

    1st pass - functionality

--- atm.js
+++ atm.js
@@ -1,3 +1,90 @@
-$(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-});
+var checkAdd = $("#checking").children("input.deposit");
+var checkSub = $("#checking").children("input.withdraw");
+var savingsAdd = $("#savings").children("input.deposit");
+var savingsSub = $("#savings").children("input.withdraw");
+var checkBal = $("#checking").children(".balance");
+var savingsBal = $("#savings").children(".balance");
+
+/*
+var checkInput = $("#checking").children("input.input").val
+var savingsInput = $("#savings").children("input.input").val
+*/
+
+
+var BegBalChk = 0;  //Global Checking Account Bal
+var BegBalSav = 0;  //Global Savings Account Bal
+
+//Functions for account states; positive or negative.
+var makeCheckBalRed = function(){
+   var number = checkBal.text();
+   number = parseInt(number);
+   console.log(number)
+   if (number < 1) {
+   $('#checking.account').addClass('zero');
+   }
+}
+
+var makeSavingsBalRed = function(){
+   var number = savingsBal.text();
+   number = parseInt(number);
+   console.log(number)
+   if(number < 1) {
+   $('#savings.account').addClass('zero');
+   }
+}
+
+var makeCheckBalGreen = function(){
+   var number = checkBal.text();
+   number = parseInt(number);
+   console.log(number)
+   if (number > 0) {
+   $('#checking.account').removeClass('zero');
+   }
+}
+
+var makeSavingsBalGreen = function(){
+   var number = savingsBal.text();
+   number = parseInt(number);
+   console.log(number)
+   if(number > 0) {
+   $('#savings.account').removeClass('zero');
+   }
+}
+
+//Event listeners
+$("#checking .deposit").on("click", checkingAmtDep);
+function checkingAmtDep() {
+  var checkInput = $("#checking").children("input.input").val();
+  var newBal = parseInt(checkInput);
+  BegBalChk = BegBalChk + newBal;
+  checkBal.text(BegBalChk)
+  makeCheckBalGreen();
+}
+
+$("#checking .withdraw").on("click", checkingAmtWithdrawn);
+function checkingAmtWithdrawn() {
+  var checkWithdr = $("#checking").children("input.input").val();
+  var newBal = parseInt(checkWithdr);
+  console.log(newBal)
+  BegBalChk = BegBalChk - newBal;
+  checkBal.text(BegBalChk)
+  makeCheckBalRed();
+}
+
+$("#savings .deposit").on("click", savingsAmtDep);
+function savingsAmtDep() {
+  var savingsInput = $("#savings").children("input.input").val();
+  var newBal = parseInt(savingsInput);
+  BegBalSav = BegBalSav + newBal;
+  savingsBal.text(BegBalSav)
+  makeSavingsBalGreen();
+}
+
+$("#savings .withdraw").on("click", savingsAmtWithdrawn);
+function savingsAmtWithdrawn() {
+  var savingsWithdr = $("#savings").children("input.input").val();
+  var newBal = parseInt(savingsWithdr);
+  BegBalSav = BegBalSav - newBal;
+  savingsBal.text(BegBalSav)
+  makeSavingsBalRed();
+}
