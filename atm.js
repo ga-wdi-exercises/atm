737143753d1a5a259b9e9153af375b881e0597f7
YOUR FULL NAME <lun.mariancik@gmail.com>
2016-09-16 08:47:13 -0400

    added savings account deposit input

--- atm.js
+++ atm.js
@@ -1,27 +1,43 @@
-// $(document).ready(function(){
-var balance = 0;
+$(document).ready(function(){
+  var balance = 0;
+  var savings = 0;
+  var checking = 0;
+  var balance1 = 0;
+  var deposit = 0;
 
 
   //Add a click listener to the checking account's "Deposit" button
   $(".deposit").on("click", function() {
     console.log("hello");
-  })
+  });
   //On clicking "Deposit", it should get the user input
   $(".withdraw").on("click", function() {
     var input = $(':input').val();
     console.log(input);
-  })
+  });
 
   //On clicking "Deposit", it should update the "balance" with the user input
-  $(".deposit").on("click", function() {
-      balance = balance + eval($('input.input').val());
-      console.log(balance);
-      $("#checking .balance").text(balance);
-  })
+  $("#checking .deposit").on("click", function() {
+    balance = balance + eval($('input.input').val());
+    console.log(balance);
+    $("#checking .balance").text(balance);
+  });
   //On "Withdraw", it updates the balance
   $(".withdraw").on("click", function() {
     balance = balance - eval($('input.input').val());
     console.log(balance);
     $("#checking .balance").text(balance);
+  });
+  //On clicking 'savings account' "Deposit", it should get the user input
+
+  $('#savings .deposit').on('click', function(){
+    var deposit = $("#savings input.input").val();
+    var deposit = parseInt(deposit);
+    savings = savings + deposit;
+    console.log(savings);
+    $('#savings .balance').text(savings);
   })
-// });
+
+
+
+  });
