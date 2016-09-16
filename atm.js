0324226e2c9a59021090ff0f6a2f877d3e1e9815
Andrew Whitley <awhitley1233@gmail.com>
2016-09-16 11:47:37 -0400

    added all atm logic including overdraft protection

--- atm.js
+++ atm.js
@@ -16,37 +16,61 @@ var atm = {
   depositToChecking : function() {
     checkingBalance += eval(checkingInput.val());
     checkingDisplay.html('$'+checkingBalance);
+    checkingInput.val('');
   },
   depositToSavings : function() {
     savingsBalance += eval(savingsInput.val());
     savingsDisplay.html('$'+savingsBalance);
+    savingsInput.val('');
   },
   withdrawFromChecking : function() {
     var amount = eval(checkingInput.val());
     if (checkingBalance - amount > 0) {
-      checkingBalance -= eval(checkingInput.val());
+      checkingBalance -= amount;
       checkingDisplay.html('$'+checkingBalance);
+    } else if ((savingsBalance+checkingBalance) > 0) {
+      this.overdraftProtection("fromSavings", amount);
     } else {
-      alert('There are insufficient funds in the selected account to process your request');
+      alert('There are insufficient funds in your account to process your request');
     };
+    checkingInput.val('');
   },
   withdrawFromSavings : function() {
     var amount = eval(savingsInput.val());
     if (savingsBalance - amount > 0) {
-      savingsBalance -= eval(savingsInput.val());
+      savingsBalance -= amount;
       savingsDisplay.html('$'+savingsBalance);
+    } else if ((savingsBalance+checkingBalance) > 0) {
+        this.overdraftProtection("fromChecking", amount);
     } else {
-      alert('There are insufficient funds in the selected account to process your request');
+      alert('There are insufficient funds in your account to process your request');
     };
+    savingsInput.val('');
   },
-  overdraftProtection : function() {
+  overdraftProtection : function(fromAccount, amount) {
+    switch (fromAccount) {
+      case "fromChecking" :
+        var overdraftAmount = (amount - savingsBalance);
+        checkingBalance -= overdraftAmount;
+        savingsBalance -= savingsBalance;
+        alert('Overdraft Protection has transferred $'+overdraftAmount+' from your Checking Account to cover the withdrawal');
+        break;
+      case "fromSavings" :
+        var overdraftAmount = (amount - checkingBalance);
+        savingsBalance -= overdraftAmount;
+        checkingBalance -= checkingBalance;
+        alert('Overdraft Protection has transferred $'+overdraftAmount+' from your Savings Account to cover the withdrawal');
+        break;
+    };
+    checkingDisplay.html('$'+checkingBalance);
+    savingsDisplay.html('$'+savingsBalance);
   }
 };
 
 //set event listeners to trigger ATM functionality
-checkingDeposit.on('click', atm.depositToChecking);
-checkingWithdraw.on('click', atm.withdrawFromChecking);
-savingsDeposit.on('click', atm.depositToSavings);
-savingsWithdraw.on('click', atm.withdrawFromSavings);
+checkingDeposit.on('click', atm.depositToChecking.bind(atm));
+checkingWithdraw.on('click', atm.withdrawFromChecking.bind(atm));
+savingsDeposit.on('click', atm.depositToSavings.bind(atm));
+savingsWithdraw.on('click', atm.withdrawFromSavings.bind(atm));
 
 // });
