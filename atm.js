8e2ef4e332a5c440d3c75d8759ebe48dcce6c984
Nick Oki <nick.oki@live.com>
2016-09-16 10:41:10 -0400

    Log JS functionality

--- atm.js
+++ atm.js
@@ -8,24 +8,33 @@ $(document).ready(function(){
 // Account object constructor
 function Account() {
 	return {
+		accountName: "",
 		domElement: $,
 		balance: 0,
 		deposit: function(amt) {
 			this.balance += amt;
 			this.refreshBalance();
-			// TODO log transaction
+			logTransaction(this.accountName, "deposit", amt, this.balance);
 		},
 		withdraw: function(amt, type) {
 			this.balance -= amt;
 			this.refreshBalance();
-			// TODO log transaction
+			logTransaction(this.accountName, "withdraw", amt, this.balance);
 		},
 		transfer: function(amt, toAccount) {
 			this.balance -= amt;
 			toAccount.balance += amt;
 			this.refreshBalance();
 			toAccount.refreshBalance();
-			// TODO log transaction
+			// TODO log transaction, for both!
+			if (this.accountName === "checking") {
+				logTransaction(this.accountName, "transfer", amt, this.balance);
+				//TODO move to recursive logTransaction call?
+				logTransaction(savingsAccount.accountName, "transfer", amt, savingsAccount.balance);
+			} else if (this.accountName === "savings") {
+				logTransaction(this.accountName, "transfer", amt, this.balance);
+				logTransaction(checkingAccount.accountName, "transfer", amt, checkingAccount.balance);
+			}
 		},
 		refreshBalance: function() {
 			var roundedBalance = parseFloat(Math.round(this.balance * 100) / 100).toFixed(2);
@@ -34,14 +43,58 @@ function Account() {
 	}
 }
 
+function logTransaction(account, type, amt, balance) {
+	// Begin html table row
+	var logEntry = '<tr>';
+
+	// Add date
+	var date = getDate();
+	logEntry += '<td>' + date + '</td>'; // TODO fix date
+
+	// Add account
+	logEntry += '<td class="capitalize">' + account + '</td>';
+
+	// Add type w coloring
+	if (type === 'deposit') {
+		logEntry += '<td class="green capitalize">' + type + '</td>';
+	} else if (type === 'withdraw') {
+		logEntry += '<td class="red capitalize">' + type + '</td>';
+	} else {
+		logEntry += '<td class="capitalize">' + type + '</td>';
+	}
+
+	// Add amount w coloring
+	if (type === 'deposit') {
+		logEntry += '<td class="green">$' + amt + '</td>';
+	} else if (type === 'withdraw') {
+		logEntry += '<td class="red">$' + amt + '</td>';
+	} else {
+		logEntry += '<td>$' + amt + '</td>';
+	}
+
+	// Add balance
+	logEntry += '<td>$' + balance + '</td>';
+
+	// End html table row
+	logEntry += '</tr>';
+
+	$('#log .labels').after(logEntry);
+	console.log(account);
+	console.log(type);
+	console.log(amt);
+	console.log(balance);
+}
+
 
 // GLOBAL OBJECTS
 // Checking account
 var checkingAccount = new Account();
+checkingAccount.accountName = "checking";
 checkingAccount.domElement = $(checking);
 
 // Savings account
 var savingsAccount = new Account();
+savingsAccount.accountName = "savings";
 savingsAccount.domElement = $(savings);
 
 // Log, to hold transactions
@@ -59,9 +112,8 @@ var transaction = {
 }
 
 
-// EVENT LISTENERS
-// TODO refactor listeners
 
+// EVENT LISTENERS
 $('.deposit').on('click', function() {
 	// Deposit checking
 	if ($(this).parent().attr('id') === "checking") {
@@ -126,6 +178,7 @@ function formatInput(input) {
 	return value;
 }
 
+// Check for overdraw
 function overdrawChecking(amt) {
 	// If withdrawing more than balance, withdraw from savings
 	var overdraw = amt - checkingAccount.balance;
@@ -138,18 +191,15 @@ function overdrawChecking(amt) {
 	}
 }
 
-// TODO transfer function
-
-
-
-// TODO Overdraft protection
-// TODO add transaction history
-
-/*
-var transaction = {
-	date: ,
-	time: ,
-	type: // deposit or withdraw,
-	amount: ,
-	newBalance: ,
-*/
+// Format date
+function getDate() {
+	var d = new Date();
+	var month = d.getMonth() + 1;
+	var day = d.getDate();
+	var time = d.getHours() + ":" + d.getMinutes();
+	var output = (month < 10 ? '0' : '') + month + '/' +
+		(day < 10 ? '0' : '') + day + ', ' +
+		d.getFullYear() + '/' +
+		time;
+	return output;
+}
