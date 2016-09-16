b705c9d3ad51ee0a46c48e06794a005d4efe2c6e
William Keating <keating49@gmail.com>
2016-09-16 10:57:50 -0400

    balance

--- atm.js
+++ atm.js
@@ -1,3 +1,38 @@
-$(document).ready(function(){
+
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+$(document).ready(function(){
 });
+
+
+
+// var checkingDeposit = ("#checking.input");
+//
+// var checkingWithdraw = $("#checking.withdraw");
+// var savingsDeposit = $("#savings.input");
+// var savingsWithdraw = $("#savings.withdraw");
+// var checkingBalance = $("#checking.balance");
+// var savingsBalance = $("#savings.balance");
+//
+var balance = 100;
+var input = 0;
+
+// listeners??
+$("input.depositC").click(function(){
+  console.log("Hello");
+  $("div.balance").text("$"+(input + balance));
+  });
+
+$("input.depositS").click(function(){
+  console.log("Hello");
+
+$("div.balance").text("$"+(input + balance));
+});
+
+// listeners??
+// $("#checking.withdraw").on("click", "withdraw")
+// $("#savings.withdraw").on("click", "withdraw")
+
+// I need to make the dollar amount from .input to go to display in .balance when I hit button .deposit
+//
+// make sure i can read the input and convert into a number
+// parseFloat
