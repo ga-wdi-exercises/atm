1606c53017a9850b667e052663ecb6785f8fc455
Liza Floyd <lizafloyd@gmail.com>
2016-09-16 10:54:57 -0400

    checking overdraft protocol working

--- atm.js
+++ atm.js
@@ -8,6 +8,11 @@ var saveIn = $("#savings").children("input.input")
 var checkBal = $("#checking").children(".balance")
 var saveBal = $("#savings").children(".balance")
 
+var checkTran = $("#checking").children("input.transferIn")
+var checkTranB = $("#checking").children("input.transfer")
+var saveTran = $("#savings").children("input.transferIn")
+var saveTranB = $("#savings").children("input.transfer")
+
 var actCheckBal = 0
 var actSaveBal = 0
 
@@ -28,8 +33,6 @@ red()
 checkAdd.on("click", deposit)
 checkMin.on("click", withdraw)
 
-
-
 function deposit(e) {
   var checkValD = checkIn.val()
   var balance = parseInt(checkValD)
@@ -42,7 +45,7 @@ function withdraw(e) {
   var checkValW = checkIn.val()
   var balance = parseInt(checkValW)
   if (balance > actCheckBal) {
-    alert("Transaction not possible.")
+    checkOverdraft()
   } else {
   actCheckBal = actCheckBal - balance
   checkBal.text("$" + actCheckBal)
@@ -73,4 +76,51 @@ function withdrawSave(e) {
   }
   red()
   e.preventDefault()
+}
+
+checkTranB.on("click", checkTransfer)
+saveTranB.on("click", saveTransfer)
+
+function checkTransfer(e) {
+  var tranVal = checkTran.val()
+  var balance = parseInt(tranVal)
+  if(actCheckBal >= balance) {
+  actSaveBal = actSaveBal + balance
+  actCheckBal = actCheckBal - balance
+  saveBal.text("$" + actSaveBal)
+  checkBal.text("$" + actCheckBal)
+} else {
+  alert("Transaction not possible.")
+}
+  red()
+  e.preventDefault()
+}
+function saveTransfer(e) {
+  var tranVal = saveTran.val()
+  var balance = parseInt(tranVal)
+  if(actSaveBal >= balance) {
+  actCheckBal = actCheckBal + balance
+  actSaveBal = actSaveBal - balance
+  saveBal.text("$" + actSaveBal)
+  checkBal.text("$" + actCheckBal)
+} else {
+  alert("Transaction not possible.")
+}
+  red()
+  e.preventDefault()
+}
+
+function checkOverdraft() {
+  var checkValW = checkIn.val()
+  var balance = parseInt(checkValW)
+  if ((actCheckBal + actSaveBal) >= balance) {
+    alert("overdraft protocol")
+    var remainder = actCheckBal - balance
+    actCheckBal = 0
+    actSaveBal = actSaveBal + remainder
+    saveBal.text("$" + actSaveBal)
+    checkBal.text("$" + actCheckBal)
+  } else {
+      alert("Transaction not possible.")
+  }
 }
