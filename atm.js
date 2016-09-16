3a873e4eed89f3b9e4a04e96b0fd410b38af59f9
manish pradhan <pradhanmanish33@gmail.com>
2016-09-16 14:33:19 -0400

    commit 1

--- atm.js
+++ atm.js
@@ -1,8 +1,68 @@
-if (jQuery) {
-     // jQuery is loaded
-    $("body").css("background", "red");
-  };
-
+//code for testing jQuery
 $(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+$("body").css("background", "red");
 });
+
+
+//code for deposit money to checking a/c.
+var balanceCheck = $("div.balanceC")
+var inputCheck = $("input.inputC")
+var balanceSave = $("div.balanceS")
+var inputSave = $("input.inputS")
+
+
+
+
+$ ("input.depositC").click(function(){
+  console.log("HELLO");
+
+
+  $("div.balanceC").text("$"+(parseInt(inputCheck.val() + balanceCheck.val())));
+
+});
+
+//code for withdrawing money from checking a/c.
+
+$ ("input.withdrawC").click(function(){
+  console.log("GOODMORNING");
+
+
+  $("div.balanceC").text("$"+(parseInt(balanceCheck.val() - inputCheck.val())));
+
+});
+
+
+
+
+//code for deposit money to savings a/c.
+
+$ ("input.depositS").click(function(){
+  console.log("HELLO");
+
+
+$("div.balanceS").text("$"+(parseInt(inputSave.val() + balanceSave.val())));
+
+});
+
+//code for withdrawing money from savings a/c.
+
+$ ("input.withdrawS").click(function(){
+  console.log("GOODMORNING");
+
+
+  $("div.balanceS").text("$"+(parseInt(balanceSave.val() - inputSave.val())));
+
+});
+
+
+
+
+// var overDraft = function(){
+//   var balanceCheck = $("div.balanceC")
+//
+// 	if(balanceCheck < 0){
+// 		console.log("Incorrect Withdrawal Amount");
+// 		$("div.balancesC").text(toString("wrong amount"));
+//    }
+//
+// }
