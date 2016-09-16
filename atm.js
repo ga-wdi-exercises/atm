dfe6f9ab5a898e9939f9dc7a2042ace22380fa31
Mark Sweet <marks828@gmail.com>
2016-09-16 15:31:40 -0400

    added overdraft

--- atm.js
+++ atm.js
@@ -6,25 +6,41 @@ var checkBalance = 0
 var saveBalance = 0
 
 
+
+
 // CHECKING
   // Deposit
 $('#checking').children('input.deposit').on('click', function(){
   var dmoney = parseInt($('#checking').children('.input').val());
+  console.log(checkBalance)
   checkBalance = checkBalance + dmoney
   $(this).siblings('.balance').html("$" + checkBalance);
   $('.input').val('');
+  console.log(checkBalance);
 })
   // Withdraw
 $('#checking').children('input.withdraw').on('click', function(){
   var wmoney = parseInt($('#checking > .input').val());
+  console.log(checkBalance);
+  console.log(wmoney);
+  if (checkBalance < wmoney){
+    if (wmoney < saveBalance){
+      saveBalance = saveBalance - wmoney
+      $('#savings > .balance').html("$" + saveBalance);
+    }
+    // overdraft function
+    console.log('overdraft');
+  }
   checkBalance = checkBalance - wmoney
   $(this).siblings().html("$"  + checkBalance)
   $('.input').val('');
-  if(wmoney > checkBalance){
-    if (saveBalance > checkBalance - wmoney){
-      checkBalance = saveBalance - (checkBalance - wmoney)
-    }
-  }
+  console.log(checkBalance);
+
+  // if(wmoney > checkBalance){
+  //   if (saveBalance > checkBalance - wmoney){
+  //     checkBalance = saveBalance - (checkBalance - wmoney)
+  //   }
+  // }
 })
 
 // SAVINGS
