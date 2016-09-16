3afbd1db0c6b887d01ad6646ed1e56719275d0ae
Ian Hollands <ihollands@outlook.com>
2016-09-16 14:49:11 -0400

    atm as object format - cleaner code

--- atm.js
+++ atm.js
@@ -1,17 +1,17 @@
 
 var checkingZero = function() {
-    if (checking.current === 0) {
-      checking.account.attr("class", "zero")
-    } else if (checking.current > 0) {
-      checking.account.attr("class", "account");
+  if (atm.checking.current === 0) {
+    atm.checking.account.attr("class", "zero")
+  } else if (atm.checking.current > 0) {
+    atm.checking.account.attr("class", "account");
   }
 }
 
 var savingsZero = function() {
-    if (savings.current === 0) {
-      savings.account.attr("class", "zero")
-    } else if (savings.current > 0) {
-      savings.account.attr("class", "account");
+  if (atm.savings.current === 0) {
+    atm.savings.account.attr("class", "zero")
+  } else if (atm.savings.current > 0) {
+    atm.savings.account.attr("class", "account");
   }
 }
 
@@ -23,45 +23,44 @@ var atm = {
     withdrawButton: $("#checking .withdraw"),
     depositButton: $("#checking .deposit"),
     current: 0,
-    zeroCheck: function() {
-      checkingZero();
-      savingsZero();
-    },
     deposit: function() {
-      $(this).balance.html("$" + ($(this).current + parseInt($(this).input.val(),10)));
-      $(this).current = $(this).current + parseInt($(this).input.val(),10);
-      $(this).zeroCheck;
+      this.balance.html("$" + (this.current + parseInt(this.input.val(),10)));
+      this.current = this.current + parseInt(this.input.val(),10);
+      checkingZero();
     },
     withdraw: function() {
-      if (parseInt($(this).input.val(),10) < $(this).current) {
-        $(this).balance.html("$" + ($(this).current - parseInt($(this).input.val(),10)));
-        $(this).current = $(this).current - parseInt($(this).input.val(),10);
-        $(this).zeroCheck;
-      } else if (parseInt($(this).input.val(),10) === $(this).current) {
-        $(this).balance.html("$" + (0));
-        $(this).current = 0;
-        $(this).zeroCheck;
-      } else if (parseInt($(this).input.val(),10) === ($(this).current + savings.current)) {
-        $(this).balance.html("$" + (0));
-        $(this).current = 0;
-        savings.balance.html("$" + (0));
-        savings.current = 0;
-        $(this).zeroCheck;
-      } else if (parseInt($(this).input.val(),10) > $(this).current && parseInt($(this).input.val(),10) < ($(this).current + savings.current)) {
-        var reduceSavings = parseInt($(this).input.val(),10) - $(this).current;
-        $(this).balance.html("$" + (0));
-        $(this).current = 0;
-        savings.balance.html("$" + (savings.current - reduceSavings));
-        savings.current = savings.current - reduceSavings;
-        $(this).zeroCheck;
-      } else if (parseInt($(this).input.val(),10) > ($(this).current + savings.current)) {
-        alert("Insufficient cumulative funds. Please enter an amount NOT to exceed $" + ($(this).current + savings.current));
-        $(this).zeroCheck;
+      if (parseInt(this.input.val(),10) < this.current) {
+        this.balance.html("$" + (this.current - parseInt(this.input.val(),10)));
+        this.current = this.current - parseInt(this.input.val(),10);
+        checkingZero();
+      } else if (parseInt(this.input.val(),10) === this.current) {
+        this.balance.html("$" + (0));
+        this.current = 0;
+        checkingZero();
+      } else if (parseInt(this.input.val(),10) === (this.current + atm.savings.current)) {
+        this.balance.html("$" + (0));
+        this.current = 0;
+        atm.savings.balance.html("$" + (0));
+        atm.savings.current = 0;
+        checkingZero();
+        savingsZero();
+      } else if (parseInt(this.input.val(),10) > this.current && parseInt(this.input.val(),10) < (this.current + atm.savings.current)) {
+        var reduceSavings = parseInt(this.input.val(),10) - this.current;
+        this.balance.html("$" + (0));
+        this.current = 0;
+        atm.savings.balance.html("$" + (atm.savings.current - reduceSavings));
+        atm.savings.current = atm.savings.current - reduceSavings;
+        checkingZero();
+        savingsZero();
+      } else if (parseInt(this.input.val(),10) > (this.current + atm.savings.current)) {
+        alert("Insufficient cumulative funds. Please enter an amount NOT to exceed $" + (this.current + atm.savings.current));
+        checkingZero();
+        savingsZero();
       }
-      }
+    }
   },
 
-  savings = {
+  savings: {
     input: $("#savings .input"),
     balance: $("#savings .balance"),
     account: $("#savings"),
@@ -69,40 +68,44 @@ var atm = {
     depositButton: $("#savings .deposit"),
     current: 0,
     deposit: function() {
-      $(this).balance.html("$" + ($(this).current + parseInt($(this).input.val(),10)));
-      $(this).current = $(this).current + parseInt($(this).input.val(),10);
-      $(this).zeroCheck;
+      this.balance.html("$" + (this.current + parseInt(this.input.val(),10)));
+      this.current = this.current + parseInt(this.input.val(),10);
+      savingsZero();
     },
     withdraw: function() {
-      if (parseInt($(this).input.val(),10) < $(this).current) {
-        $(this).balance.html("$" + ($(this).current - parseInt($(this).input.val(),10)));
-        $(this).current = $(this).current - parseInt($(this).input.val(),10);
-        $(this).zeroCheck;
-      } else if (parseInt($(this).input.val(),10) === $(this).current) {
-        $(this).balance.html("$" + (0));
-        $(this).current = 0;
-        $(this).zeroCheck;
-      } else if (parseInt($(this).input.val(),10) === ($(this).current + savings.current)) {
-        $(this).balance.html("$" + (0));
-        $(this).current = 0;
-        savings.balance.html("$" + (0));
-        savings.current = 0;
-        $(this).zeroCheck;
-      } else if (parseInt($(this).input.val(),10) > $(this).current && parseInt($(this).input.val(),10) < ($(this).current + savings.current)) {
-        var reduceSavings = parseInt($(this).input.val(),10) - $(this).current;
-        $(this).balance.html("$" + (0));
-        $(this).current = 0;
-        savings.balance.html("$" + (savings.current - reduceSavings));
-        savings.current = savings.current - reduceSavings;
-        $(this).zeroCheck;
-      } else if (parseInt($(this).input.val(),10) > ($(this).current + savings.current)) {
-        alert("Insufficient cumulative funds. Please enter an amount NOT to exceed $" + ($(this).current + savings.current));
-        $(this).zeroCheck;
-      }
+      if (parseInt(this.input.val(),10) < this.current) {
+        this.balance.html("$" + (this.current - parseInt(this.input.val(),10)));
+        this.current = this.current - parseInt(this.input.val(),10);
+        savingsZero();
+      } else if (parseInt(this.input.val(),10) === this.current) {
+        this.balance.html("$" + (0));
+        this.current = 0;
+        savingsZero();
+      } else if (parseInt(this.input.val(),10) === (this.current + atm.checking.current)) {
+        this.balance.html("$" + (0));
+        this.current = 0;
+        atm.checking.balance.html("$" + (0));
+        atm.checking.current = 0;
+        checkingZero();
+        savingsZero();
+      } else if (parseInt(this.input.val(),10) > this.current && parseInt(this.input.val(),10) < (this.current + atm.checking.current)) {
+        var reduceSavings = parseInt(this.input.val(),10) - this.current;
+        this.balance.html("$" + (0));
+        this.current = 0;
+        atm.checking.balance.html("$" + (atm.checking.current - reduceSavings));
+        atm.checking.current = atm.checking.current - reduceSavings;
+        checkingZero();
+        savingsZero();
+      } else if (parseInt(this.input.val(),10) > (this.current + atm.checking.current)) {
+        alert("Insufficient cumulative funds. Please enter an amount NOT to exceed $" + (this.current + atm.checking.current));
+        checkingZero();
+        savingsZero();
       }
     }
+  }
+}
 
-atm.checking.withdrawButton.on("click", atm.checking.withdraw);
-atm.checking.depositButton.on("click", atm.checking.deposit);
-atm.savings.withdrawButton.on("click", atm.savings.withdraw);
-atm.savings.depositButton.on("click", atm.savings.deposit);
+atm.checking.withdrawButton.on("click", atm.checking.withdraw.bind(atm.checking));
+atm.checking.depositButton.on("click", atm.checking.deposit.bind(atm.checking));
+atm.savings.withdrawButton.on("click", atm.savings.withdraw.bind(atm.savings));
+atm.savings.depositButton.on("click", atm.savings.deposit.bind(atm.savings));
