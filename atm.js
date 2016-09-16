d0cdca1730eac34a9ac48f48e2c9ef4cb1616bb4
Latoya Watson <latoyamwatson@gmail.com>
2016-09-16 11:06:26 -0400

    fixed NaN bug, atm working. Need to add logic for trying to take out too much money when withdrawing

--- atm.js
+++ atm.js
@@ -1,59 +1,55 @@
-// Four functions
-// Two Deposit Functions and
-// Two withdraw functions
-// Four event listener
-// Each Function will be an event listener
-
 // Variables
-var checkingAdd = $("#checking").children("input.deposit");
-var checkingSub = $("#checking").children("input.withdraw");
-var savingAdd = $("#savings").children("input.deposit");
-var savingSub = $("#savings").children("input.withdraw");
 
-var checkingBalance = $("#checking").children(".balance");
-var savingsBalance = $("#savings").children(".balance");
+var checkingAdd = $("#checking input.deposit");
+var checkingSub = $("#checking input.withdraw");
+var savingAdd = $("#savings input.deposit");
+var savingSub = $("#savings input.withdraw");
 
-var checkingInput = ($("#checking").children("input.input").val());
-var savingsInput = $("#savings").children("input.input").val();
+var checkingBalance = $("#checking .balance");
+var savingsBalance = $("#savings .balance");
+
+var checking = 0;
+var savings = 0;
 
 // Function Time
 
-var Balance = 0;
-
-$("#checking .deposit").on("click", handleChecking);
-$("#checking .withdraw").on("click", handleCheckingWithdraw);
-
 function handleChecking() {
-  var checkingInput = parseInt($("#checking").children("input.input").val());
-  var newBalance = checkingInput;
-  Balance = Balance + newBalance;
-  checkingBalance.text("$" + Balance);
+  var checkingInput = parseInt($("#checking input.input").val());
+  checking = checking + checkingInput;
+  checkingBalance.text("$" + checking);
 }
 
 function handleCheckingWithdraw() {
-  var checkingInput = parseInt($("#checking").children("input.input").val());
-  var newBalance = parseInt(checkingInput);
-  Balance = Balance - newBalance;
-  checkingBalance.text("$" + Balance);
+  checkingInput = parseInt($("#checking input.input").val());
+  //  checkingInput = parseInt($("#checking").children("input.input").val());
+   newBalance = checkingInput;
+   checking = (checking - newBalance); // make sure this only happens when enough $$
+    if (checking < 0) {
+      alert("Sorry. You don't have enough money.");
+    } else {
+      checkingBalance.text("$" + checking);
+  }
 }
 
-// console.log(CheckingInput);
-// }
-//
-// // Function Two
-//
-// checkingAdd.on("click", handleBalance);
-//
-// function handleBalance() {
-//   var balance = CheckingInput.val();
-//   CheckingBalance.text(balance);
-// }
-//
-// // function handleBalance() {
-// //   var CheckingBalance = parseInt($("#checking").children(".balance").val());
-// //   var NewAmount = CheckingBalance + CheckingInput;
-// //   console.log(NewAmount);
-// // }
-//
-//
-// // var NewAmount = CheckingBalance + CheckingInput;
+function handleSavings() {
+  savingsInput = parseInt($("#savings input.input").val());
+  newBalance = savingsInput;
+  savings = savings + newBalance;
+  savingsBalance.text("$" + savings);
+}
+
+function handleSavingsWithdraw() {
+   savingsInput = parseInt($("#savings input.input").val());
+   newBalance = savingsInput;
+   savings = (savings - newBalance); // make sure this only happens when enough $$
+    if (savings < 0) {
+      alert("Sorry. You don't have enough money.");
+    } else {
+      savingsBalance.text("$" + savings);
+  }
+}
+
+$("#checking .deposit").on("click", handleChecking);
+$("#checking .withdraw").on("click", handleCheckingWithdraw);
+$("#savings .deposit").on("click", handleSavings);
+$("#savings .withdraw").on("click", handleSavingsWithdraw);
