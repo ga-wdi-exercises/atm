9c77400d573a167d85209f6076014a314c1701d0
Chris Jaffier <chrisd@Chriss-MacBook-Pro.local>
2016-09-16 12:36:59 -0400

    Deposit logic added

--- atm.js
+++ atm.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
 });
+var balance = 0
+var deposit = 0
+var withdraw = 0
+
+$("div.balance").html(parseInt("0"));
+$(".deposit").on("click", addMoney);
+
+function addMoney (input) {
+var input = parseInt(($(".input").val()));
+balance = balance + input
+$(".balance").html(balance);
+
+}
