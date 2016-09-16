f8e5a85ec64fbca302fd4a41bae682f024db8503
Doray Gisele Owens <doray.owens@gmail.com>
2016-09-16 12:12:05 -0400

    deposit works

--- atm.js
+++ atm.js
@@ -1,11 +1,75 @@
 $(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
 });
-//variables I'll need: balance, amountWithdraw, amountDeposit,
+//variables
+var checkingInput = $("#checking .input")
+var checkingBalance = $("#checking .balance")
+var checkingDepositButton = $("#checking .deposit")
+var checkingWithdrawalButton = $("#checking .withdrawal")
 
-//When I click on Deposit, add value to checking total.
+var myBalance = 0;
 
-//When I click Withdraw, subtract value from checking total.
+checkingDepositButton.on("click", function(){
+  // get balance html from checkingBalance
+  //var balance = checkingBalance.html()
+
+
+
+  var input = checkingInput.val()
+  input = input.replace(/[^\d\.]/g, '');
+  console.log(input);
+  input = parseInt(input)
+
+  // newBalance = balance + checkingInput
+  myBalance = myBalance + input
+  //update checkingBalance html to newBalance
+  console.log(myBalance);
+
+  updateBalance(myBalance)
+})
+
+
+
+checkingWithdrawalButton.on("click", function(){
+  // get balance html from checkingBalance
+  // newBalance = balance - checkingInput
+
+  //update checkingBalance html to newBalance
+
+  var input = checkingInput.val()
+    input = input.replace(/[^\d\.]/g, '');
+    console.log(input);
+    input = parseInt(input)
+
+//newBalance = balance - checkingInput
+    myBalance = myBalance - input
+    //update checkingBalance html to newBalance
+    console.log(myBalance);
+
+    updateBalance(myBalance)
+})
+
+
+
+
+
+
+
+
+function updateBalance(newBalance){
+  checkingBalance.html('$' + newBalance)
+
+}
+
+
+
+
+
+
+//variables I'll need: balance, amount of Withdraw, amount of Deposit,
+
+//When I click on Deposit, add value to checking balance.
+
+//When I click Withdraw, subtract value from checking balance.
 
 //User can not withdraw more than available balance.
 
