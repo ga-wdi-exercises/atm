a9fc7c8de3512072563d85043acb87bfb9014fac
robert <robert.k25@gmail.com>
2016-10-12 11:33:34 -0400

    adds saving functionality

--- atm.js
+++ atm.js
@@ -7,14 +7,30 @@ $(document).ready(function(){
 
   var savingsDepositButton = $("#savings>input.deposit");
   var savingsWithdrawButton = $("#savings>input.withdraw");
+  var savingsDisplay =  $("#savings>div.balance");
+  var savingsInput = $("#savings>input.input");
 
   var checkingTotal = 0;
+  var savingsTotal = 0;
 
   checkingDepositButton.on("click",depositChecking);
   checkingWithdrawButton.on("click", withdrawChecking)
 
-  function withdrawChecking() {
+  savingsWithdrawButton.on("click", withdrawSavings);
+  savingsDepositButton.on("click", depositSavings)
 
+  function depositSavings() {
+    var amount = parseFloat(savingsInput.val());
+    amount.toFixed(2);
+    savingsTotal += amount;
+    updateTotal(savingsTotal, savingsDisplay);
+  }
+
+  function withdrawSavings(event) {
+    var amount = parseFloat(savingsInput.val());
+    amount.toFixed(2);
+    savingsTotal -= amount;
+    updateTotal(savingsTotal, savingsDisplay);
   }
 
   function depositChecking(event) {
