ca170fe10afa3f469f65e6842975c7cd9e3b965c
Rob Romaniw <robertromaniw@gmail.com>
2016-10-12 10:35:40 -0400

    Commit 1: Deposit Button displays input in console.log

--- atm.js
+++ atm.js
@@ -1,3 +1,12 @@
-$(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+// $("body").on("click", function() {
+//     $("body").css("background-color", "red");
+// });
+
+
+$("#checking .deposit").on("click", function() {
+    // event.preventDefault();
+    var input = $('#checking .input').select().val();
+    console.log(input);
+    // console.log("deposit button was clicked");
+
 });
