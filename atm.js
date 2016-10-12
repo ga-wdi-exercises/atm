a4ecc32cab3ec4e44aa4657d1df3643e2b343a56
Thinh Nguyen <nguyendthinh@outlook.com>
2016-10-12 13:51:28 -0400

    did the assignment

--- atm.js
+++ atm.js
@@ -1,3 +1,67 @@
 $(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+
+/* Checking */
+
+var checkingBalance = 0;
+
+var checkingDeposit = $("#checking .deposit");
+var displayCheckingBalance = $("#checking .balance");
+var checkingInput = $("#checking .input");
+
+checkingDeposit.on("click", depositCheckingMoney);
+
+function depositCheckingMoney() {
+  var checkingEntry = Number($(checkingInput).val());
+  checkingBalance += checkingEntry;
+  displayCheckingBalance.html("$" + Number(checkingBalance));
+  // displayCheckingBalance.val(checkingBalance);
+}
+
+var checkingWithdraw = $("#checking .withdraw");
+
+checkingWithdraw.on("click", withdrawCheckingMoney);
+
+function withdrawCheckingMoney() {
+  var checkingEntry = Number($(checkingInput).val());
+  checkingBalance -= checkingEntry;
+  displayCheckingBalance.html("$" + Number(checkingBalance));
+  displayCheckingBalance.val(checkingBalance);
+}
+
+/* Savings */
+
+var savingsBalance = 0;
+
+var savingsDeposit = $("#savings .deposit");
+var displaySavingsBalance = $("#savings .balance");
+var savingsInput = $("#savings .input");
+
+savingsDeposit.on("click", depositSavingsMoney);
+
+function depositSavingsMoney() {
+  var savingsEntry = Number($(savingsInput).val());
+  savingsBalance += savingsEntry;
+  displaySavingsBalance.html("$" + Number(savingsBalance));
+  displaySavingsBalance.val(savingsBalance);
+}
+
+var savingsWithdraw = $("#savings .withdraw");
+
+savingsWithdraw.on("click", withdrawSavingsMoney);
+
+function withdrawSavingsMoney() {
+  var savingsEntry = Number($(savingsInput).val());
+  savingsBalance -= savingsEntry;
+  displaySavingsBalance.html("$" + Number(savingsBalance));
+  displaySavingsBalance.val(savingsBalance);
+}
+
+// /* Bonus */
+//
+// if (checkingInput.val() > (Number(checkingBalance) + Number(withdrawBalance)) {
+//   alert("ERROR");
+// } else {
+//   alert("IT WORKS");
+// }
+
 });
