c8c8ad9e7b073b5540d6c83cd62d5a5190433653
Bradley Cravens <bradleycravens@gmail.com>
2016-10-12 12:04:14 -0400

    inital commit

--- atm.js
+++ atm.js
@@ -1,3 +1,35 @@
 $(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+var checkingBalance = 0;
+var savingBalance = 0;
+
+  function checkings (){
+    $("#checking .deposit").on("click", function(){
+    var userInput = Number($("#checking .input").val());
+    checkingBalance += userInput;
+    $("#checking .balance").html(checkingBalance);
+    });
+
+    $("#checking .withdraw").on("click", function(){
+    var userInput = Number($("#checking .input").val());
+    checkingBalance -= userInput;
+    $("#checking .balance").html(checkingBalance);
+    });
+  }
+
+  function savings (){
+    $("#savings .deposit").on("click", function(){
+    var userInput = Number($("#savings .input").val());
+    savingBalance += userInput;
+    $("#savings .balance").html(savingBalance);
+    });
+
+    $("#savings .withdraw").on("click", function(){
+    var userInput = Number($("#savings .input").val());
+    savingBalance -= userInput;
+    $("#savings .balance").html(savingBalance);
+    });
+  }
+
+checkings();
+savings();
 });
