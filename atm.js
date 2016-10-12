f094ce0412b0584beadcd0aafb55ab3efe051203
Robert Zehnder <robzcodes@gmail.com>
2016-10-12 11:12:38 -0400

    Adds deposit and withdraw buttons for checking an savings

--- atm.js
+++ atm.js
@@ -1,25 +1,60 @@
 $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-var depositBalance = 0;
+var savingsBalance = 0;
 var checkingBalance = 0;
 
 console.log("asfd");
 
-  //Step 2/3/4 - Click listener/Log User Input/Show user deposit
+  //Deposit money into Checking Account
   $("#checking .deposit").on('click', function (evt) {
     evt.preventDefault();
 
     var entry = Number($("#checking .input").val()); //Capture user entry, turns it into a number
     checkingBalance += entry; //Updates checkingBalance variable to hold for future
 
-    $('#checking .balance').html("$" + checkingBalance); //Updates HTML with entry + balance
+    $('#checking .balance').text("$" + checkingBalance); //Updates HTML with entry + balance
     $('#checking .balance').val(checkingBalance); //Updates Value of bank account with entry
-
-    
 
   })
 
-  //Step 4
+  //Deposit money into Savings Account
+  $("#savings .deposit").on('click', function (evt) {
+    evt.preventDefault();
+
+    var entry = Number($("#savings .input").val()); //Capture user entry, turns it into a number
+    savingsBalance += entry; //Updates savingsBalance variable to hold for future
+
+    $('#savings .balance').text("$" + savingsBalance); //Updates HTML with entry + balance
+    $('#savings .balance').val(savingsBalance); //Updates Value of bank account with entry
+
+  })
+
+  //Withdraw from checking account
+
+  $("#checking .withdraw").on('click', function (evt) {
+    evt.preventDefault();
+
+    var entry = Number($("#checking .input").val()); //Capture user entry, turns it into a number
+    checkingBalance -= entry; //Updates checkingBalance variable to hold for future
+
+    $('#checking .balance').text("$" + checkingBalance); //Updates HTML with entry + balance
+    $('#checking .balance').val(checkingBalance); //Updates Value of bank account with entry
+
+  })
+
+  //Withdraw from savings account
+
+  $("#savings .withdraw").on('click', function (evt) {
+    evt.preventDefault();
+
+    var entry = Number($("#savings .input").val()); //Capture user entry, turns it into a number
+    savingsBalance -= entry; //Updates checkingBalance variable to hold for future
+
+    $('#savings .balance').text("$" + savingsBalance); //Updates HTML with entry + balance
+    $('#savings .balance').val(savingsBalance); //Updates Value of bank account with entry
+
+  })
+
 
 
 });
