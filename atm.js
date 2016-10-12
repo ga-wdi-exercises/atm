53f5f8947224bea22841d635674eb8130d8caeef
Jonathan Reiser <jlr9v@virginia.edu>
2016-10-12 10:19:27 -0400

    turns body red

--- atm.js
+++ atm.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-console.log("hello world")
+$("body").css("background", "red")
 });
