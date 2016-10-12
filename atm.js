b1fbe8d95d643fb60f783d92ed32e65fc8579ae6
Robert Zehnder <robzcodes@gmail.com>
2016-10-12 10:19:49 -0400

    Verifies deposit button listener with console statement

--- atm.js
+++ atm.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
 console.log("asfd");
+
+  //Step 2 - Click listener
+  $(".deposit").on('click', function () {
+    console.log(23563456);
+  })
 });
