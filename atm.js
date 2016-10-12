89babef47655d16e2722115e36591db9427b2b14
Diana B. Gobar <dianabgobar@gmail.com>
2016-10-12 12:10:50 -0400

    deposit/withdraw done

--- atm.js
+++ atm.js
@@ -1,12 +1,31 @@
-console.log("hello");
 
-$("body").on("click", function(){
-  $("body").css("background-color", "red");
-});
-
-$("#checking .deposit").on("click", function(){
-  console.log("hello");
-  
-  var input = $("#checking .input").select().val();
-    console.log(input);
+$(document).ready(function(){
+var checkingBalance = 0;
+var savingBalance = 0;
+  function checkings (){
+    $("#checking .deposit").on("click", function(){
+    var userInput = Number($("#checking .input").val());
+    checkingBalance += userInput;
+    $("#checking .balance").html(checkingBalance);
+    });
+    $("#checking .withdraw").on("click", function(){
+    var userInput = Number($("#checking .input").val());
+    checkingBalance -= userInput;
+    $("#checking .balance").html(checkingBalance);
+    });
+  }
+  function savings (){
+    $("#savings .deposit").on("click", function(){
+    var userInput = Number($("#savings .input").val());
+    savingBalance += userInput;
+    $("#savings .balance").html(savingBalance);
+    });
+    $("#savings .withdraw").on("click", function(){
+    var userInput = Number($("#savings .input").val());
+    savingBalance -= userInput;
+    $("#savings .balance").html(savingBalance);
+    });
+  }
+checkings();
+savings();
 });
