5fbf28e4644800d234dac96d406fee62c8e75fed
Alex Wandres <alex.r.wandres@gmail.com>
2016-10-12 13:27:56 -0400

    deposit functionality

--- atm.js
+++ atm.js
@@ -1,3 +1,154 @@
 $(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+
+var depositChecking = $("#checking .deposit")
+var withdrawChecking = $("#checking .withdraw")
+var depositSavings = $("#savings .deposit")
+var withdrawSavings = $("#savings .withdraw")
+var inputChecking = $("#checking .input")
+var inputSavings = $("#savings .input")
+var balanceDisplayC = $("#checking .balance")
+var balanceDisplayS = $("#savings .balance")
+var checkingBalance = 0
+var checkingTotal
+var savingsBalance
+var depositValue
+
+
+depositChecking.on("click", function (){
+  if (isNaN(inputChecking.val())) {
+    console.log("error")
+  } else {
+    var depositValue = inputChecking.val();
+    checkingTotal = parseInt(depositValue) + parseInt(checkingBalance)
+    checkingBalance = checkingTotal
+    balanceDisplayC.text('$' + checkingTotal)
+    console.log(checkingBalance)
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
