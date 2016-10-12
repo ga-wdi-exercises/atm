b5dd440038d4607b3491623b0b254b687af4a434
Jonathan Reiser <jlr9v@virginia.edu>
2016-10-12 12:51:37 -0400

    Final Draft

--- atm.js
+++ atm.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-var currentBalance = 0;
+var checkingBalance = 0;
+var savingsBalance = 0;
 //step 1
 $("body").css("background", "red")
 
@@ -8,13 +9,46 @@ $("body").css("background", "red")
 $("#checking .deposit").on("click", function(){
 
   var entry = Number($("#checking .input").val());
-  currentBalance += entry;
-
-  $("#checking .balance").html( "$" + currentBalance);
-  $("#checking .balance").val(currentBalance);
-
-  //currentBalance = $("#checking .balance").val();
-  console.log(currentBalance);
+  checkingBalance += entry;
+  $("#checking .balance").text( "$" + checkingBalance);
+  $("#checking .balance").val(checkingBalance);
 })
+
+//checking withdrawal
+$("#checking .withdraw").on("click", function(){
+  var entry = Number($("#checking .input").val());
+  checkingBalance -= entry;
+  overdraft();
+})
+
+//same but with savings
+$("#savings .deposit").on("click", function(){
+  var entry = Number($("#savings .input").val());
+  savingsBalance += entry;
+  $("#savings .balance").text( "$" + savingsBalance);
+  $("#savings .balance").val(savingsBalance);
+})
+
+//step 6
+$("#savings .withdraw").on("click", function(){
+
+  var entry = Number($("#savings .input").val());
+  savingsBalance -= entry;
+  $("#savings .balance").text( "$" + savingsBalance);
+  $("#savings .balance").val(savingsBalance);
+})
+
+//overdraft protection
+function overdraft(){
+if (checkingBalance < 0){
+  console.log("overdraft")
+  savingsBalance += checkingBalance;
+  checkingBalance = 0;
+}
+  $("#savings .balance").text( "$" + savingsBalance);
+  $("#savings .balance").val(savingsBalance);
+  $("#checking .balance").text( "$" + checkingBalance);
+  $("#checking .balance").val(checkingBalance);
+}
 
 });
