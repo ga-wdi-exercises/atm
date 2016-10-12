1a5e19f9ec59f3d4415674e708b3a57ca394e0a2
Jonathan Reiser <jlr9v@virginia.edu>
2016-10-12 10:32:05 -0400

    updates user input on ddeposit into balence

--- atm.js
+++ atm.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+var balence = 0;
+//step 1
 $("body").css("background", "red")
+
+//step 2/3
 $("#checking .deposit").on("click", function(){
-  console.log("test")
+  balence = $("#checking input").val()
+  console.log(balence);
 })
+
 });
