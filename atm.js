e149ab633981d5410a3fbe1a9743ea6178122bff
Robert Zehnder <robzcodes@gmail.com>
2016-10-12 10:37:26 -0400

    Adds update balance functionality

--- atm.js
+++ atm.js
@@ -1,11 +1,18 @@
 $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+var depositBalance = 0;
+var checkingBalance = 0;
+
 console.log("asfd");
 
   //Step 2/3 - Click listener/Log User Input
   $("#checking .deposit").on('click', function () {
-    console.log($("#checking input").val());
+
+    checkingBalance = $("#checking .input").val();
+    $('#checking .balance').html("$" + checkingBalance);
   })
 
   //Step 4
+
+
 });
