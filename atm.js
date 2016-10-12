aa8ef226ad77546f5957365f40fbcf49b296bafb
Sam Catherman <Sam.Catherman@gmail.com>
2016-10-12 11:06:13 -0400

    updates checking balance by adding entries

--- atm.js
+++ atm.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
 
 var checkingBalance = 0;
-
+//when clicked
   $("#checking .deposit").on("click", function(){
-    checkingBalance = $("#checking .input").val();
+//store user input to variable entry
+  var entry = Number($("#checking .input").val()); //listen for user input
+    checkingBalance += entry;
+    //display entry to the checking account div
     $("#checking .balance").html("$" + checkingBalance);
+    //updates value of checkingBalance
+    $("#checking .balance").val(checkingBalance);
+    console.log(checkingBalance);
   })
 });
