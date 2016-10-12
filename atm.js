64df45faff090cf2f0fec8a803cd8719070dfc8e
Rob Romaniw <robertromaniw@gmail.com>
2016-10-12 12:02:07 -0400

    Deposit / Withdraw works on Checking / Savings

--- atm.js
+++ atm.js
@@ -1,12 +1,54 @@
+$( document ).ready(function() {
+
+
+// Ensure JQuery Links
 // $("body").on("click", function() {
 //     $("body").css("background-color", "red");
 // });
 
+var checkingBalance = 0;
+var savingsBalance = 0;
 
-$("#checking .deposit").on("click", function() {
-    // event.preventDefault();
-    var input = $('#checking .input').select().val();
-    console.log(input);
-    // console.log("deposit button was clicked");
+//Deposit money into Checking Account
+$("#checking .deposit").on("click", function(evt) {
+    evt.preventDefault();
+    var input = Number($('#checking .input').val()); //Captures user input, turns it into a number
+    checkingBalance += input; // Updates checkingBalance variable
+    $("#checking .balance").html("$ "+ checkingBalance) //Updates HTML with Balance
+});
+
+//Widthdraw money from Checking Account
+$("#checking .withdraw").on("click", function(evt) {
+    evt.preventDefault();
+    var input = Number($('#checking .input').val()); //Captures user input, turns it into a number
+    checkingBalance -= input; // Updates checkingBalance variable
+    $("#checking .balance").html(checkingBalance) //Updates HTML with Balance
+});
+
+//Deposit money into Savings Account
+$("#savings .deposit").on("click", function(evt) {
+    evt.preventDefault();
+    var input = Number($('#savings .input').val()); //Captures user input, turns it into a number
+    savingsBalance += input; // Updates savingsBalance variable
+    $("#savings .balance").html(savingsBalance) //Updates HTML with Balance
+});
+
+//Widthdraw money from Savings Account
+$("#savings .withdraw").on("click", function(evt) {
+    evt.preventDefault();
+    var input = Number($('#savings .input').val()); //Captures user input, turns it into a number
+    savingsBalance -= input; // Updates savingsBalance variable
+    $("#savings .balance").html(savingsBalance) //Updates HTML with Balance
+});
+
+
+
+
+
 
 });
+
+//   $('#checking .balance').text("$" + checkingBalance); //Updates HTML with entry + balance
+//   $('#checking .balance').val(checkingBalance); //Updates Value of bank account with entry
+// })
+//
