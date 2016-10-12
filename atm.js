650d074688a98573aa1bc3d2aa431d4d662fddef
robert <robert.k25@gmail.com>
2016-10-12 11:00:19 -0400

    adds deposit functionality to checking account

--- atm.js
+++ atm.js
@@ -6,12 +6,23 @@ $(document).ready(function(){
   var savingsDepositButton = $("#savings>input.deposit");
   var savingsWithdrawButton = $("#savings>input.withdraw");
 
+  var checkingTotal = 0;
+
+  checkingDepositButton.on("click",depositChecking);
+
   function withdrawChecking() {
 
   }
 
-  function depositChecking() {
+  function depositChecking(event) {
+    var amount = parseFloat($("#checking>input.input").val());
+    checkingTotal += amount;
+    updateTotal(checkingTotal,"#checking>.balance");
+    }
 
-  }
+    function updateTotal(amount, displayLocator) {
+      $(displayLocator).empty();
+      $(displayLocator).text(`\$${amount}`);
+    }
 
 });
