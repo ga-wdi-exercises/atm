b9f622c3e6e76cba3db2dad2c5e953abf08d46c1
Jonathan Reiser <jlr9v@virginia.edu>
2016-10-12 10:25:06 -0400

    adds event listener to checkings deposit button

--- atm.js
+++ atm.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
 $("body").css("background", "red")
+$("#checking .deposit").on("click", function(){
+  console.log("test")
+})
 });
