ec13c371f4384886073216d9dc189ef18da175e2
Robert Zehnder <robzcodes@gmail.com>
2016-10-12 11:04:30 -0400

    Step 5 - Updates the balance with the user input

--- atm.js
+++ atm.js
@@ -5,11 +5,18 @@ var checkingBalance = 0;
 
 console.log("asfd");
 
-  //Step 2/3 - Click listener/Log User Input
-  $("#checking .deposit").on('click', function () {
+  //Step 2/3/4 - Click listener/Log User Input/Show user deposit
+  $("#checking .deposit").on('click', function (evt) {
+    evt.preventDefault();
 
-    checkingBalance = $("#checking .input").val();
-    $('#checking .balance').html("$" + checkingBalance);
+    var entry = Number($("#checking .input").val()); //Capture user entry, turns it into a number
+    checkingBalance += entry; //Updates checkingBalance variable to hold for future
+
+    $('#checking .balance').html("$" + checkingBalance); //Updates HTML with entry + balance
+    $('#checking .balance').val(checkingBalance); //Updates Value of bank account with entry
+
+    
+
   })
 
   //Step 4
