c67c0713f06b8fc398bc192b0f10a7bf10f51bcd
Jonathan Reiser <jlr9v@virginia.edu>
2016-10-12 11:04:25 -0400

    updates balance with user input 2

--- atm.js
+++ atm.js
@@ -1,13 +1,20 @@
 $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-var checkingBalance = 0;
+var currentBalance = 0;
 //step 1
 $("body").css("background", "red")
 
 //step 2/3
 $("#checking .deposit").on("click", function(){
-  checkingBalance = $("#checking .input").val();
-  $("#checking .balance").html( "$" + checkingBalance);
+
+  var entry = Number($("#checking .input").val());
+  currentBalance += entry;
+
+  $("#checking .balance").html( "$" + currentBalance);
+  $("#checking .balance").val(currentBalance);
+
+  //currentBalance = $("#checking .balance").val();
+  console.log(currentBalance);
 })
 
 });
