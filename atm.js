bb1714bd23cf0f4106ca5d918712164f804e2dc7
Sam Catherman <Sam.Catherman@gmail.com>
2016-10-12 13:22:52 -0400

    adds overdraft function

--- atm.js
+++ atm.js
@@ -12,7 +12,7 @@ var savingsBalance = 0;
     $("#checking .balance").html("$" + checkingBalance);
     //updates value of checkingBalance
     $("#checking .balance").val(checkingBalance);
-    console.log(checkingBalance);
+    // console.log(checkingBalance);
   })
   //adds deposits to savings account
   $("#savings .deposit").on("click", function(){
@@ -28,8 +28,10 @@ var savingsBalance = 0;
       checkingBalance -= checkingWithdrawal;
       $("#checking .balance").html("$" + checkingBalance);
       $("#checking .balance").val(checkingBalance);
-      console.log(checkingBalance);
+      // console.log(checkingBalance);
+      overDraft();
   })
+
   //withdraws from savings account
   $("#savings .withdraw").on("click", function(){
     var savingsWithdrawal = Number($("#savings .input").val());
@@ -38,4 +40,22 @@ var savingsBalance = 0;
       $("#savings .balance").val(savingsBalance);
       console.log(savingsBalance);
   })
+
+  //overdraft protection
+  function overDraft(){
+    var withdrawal = Number($("#checking .input").val());
+  if (checkingBalance < withdrawal){
+      savingsBalance += checkingBalance;
+      checkingBalance = 0;
+      console.log(savingsBalance);
+      $("#checking .balance").html("$" + checkingBalance);
+      $("#checking .balance").val(checkingBalance);
+
+      $("#savings .balance").html("$" + savingsBalance);
+      $("#savings .balance").val(savingsBalance);
+      return;
+  } else if ((checkingBalance + savingsBalance) < withdrawal){
+     alert("insufficient funds to make this transaction. Please try a different amount");
+  }
+  }
 });
