90346c8da401fc63f3e3c2e3f972bead55656495
Rob Romaniw <robertromaniw@gmail.com>
2016-10-13 10:20:24 -0400

    Commit 3: Balances cannot drop below -bash

--- atm.js
+++ atm.js
@@ -1,54 +1,62 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 
 
-// Ensure JQuery Links
-// $("body").on("click", function() {
-//     $("body").css("background-color", "red");
-// });
+    // Ensure JQuery Links
+    // $("body").on("click", function() {
+    //     $("body").css("background-color", "red");
+    // });
 
-var checkingBalance = 0;
-var savingsBalance = 0;
+    var checkingBalance = 0;
+    var savingsBalance = 0;
 
-//Deposit money into Checking Account
-$("#checking .deposit").on("click", function(evt) {
-    evt.preventDefault();
-    var input = Number($('#checking .input').val()); //Captures user input, turns it into a number
-    checkingBalance += input; // Updates checkingBalance variable
-    $("#checking .balance").html("$ "+ checkingBalance) //Updates HTML with Balance
+    //Deposit money into Checking Account
+    function checkingDeposit() {
+        $("#checking .deposit").on("click", function(evt) {
+            evt.preventDefault();
+            var input = Number($('#checking .input').val()); //Captures user input, turns it into a number
+            checkingBalance += input; // Updates checkingBalance variable
+            $("#checking .balance").html("$ " + checkingBalance); //Updates HTML with Balance
+        });
+    }
+    //Widthdraw money from Checking Account
+    function checkingWithdrawl() {
+        $("#checking .withdraw").on("click", function(evt) {
+            evt.preventDefault();
+            var input = Number($('#checking .input').val()); //Captures user input, turns it into a number
+            if (checkingBalance >= input) {
+            checkingBalance -= input; // Updates checkingBalance variable
+            $("#checking .balance").html("$ " + checkingBalance); //Updates HTML with Balance
+          } else {
+            alert ("Cannot perform function: Withdrawl amount exceeds balance");
+          }
+        });
+    }
+    //Deposit money into Savings Account
+    function savingsDeposit() {
+        $("#savings .deposit").on("click", function(evt) {
+            evt.preventDefault();
+            var input = Number($('#savings .input').val()); //Captures user input, turns it into a number
+            savingsBalance += input; // Updates savingsBalance variable
+            $("#savings .balance").html("$ " + savingsBalance); //Updates HTML with Balance
+        });
+    }
+    //Widthdraw money from Savings Account
+    function savingsWithdrawl() {
+        $("#savings .withdraw").on("click", function(evt) {
+            evt.preventDefault();
+            var input = Number($('#savings .input').val()); //Captures user input, turns it into a number
+            if (savingsBalance >= input) {
+            savingsBalance -= input; // Updates savingsBalance variable
+            $("#savings .balance").html("$ " + savingsBalance); //Updates HTML with Balance
+          } else {
+            alert("Cannot perform function: Withdrawl amount exceeds balance");
+          }
+        });
+    }
+
+checkingDeposit();
+checkingWithdrawl();
+savingsDeposit();
+savingsWithdrawl();
+
 });
-
-//Widthdraw money from Checking Account
-$("#checking .withdraw").on("click", function(evt) {
-    evt.preventDefault();
-    var input = Number($('#checking .input').val()); //Captures user input, turns it into a number
-    checkingBalance -= input; // Updates checkingBalance variable
-    $("#checking .balance").html(checkingBalance) //Updates HTML with Balance
-});
-
-//Deposit money into Savings Account
-$("#savings .deposit").on("click", function(evt) {
-    evt.preventDefault();
-    var input = Number($('#savings .input').val()); //Captures user input, turns it into a number
-    savingsBalance += input; // Updates savingsBalance variable
-    $("#savings .balance").html(savingsBalance) //Updates HTML with Balance
-});
-
-//Widthdraw money from Savings Account
-$("#savings .withdraw").on("click", function(evt) {
-    evt.preventDefault();
-    var input = Number($('#savings .input').val()); //Captures user input, turns it into a number
-    savingsBalance -= input; // Updates savingsBalance variable
-    $("#savings .balance").html(savingsBalance) //Updates HTML with Balance
-});
-
-
-
-
-
-
-});
-
-//   $('#checking .balance').text("$" + checkingBalance); //Updates HTML with entry + balance
-//   $('#checking .balance').val(checkingBalance); //Updates Value of bank account with entry
-// })
-//
