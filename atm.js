3e03bd41f26656d6eee107ac3e4f74a57aabeba9
Bradley Cravens <bradleycravens@gmail.com>
2016-10-13 11:22:32 -0400

    commit

--- atm.js
+++ atm.js
@@ -1,35 +1,55 @@
 $(document).ready(function(){
-var checkingBalance = 0;
-var savingBalance = 0;
+savingBalance = 0;
+checkingBalance = 0;
 
-  function checkings (){
+  function checkingDeposit() {
     $("#checking .deposit").on("click", function(){
     var userInput = Number($("#checking .input").val());
     checkingBalance += userInput;
-    $("#checking .balance").html(checkingBalance);
-    });
-
-    $("#checking .withdraw").on("click", function(){
-    var userInput = Number($("#checking .input").val());
-    checkingBalance -= userInput;
-    $("#checking .balance").html(checkingBalance);
+    $("#checking .balance").html("$"+checkingBalance);
     });
   }
 
-  function savings (){
+  $("#checking .balance").html("$"+checkingBalance);
+
+  function checkingWithdrawl() {
+    $("#checking .withdraw").on("click", function(){
+    var userInput = Number($("#checking .input").val());
+      if (userInput > savingBalance + checkingBalance) {
+        alert("Out of money dawg");
+      } else if (checkingBalance - userInput < 0) {
+        var overdraw = checkingBalance - userInput;
+        savingBalance = savingBalance + overdraw;
+        checkingBalance = checkingBalance - overdraw - userInput;
+        $("#checking .balance").html("$"+checkingBalance);
+        $("#savings .balance").html("$"+savingBalance);
+      } else {
+        checkingBalance = checkingBalance - userInput;
+        $("#checking .balance").html("$"+checkingBalance);
+      }
+    });
+  }
+  function savingDeposit() {
     $("#savings .deposit").on("click", function(){
     var userInput = Number($("#savings .input").val());
     savingBalance += userInput;
-    $("#savings .balance").html(savingBalance);
+    $("#savings .balance").html("$"+savingBalance);
     });
-
+  }
+  function savingWithdraw() {
     $("#savings .withdraw").on("click", function(){
     var userInput = Number($("#savings .input").val());
-    savingBalance -= userInput;
-    $("#savings .balance").html(savingBalance);
+    if (savingBalance >= userInput) {
+      savingBalance -= userInput;
+      $("#savings .balance").html("$"+savingBalance);
+    } else {
+      alert("Out of money dawg");
+    }
     });
   }
 
-checkings();
-savings();
+checkingDeposit();
+checkingWithdrawl();
+savingDeposit();
+savingWithdraw();
 });
