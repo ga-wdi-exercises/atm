14cc73db1ef49c902b5dab55faf6d7f79d4cdf21
Diana B. Gobar <dianabgobar@gmail.com>
2016-10-13 10:27:17 -0400

    added if-else, can't go below zero

--- atm.js
+++ atm.js
@@ -1,31 +1,44 @@
-
 $(document).ready(function(){
+
 var checkingBalance = 0;
 var savingBalance = 0;
-  function checkings (){
+
+  function checkingsDeposit (){
     $("#checking .deposit").on("click", function(){
     var userInput = Number($("#checking .input").val());
     checkingBalance += userInput;
-    $("#checking .balance").html(checkingBalance);
-    });
-    $("#checking .withdraw").on("click", function(){
-    var userInput = Number($("#checking .input").val());
-    checkingBalance -= userInput;
-    $("#checking .balance").html(checkingBalance);
+    $("#checking .balance").html("$"+checkingBalance);
     });
   }
-  function savings (){
+  function checkingsWithdrawl(){
+    $("#checking .withdraw").on("click", function(){
+    var userInput = Number($("#checking .input").val());
+      if (checkingBalance >= userInput){
+        checkingBalance -= userInput;
+        $("#checking .balance").html("$"+checkingBalance);
+      } else {
+        alert ("no monayyyyy");
+      }
+    });
+  }
+
+  function savingsDeposit (){
     $("#savings .deposit").on("click", function(){
     var userInput = Number($("#savings .input").val());
     savingBalance += userInput;
-    $("#savings .balance").html(savingBalance);
+    $("#savings .balance").html("$"+savingBalance);
     });
+  function savingsWithdrawl (){
     $("#savings .withdraw").on("click", function(){
     var userInput = Number($("#savings .input").val());
     savingBalance -= userInput;
-    $("#savings .balance").html(savingBalance);
+    $("#savings .balance").html("$"+savingBalance);
     });
   }
-checkings();
-savings();
+}
+
+checkingsDeposit();
+checkingsWithdrawl();
+savingsDeposit();
+savingsWithdrawl();
 });
