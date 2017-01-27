5f57ce121c41c614c6f979cc2e5feab7772bd54b
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-01-27 12:52:35 -0500

    implemented more functionality

--- atm.js
+++ atm.js
@@ -1,32 +1,59 @@
 var checkingBalance = 0;
 var savingsBalance = 0;
 
+checkZero();
+saveZero();
+
+function checkZero(){
+  if(+checkingBalance == 0){
+    $("#checking").addClass("zero");
+  }else if(+checkingBalance > 0){
+    $("#checking").removeClass("zero");
+  }
+}
+
+function saveZero(){
+  if(+savingsBalance == "0"){
+    $("#savings").addClass("zero");
+  }else if(+savingsBalance > "0"){
+    $("#savings").removeClass("zero");
+  }
+}
+
 $(document).ready(function(){
 
   $("#checking > .deposit").on("click", function(){
     var checkingInput = $("#checking > .input").val()
     checkingBalance = +checkingInput + +checkingBalance;
     $("#checking > .balance").text("$" + checkingBalance);
-
+    checkZero();
     });
 
   $("#checking > .withdraw").on("click", function(){
     var checkingInput = $("#checking > .input").val()
+    if(+checkingInput > +checkingBalance){
+      alert("You cannot withdraw this amount.")
+    }else{
     checkingBalance = +checkingBalance - +checkingInput;
-    $("#checking > .balance").text("$" + checkingBalance);
+    $("#checking > .balance").text("$" + checkingBalance);}
+    checkZero();
   });
 
   $("#savings > .deposit").on("click", function(){
     var savingsInput = $("#savings > .input").val()
     savingsBalance = +savingsInput + +savingsBalance;
     $("#savings > .balance").text("$" + savingsBalance);
-
+    saveZero();
     });
 
-    $("#savings > .withdraw").on("click", function(){
-      var savingsInput = $("#savings > .input").val()
-      savingsBalance = +savingsBalance - +savingsInput;
-      $("#savings > .balance").text("$" + savingsBalance);
+  $("#savings > .withdraw").on("click", function(){
+    var savingsInput = $("#savings > .input").val()
+    if(+savingsInput > +savingsBalance){
+      alert("You cannot withdraw this amount.")
+    }else{
+    savingsBalance = +savingsBalance - +savingsInput;
+    $("#savings > .balance").text("$" + savingsBalance);}
+    saveZero();
     });
 
 
