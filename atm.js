14cfc033d85244e3fff13d8ed1c00ea72191f311
Samuel Clements <sdkclements@gmail.com>
2017-01-27 13:27:41 -0500

    added my psuedo code and refactoring ideas and examples of them

--- atm.js
+++ atm.js
@@ -1,3 +1,28 @@
+//set up click responses for button clicks
+// write 4 functions check withdraw/deposit / savings widthdraw/deposit
+//each function should take in both the number they typed in and the current balance
+
+
+
+
+
+
+// ------- refactoring   -------
+ // single function to update balance anytime a button is pressed
+ // function updateBalance(){
+ //    curentballance = parseInt($(this).siblings('.balance').html().substring(1))
+ //    inputVal = parseInt($(this).siblings('.input').val())
+
+
+ //    }
+ 	//change the number to red if its 0;
+ // function checkforZero(){
+	
+// }
+
+
+// ---------- original -----------------
+
 $(document).ready(function() {
     console.log('jquery loaded');
     $('#checkD').click(function(){
@@ -17,25 +42,7 @@ $(document).ready(function() {
  
 });
 
-// var bal = $('#checkbal').text();
-// var ballance = bal.slice(1)
 
-
-// function checkvalues(ele){
-// 	$(ele).val();
-// }
-
-// var checkings = {
-// 	oldval: $('#inputCheck').val(),
-// 	newVal: $()
-// }
-// function CheckWithdrawOk(){
-// 	valToWithdraw = 
-// 	if (valToWithdraw > currentBal)
-// 		alert('you cannot take out that much');
-// }
-
-// basic adding to account balance
 function withdrawChecking(){
 	bal = $('#checkbal').text();
 	ballance = parseInt(bal)
@@ -71,13 +78,12 @@ function withdrawSavings(){
 function updateCheckBalance(){
 	bal = $('#checkbal').text();
 	ballance = parseInt(bal)
-	console.log(ballance);
-	// ballance = bal.slice(1);
+	
 	valTo = $('#inputCheck').val();
 	valtoadd = parseInt(valTo)
-	console.log(valtoadd)
+	
 	updated = (ballance + valtoadd);
-	console.log(updated);
+	
 	$('#checkbal').text(updated);
 
 
@@ -100,5 +106,35 @@ function updateSavingsBalance(){
 }
 
 
+// var bal = $('#checkbal').text();
+// var ballance = bal.slice(1)
+
+
+// function checkvalues(ele){
+// 	$(ele).val();
+// }
+
+// var checkings = {
+// 	oldval: $('#inputCheck').val(),
+// 	newVal: $()
+// }
+// function CheckWithdrawOk(){
+// 	valToWithdraw = 
+// 	if (valToWithdraw > currentBal)
+// 		alert('you cannot take out that much');
+// }
+
+// basic adding to account balance
+
+
+// function prependWith(){
+
+// }
+
+// var balance = parseInt($(this).siblings('.balance').html().substring(1);
+//     balance = parseInt(balance);
+
+
+   
 
 // var bal = $('#checkbal').text('$100');
