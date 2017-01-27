864cf3323df96182407a758498316f0e7055623e
Dominick Houston <dominick.houston14@gmail.com>
2017-01-27 10:59:59 -0500

    Commit 2 Input value

--- atm.js
+++ atm.js
@@ -1,11 +1,14 @@
+amount = $("input").val();
+
 $(document).ready(function(){
   $("body").css("background", "orange")
 });
 
 $(".deposit").on("click", function() {
-  console.log("hello");
-  console.log("dom");
+  var amount = $("input").val();
+  console.log(amount);
 })
+
 
 // $("button").on("click", function () {
 //   console.log("dom");
