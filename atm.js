2cc515c1c70fa25409fabec8d8fa3e3755ebab65
Bill Bobbitt <wmbobbitt@gmail.com>
2017-01-27 11:43:38 -0500

    withdraw out of savings works for checking

--- atm.js
+++ atm.js
@@ -14,16 +14,34 @@ $('#checking .withdraw').on("click", function() {
     checkingBalance -= parseInt($('#checking .input').val());
     console.log(checkingBalance)
     $('#checking .balance').html(`$${checkingBalance}`)
-  } else {
-    var checkingDwindle = prompt(`You only have $${checkingBalance} left. Would you like to take the rest? Y/N`)
+  } else if (savingsBalance >= (parseInt($('#checking .input').val()) - checkingBalance)) {
+      var checkingDwindle = prompt(`You only have $${checkingBalance} left. Would you like to take the rest from your Savings Account? Y/N`)
       if (checkingDwindle === "Y") {
+        savingsBalance = savingsBalance - (parseInt($('#checking .input').val()) - checkingBalance);
         checkingBalance = 0;
         console.log(checkingBalance)
         $('#checking').addClass("zero")
         $('#checking .balance').html(`$${checkingBalance}`)
+        if (savingsBalance === 0) {
+          $('#savings').addClass("zero")
+          $('#savings .balance').html(`$${savingsBalance}`)
+        }
       } else {
         alert('Thank you, come again!')
       }
+  } else {
+    var checkingDrain = prompt(`You only have $${checkingBalance} in Checking and $${savingsBalance} in Savings. Would you like to empty both? Y/N`)
+    if (checkingDrain === "Y") {
+      checkingBalance = 0;
+      savingsBalance = 0;
+      console.log(checkingBalance)
+      $('#checking').addClass("zero")
+      $('#checking .balance').html(`$${checkingBalance}`)
+      $('#savings').addClass("zero")
+      $('#savings .balance').html(`$${savingsBalance}`)
+    } else {
+      alert('Thank you, come again!')
+    }
   }
 
 })
@@ -40,7 +58,7 @@ $('#savings .withdraw').on("click", function() {
     console.log(savingsBalance)
     $('#savings .balance').html(`$${savingsBalance}`)
   } else {
-    var savingsDwindle = prompt(`You only have $${savingsBalance} left. Would you like to take the rest? Y/N`)
+    var savingsDwindle = prompt(`You only have $${savingsBalance} left. Would you like to take the rest from your Checking Account? Y/N`)
       if (savingsDwindle === "Y") {
         savingsBalance = 0;
         console.log(savingsBalance)
