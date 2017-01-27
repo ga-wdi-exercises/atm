2b1b5cbb448f8354a6305f12616adee2ba51fdef
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-01-27 11:50:11 -0500

    checking and savings accounts made to work

--- atm.js
+++ atm.js
@@ -1,3 +1,33 @@
+var checkingBalance = 0;
+var savingsBalance = 0;
+
 $(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+
+  $("#checking > .deposit").on("click", function(){
+    var checkingInput = $("#checking > .input").val()
+    checkingBalance = +checkingInput + +checkingBalance;
+    $("#checking > .balance").text("$" + checkingBalance);
+
+    });
+
+  $("#checking > .withdraw").on("click", function(){
+    var checkingInput = $("#checking > .input").val()
+    checkingBalance = +checkingBalance - +checkingInput;
+    $("#checking > .balance").text("$" + checkingBalance);
+  });
+
+  $("#savings > .deposit").on("click", function(){
+    var savingsInput = $("#savings > .input").val()
+    savingsBalance = +savingsInput + +savingsBalance;
+    $("#savings > .balance").text("$" + savingsBalance);
+
+    });
+
+    $("#savings > .withdraw").on("click", function(){
+      var savingsInput = $("#savings > .input").val()
+      savingsBalance = +savingsBalance - +savingsInput;
+      $("#savings > .balance").text("$" + savingsBalance);
+    });
+
+
 });
