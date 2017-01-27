8c0ffc0c2d65ce69a7cc6cb8adad1f429ae7d996
Ebenezer Williams <ebenezerw@me.com>
2017-01-27 11:51:37 -0500

    step 4

--- atm.js
+++ atm.js
@@ -1,7 +1,14 @@
+var amount = $("input").val();
+
+
 $(document).ready(function(){
 	$("body").css("background", "red")
 });
 
 $("#checking").children(".deposit").on("click", function () {
 	console.log($("input").val());
-} )
+} );
+
+$("#checking").children(".deposit").on("click", function () {
+	$(".checkingBal").html("$" + amount);
+} );
