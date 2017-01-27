684d026ee8df4cbc7ed16d15282b9f16b839e932
Nairul <awhitehurst4@gmail.com>
2017-01-27 15:25:35 -0500

    initial commit

--- atm.js
+++ atm.js
@@ -1,3 +1,67 @@
-$(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-});
+var checkingBalance = 0;
+var savingsBalance = 0;
+//Deposit into Checking
+$("#checking .deposit").on("click", function(){
+  var checkingInput = $("#checking .input").val()
+  checkingBalance = +checkingInput + +checkingBalance;
+  $("#checking .balance").html("$" + checkingBalance);
+  if (checkingBalance === 0) {
+    $("#checking").css("background-color", "red");
+  }
+  else if (checkingBalance > 0) {
+      $("#checking").css("background-color", "green");
+    }
+  if (checkingBalance < 0) {
+      $("#checking .balance").html("$" + 0);
+    }
+})
+//Withdraw from Checking
+$("#checking .withdraw").on("click", function(){
+  var checkingInput = $("#checking .input").val()
+  checkingBalance = +checkingBalance - +checkingInput;
+  $("#checking .balance").html("$" + checkingBalance);
+  if (checkingBalance === 0) {
+    $("#checking").css("background-color", "red");
+  }
+  else if (checkingBalance > 0) {
+      $("#checking").css("background-color", "green");
+    }
+  if (checkingBalance < 0) {
+      $("#checking .balance").html("$" + 0);
+    }
+})
+//Deposit into Savings
+$("#savings .deposit").on("click", function(){
+  var savingsInput = $("#savings .input").val()
+  savingsBalance = +savingsBalance + +savingsInput;
+  $("#savings .balance").html("$" + savingsBalance);
+  if (savingsBalance === 0) {
+    $("#savings").css("background-color", "red");
+  }
+  else if (savingsBalance > 0) {
+      $("#savings").css("background-color", "green");
+    }
+    if (savingsBalance < 0) {
+      $("#savings .balance").html("$" + 0);
+    }
+})
+//Withdraw from Savings
+$("#savings .withdraw").on("click", function(){
+  var savingsInput = $("#savings .input").val()
+  savingsBalance = +savingsBalance - +savingsInput;
+  $("#savings .balance").html("$" + savingsBalance);
+  if (savingsBalance === 0) {
+    $("#savings").css("background-color", "red");
+    }
+  else if (savingsBalance > 0) {
+      $("#savings").css("background-color", "green");
+    }
+  if (savingsBalance < 0) {
+    $("#savings .balance").html("$" + 0);
+    }
+})
+
+//var savingsInput = $("#savings .input").val()
+//if (savingsInput == 5) {
+//  console.log("Input is $5");
+//}
