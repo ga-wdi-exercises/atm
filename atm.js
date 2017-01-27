25b95131b3fa56a79011d1d6f5b42e64d0fb51a1
Will Hawkins <willhawkins123@yahoo.com>
2017-01-27 10:48:54 -0500

    Checking Deposit Replaces Checking Balance

--- atm.js
+++ atm.js
@@ -7,8 +7,12 @@ $("#checking .input").val("Something");
 //Create Checking Input Variable
 var checkingInput;
 
-//Upon Clicking, grab Checking Input and Console Log It
+//Create Checking Balance Variable
+var checkingBalance = $("#checking .balance").html();
+
+//Upon Clicking, grab Checking Input, Console Log It, Make That Checking Balance
 $("#checking .deposit").on("click", function(){
   checkingInput = $("#checking .input").val();
   console.log(checkingInput);
+  checkingBalance = $("#checking .balance").html("$ " + checkingInput);
 })
