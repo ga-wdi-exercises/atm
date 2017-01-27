5cf3075f6900cb5e88c6beabdb7c08bc189125c8
Bill Bobbitt <wmbobbitt@gmail.com>
2017-01-27 11:02:37 -0500

    deposit and withdraw works for both

--- atm.js
+++ atm.js
@@ -1,7 +1,30 @@
 $(document).ready(function(){
 
+var checkingBalance = 0;
+var savingsBalance = 0;
+
 $('#checking .deposit').on("click", function() {
-  console.log('hello')
+  checkingBalance += parseInt($('#checking .input').val());
+  console.log(checkingBalance)
+  $('#checking .balance').html(`$${checkingBalance}`)
+})
+
+$('#checking .withdraw').on("click", function() {
+  checkingBalance -= parseInt($('#checking .input').val());
+  console.log(checkingBalance)
+  $('#checking .balance').html(`$${checkingBalance}`)
+})
+
+$('#savings .deposit').on("click", function() {
+  savingsBalance += parseInt($('#savings .input').val());
+  console.log(savingsBalance)
+  $('#savings .balance').html(`$${savingsBalance}`)
+})
+
+$('#savings .withdraw').on("click", function() {
+  savingsBalance -= parseInt($('#savings .input').val());
+  console.log(savingsBalance)
+  $('#savings .balance').html(`$${savingsBalance}`)
 })
 
 });
