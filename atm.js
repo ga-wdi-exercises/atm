6d94a4d7aa674e2eac45d82e63074d748d55a8e6
Dan Lin <dlin2102@gmail.com>
2017-01-27 10:53:14 -0500

    commit1

--- atm.js
+++ atm.js
@@ -1,3 +1,5 @@
 $(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+});
+$( ".deposit" ).click(function() {
+  console.log( "Handler for .click() called." );
 });
