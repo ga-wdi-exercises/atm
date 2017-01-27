6a394625e28df41dfff2e205bf9294d7617c7f0f
Erica Norby <ericanorby@gmail.com>
2017-01-27 11:06:24 -0500

    initial commit, deposit and withdraw functions working with overdraft protection

--- atm.js
+++ atm.js
@@ -1,3 +1,47 @@
+var checkingTotal = 0;
+var savingsTotal = 0;
+
 $(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+
+//add value to checking and update checking total
+function addValueChecking(){
+  var userInputChecking = $("#checking > .input").val()
+  checkingTotal = +checkingTotal + +userInputChecking;
+  $("#checking > .balance").text("$" + checkingTotal)
+}
+
+//subtract value from checking and update checking total
+function subValueChecking(){
+  var userInputChecking = $("#checking > .input").val()
+  if (userInputChecking < checkingTotal) {
+    checkingTotal = +checkingTotal - +userInputChecking;
+  } else {
+    alert("You cannot withdraw money that you don't have!");
+  }
+  $("#checking > .balance").text("$" + checkingTotal)
+}
+
+//add value to savings and update savings total
+function addValueSavings(){
+  var userInputSavings = $("#savings > .input").val()
+  savingsTotal = +savingsTotal + +userInputSavings;
+  $("#savings > .balance").text("$" + savingsTotal)
+}
+
+//subtract value from savings and update savings total
+function subValueSavings(){
+  var userInputSavings = $("#savings > .input").val()
+  if (userInputSavings < savingsTotal) {
+    savingsTotal = +savingsTotal - +userInputSavings;
+  } else {
+    alert("You cannot withdraw money that you don't have!")
+  }
+  $("#savings > .balance").text("$" + savingsTotal)
+}
+
+$("#checking > .deposit").click(addValueChecking)
+$("#checking > .withdraw").click(subValueChecking)
+$("#savings > .deposit").click(addValueSavings)
+$("#savings > .withdraw").click(subValueSavings)
+
 });
