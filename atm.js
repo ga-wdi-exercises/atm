0061823f1b69320836f958b0109e845f57f08b63
Dan Lin <dlin2102@gmail.com>
2017-01-27 14:16:24 -0500

    step3

--- atm.js
+++ atm.js
@@ -1,5 +1,49 @@
 $(document).ready(function(){
 });
-$( ".deposit" ).click(function() {
-  console.log( "Handler for .click() called." );
-});
+/*$( ".deposit" ).click(function() {
+  console.log( "it's working" );
+});*/
+//var checkingDeposit = $( "#checking" ).children(".deposit");
+//console.log("checkingDeposit");
+
+//checkingDeposit.click(function(){
+//  console.log("works again");
+//});
+
+/*var input = $( "#checking" ).children(".input").val()
+
+(checkingDeposit).click(function() {
+  console.log(input.val());
+});*/
+
+var checkDeposit = $("#checking.deposit");
+var checkingInput = $("#checking.input");
+var checkingBalance = 0;
+
+  $("#checking .deposit").on("click", function(){
+    var checkingInput = $("#checking .input").val()
+    checkingBalance = +checkingInput + +checkingBalance;
+    $("#checking .balance").html("$" + checkingBalance);
+  })
+
+
+
+
+
+
+/*  newDeposit.append(checkInput)
+
+function(){
+  for(i=0;i < newDeposit.length; i++)
+  return newDeposit+=checkInput
+
+}
+
+/*var currentBalence = 0
+var newBalance = checkBalance.html(checkInput.val());
+
+function add(currentBalence, checkInput){
+if(newBalance < 0;);
+currentBalence+=checkInput
+
+}*/
