9e21c8121f60544eb60255e5df308f4b537ebbef
jared edwards <jredwards519@gmail.com>
2017-01-27 11:34:37 -0500

    Added a deposit listener and function

--- atm.js
+++ atm.js
@@ -1,3 +1,95 @@
 $(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+// $('body').css('background','red');--jquery test
+//get the number displayed in the balance field
+var checkingBalance = $('#checking').find('.balance');
+// console.log(checkingBalance);
+
+
+function getCheckingBalance() {
+  //gets the input from user for checking
+  var checkingInput = $('#checking').find('input').val();
+  //gets current balance value
+  var checkingBalanceValue = $('#checking').find('.balance').html();
+  //remove the dollar sign for manipulation
+  var checkingBalanceNumber = checkingBalanceValue.substr(1,checkingBalanceValue.length);
+
+  return checkingBalanceNumber;
+}
+
+
+function updateCheckingBalance() {
+
+
+}
+
+// function withdrawChecking(currentBalance, withdrawalAmount){
+//   var newBalance = parseInt(currentBalance)-parseInt(withdrawalAmount);
+//   checkingBalance.html(`$${newBalance}`);
+// }
+
+function depositChecking(currentBalance, depositAmount){
+  var newBalance = parseInt(currentBalance)+ parseInt(depositAmount);
+  checkingBalance.html(`$${newBalance}`);
+}
+
+
+// add click function for the deposit button on checking account
+$('#checking').find('.deposit').click(function(){
+    var checkingInput = $('#checking').find('input').val();
+    depositChecking(getCheckingBalance(),checkingInput);
+
 });
+
+//add click listener to withdraw button
+$('#checking').find('.withdraw').click(function(){
+  console.log('clicked');
+  var checkingInput = $('#checking').find('input').val();
+  withdrawChecking();
+
+})
+
+
+
+
+
+
+});
+
+
+/*
+-get amount of the bank account
+-DONE  validate the amount from user, - make sure its a number (typeOf)
+- DONE get the value of the number inside the box of checking -- validate that number against balance
+- make sure its a number (typeOf)
+-when deposit is presssed add that number to the number in the box click event listener
+- add value to the display
+
+-when withdraw is pressed subtract that nmber from the number in the box
+- get the value of the number inside the box of checking -- validate that number
+- make sure its a number (typeOf)
+-when deposit is presssed subtract that number to the number in the box click event listener
+- update display
+-
+*/
+
+
+
+
+
+
+/*
+function depositChecking() {
+
+  var checkingInput = $('#checking').find('input').val();
+  var checkingBalanceValue = $('#checking').find('.balance').html();
+  var checkingBalanceNumber = checkingBalanceValue.substr(1,checkingBalanceValue.length);
+}
+
+function withdrawChecking() {
+  var checkingInput = $('#checking').find('input').val();
+  var checkingBalanceValue = $('#checking').find('.balance').html();
+  var checkingBalanceNumber = checkingBalanceValue.substr(1,checkingBalanceValue.length);
+  var newBalance = parseInt(currentBalance)+ parseInt(depositAmount);
+  checkingBalance.html(`$${newBalance}`);
+}
+*/
