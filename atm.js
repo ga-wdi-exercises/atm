37fd163db78af4df6a18c12228fa0cc170da12fd
Eva Lai <evalai.dc@gmail.com>
2017-01-27 12:36:34 -0500

    both accounts work

--- atm.js
+++ atm.js
@@ -2,31 +2,55 @@ $(document).ready(function(){
 
 });
 
-var totalAmt = 0;
+var totalAmtChk = 0;
 // Checking Account's deposit button listen to click
 function depMoneyChk() {
   // amt of checking deposit
   var inputValue1 = $('#checking>.input').val();
   //adding user input to the current balance
-  totalAmt = +inputValue1 + +totalAmt;
+  totalAmtChk = +inputValue1 + +totalAmtChk;
   // changing balance to amt of deposit
-  $('#checking>.balance').text('$'+totalAmt);
+  $('#checking>.balance').text('$'+totalAmtChk);
 }
 
 $('#checking>.deposit').on('click',depMoneyChk)
 
 // Checking Account's withdrawal button listen to click
 function widrMoneyChk() {
-  // amt of checking deposit
+  // amt of checking withdrawal
   var inputValue1 = $('#checking>.input').val();
   //adding user input to the current balance
-  totalAmt = +inputValue1 - +totalAmt;
-  // changing balance to amt of deposit
-  $('#checking>.balance').text('$'+totalAmt);
+  totalAmtChk = +totalAmtChk - +inputValue1 ;
+  // changing balance to amt of withdrawal
+  $('#checking>.balance').text('$'+totalAmtChk);
 }
 
 $('#checking>.withdraw').on('click',widrMoneyChk)
 
+// SAVINGS ACCOUNT
+var totalAmtSav =0;
+function depMoneySav() {
+  // amt of savings deposit
+  var inputValue2 = $('#savings>.input').val();
+  //adding user input to the current balance
+  totalAmtSav = +inputValue2 + +totalAmtSav;
+  // changing balance to amt of deposit
+  $('#savings>.balance').text('$'+totalAmtSav);
+}
+
+$('#savings>.deposit').on('click',depMoneySav)
+
+// Checking Account's withdrawal button listen to click
+function widrMoneySav() {
+  // amt of checking deposit
+  var inputValue2 = $('#savings>.input').val();
+  //adding user input to the current balance
+  totalAmtSav = +totalAmtSav - +inputValue2;
+  // changing balance to amt of deposit
+  $('#savings>.balance').text('$'+totalAmtSav);
+}
+
+$('#savings>.withdraw').on('click',widrMoneySav)
 
 
 //
