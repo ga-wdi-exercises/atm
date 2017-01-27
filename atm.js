6d7c7454e0a8dcdd9bd2d84968e4dbe4cc1f02ad
Mitchell Carroll <jmitchellcarroll@gmail.com>
2017-01-27 16:41:02 -0500

    withdraw

--- atm.js
+++ atm.js
@@ -1,73 +1,60 @@
 $(document).ready(function(){
   var checking = 0
   var savings = 0
-  var checkingDeposit = $("#checking").find(".deposit")
+  var checkingDeposit = $("#checking").find(".deposit");
   var checkingWithdraw = $("#checking").find(".withdraw")
   var checkingInput = $("#checking").find(".input")
   var checkingBalance = $("#checking").find(".balance")
-  var savingsDeposit = $("#savings").find(".deposit")
+  var savingsDeposit = $("#savings").find(".deposit");
   var savingsWithdraw = $ ("#savings").find(".withdraw")
-  var savingsInput = $("#savings").find(".input")
+  var savingsInput = $("#savings").find(".input");
   var savingsBalance = $ ("#savings").find(".balance")
+
 
   function depositToAccount(account) {
     if(account == 'checking') {
-      var amount = parseInt(checkingInput.val())
-      checking += amount
-      checkingBalance.text("$" + checking)
+      var amount = parseInt(checkingInput.val());
+      checking += amount;
+      checkingBalance.text("$" + checking);
     }
     if(account == 'savings') {
-      var amount = parseInt(savingsInput.val())
-      saving += amount
-      savingsBalance.text("$" + savings)
+      var amount = parseInt(savingsInput.val());
+      savings += amount;
+      savingsBalance.text("$" + savings);
     }
   }
 
-  //function withdrawFromAccount(account) {
-    if(account == 'checking') {
-      var amount = parseInt(checking.input.val())
-      checking -= amount
-      checkingBalance.text("$" + checking)
-    }
-      else(account == 'checking'){
-      var amount > parseInt(checkingBalance.val())
-      checkingBalance.text("$" + checking)
+  function withdrawFromAccount(account) {
+    // var checkingAccountBalance = checkingBalance.html()
+    if (account == 'checking') {
+      var amount = parseInt(checkingInput.val());
+      if (amount <= checking) {
+        checking -= amount;
       }
-    if(account == 'savings') {
-      var amount = parseInt(savings.Input.val())
-      checking -= amount
-      savingsBalance.text("$" + savings)
+      checkingBalance.text("$" + checking);
+      // var amount > parseInt(checkingBalance.val());
+      checkingBalance.text("$" + checking);
     }
-      else(account == 'savings'){
-        var amount > parseInt(savingsBalance.val())
-        savingsBalance.text("$" + savings)
+    if (account == 'savings') {
+      var amount = parseInt(savingsInput.val());
+      if (account <= savings) {
+            checking -= amount;
       }
-  }
-
-
-  checkingDeposit.on("click", function() {depositToAccount("checking")})
-  savingsDeposit.on("click", function() {depositToAccount("savings")})
-  checkingWithdraw.on("click", function() {withdrawFromAccount("checking")})
-  savingsWithdraw.on("click", function() {withdrawFromAccount("savings")})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      savingsBalance.text("$" + savings);
+  //     var amount > parseInt(savingsBalance.val())
+      savingsBalance.text("$" + savings);
+   }
+}
+  checkingDeposit.on("click", function() {
+    depositToAccount("checking");
+  });
+  savingsDeposit.on("click", function() {
+    depositToAccount("savings");
+  });
+  checkingWithdraw.on("click", function() {
+    withdrawFromAccount("checking");
+  });
+  savingsWithdraw.on("click", function() {
+    withdrawFromAccount("savings");
+  });
 });
