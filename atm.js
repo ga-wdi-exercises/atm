9e95fcf9236334065c19383079aa6768650b55a2
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-01-27 11:00:18 -0500

    pseudocode entered in js script file

--- atm.js
+++ atm.js
@@ -1,3 +1,59 @@
+//Given ATM Object with a checking and savings account
+//Need to make ATM object functional, how do we do that?
+//Make sure functions are not longer than five lines,(Sandi Metz's Rule 2)
+
+//Test that the Javascript library, jQuery is working
+
 $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
 });
+
+//Users of the ATM object with a Checking Account
+//Users of the ATM object can deposit money into bank account
+//Set the value of 0 to equal red
+
+
+//Create an event that listens for click on deposit button from user when a deposit is made
+//When deposit button is clicked it will console.log('hello')
+
+
+//When deposit button is clicked it will return the userinput
+//When userinput is returned it will console.log('get money')
+
+//When the deposit button is clicked, the balance container will update (in add. to current) with userinput
+
+
+//Users of the ATM object can withdraw money from bank account
+//Create an event that listens for click on withdraw button from user when a withdraw is made
+//When withdraw button is clicked it will console.log('bye')
+
+//When withdrawn button is clicked it will return the userinput
+//When userinput is returned it will console.log('ballin')
+
+//When the withdraw button is clicked, the balance container will update (in add. to current) with userinput
+
+//----------------------------------------------------
+
+//Users of the ATM object with a Savings Account
+//Users of the ATM object can deposit money into bank account
+//Set the value of 0 to equal red
+
+
+//Create an event that listens for click on deposit button from user when a deposit is made
+//When deposit button is clicked it will console.log('hello')
+
+
+//When deposit button is clicked it will return the userinput
+//When userinput is returned it will console.log('get money')
+
+//When the deposit button is clicked, the balance container will update (in add. to current) with userinput
+
+
+//Users of the ATM object can withdraw money from bank account
+//Create an event that listens for click on withdraw button from user when a withdraw is made
+//When withdraw button is clicked it will console.log('bye')
+
+//When withdrawn button is clicked it will return the userinput
+//When userinput is returned it will console.log('ballin')
+
+//When the withdraw button is clicked, the balance container will update (in add. to current) with userinput
