8b8abc22e0cfaba748728aa9c51a2edcc9a323e9
Nat Carter <n.goldman.carter@gmail.com>
2017-01-27 10:17:05 -0500

    added comment to allow initial commit and pull request

--- atm.js
+++ atm.js
@@ -1,3 +1,5 @@
 $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
 });
+
+// Added comment here to allow initial commit and pull request.
