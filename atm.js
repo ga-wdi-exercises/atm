26aec43f758310d7c0174feb7e1a62b5faa3815f
Kay Ehni <kay.ehnigmail.com>
2017-01-27 12:05:13 -0500

    completed first three steps

--- atm.js
+++ atm.js
@@ -1,3 +1,46 @@
-$(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-});
+
+  var cDeposit;
+  var cWithdraw;
+  var sDeposit;
+  var sWithdraw;
+
+  var cBalance = parseInt($("#checking > div").html().replace("$", ""));
+  var sBalance = parseInt($("#savings > div").html().replace("$", ""));
+
+$("#checking .deposit").click(function(){ //checking deposit button
+  cDeposit = parseInt($("#checking .input:text").val());
+  cBalance = cBalance + cDeposit;
+  $("#checking > div").html(cBalance);
+})
+
+$("#checking .withdraw").click(function(){ //checking withdraw button
+  cWithdraw = parseInt($("#checking .input:text").val());
+  var cBalancePrev = cBalance;
+  cBalance = cBalance - cWithdraw;
+  if (cBalance < 0) {
+    $("#checking > div").html(cBalancePrev); //checks to see if withdraw will make balance negative
+    cBalance = cBalancePrev;
+  }
+  else {
+  $("#checking > div").html(cBalance); //if it will, keeps balance at what it was
+  }
+})
+
+$("#savings .deposit").click(function(){
+  sDeposit = parseInt(($("#savings .input:text").val()));
+  sBalance = sBalance + sDeposit;
+  $("#savings > div").html(sBalance);
+})
+
+$("#savings .withdraw").click(function(){  //savings withdraw  button
+  sWithdraw = parseInt(($("#savings .input:text").val()));
+  var sBalancePrev = sBalance;
+  sBalance = sBalance - sWithdraw;
+  if (sBalance < 0) {                       //checks to see if withdraw will make balance negative
+    $("#checking > div").html(sBalancePrev); //if it will, keeps balance at what it was
+    sBalance = sBalancePrev;
+  }
+  else {
+  $("#savings > div").html(sBalance);
+  }
+})
