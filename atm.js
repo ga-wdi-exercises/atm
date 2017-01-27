ac186ed2d078dc1590601d3ec083f5bb7a3d0328
Hannah Schuster <hschuster9@gmail.com>
2017-01-27 11:04:55 -0500

    through commit 4 checking

--- atm.js
+++ atm.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+  console.log("hello world");
 });
+
+
+$('.deposit').on('click', function(){
+var userInputValue = $('.input').val();
+  console.log(userInputValue);
+  $('#checking .balance').html(userInputValue);
+}
+)
