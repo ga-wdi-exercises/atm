3ed483a4572e92897d23afffa00aed0b53b829e1
Taylor Scott <TaylorScott@Taylors-MacBook-Pro.local>
2017-01-27 16:13:36 -0500

    06

--- atm.js
+++ atm.js
@@ -22,8 +22,6 @@ withdrawChecking.on("click", sayGoodbye)
 depositSavings.on("click", sayHowdy)
 withdrawSavings.on("click", sayLater)
 
-
-
 $("#depositChecking").on("click", function(){
   var deposit = parseInt($("#inputChecking").val())
   var balance = parseInt($("#balanceChecking").text().replace("$",""))
@@ -34,12 +32,27 @@ $("#depositChecking").on("click", function(){
 $("#withdrawChecking").on("click", function(){
   var withdraw = parseInt($("#inputChecking").val())
   var balance = parseInt($("balanceChecking").text().replace("$",""))
-  var newBalance = "$" + (balance - withdraw)
-  $("#balanceChecking").text(newBalance)
+    if ((balance - withdraw) >= 0){
+      var newBalance = "$" + (balance + -withdraw);
+    }
+      $("#balanceChecking").text(newBalance)
 })
 
+$("#depositSavings").on("click", function(){
+  var deposit = parseInt($("#inputSavings").val())
+  var balance = parseInt($("#balanceSavings").text().replace("$",""))
+  var newBalance = "$" + (balance + deposit)
+  $("#balanceSavings").text(newBalance)
+})
 
-
+$("#withdrawSavings").on("click", function(){
+  var withdraw = parseInt($("#inputSavings").val())
+  var balance = parseInt($("balanceSavings").text().replace("$",""))
+    if ((balance - withdraw) >= 0){
+      var newBalance = "$" + (balance + -withdraw);
+    }
+      $("#balanceSavings").text(newBalance)
+})
 
 
 
