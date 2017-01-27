a47c43bbce9409dca5931e5ac7ee41f8c3a124d8
Collin OConnell <ococncol@gmail.com>
2017-01-27 11:09:18 -0500

    made deposit button work for checking

--- atm.js
+++ atm.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
 });
+
+//checking change-text
+// var checking = $('#checking').children().eq(1).text().val();
+// console.log(checking);
+//savings change text
+// var savings = $('#savings').children().eq(1).text();
+// console.log(savings);
+
+$('#checking').children('.deposit').on('click', function(){
+  console.log($("input").val());
+});
