137f6f084f07da70951a4af9bbc72827bf12e721
Elena Santana <fanelenan7@gmail.com>
2017-01-27 12:26:06 -0500

    adding and withdrawing, need to unstick boxes' values from each other

--- atm.js
+++ atm.js
@@ -1,14 +1,33 @@
 $(document).ready(function(){
 // setting all the variables
-  var value = null
+  var value = 0
   // var valueSavings = 0
+  var hasInput = true
   var balance
   // var balanceSavings
 
 // deposit on button click
 $(".deposit").click(function() {
+  //get NaN if input is empty
+  if ($(".input").val() !== NaN) {
+    // console.log($(this))
+    balance = value + parseInt($(this).siblings(".input").val())
+    $(this).siblings(".balance").html(`$${balance}`)
+    // console.log($(".balance").val())
+    value = balance
+  } else {
+    console.log("nope");
+    hasInput = false
+    return
+}
+  //reset the input box
+  $(".input").val(function(){
+    $(this).attr("placeholder, enter and amount")
+})
+})
+$(".withdraw").click(function() {
   // console.log($(this))
-  balance = value + parseInt($(".input").val())
+  balance = value - parseInt($(this).siblings(".input").val())
   $(this).siblings(".balance").html(`$${balance}`)
   // console.log($(".balance").val())
   value = balance
@@ -17,6 +36,5 @@ $(".deposit").click(function() {
     $(this).attr("placeholder, enter and amount")
 })
 })
-
 
 });
