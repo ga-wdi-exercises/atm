bd30a75db0c51d3ada3c3934bc3cd9d61783ac51
Mike Nabil <maikel_nabil@yahoo.com>
2017-01-27 11:15:29 -0500

    checking deposit is now functional

--- atm.js
+++ atm.js
@@ -1,18 +1,32 @@
   $(document).ready(function(){
-  // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+
+  // Checking account
+  // set checking balance
+  // var cbalance = $('#checkingBalance').text();
+  cbalance = +$('#checkingBalance').text();
+  cdepositsum = +0;
+
+  //Set up Checking Deposit Function
+  function depositChecking(){
+    cbalance = +cbalance + +cdepositsum;
+  }
+
+  // event listen for Function deposit
+  $('.cdeposit').click(function(num1, num2) {
+    cdepositsum = $('.cinput').val();
+    depositChecking();
+    $('#checkingBalance').text(cbalance);
+  })
+
+  // event listen for function withdraw
+  //   if account go negative, ignore transaction
+  //   if account reaches zero, turn background red
+  //
+  // saving account
+  // set saving balance
+  // event listen for Function deposit
+  // event listen for function withdraw
+  //   if account go negative, ignore transaction
+  //   if account reaches zero, turn background red
+
   });
-
-
-  //Checking account
-  //set checking balance
-  //event listen for Function deposit
-  //event listen for function withdraw
-    //if account go negative, ignore transaction
-    //if account reaches zero, turn background red
-
-  //saving account
-  //set saving balance
-  //event listen for Function deposit
-  //event listen for function withdraw
-    //if account go negative, ignore transaction
-    //if account reaches zero, turn background red
