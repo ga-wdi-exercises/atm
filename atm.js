4c14074f19445120a651de9d942ae0dcea8116c7
Mike Nabil <maikel_nabil@yahoo.com>
2017-01-27 10:39:37 -0500

    adding pseudocode, and separating the dollar sign from the value

--- atm.js
+++ atm.js
@@ -1,5 +1,18 @@
-console.log('hello');
+  $(document).ready(function(){
+  // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+  });
 
-$(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-});
+
+  //Checking account
+  //set checking balance
+  //event listen for Function deposit
+  //event listen for function withdraw
+    //if account go negative, ignore transaction
+    //if account reaches zero, turn background red
+
+  //saving account
+  //set saving balance
+  //event listen for Function deposit
+  //event listen for function withdraw
+    //if account go negative, ignore transaction
+    //if account reaches zero, turn background red
