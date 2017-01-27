0078a2efa526aa827a3c6fc73abf515937727ced
Elena Santana <fanelenan7@gmail.com>
2017-01-27 11:16:12 -0500

    trying to unstick two boxes from each other with

--- atm.js
+++ atm.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+// setting all the variables
+  var value = 0
+  var balance
+// var inputValue
+// deposit on button click
+$(".deposit").click(function() {
+  console.log($(this))
+  balance = value + parseInt($(".input").val())
+  $(this).siblings(".input").html(`$${balance}`)
+  // $(".balance")
+
+  //reset the
+  $(".input").val(function(){
+    $(this).attr("placeholder, enter and amount")
+})
+})
+
 });
