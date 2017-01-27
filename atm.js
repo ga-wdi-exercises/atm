09629c039eb2098b43d6411686443b44a1c55bb5
Dominick Houston <dominick.houston14@gmail.com>
2017-01-27 13:17:32 -0500

    Commit 4  Checking Deposit & Withdraw works q

--- atm.js
+++ atm.js
@@ -1,13 +1,42 @@
 amount = $("input").val();
+CheckingBalance = "";
+amountWithdaw = 0;
+amountDeposit = 0;
+insufficientFunds = 0;
 
 $(document).ready(function(){
   $("body").css("background", "orange")
 });
 
 $("#checking").children(".deposit").on("click", function (){
-  var amount = $("input").val();
-  console.log(amount);
+  amountDeposit += parseInt($("#checking .input").val());
+  console.log("Checking Deposit of amount: " + amountDeposit);
+  $("#checking .balance").html("$" + amountDeposit);
 })
+
+$("#checking").children(".withdraw").on("click", function (){
+  amountDeposit -= parseInt($("#checking .input").val());
+  console.log("Checking Deposit of amount: " + amountDeposit);
+  $("#checking .balance").html("$" + amountDeposit);
+  // if (CheckingBalance < 0){
+  //   prompt("You broke, Get Money");
+  // }
+
+})
+
+
+
+// amountWithdaw -= parseInt($("#checking .input").val());
+//Withdraw function
+// parseInt($("input").val()));
+
+// $("#checking .balance").html(amountChecking);
+
+  // console.log(amount);
+
+// html(parseInt($())
+  // $("#checking").html();
+
 
 // $("button").on("click", function () {
 //   console.log("dom");
