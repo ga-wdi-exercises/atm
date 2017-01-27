b933cd40add0cd2da56906049c0a4e03e0ece866
Will Hawkins <willhawkins123@yahoo.com>
2017-01-27 11:23:44 -0500

    Deposit and Withdraw Work on Both

--- atm.js
+++ atm.js
@@ -1,20 +1,61 @@
 $(document).ready(function(){
 });
 
-//Temporary Value in Checking Input
+//Temporary Value in Checking and Savings Inputs
 $("#checking .input").val(100);
+$("#savings .input").val(100);
 
-//Create Checking Input Variable
+//Create Checking Input and Checking Balance Variables
 var checkingInput = 0;
-
-//Create Checking Balance Variable
 var checkingBalance = 0;
+
+//Create Savings Input and Savings Balance Variables
+var savingsInput = 0;
+var savingsBalance = 0;
+
+//Create Savings Input and Savings Balance Variables
 
 //Upon Clicking, grab Checking Input, Console Log It, Make That Checking Balance
 $("#checking .deposit").on("click", function(){
   checkingInput = $("#checking .input").val();
-  checkingBalance = +checkingBalance + +checkingInput
+  checkingBalance = +checkingBalance + +checkingInput;
   console.log(checkingBalance);
   $("#checking .balance").html("$ " + checkingBalance);
   $("#checking .input").val("");
+})
+
+$("#checking .withdraw").on("click", function(){
+  checkingInput = $("#checking .input").val();
+  if (+checkingBalance - +checkingInput < 0) {
+    alert("You don't have enough money for that!");
+  } else {
+    checkingBalance = +checkingBalance - +checkingInput;
+  }
+  console.log(checkingBalance);
+  $("#checking .balance").html("$ " + checkingBalance);
+  $("#checking .input").val("");
+})
+
+
+// SAVINGS THINGS ARE BELOW HERE
+
+//Upon Clicking, grab Savings Input, Console Log It, Make That Checking Balance
+$("#savings .deposit").on("click", function(){
+  savingsInput = $("#savings .input").val();
+  savingsBalance = +savingsBalance + +savingsInput;
+  console.log(savingsBalance);
+  $("#savings .balance").html("$ " + savingsBalance);
+  $("#savings .input").val("");
+})
+
+$("#savings .withdraw").on("click", function(){
+  savingsInput = $("#savings .input").val();
+  if (+savingsBalance - +savingsInput < 0) {
+    alert("You don't have enough money for that!");
+  } else {
+    savingsBalance = +savingsBalance - +savingsInput;
+  }
+  console.log(savingsBalance);
+  $("#savings .balance").html("$ " + savingsBalance);
+  $("#savings .input").val("");
 })
