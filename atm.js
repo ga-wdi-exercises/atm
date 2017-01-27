30a1d25a398a39037653df7eacfe29c622689424
Mike Nabil <maikel_nabil@yahoo.com>
2017-01-27 11:36:27 -0500

    savings buttons are now functional

--- atm.js
+++ atm.js
@@ -5,7 +5,7 @@
         // Set checking balance
         cbalance = +$('#checkingBalance').text();
 
-        // Set new sum
+        // Set new checking sum
         cnewsum = +0;
 
         //Set up Checking Deposit Function
@@ -32,19 +32,46 @@
           $('#checkingBalance').text(cbalance);
         })
 
+        //   if account go negative, ignore transaction
+        //   if account reaches zero, turn background red
+        //   if a text added, ignore transaction
 
 
-  //   if account go negative, ignore transaction
-  //   if account reaches zero, turn background red
-  //
-  // saving account
-  // set saving balance
-  // event listen for Function deposit
-  // event listen for function withdraw
-  //   if account go negative, ignore transaction
-  //   if account reaches zero, turn background red
+        
+  // B. savings account
 
+        // Set savings balance
+        sbalance = +$('#savingsBalance').text();
 
-//if a text added, ignore transaction
+        // Set new savings sum
+        snewsum = +0;
+
+        //Set up Savings Deposit Function
+        function depositSavings(){
+          sbalance = +sbalance + +snewsum;
+        }
+
+        //Set up savings Withdraw Function
+        function withdrawSavings(){
+          sbalance = +sbalance - +snewsum;
+        }
+
+        // event listen for Function savings deposit
+        $('.sdeposit').click(function(num1, num2) {
+          snewsum = $('.sinput').val();
+          depositSavings();
+          $('#savingsBalance').text(sbalance);
+        })
+
+        // event listen for function savings withdraw
+        $('.swithdraw').click(function(num1, num2) {
+          snewsum = $('.sinput').val();
+          withdrawSavings();
+          $('#savingsBalance').text(sbalance);
+        })
+
+      //   if account go negative, ignore transaction
+      //   if account reaches zero, turn background red
+      //   if a text added, ignore transaction
 
   });
