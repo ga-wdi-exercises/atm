20b63ccbfba424228a0a4ee39af32e8253a74053
Collin OConnell <ococncol@gmail.com>
2017-01-27 12:15:21 -0500

    made it so the value inside of the content updates with the ammount in the input

--- atm.js
+++ atm.js
@@ -1,14 +1,27 @@
-$(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-});
+//
+// $(document).ready(function(){
+// // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+// });
 
-//checking change-text
-// var checking = $('#checking').children().eq(1).text().val();
-// console.log(checking);
-//savings change text
-// var savings = $('#savings').children().eq(1).text();
-// console.log(savings);
+// $("#checking").children(".deposit").on("click", function(){
+//  console.log($("input").val());
+// });
+//
+// $("#checking").children(".deposit").on("click", function(){
+//   var amount = $("input").val();
+//   $(".checkBal").html(amount);
+// });
+//
 
-$('#checking').children('.deposit').on('click', function(){
+
+
+// $("#checking").children(".deposit").on("click", function() {
+//
+//   console.log($("input").val());
+// });
+
+$("#checking").children(".deposit").on("click", function(){
+  amount = $("input").val();
   console.log($("input").val());
+  $(".checkBal").html("$" + amount);
 });
