fccc7ced806e73296f92ea1fc0976818b913a35c
Fiona <h.natapra@gmail.com>
2017-01-27 12:21:59 -0500

    initial commit

--- atm.js
+++ atm.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
 });
+
+function changeBackgroundColorToRed(){
+  $("body").css("background-color", "red");
+};
+
+changeBackgroundColorToRed();
+
+var depositCheck = $("#checking .deposit");
+var checkingInput = $("checking .input");
+
+
+depositCheck.on("click", function(){
+  $("#checking .balance").console.log("$" + checkingInput.value)
+});
