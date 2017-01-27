d9af9e8d47aad152494624fc092e81c65825d5a8
Bill Bobbitt <wmbobbitt@gmail.com>
2017-01-27 11:20:00 -0500

    withdrawing to 0 turns red, below 0 not allowed

--- atm.js
+++ atm.js
@@ -10,9 +10,22 @@ $('#checking .deposit').on("click", function() {
 })
 
 $('#checking .withdraw').on("click", function() {
-  checkingBalance -= parseInt($('#checking .input').val());
-  console.log(checkingBalance)
-  $('#checking .balance').html(`$${checkingBalance}`)
+  if (parseInt($('#checking .input').val()) <= checkingBalance) {
+    checkingBalance -= parseInt($('#checking .input').val());
+    console.log(checkingBalance)
+    $('#checking .balance').html(`$${checkingBalance}`)
+  } else {
+    var checkingDwindle = prompt(`You only have $${checkingBalance} left. Would you like to take the rest? Y/N`)
+      if (checkingDwindle === "Y") {
+        checkingBalance = 0;
+        console.log(checkingBalance)
+        $('#checking').addClass("zero")
+        $('#checking .balance').html(`$${checkingBalance}`)
+      } else {
+        alert('Thank you, come again!')
+      }
+  }
+
 })
 
 $('#savings .deposit').on("click", function() {
@@ -22,9 +35,22 @@ $('#savings .deposit').on("click", function() {
 })
 
 $('#savings .withdraw').on("click", function() {
-  savingsBalance -= parseInt($('#savings .input').val());
-  console.log(savingsBalance)
-  $('#savings .balance').html(`$${savingsBalance}`)
-})
+  if (parseInt($('#savings .input').val()) <= savingsBalance) {
+    savingsBalance -= parseInt($('#savings .input').val());
+    console.log(savingsBalance)
+    $('#savings .balance').html(`$${savingsBalance}`)
+  } else {
+    var savingsDwindle = prompt(`You only have $${savingsBalance} left. Would you like to take the rest? Y/N`)
+      if (savingsDwindle === "Y") {
+        savingsBalance = 0;
+        console.log(savingsBalance)
+        $('#savings').addClass("zero")
+        $('#savings .balance').html(`$${savingsBalance}`)
+      } else {
+        alert('Thank you, come again!')
+      }
+  }
 
 });
+
+})
