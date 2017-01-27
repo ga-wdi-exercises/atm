009d1d6535b563b88de1036741f67124e5515f4a
Mike Nabil <maikel_nabil@yahoo.com>
2017-01-27 14:32:28 -0500

    overdraft protection installed

--- atm.js
+++ atm.js
@@ -1,22 +1,68 @@
   $(document).ready(function(){
 
+    // Defining variables
+
+      // Set checking balance
+      // cbalance = +$('#checkingBalance').text();
+      cbalance = 100;
+
+      // Set new checking sum
+      cnewsum = +0;
+
+      // Set savings balance
+      sbalance = +100;
+
+      // Set new savings sum
+      snewsum = +0;
+
+      // Set up overall balance
+      compined = +cbalance + +sbalance;
+
+
   // A. Checking account
-
-        // Set checking balance
-        cbalance = +$('#checkingBalance').text();
-
-        // Set new checking sum
-        cnewsum = +0;
 
         //Set up Checking Deposit Function
         function depositChecking(){
           cbalance = +cbalance + +cnewsum;
+          compined = +cbalance + sbalance;
         }
 
         //Set up Checking Withdraw Function
         function withdrawChecking(){
           cbalance = +cbalance - +cnewsum;
+          compined = +cbalance + sbalance;
         }
+
+        //Overdeaft Functionality for Checking account
+        function qualifyCOverdarft(){
+          if ( +compined >= +cnewsum ){
+            alert('Your account does not have enough funds, but you qualify for overdraft');
+            sbalance = +compined - +cnewsum;
+            cbalance= 0;
+            $('#checkingBalance').text(cbalance);
+            redCAlarm();
+            $('#savingsBalance').text(sbalance);
+            redSAlarm();
+          } else {
+            alert('Sorry! You do not qualify for overdraft protection!');
+          }
+        }
+
+        // if account reaches zero, turn background red
+        function redCAlarm(){
+          if (+cbalance <= 0){
+            $('#checking').addClass('zero');
+          }
+        }
+
+        // if account returns positive, the red background goes away
+        function greenCAlarm(){
+          if (+cbalance > 0){
+            $('#checking').removeClass('zero');
+          }
+        }
+
+        //Event Listeners
 
         // event listen for Function checking deposit
         $('.cdeposit').click(function(num1, num2) {
@@ -39,8 +85,10 @@
             alert('This is not a number. You can only use numbers and dots!');
           } else if (cnewsum < 0){
             alert('Sorry! You can not use negative values!');
+          } else if (cbalance == 0){
+            alert('Sorry! You account is empty!');
           } else if( (+cbalance - +cnewsum) < 0) {
-            alert('Sorry! You do not have enough credit to perform this transaction!');
+            qualifyCOverdarft();
           } else {
             withdrawChecking();
             $('#checkingBalance').text(cbalance);
@@ -48,38 +96,51 @@
           }
         })
 
-        // if account reaches zero, turn background red
-        function redCAlarm(){
-          if (+cbalance <= 0){
-            $('#checking').addClass('zero');
-          }
-        }
-
-        // if account returns positive, the red background goes away
-        function greenCAlarm(){
-          if (+cbalance > 0){
-            $('#checking').removeClass('zero');
-          }
-        }
-
 
   // B. savings account
-
-        // Set savings balance
-        sbalance = +$('#savingsBalance').text();
-
-        // Set new savings sum
-        snewsum = +0;
 
         //Set up Savings Deposit Function
         function depositSavings(){
           sbalance = +sbalance + +snewsum;
+          compined = +cbalance + sbalance;
         }
 
         //Set up savings Withdraw Function
         function withdrawSavings(){
           sbalance = +sbalance - +snewsum;
+          compined = +cbalance + sbalance;
         }
+
+        //Overdeaft Functionality for Savings account
+        function qualifySOverdarft(){
+          if ( +compined >= +snewsum ){
+            alert('Your account does not have enough funds, but you qualify for overdraft');
+            cbalance = +compined - +snewsum;
+            sbalance= 0;
+            $('#checkingBalance').text(cbalance);
+            redCAlarm();
+            $('#savingsBalance').text(sbalance);
+            redSAlarm();
+          } else {
+            alert('Sorry! You do not qualify for overdraft protection!');
+          }
+        }
+
+        // if account reaches zero, turn background red
+        function redSAlarm(){
+          if (+sbalance <= 0){
+            $('#savings').addClass('zero');
+          }
+        }
+
+        // if account returns positive, the red background goes away
+        function greenSAlarm(){
+          if (+sbalance > 0){
+            $('#savings').removeClass('zero');
+          }
+        }
+
+        //Event Listeners
 
         // event listen for Function savings deposit
         $('.sdeposit').click(function(num1, num2) {
@@ -101,28 +162,16 @@
           if (isNaN(snewsum) === true) {
               alert('This is not a number. You can only use numbers and dots!');
           } else if (snewsum < 0){
-            alert('Sorry! You can not use negative values!');
+              alert('Sorry! You can not use negative values!');
+          } else if (sbalance == 0){
+              alert('Sorry! You account is empty!');
           } else if( (+sbalance - +snewsum) < 0) {
-              alert('Sorry! You do not have enough credit to perform this transaction!');
+              qualifySOverdarft();
           } else {
               withdrawSavings();
               $('#savingsBalance').text(sbalance);
               redSAlarm();
           }
         })
-
-        // if account reaches zero, turn background red
-        function redSAlarm(){
-          if (+sbalance <= 0){
-            $('#savings').addClass('zero');
-          }
-        }
-
-        // if account returns positive, the red background goes away
-        function greenSAlarm(){
-          if (+sbalance > 0){
-            $('#savings').removeClass('zero');
-          }
-        }
 
   });
