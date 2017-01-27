2f688590a4f8cc277816d077d7127658c49c36d3
Savraj Singh <savrajsingh@gmail.com>
2017-01-27 10:40:26 -0500

    Initial

--- atm.js
+++ atm.js
@@ -1,3 +1,43 @@
+
+
 $(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+
+
+//This section is for checking
+  var inputChecking = $("#checking .input")
+    inputChecking.on("input", function(){
+
+    })
+
+  var depositChecking = $("#checking .deposit")
+    depositChecking.on("click", function(){
+
+    })
+
+  var withdrawChecking = $("#checking .withdraw")
+    withdrawChecking.on("click", function(){
+
+    })
+
+
+//This section is for Savings
+  var inputSavings = $("#savings .input")
+    inputSavings.on("input", function(){
+
+    })
+
+  var depositSavings = $("#savings .deposit")
+    depositSavings.on("click", function(){
+
+    })
+
+  var withdrawSavings = $("#savings .withdraw")
+    withdrawSavings.on("click", function(){
+
+    })
+
+
+
+
+
 });
