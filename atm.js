f7dab4f81b67064fe07c27e626fe60876e139b96
Bill Bobbitt <wmbobbitt@gmail.com>
2017-01-27 11:52:20 -0500

    cross withdrawing works

--- atm.js
+++ atm.js
@@ -22,6 +22,7 @@ $('#checking .withdraw').on("click", function() {
         console.log(checkingBalance)
         $('#checking').addClass("zero")
         $('#checking .balance').html(`$${checkingBalance}`)
+        $('#savings .balance').html(`$${savingsBalance}`)
         if (savingsBalance === 0) {
           $('#savings').addClass("zero")
           $('#savings .balance').html(`$${savingsBalance}`)
@@ -57,16 +58,35 @@ $('#savings .withdraw').on("click", function() {
     savingsBalance -= parseInt($('#savings .input').val());
     console.log(savingsBalance)
     $('#savings .balance').html(`$${savingsBalance}`)
-  } else {
-    var savingsDwindle = prompt(`You only have $${savingsBalance} left. Would you like to take the rest from your Checking Account? Y/N`)
+  } else if (checkingBalance >= (parseInt($('#savings .input').val()) - savingsBalance)) {
+      var savingsDwindle = prompt(`You only have $${savingsBalance} left. Would you like to take the rest from your Checking Account? Y/N`)
       if (savingsDwindle === "Y") {
+        checkingBalance = checkingBalance - (parseInt($('#savings .input').val()) - savingsBalance);
         savingsBalance = 0;
         console.log(savingsBalance)
         $('#savings').addClass("zero")
         $('#savings .balance').html(`$${savingsBalance}`)
+        $('#checking .balance').html(`$${checkingBalance}`)
+        if (checkingBalance === 0) {
+          $('#checking').addClass("zero")
+          $('#checking .balance').html(`$${checkingBalance}`)
+        }
       } else {
         alert('Thank you, come again!')
       }
+  } else {
+    var savingsDrain = prompt(`You only have $${savingsBalance} in Savings and $${checkingBalance} in Checking. Would you like to empty both? Y/N`)
+    if (savingsDrain === "Y") {
+      checkingBalance = 0;
+      savingsBalance = 0;
+      console.log(savingsBalance)
+      $('#checking').addClass("zero")
+      $('#checking .balance').html(`$${checkingBalance}`)
+      $('#savings').addClass("zero")
+      $('#savings .balance').html(`$${savingsBalance}`)
+    } else {
+      alert('Thank you, come again!')
+    }
   }
 
 });
