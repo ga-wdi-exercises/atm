9d70caeef04825f8fd7e7add8a08823fdf644f78
Andy Flickinger <atflick@gmail.com>
2017-01-27 12:34:51 -0500

    finished, but going to try to use an object

--- atm.js
+++ atm.js
@@ -1,67 +1,83 @@
 $(document).ready(function(){
 
-var checkingInput = $("#checking").find($(".input"));
-var checkingDeposit = $("#checking").find($(".deposit"));
-var checkingWithdraw = $("#checking").find($(".withdraw"));
-var checkingBalance = $("#checking").find($("span"));
+  var checkingInput = $("#checking").find($(".input"));
+  var checkingDeposit = $("#checking").find($(".deposit"));
+  var checkingWithdraw = $("#checking").find($(".withdraw"));
+  var checkingBalance = $("#checking").find($("span"));
 
+  var savingsInput = $("#savings").find($(".input"));
+  var savingsDeposit = $("#savings").find($(".deposit"));
+  var savingsWithdraw = $("#savings").find($(".withdraw"));
+  var savingsBalance = $("#savings").find($("span"));
 
-var savingsInput = $("#savings").find($(".input"));
-var savingsDeposit = $("#savings").find($(".deposit"));
-var savingsWithdraw = $("#savings").find($(".withdraw"));
-var savingsBalance = $("#savings").find($("span")).html();
+  checkingDeposit.click(depositChecking);
+  checkingWithdraw.click(withdrawChecking);
 
-checkingDeposit.click(depositChecking);
-checkingWithdraw.click(withdrawChecking);
-
-savingsDeposit.click(depositSavings);
-savingsWithdraw.click(withdrawSavings);
+  savingsDeposit.click(depositSavings);
+  savingsWithdraw.click(withdrawSavings);
   // depositing into checking
-function depositChecking() {
-  // pulls number from page and trims decimals if exist
-  var amount = toNumber(checkingInput.val());
-  var balance = parseInt(checkingBalance.text());
-  if (isNaN(amount)) {
-    alert("You must enter a real number");
-  } else {
-  checkingBalance.text(balance + amount);
+  function depositChecking() {
+    // pulls number from page and trims decimals if exist
+    var amount = toNumber(checkingInput.val());
+    var balance = parseInt(checkingBalance.text());
+    if (isNaN(amount)) {
+      alert("You must enter a real number.");
+    } else {
+      checkingBalance.text(balance + amount);
+    }
   }
-}
   // withdraw checking
-function withdrawChecking() {
-  // pulls number from page and trims decimals if exist
-  var amount = toNumber(checkingInput.val());
-  var balance = parseInt(checkingBalance.text());
-  // checks to make sure
-  if (amount > balance) {
-    alert("You do not have enough money to withdraw")
-  } else {
-  checkingBalance.text(balance - amount);
+  function withdrawChecking() {
 
-  }
-}
-
-function depositSavings() {
-  // depositing into savings
-  var amount = toNumber(savingsInput.val());
-  var balance = parseInt(savingsBalance.text());
-  if (isNaN(amount)) {
-    alert("You must enter a real number");
-  } else {
-  savingsBalance.text(balance + amount);
+    // pulls number from page and trims decimals if exist
+    var amount = toNumber(checkingInput.val());
+    var balance = parseInt(checkingBalance.text());
+    // checks to make sure
+    if (amount > balance) {
+      alert("You do not have enough money to withdraw from.")
+    } else {
+      checkingBalance.text(balance - amount);
+    }
   }
 
-}
+  function depositSavings() {
+    // depositing into savings
+    var amount = toNumber(savingsInput.val());
+    var balance = parseInt(savingsBalance.text());
+    if (isNaN(amount)) {
+      alert("You must enter a real number.");
+    } else {
+      savingsBalance.text(balance + amount);
+    }
+  }
 
-function withdrawSavings(){
-  // withdraw savings
+  function withdrawSavings(){
+    // withdraw savings
+    var amount = toNumber(savingsInput.val());
+    var balance = parseInt(savingsBalance.text());
+    // checks to make sure
+    if (amount > balance) {
+      alert("You do not have enough money to withdraw from.")
+    } else {
+      savingsBalance.text(balance - amount);
+    }
+  }
 
-}
+  $("button, input[type='button']").click(colorCheck);
 
-function toNumber(input) {
-  return Math.round(input*100)/100;
-}
+  function colorCheck(){
+    var parentBox = $(this).parent();
+    var balance = parseInt(parentBox.find("span").text());
+    if (balance === 0) {
+      parentBox.addClass("zero");
+    } else if (balance > 0) {
+      parentBox.removeClass("zero");
+    }
+  }
 
+  function toNumber(input) {
+    return Math.round(input*100)/100;
+  }
 });
 
 // 2 buttons for each side
