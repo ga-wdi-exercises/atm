7d809e4cca4a9a187b5bff71ce82dd5bc6b838b2
Ebenezer Williams <ebenezerw@me.com>
2017-01-27 11:09:01 -0500

    did step 3 over to select only the deposit button

--- atm.js
+++ atm.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
 	$("body").css("background", "red")
 });
 
-$(".deposit").on("click", function () {
+$("#checking").children(".deposit").on("click", function () {
 	console.log($("input").val());
 } )
