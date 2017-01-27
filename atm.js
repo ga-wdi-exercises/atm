311cbcf4b7324d2a5fb4d0356f7121ff081094a5
Ebenezer Williams <ebenezerw@me.com>
2017-01-27 13:13:03 -0500

    deposti and withdraw functions working for checking account

--- atm.js
+++ atm.js
@@ -1,14 +1,20 @@
-var amount = $("input").val();
 
+checkingAmount = 0;
 
 $(document).ready(function(){
 	$("body").css("background", "red")
 });
 
+
 $("#checking").children(".deposit").on("click", function () {
+	checkingAmount += parseInt($("#checking .input").val());
+	$("#checking .balance").html("$" + checkingAmount);
 	console.log($("input").val());
 } );
 
-$("#checking").children(".deposit").on("click", function () {
-	$(".checkingBal").html("$" + amount);
+$("#checking").children(".withdraw").on("click", function () {
+	checkingAmount -= parseInt($("#checking .input").val());
+	$("#checking .balance").html("$" + checkingAmount);
+	console.log($("input").val());
 } );
+
