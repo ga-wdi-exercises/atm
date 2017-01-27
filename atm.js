42f6018ca5e9b378f5730e5d565dd6a7fcb53b26
Elena Santana <fanelenan7@gmail.com>
2017-01-27 16:30:40 -0500

    done, but want to implement a while loop

--- atm.js
+++ atm.js
@@ -1,20 +1,26 @@
 $(document).ready(function(){
 // setting all the variables
-  var value = 0
-  // var valueSavings = 0
+  // var value = 0
+  var valueChecking = 0
+  var valueSavings = 0
   var hasInput = true
-  var balance
+  var balanceChecking
+  var balanceSavings
   // var balanceSavings
 
 // deposit on button click
 $(".deposit").click(function() {
-  //get NaN if input is empty
-  if ($(".input").val() !== NaN) {
+  //not get NaN if input is empty
+  if ($("#checking .input").val() !== '') {
     // console.log($(this))
-    balance = value + parseInt($(this).siblings(".input").val())
-    $(this).siblings(".balance").html(`$${balance}`)
-    // console.log($(".balance").val())
-    value = balance
+    balanceChecking = valueChecking + parseInt($(this).siblings("#checking .input").val())
+    $(this).siblings("#checking .balance").html(`$${balanceChecking}`)
+    valueChecking = balanceChecking
+  } else if ($("#savings .input").val() !== '') {
+    balanceSavings = valueSavings + parseInt($(this).siblings("#savings .input").val())
+    $(this).siblings("#savings .balance").html(`$${balanceSavings}`)
+
+    valueSavings = balanceSavings
   } else {
     console.log("nope");
     hasInput = false
@@ -26,11 +32,38 @@ $(".deposit").click(function() {
 })
 })
 $(".withdraw").click(function() {
-  // console.log($(this))
-  balance = value - parseInt($(this).siblings(".input").val())
-  $(this).siblings(".balance").html(`$${balance}`)
-  // console.log($(".balance").val())
-  value = balance
+  if ($("#checking .input").val() !== '') {
+    console.log($(this))
+    balanceChecking = valueChecking - parseInt($(this).siblings("#checking .input").val())
+      if (balanceChecking > 0) {
+        $("#checking ").css("background-color", "red")
+      } else {
+        $("#checking ").css("background-color", '#6C9A74')
+      }
+      if (balanceChecking < 0) {
+        hasInput = false
+        return
+      }
+    valueChecking = balanceChecking
+    $(this).siblings("#checking .balance").html(`$${balanceChecking}`)
+  } else if ($("#savings .input").val() !== '') {
+    balanceSavings = valueSavings - parseInt($(this).siblings("#savings .input").val())
+      if (balanceChecking > 0) {
+        $("#checking ").css("background-color", "red")
+      } else {
+        $("#checking ").css("background-color", '#6C9A74')
+      }
+      if (balanceSavings < 0) {
+        hasInput = false
+        return
+      }
+    valueSavings = balanceSavings
+    $(this).siblings("#savings .balance").html(`$${balanceSavings}`)
+  } else {
+    console.log("nope");
+    hasInput = false
+    return
+}
   //reset the input box
   $(".input").val(function(){
     $(this).attr("placeholder, enter and amount")
