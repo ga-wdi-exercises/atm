025cabe16eb16c13f8b93fb701c748ec9808f034
Eva Lai <evalai.dc@gmail.com>
2017-01-27 15:03:59 -0500

    alert box and color change

--- atm.js
+++ atm.js
@@ -6,23 +6,40 @@ var totalAmtChk = 0;
 // Checking Account's deposit button listen to click
 function depMoneyChk() {
   // amt of checking deposit
-  var inputValue1 = $('#checking>.input').val();
+  var inputValue = $('#checking>.input').val();
   //adding user input to the current balance
-  totalAmtChk = +inputValue1 + +totalAmtChk;
+  totalAmtChk = +inputValue + +totalAmtChk;
   // changing balance to amt of deposit
   $('#checking>.balance').text('$'+totalAmtChk);
+  if ( +totalAmtChk<=0) {
+    noMoney();
+  }
+  else {
+    someMoney();
+  }
 }
-
 $('#checking>.deposit').on('click',depMoneyChk)
 
 // Checking Account's withdrawal button listen to click
 function widrMoneyChk() {
   // amt of checking withdrawal
-  var inputValue1 = $('#checking>.input').val();
-  //adding user input to the current balance
-  totalAmtChk = +totalAmtChk - +inputValue1 ;
+  var inputValue = $('#checking>.input').val();
+  if (inputValue <= totalAmtChk) {
+    //adding user input to the current balance
+    totalAmtChk = +totalAmtChk - +inputValue;
+  }
+    //(+inputValue < +totalAmt)
+  else {
+        alert("Sorry, you do not have sufficient funds.")
+  };
   // changing balance to amt of withdrawal
   $('#checking>.balance').text('$'+totalAmtChk);
+  if (+totalAmtChk<=0) {
+    noMoney();
+  }
+  else {
+    someMoney()
+  }
 }
 
 $('#checking>.withdraw').on('click',widrMoneyChk)
@@ -31,11 +48,17 @@ $('#checking>.withdraw').on('click',widrMoneyChk)
 var totalAmtSav =0;
 function depMoneySav() {
   // amt of savings deposit
-  var inputValue2 = $('#savings>.input').val();
+  var inputValue = $('#savings>.input').val();
   //adding user input to the current balance
-  totalAmtSav = +inputValue2 + +totalAmtSav;
+  totalAmtSav = +inputValue + +totalAmtSav;
   // changing balance to amt of deposit
   $('#savings>.balance').text('$'+totalAmtSav);
+  if (+totalAmtSav<=0) {
+    noMoney();
+  }
+  else {
+    someMoney()
+  }
 }
 
 $('#savings>.deposit').on('click',depMoneySav)
@@ -43,38 +66,38 @@ $('#savings>.deposit').on('click',depMoneySav)
 // Checking Account's withdrawal button listen to click
 function widrMoneySav() {
   // amt of checking deposit
-  var inputValue2 = $('#savings>.input').val();
-  //adding user input to the current balance
-  totalAmtSav = +totalAmtSav - +inputValue2;
+  var inputValue = $('#savings>.input').val();
+  if (inputValue <= totalAmtSav) {
+    //adding user input to the current balance
+    totalAmtSav = +totalAmtSav - +inputValue;
+  }
+    //(+inputValue < +totalAmt)
+  else {
+        alert("Sorry, you do not have sufficient funds.")
+  };
   // changing balance to amt of deposit
   $('#savings>.balance').text('$'+totalAmtSav);
+  if (+totalAmtSav<=0) {
+    noMoney();
+  }
+  else {
+    someMoney()
+  }
 }
 
 $('#savings>.withdraw').on('click',widrMoneySav)
 
-
-//
-// pseudocode for ATM
-//CHECKING:
-// deposit:
-// // input dollar amount --> to deposit
-//   //  add click listener to checking's deposit button
-      //      to go to
-//     //  function should add to the current balance
-          // add the inputted text balance to the current balance.
-//
-//
-//
-//
-// withdrawal:
-//   //  add click listener to checking's deposit button
-      //      to go to
-//     //  function should subtract from the current balance
-          // subtract the inputted text balance from the current balance.
-//
-
-
-
+// when account balance <=0
+function noMoney() {
+    if (+totalAmtSav<=0 || +totalAmtChk<=0) {
+    $('.account').addClass(' zero');
+  }
+}
+function someMoney() {
+    if (+totalAmtSav>0 || +totalAmtChk>0) {
+    $('.account').removeClass(' zero');
+  }
+}
 // if withdraw too much, alert will tell you that there is too much.
 
 // if the account balance in saving is higher withdraw from the other account too.
