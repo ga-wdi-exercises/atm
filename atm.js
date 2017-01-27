3343550f69db710526c2619cf34f2012739c277f
Will Hawkins <willhawkins123@yahoo.com>
2017-01-27 11:50:47 -0500

    Basic Assignment Complete

--- atm.js
+++ atm.js
@@ -1,10 +1,6 @@
 $(document).ready(function(){
 });
 
-//Temporary Value in Checking and Savings Inputs
-$("#checking .input").val(100);
-$("#savings .input").val(100);
-
 //Create Checking Input and Checking Balance Variables
 var checkingInput = 0;
 var checkingBalance = 0;
@@ -13,17 +9,33 @@ var checkingBalance = 0;
 var savingsInput = 0;
 var savingsBalance = 0;
 
-//Create Savings Input and Savings Balance Variables
+//Check for Balance of Zero at Load
+if (checkingBalance > 0) {
+  $("#checking").removeClass("zero");
+}
+if (checkingBalance == 0) {
+  $("#checking").addClass("zero");
+}
+if (savingsBalance == 0) {
+  $("#savings").addClass("zero");
+}
+if (savingsBalance > 0) {
+  $("#savings").removeClass("zero");
+}
 
-//Upon Clicking, grab Checking Input, Console Log It, Make That Checking Balance
+
+//CHECKING DEPOSIT
 $("#checking .deposit").on("click", function(){
   checkingInput = $("#checking .input").val();
   checkingBalance = +checkingBalance + +checkingInput;
-  console.log(checkingBalance);
   $("#checking .balance").html("$ " + checkingBalance);
   $("#checking .input").val("");
+  if (checkingBalance > 0) {
+    $("#checking").removeClass("zero");
+  }
 })
 
+//CHECKING WITHDRAW
 $("#checking .withdraw").on("click", function(){
   checkingInput = $("#checking .input").val();
   if (+checkingBalance - +checkingInput < 0) {
@@ -34,20 +46,24 @@ $("#checking .withdraw").on("click", function(){
   console.log(checkingBalance);
   $("#checking .balance").html("$ " + checkingBalance);
   $("#checking .input").val("");
+  if (checkingBalance == 0) {
+    $("#checking").addClass("zero");
+  }
 })
 
-
-// SAVINGS THINGS ARE BELOW HERE
-
-//Upon Clicking, grab Savings Input, Console Log It, Make That Checking Balance
+//SAVINGS DEPOSIT
 $("#savings .deposit").on("click", function(){
   savingsInput = $("#savings .input").val();
   savingsBalance = +savingsBalance + +savingsInput;
   console.log(savingsBalance);
   $("#savings .balance").html("$ " + savingsBalance);
   $("#savings .input").val("");
+  if (savingsBalance > 0) {
+    $("#savings").removeClass("zero");
+  }
 })
 
+//SAVINGS WITHDRAW
 $("#savings .withdraw").on("click", function(){
   savingsInput = $("#savings .input").val();
   if (+savingsBalance - +savingsInput < 0) {
@@ -58,4 +74,7 @@ $("#savings .withdraw").on("click", function(){
   console.log(savingsBalance);
   $("#savings .balance").html("$ " + savingsBalance);
   $("#savings .input").val("");
+  if (savingsBalance == 0) {
+    $("#savings").addClass("zero");
+  }
 })
