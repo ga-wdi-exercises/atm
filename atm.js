648d93a0468a6075ee2b7608b51209c01c5ea63a
Bedig Galladian <bgalladian@gmail.com>
2017-01-27 10:56:31 -0500

    added click function to deposit button

--- atm.js
+++ atm.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+})
+
+$( ".deposit" ).click(function() {
+  console.log( "hello" );
 });
