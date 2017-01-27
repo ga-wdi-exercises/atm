f886854c0047e37fd725deee36557c63d54be3aa
Bedig Galladian <bgalladian@gmail.com>
2017-01-27 11:21:58 -0500

    added var for checkDeposit

--- atm.js
+++ atm.js
@@ -2,6 +2,12 @@ $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
 })
 
-$( ".deposit" ).click(function() {
+
+/*$( ".deposit" ).click(function() {
   console.log( "hello" );
-});
+});*/
+
+//Click function for checking deposit button//
+ var checkDeposit = $("#checking").children(".deposit").click(function() {
+ alert("Hello World!");
+ });
