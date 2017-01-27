0a63b1109b06952f5596a6998285fb1af76db3d7
Hannah Schuster <hschuster9@gmail.com>
2017-01-27 16:34:46 -0500

    updated

--- atm.js
+++ atm.js
@@ -1,10 +1,49 @@
-
 //checking deposit function
-$('.deposit').on('click', function(){
-  var userInputValue = parseInt($('.input').val());
-  var currentBalance = parseInt($('.balance').text().replace("$",""));
+$('#checking .deposit').on('click', function(){
+  var userInputValue = parseInt($('#checking .input').val());
+  var currentBalance = parseInt($('#checking .balance').text().replace("$",""));
   var updatedBalance = "$" + (currentBalance + userInputValue);
   $('#checking .balance').text(updatedBalance);
+
+})
+
+//checking withdraw function
+$('#checking .withdraw').on('click', function(){
+  var userInputValue = parseInt($('#checking .input').val());
+  var currentBalance = parseInt($('#checking .balance').text().replace("$",""));
+  var updatedBalance = "$" + (currentBalance - userInputValue);
+
+  if (userInputValue > currentBalance) {
+      alert("Sorry, you do not have enough funds")
+      $('#checking .balance').text("$" + currentBalance);
+  }
+  else
+      $('#checking .balance').text(updatedBalance);
+
+
+})
+
+//savings deposit function
+$('#savings .deposit').on('click', function(){
+  var userInputValue = parseInt($('#savings .input').val());
+  var currentBalance = parseInt($('#savings .balance').text().replace("$",""));
+  var updatedBalance = "$" + (currentBalance + userInputValue);
+  $('#savings .balance').text(updatedBalance);
+
+
+})
+
+//savings withdraw function
+$('#savings .withdraw').on('click', function(){
+  var userInputValue = parseInt($('#savings .input').val());
+  var currentBalance = parseInt($('#savings .balance').text().replace("$",""));
+  var updatedBalance = "$" + (currentBalance - userInputValue);
+  if (userInputValue > currentBalance) {
+      alert("Sorry, you do not have enough funds")
+      $('#savings .balance').text("$" + currentBalance);
+  }
+  else
+      $('#savings .balance').text(updatedBalance);
 
 
 })
