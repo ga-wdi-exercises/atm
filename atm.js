d3f65d3799131f1318b060d0947991d6205d0ff0
Bedig Galladian <bgalladian@gmail.com>
2017-01-27 14:23:48 -0500

    made a lot of changes and got basic function.  Copy and pasted my functions for checking and replaced the paths for savings

--- atm.js
+++ atm.js
@@ -1,13 +1,27 @@
-$(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-})
+var checkingBalance = 0;
+var savingsBalance = 0;
 
 
-/*$( ".deposit" ).click(function() {
-  console.log( "hello" );
-});*/
+$("#checking .deposit").on("click", function(){
+ var checkingInput = $("#checking .input").val()
+ checkingBalance = +checkingInput + +checkingBalance;
+ $("#checking .balance").html("$" + checkingBalance);
+});
 
-//Click function for checking deposit button//
- var checkDeposit = $("#checking").children(".deposit").click(function() {
- alert("Hello World!");
- });
+$("#checking .withdraw").on("click", function(){
+ var checkingInput = $("#checking .input").val()
+ checkingBalance = +checkingBalance - +checkingInput;
+ $("#checking .balance").html("$" + checkingBalance);
+});
+
+$("#savings .deposit").on("click", function(){
+ var savingsInput = $("#savings .input").val()
+ savingsBalance = +savingsInput + +savingsBalance;
+ $("#savings .balance").html("$" + savingsBalance);
+});
+
+$("#savings .withdraw").on("click", function(){
+ var savingsInput = $("#savings .input").val()
+ savingsBalance = +savingsBalance - +savingsInput;
+ $("#savings .balance").html("$" + savingsBalance);
+});
