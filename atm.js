92dc4f66f03423a2bc1cb13fbbe92cba4c40c3fe
Nat Carter <n.goldman.carter@gmail.com>
2017-01-27 21:35:32 -0500

    finished adding comments

--- atm.js
+++ atm.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
         bal += amount;
       // Otherwise ...
       } else {
-        // Pass account, balance, bal, and amount into the withdraw function (defined below).
+        // Pass account, balance, bal, and amount into the withdraw function (defined below), and assign its return value to bal.
         bal = withdraw(account, balance, bal, amount);
       }
       // Convert bal back to a string, prepend '$', and replace balance's html with the result.
@@ -30,35 +30,53 @@ $(document).ready(function(){
       updateColors();
     }
   });
-
   // This function handles withdraws.
   function withdraw(account, balance, bal, amount) {
+    // If account has sufficient funds ...
     if (bal >= amount) {
+      // Subtract amount from bal.
       bal -= amount;
+      // Return bal to the anonymous function in the click handler; the bal variable there will be updated, and the flow will continue.
       return bal;
+    // If account does not have sufficient funds ...
     } else {
+      // Calculate the overdraft, and assign it to an 'overdraft' variable.
       var overdraft = Math.abs(bal - amount);
+      // Get the other .account's .balance and assign it to an 'altBalance' variable.
       var altBalance = account.siblings('.account').find('.balance');
+      // Extract the numerical portion of altBalance, convert it to a number, and assign it to an 'altBal' variable.
       var altBal = parseInt(altBalance.html().slice(1));
+      // If altBal is great enough to cover the overdraft ...
       if (altBal - overdraft >= 0) {
+        // Zero out bal (which, remember, represents the balance in the first account).
         bal = 0;
+        // Subtract the overdraft from altBal.
         altBal -= overdraft;
+        // Convert altBal back to a string, prepend '$', and replace altBalance's html with the result.
         altBalance.html('$' + altBal.toString());
+        // Return bal to the anonymous function in the click handler; the bal variable there will be updated, and the flow will continue.
         return bal;
+      // If the alternative account does not have sufficient funds either ...
       } else {
+        // Throw an alert.
         alert('Insufficient Funds!');
       }
     }
   }
-
   // This function updates the background colors of each account after every transaction.
   function updateColors() {
+    // For each account ...
     $('.account').each(function() {
       var account = $(this);
+      // Extract its balance.
       var bal = parseInt(account.find('.balance').html().slice(1));
+      // If its balance is $0 ...
       if (bal === 0) {
+        // Make sure it has the 'zero' class.
         account.attr('class', 'account zero');
+      // Otherwise ...
       } else {
+        // Make sure it does not have the 'zero' class.
         account.attr('class', 'account');
       }
     });
