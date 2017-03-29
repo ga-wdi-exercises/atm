89d6bd21a2e05ff1dba57b4ac12d0c9277f9bd1c
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-03-29 15:25:28 -0400

    added variables for buttons and inputs

--- atm.js
+++ atm.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
   console.log('jQuery Works!')
+
+  // checking variables
+  var checkingDeposit = $('#checking input.deposit');
+  var checkingWithdraw = $('#checking input.withdraw');
+  var savingsInput = $('#checking input.input')
+
+  //savings variables
+  var savingsDeposit = $('#savings input.deposit');
+  var savingsWithdraw = $('#savings input.deposit');
+  var savingsInput = $('#savings input.input');
 });
