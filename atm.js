58de0a499cb6651d4c6fcd4e5daa95ab6a28819e
meagdoh <meagdoherty@gmail.com>
2017-03-29 15:50:26 -0400

    Add variables

--- atm.js
+++ atm.js
@@ -1,3 +1,24 @@
-$(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-});
+var checkingBalance = ("#checking .balance")
+var checkingInput = ("#checking .input")
+var checkingDeposit = ("#checking .deposit")
+var checkingWithdraw = ("#checking .withdraw")
+
+var savingsBalance = ("#savings .balance")
+var savingsInput = ("#savings .input")
+var savingsDeposit = ("#savings .deposit")
+var savingsWithdraw = ("#savings .withdraw")
+
+
+//We need click listeners for
+// 1) Deposit button
+// 2) Withdraw button
+
+
+
+
+
+// //$(document).ready(function(){
+// // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+// });
+
+// $("body").css("background-color", "red");
