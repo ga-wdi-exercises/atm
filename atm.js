039e30855810575339d8da44d0813f863ac4069d
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-03-29 19:37:13 -0400

    get deposit button working

--- atm.js
+++ atm.js
@@ -1,13 +1,63 @@
 $(document).ready(function(){
-  console.log('jQuery Works!')
+  console.log('jQuery Works!');
 
   // checking variables
-  var checkingDeposit = $('#checking input.deposit');
-  var checkingWithdraw = $('#checking input.withdraw');
-  var savingsInput = $('#checking input.input')
+  // buttons
+  var checkingDeposit = $('#checking .deposit');
+  var checkingWithdraw = $('#checking .withdraw');
+  // input
+  var checkingInput = $('#checking .input');
+  //balance
+  var checkingBalanceDisplay = $('#checking .balance');
 
-  //savings variables
-  var savingsDeposit = $('#savings input.deposit');
-  var savingsWithdraw = $('#savings input.deposit');
-  var savingsInput = $('#savings input.input');
+  // savings variables
+  // buttons
+  var savingsDeposit = $('#savings .deposit');
+  var savingsWithdraw = $('#savings .withdraw');
+  // input
+  var savingsInput = $('#savings .input');
+  //balance
+  var savingsBalanceDisplay = $('#savings .balance');
+
+  // var checkingInputVal = checkingInput.val();
+  // var checkingsUpdateBalance = checkingBalance.text('$' + checkingInputVal);
+
+  var savingBalance = 0;
+  var checkingBalance = 0;
+
+  //checking functions
+  //fires when you click the checking deposit button
+  var depositChecking = function() {
+    var checkingInputVal = checkingInput.val();
+    var newValue = parseInt(checkingInputVal);
+
+    checkingBalance += newValue;
+    checkingBalanceDisplay.text('$' + checkingBalance);
+  };
+
+  //fires when you click the checking withdraw button
+  var withdrawChecking = function() {
+    console.log('withdrew some money')
+    var checkingInputVal = checkingInput.val();
+    var newValue = parseInt(checkingInputVal);
+
+    checkingBalance -= checkingInputVal;
+    console.log(checkingBalance);
+  };
+
+  //savings functions
+  //fires when you click the savings deposit button
+  var depositSavings = function() {
+    console.log('made a savings deposit')
+  };
+  //fires when you click the savings withdraw button
+  var withdrawSavings = function() {
+    console.log('made a savings withdrawal')
+  };
+
+  checkingDeposit.on('click', depositChecking)
+  checkingWithdraw.on('click', withdrawChecking)
+
+  savingsDeposit.on('click', depositSavings)
+  savingsWithdraw.on('click', withdrawSavings)
 });
