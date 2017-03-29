a114d4aa02f11e6c65a0e5b69cc6ab22407310a1
Curtis Ostle <curtisostle@gmail.com>
2017-03-29 16:48:40 -0400

    checking account working

--- atm.js
+++ atm.js
@@ -1,10 +1,31 @@
 var checkingDeposit = $('#checking input.deposit');
-var checkingInput = $('#checking input.input');
-var checkingBalance = $('#checking div.balance');
+  var checkingWithdraw = $('#checking input.withdraw');
+  var checkingInput = $('#checking input.input')
+  var savingsDeposit = $('#savings input.deposit');
+    var savingsWithdraw = $('#savings input.deposit');
+    var checkingBalance = $('#checking div.balance');
+    var savingsInput = $('#savings input.input');
 
-checkingDeposit.on('click', sayHello);
 
-function sayHello() {
+var savedChecking = 0;
+
+checkingWithdraw.on('click', handleWithdraw)
+checkingDeposit.on('click', handleDeposit);
+
+function handleDeposit() {
+  let inputNumber = parseInt(checkingInput.val());
   console.log('Hello');
-  checkingBalance.html('$' + checkingInput.val());
+  savedChecking += inputNumber;
+  checkingBalance.html('$' + savedChecking);
+}
+
+function handleWithdraw() {
+  let inputNumber = parseInt(checkingInput.val());
+  if(savedChecking < inputNumber) {
+    console.log('not enough dough!');
+  }
+  else {
+    savedChecking -= inputNumber
+    checkingBalance.html('$' + savedChecking);
+  }
 }
