8e2801d3baeafa9f7957f37773ded346bb31f40c
Caroline Chen <carolinecaroline@gmail.com>
2017-03-29 16:45:39 -0400

    add adding and subtracting functionality to checking account

--- atm.js
+++ atm.js
@@ -1,3 +1,45 @@
 $(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+
+// to make sure I'm targeting the right buttons
+// $("#checking .deposit").css("background-color", "red");
+// $("#checking .withdraw").css("background-color", "blue");
+// $("#checking .balance").css("background-color", "purple");
+// $("#checking .input").css("background-color", "green");
+
+var depositButton = $("#checking .deposit");
+var withdrawButton = $("#checking .withdraw");
+var balanceWindow = $("#checking .balance");
+var input = $("#checking .input");
+var balance = 0;
+
+
+
+var deposit = function() {
+	var inputValue = parseInt(input.val());	
+
+	balance = balance + inputValue;
+	
+	// var balanceAmt = current + inputValue;
+	balanceWindow.html(balance);
+};
+
+var withdraw = function() {
+	var inputValue = parseInt(input.val());	
+
+	balance = balance - inputValue;
+	
+	// var balanceAmt = current + inputValue;
+	balanceWindow.html(balance);
+};
+
+
+
+depositButton.on("click", deposit);
+
+withdrawButton.on("click", withdraw);
+
+
 });
+
+
+
