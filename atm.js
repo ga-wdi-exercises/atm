4ea8f44cc47c26d8234cebe49f08ae8d48ca9ba0
Ammar Alasadi <alasadi.ammar@gmail.com>
2017-03-29 16:48:12 -0400

    added add function/buggy

--- atm.js
+++ atm.js
@@ -1,12 +1,15 @@
 //Checking
-var display = $("#checking .balance");
+var balance = $("#checking .balance");
 var input = $("#checking .input");
 var withdraw = $("#checking .withdraw");
 var deposit = $("#checking .deposit");
 // Savings 
-var SavingsSisplay = $("#savings.balance");
+var SavingsBalance = $("#savings.balance");
 var savingsInput = $("#savings .input");
 var savingsWithdraw = $("#savings .withdraw");
 var savingDeposit = $("#savings .deposit");
 
+deposit.on("click", updateSavings);
 
+function updateSavings() {
+   	
