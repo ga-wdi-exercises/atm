d9db501ac5d56fc4283c713684149f2cbf6ebd98
Ammar Alasadi <alasadi.ammar@gmail.com>
2017-03-29 15:52:29 -0400

    Fisrst

--- atm.js
+++ atm.js
@@ -1,3 +1,12 @@
-$(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-});
+//Checking
+var display = $("#checking .balance");
+var input = $("#checking .input");
+var withdraw = $("#checking .withdraw");
+var deposit = $("#checking .deposit");
+// Savings 
+var SavingsSisplay = $("#savings.balance");
+var savingsInput = $("#savings .input");
+var savingsWithdraw = $("#savings .withdraw");
+var savingDeposit = $("#savings .deposit");
+
+
