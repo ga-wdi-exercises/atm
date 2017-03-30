b94238b398a61802bcf3860da3f7ef4573ba2d42
Erin <erinedawson@gmail.com>
2017-03-30 13:39:16 -0400

    set checking functions(succesful)

--- atm.js
+++ atm.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-});
+  var checkingDeposit = $("#checkingDeposit");
+  var checkingWithdraw = $("#checkingWithdraw");
+  var savingsDeposit = $("#savingsDeposit");
+  var savingsWithdraw = $("#savingsWithdraw");
+
+  //now you need to return the deposit amount//
+
+  $(checkingDeposit).on('click', function(){
+    var deposit = parseInt($("#checkingInput").val());
+    var balance = parseInt($(".checkingbalance").text().replace("$", " "))
+    var newBalance = "$" + (deposit + balance);
+    $(".checkingbalance").text(newBalance)
+  })
+
+//do the same thing, but withdraw//
+$(checkingWithdraw).on('click', function(){
+  var withdraw = parseInt($("#checkingInput").val());
+  var balance = parseInt($(".checkingbalance").text().replace("$", ""))
+  var newBalance = "$" + (balance - withdraw);
+  $(".checkingbalance").text(newBalance)
+
+})
+
+})
