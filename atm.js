90aed9d124a8be97877370169e20ea1c23f323aa
Ick Hyun Kwon <kwonih1020@gmail.com>
2017-03-30 09:06:17 -0400

    edited

--- atm.js
+++ atm.js
@@ -1,3 +1,28 @@
-$(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+$(document).ready(function() {
+
+    // console.log("Hello");
+
+
+    var checkBalance = $("#checking .balance");
+    var checkDeposit = $("#checking input.deposit");
+    var checkWithdraw = $("#checking div.withdraw");
+    var checkInput = $("#checking input.input");
+
+    var savBalance = $("#savings div.balance");
+    var savDeposit = $("#savings div.deposit");
+    var savingWithdraw = $("#savings div.withdraw");
+    var savInput = $("#savings div.input");
+
+    var balance = 0;
+    var savingBalance = 0;
+
+    checkDeposit.on("click", addAmount);
+
+    function addAmount() {
+        var value = parseInt(checkInput.val());
+        balance = value + balance
+        checkBalance.text("$" + balance);
+        console.log(value);
+    }
+    // console.log(checkDeposit)
 });
