109b7ef73285741e67e550a813b0d1f4f5d75985
Curtis Ostle <curtisostle@gmail.com>
2017-03-30 14:08:25 -0400

    finished all functionality w/ bonus

--- atm.js
+++ atm.js
@@ -1,17 +1,22 @@
-  var checkingDeposit = $('#checking input.deposit');
-  var checkingWithdraw = $('#checking input.withdraw');
-  var checkingInput = $('#checking input.input')
-  var checkingBalance = $('#checking div.balance');
+//select jquery elements
+var checkingDeposit = $('#checking input.deposit');
+var checkingWithdraw = $('#checking input.withdraw');
+var checkingInput = $('#checking input.input')
+var checkingBalance = $('#checking div.balance');
+
+var savingsWithdraw = $('#savings input.withdraw');
+var savingsInput = $('#savings input.input');
+var savingsBalance = $('#savings div.balance');
+var savingsDeposit = $('#savings input.deposit');
 
 
-  var savingsWithdraw = $('#savings input.withdraw');
-  var savingsInput = $('#savings input.input');
-  var savingsBalance = $('#savings div.balance');
-  var savingsDeposit = $('#savings input.deposit');
+//creat a variables to store balances and update display
+savedChecking = 0;
+checkingBalance.html('$' + savedChecking);
+savedSavings = 100;
+savingsBalance.html('$' + savedSavings);
 
-var savedChecking = 0;
-//creat a variable to store balance
-
+//functions to turn either account red if balance is 0
 function checkChecking() {
   if(!savedChecking) {
     checkingBalance.addClass('zero');
@@ -21,33 +26,6 @@ function checkChecking() {
   }
 }
 checkChecking();
-//check in balance is 0, if yes, make the background-color red.
-
-checkingWithdraw.on('click', handleCheckingWithdraw);
-checkingDeposit.on('click', handleCheckingDeposit);
-//add event handlers to buttons
-
-
-function handleCheckingDeposit() {
-  let inputNumber = parseInt(checkingInput.val());
-  console.log('Hello');
-  savedChecking += inputNumber;
-  checkingBalance.html('$' + savedChecking);
-  checkChecking();
-}
-function handleCheckingWithdraw() {
-  let inputNumber = parseInt(checkingInput.val());
-  if(savedChecking < inputNumber) {
-    console.log('not enough dough!');
-  }
-  else {
-    savedChecking -= inputNumber
-    checkingBalance.html('$' + savedChecking);
-    checkChecking();
-  }
-}
-
-var savedSavings = 0;
 
 function checkSavings() {
   if(!savedSavings) {
@@ -59,6 +37,40 @@ function checkSavings() {
 }
 checkSavings();
 
+//add event handlers to checking buttons
+checkingWithdraw.on('click', handleCheckingWithdraw);
+checkingDeposit.on('click', handleCheckingDeposit);
+
+//
+function handleCheckingDeposit() {
+  let inputNumber = parseInt(checkingInput.val());
+  console.log('Hello');
+  savedChecking += inputNumber;
+  checkingBalance.html('$' + savedChecking);
+  checkChecking();
+}
+
+function handleCheckingWithdraw() {
+  let inputNumber = parseInt(checkingInput.val());
+  if (inputNumber <= savedChecking) {
+    savedChecking -= inputNumber
+    checkingBalance.html('$' + savedChecking);
+    checkChecking();
+  }
+//if checking can't cover withdraw, take from savings.
+  else if (savedChecking < inputNumber && savedSavings > inputNumber) {
+    savedSavings -= (inputNumber - savedChecking);
+    savingsBalance.html('$' + savedSavings);
+    checkingBalance.html('$' + 0);
+    checkChecking();
+    checkSavings();
+  }
+  else {
+    console.log('NOT ENOUGH MONEY FOOL')
+    checkChecking();
+  }
+}
+//add event listeners to savings buttons
 savingsWithdraw.on('click', handleSavingsWithdraw);
 savingsDeposit.on('click', handleSavingsDeposit);
 
@@ -69,11 +81,12 @@ function handleSavingsDeposit() {
   savingsBalance.html('$' + savedSavings);
   checkSavings();
 }
-
 function handleSavingsWithdraw() {
   let inputNumber = parseInt(savingsInput.val());
   if(savedSavings < inputNumber) {
-    console.log('not enough dough!');
+    savedSavings = 0;
+    savingsBalance.html('$' + savedSavings);
+    console.log('All tapped out!');
   }
   else {
     savedSavings -= inputNumber
