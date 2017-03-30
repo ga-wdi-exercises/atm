a4dc79bc4c07ca7883889fb1efc91b4de8e43bbe
Caroline Chen <carolinecaroline@gmail.com>
2017-03-30 13:57:00 -0400

    make minor formatting changes and add notes

--- atm.js
+++ atm.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
 
-// to make sure I'm targeting the right buttons
+// to make sure I'm targeting the right buttons:
 // $("#checking .deposit").css("background-color", "red");
 // $("#checking .withdraw").css("background-color", "blue");
 // $("#checking .balance").css("background-color", "purple");
 // $("#checking .input").css("background-color", "green");
 
+//target the applicable elements and set them to variables:
 var depositButtonC = $("#checking .deposit");
 var withdrawButtonC = $("#checking .withdraw");
 var balanceWindowC = $("#checking .balance");
@@ -18,31 +19,16 @@ var balanceWindowS = $("#savings .balance");
 var inputS = $("#savings .input");
 var balanceS = 0;
 
-//notes from class:
-//use parseInt to convert it to actual number so we can do math on it.  inputs are generally strings
-//could use .text to get value of a div, but not of an input tag
-//parseInt doesn't like $ in front.  parseInt("$50") will return NaN
-//var blanace = parseInt($(".savings.balance").text().replace("$", ""))
-//good way to debug is use typeof() to see if it's a string, etc
-//parseFloat is for decimal points - won't round up, just cuts off
-//some returns break the chain of whether you're getting jquery elements
-
-//could do the parsing separately and stick to a variable, clean, and just use clean going forward in those places
-//var clean(input) {
-	// return parseInt($(input.text(),replace....))
-//}
-
-
-
 
 var depositC = function() {
+	//get the input value, convert it to a string, and set to variable called inputValue:
 	var inputValue = parseInt(inputC.val());	
-
+	//add this inputValue to the current balance to get the new balance:
 	balanceC = balanceC + inputValue;
-	
-	// var balanceAmt = current + inputValue;
+	//spit this out in the window:
 	balanceWindowC.html("$" + balanceC);
-
+	//if the new balance is zero, add the previously set up class called zero to set the background color to red.
+	//need to remove the class if the balance goes positive again:
 	if (balanceC == 0){
 		$("#checking").addClass("zero");
 	} else {
@@ -55,7 +41,6 @@ var withdrawC = function() {
 	if (balanceC - inputValue >= 0){
 	balanceC = balanceC - inputValue;
 	
-	// var balanceAmt = current + inputValue;
 	balanceWindowC.html("$" + balanceC);
 	};
 
@@ -69,7 +54,6 @@ var depositS = function() {
 
 	balanceS = balanceS + inputValue;
 	
-	// var balanceAmt = current + inputValue;
 	balanceWindowS.html("$" + balanceS);
 
 	if (balanceS == 0){
@@ -84,7 +68,6 @@ var withdrawS = function() {
 	if (balanceS - inputValue >= 0){
 	balanceS = balanceS - inputValue;
 	
-	// var balanceAmt = current + inputValue;
 	balanceWindowS.html("$" + balanceS);
 	};
 
@@ -94,9 +77,7 @@ var withdrawS = function() {
 };
 
 
-
-
-
+//target the previously defined functions using the previously set button variables and listeners
 depositButtonC.on("click", depositC);
 
 withdrawButtonC.on("click", withdrawC);
@@ -108,4 +89,16 @@ withdrawButtonS.on("click", withdrawS);
 });
 
 
+//notes from class:
+//use parseInt to convert it to actual number so we can do math on it.  inputs are generally strings
+//could use .text to get value of a div, but not of an input tag
+//parseInt doesn't like $ in front.  parseInt("$50") will return NaN
+//var balance = parseInt($(".savings.balance").text().replace("$", ""))
+//good way to debug is use typeof() to see if it's a string, etc
+//parseFloat is for decimal points - won't round up, just cuts off
+//some returns break the chain of whether you're getting jquery elements
 
+//could do the parsing separately and stick to a variable, clean, and just use clean going forward in those places
+//var clean(input) {
+	// return parseInt($(input.text(),replace....))
+//}
