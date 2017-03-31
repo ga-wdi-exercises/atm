c52f36ab6385e7b934b353edfa4f6e47b79acc04
Stormy Ramsey <stormy.ramsey@gmail.com>
2017-03-31 16:24:18 -0400

    atm hw

--- atm.js
+++ atm.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-});
+
+var checkingBalance = $("#checking .balance");
+var checkingInput = $("#checking .checkingInput");
+var checkingDeposit = $("#checking .checkingDeposit");
+var checkingWithdraw = $("#checking .checkingWithdraw");
+
+    // checking deposit
+  $(".checkingDeposit").on("click", function(){
+    $(checkingBalance).text().replace("$", "") // get balance, replace text with val
+    var deposit = parseInt(checkingInput).val()
+    var newBalance = "$" + (checkingBalance + checking)
+    $(checkingBalance).text(deposit)
+})
+
+// checking withdrawal /w overdraft protection
+
+// savings deposit
+
+// savings withdraw
+
+})
