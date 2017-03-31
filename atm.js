aa6f1ad2ac92472c1be63d1be4a951f99d24c292
Omar Chaudhry <omarchaudhry.1991@gmail.com>
2017-03-31 09:10:47 -0400

    I ran out of time so it's incomplete

--- atm.js
+++ atm.js
@@ -1,3 +1,45 @@
 $(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+//Adding or Depositing Money
+$("chkMoneyDeposit").on("click", depositMoney);
+$("savMoneyDeposit").on("click", depositMoney);
+//Subtracting or Withdrawing Money
+$("chkMoneyWithdraw").on("click", withdrawMoney);
+$("savMoneyWithdraw").on("click", withdrawMoney);
+var totalbalance = 0;
+//deposits the money
+function depositMoney(){
+  var result = previousBal.text();
+  var newbal = parseInt(result.replace("$", ""));
+  var newRecord = parseInt($(this).siblings("bankMoney").val());
+
+      $(previousBal).text(function() {
+        var total = newRecord + newbal;
+        return "$" + total;
+      });
+
+//Clear all values
+$(this).siblings("input.bankMoney").val("")
+}
+//Adds up the total sum balance
+var sumBal
+function getSumBal () {
+var chkBalance = $("#checkingBalance").text();
+var checkingBalance = parseInt(chkBalance.replace("$", ""));
+var savBalance = $("#savingsBalance").text();
+var savingsBalance = parseInt(savBalance.replace("$", ""));
+sumBal = savingsBalance + checkingBalance;
+return sumBal;
+}
+//withdraws the money
+function withdrawMoney() {
+  getSumBal();
+  var prvBal = $(this).siblings("div.totalbalance");
+  var result = prvBal.text();
+  var currentBal = parseInt(result.replace("$", ""));
+  var newWD = parseInt($(this).siblings("input.bankMoney").val());
+  $(prvBal).text()
+
+}
+
+
 });
