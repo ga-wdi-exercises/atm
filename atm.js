e8ea6a9785e4f5974f86059eb0718eceb94ac8bb
Paul Christian-Brown <pchristianbrown@gmail.com>
2017-03-31 13:31:22 -0400

    ATM start

--- atm.js
+++ atm.js
@@ -1,3 +1,48 @@
 $(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-});
+  // checking deposit
+  $("#checkingDeposit").on("click", function(){
+    var deposit = parseInt($("#checkingInput").val())
+    var balance = parseInt($(".checking.balance").text().replace("$", ""))
+    var newBalance = "$" + (balance + deposit)
+    $(".checking.balance").text(newBalance)
+  })
+
+
+  // checking withdrawal /w overdraft protection
+  $("#checkingWithdraw").on("click", function(){
+    var withdraw = parseInt($("#checkingInput").val())
+    var checkingBalance = parseInt($(".checking.balance").text().replace("$", ""))
+    var savingsBalance = parseInt($(".savings.balance").text().replace("$", ""))
+    var totalBalance = checkingBalance + savingsBalance
+    if((checkingBalance - withdraw) > 0){
+      var newBalance = "$" + (checkingBalance - withdraw)
+      $(".checking.balance").text(newBalance)
+    }
+    else if((totalBalance - withdraw) > 0){
+      var overDraft = withdraw - checkingBalance
+      $(".checking.balance").text("$0")
+      var newSavingsBalance = "$" + (savingsBalance - overDraft)
+      $(".savings.balance").text(newSavingsBalance)
+    }
+  })
+
+
+  // savings deposit
+  $("#savingsDeposit").on("click", function(){
+    var deposit = parseInt($("#savingsInput").val())
+    var balance = parseInt($(".savings.balance").text().replace("$", ""))
+    var newBalance = "$" + (balance + deposit)
+    $(".savings.balance").text(newBalance)
+  })
+
+
+  // savings withdraw
+  $("#savingsWithdraw").on("click", function(){
+    var withdraw = parseInt($("#savingsInput").val())
+    var savingsBalance = parseInt($(".savings.balance").text().replace("$", ""))
+    if((savingsBalance - withdraw) > 0){
+      var newBalance = "$" + (savingsBalance - withdraw)
+      $(".savings.balance").text(newBalance)
+    }
+  })
+})
