ecb3a8fb6dcd70ded366e85f82829ec9953c9584
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-03-30 23:36:14 -0400

    working savings withdraw, add css when amount is less than 0

--- atm.js
+++ atm.js
@@ -18,33 +18,54 @@ $(document).ready(function(){
     // Add each new deposit to transactionHistory array
     transactionHistory.push(inputVal);
     console.log(transactionHistory);
+    // Remove zero class for amounts greater than 0
+    if(newBalance > 0) {
+      $('#checking .balance').removeClass('zero');
+    }
+
   });
 
   withdrawCheckingButton.on('click',function(){
-    console.log('withdraw')
     var inputVal = parseInt($('#checking .input').val());
     var checkingBalanceDisplay = parseInt($('#checking .balance').text().replace("$",""));
+    var savingsBalanceDisplay = parseInt($('#savings .balance').text().replace("$",""));
     var newBalance = checkingBalanceDisplay - inputVal;
-
-    $('#checking .balance').text('$' + newBalance);
+    $('#checking .balance').text("$" + newBalance);
     // Add each new deposit to transactionHistory array
     transactionHistory.push(-inputVal);
     console.log(transactionHistory);
+    // Add zero class for amounts less than or equal to
+    if(newBalance < 0) {
+      $('#checking .balance').addClass('zero');
+    }
   });
 
   depositSavingsButton.on('click',function(){
-    console.log('deposit savings');
     var inputVal = parseInt($('#savings .input').val());
     var savingsBalanceDisplay = parseInt($('#savings .balance').text().replace("$",""));
     var newBalance = savingsBalanceDisplay + inputVal;
-
     $('#savings .balance').text('$' + newBalance);
+    // Add each new deposit to savTransactionHistory array
     savTransactionHistory.push(inputVal);
     console.log(savTransactionHistory);
+    // Remove zero class for amounts greater than 0
+    if(newBalance > 0) {
+      $('#savings .balance').removeClass('zero');
+    }
   })
 
   withdrawSavingsButton.on('click',function(){
-    console.log('withdraw savings');
+    var inputVal = parseInt($('#savings .input').val());
+    var savingsBalanceDisplay = parseInt($('#savings .balance').text().replace("$",""));
+    var newBalance = savingsBalanceDisplay - inputVal;
+    $('#savings .balance').text('$' + newBalance);
+    // Add each new deposit to savTransactionHistory array
+    savTransactionHistory.push(-inputVal);
+    console.log(savTransactionHistory);
+    // Add zero class for amounts less than or equal to
+    if(newBalance < 0) {
+      $('#savings .balance').addClass('zero');
+    }
   })
 
 
