95c7584fa2703e0b5f141f5f1f35706f1dfbcc96
Andres Arango <andresarangov17@gmail.com>
2017-03-30 23:59:06 -0400

    add balance minimum and color zero

--- atm.js
+++ atm.js
@@ -18,14 +18,26 @@ $(document).ready(function(){
     var balance = parseInt($("#checking .balance").text().replace("$", ""));
     var withdraw = parseInt($("#checking .input").val());
     var total = balance - withdraw;
-    $("#checking .balance").text("$" + total);
+    if (total > 0) {
+      $("#checking .balance").text("$" + total);
+    } else {
+      $("#checking .balance").text("$" + "0");
+      $("#checking").addClass("zero");
+    }
+
   });
 
   $("#savings .withdraw").on("click", function() {
     var balance = parseInt($("#savings .balance").text().replace("$", ""));
     var withdraw = parseInt($("#savings .input").val());
     var total = balance - withdraw;
-    $("#savings .balance").text("$" + total);
+    if (total > 0) {
+      $("#savings .balance").text("$" + total);
+    } else {
+      $("#savings .balance").text("$" + "0");
+      $("#savings").addClass("zero");
+    }
+
   });
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
 });
