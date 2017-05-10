71328d79cabd1c1dcb2e30a0d2f33e54ec3a83be
Scott Pierce <scottpiercew@gmail.com>
2017-05-10 12:25:41 -0400

    finished checking deposit

--- atm.js
+++ atm.js
@@ -6,26 +6,29 @@ $(document).ready(function(){
 //if Deposit is clicked amount typed in is added to account
 
 $(".deposit").on("click", function(){
-  // event.preventDefault()
+  console.log("hello")
+  var inputValue = $(".input").val()
+  var newInputValue = inputValue.replace("$", "")
+  console.log(newInputValue)
 
-  var value = $(".input").val()
-  var noDollarInput = parseInt(value)
-console.log(noDollarInput)
+  var currentBalance = $(".balance").html()
+  var noDollarCurrent = currentBalance.replace("$", "")
+  var newCurrentBalance = parseInt(noDollarCurrent)
+  console.log(newCurrentBalance)
 
-  var currentBalance = $(".balance").val()
-  var noDollarCurrent = parseInt(currentBalance)
-console.log(currentBalance)
-
-  var newBalance = noDollarCurrent + noDollarInput
-
-console.log(newBalance)
-  // console.log(typeof value)
+  $(".balance").html("$" + (+newInputValue + +newCurrentBalance))
+  console.log(typeof newCurrentBalance)
 })
-
 
 //if Withdraw is clicked balance of account is checked for sufficient funds
 //if sufficient funds allow withdraw
 //if amount typed in is not available in account give user an error message
+
+$(".withdraw").on("click", function(){
+  console.log("step two")
+  var withdrawValue = $(".input").val()
+  var wValue = withdrawValue.replace("$", "")
+})
 
 
 //if account balance is zero turn .zero red
