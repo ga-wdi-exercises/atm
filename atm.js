bd38b98eb5810b5401f3e943d7eb31310e85f46e
Mark Moeller <MarkMoellerUVA@gmail.com>
2017-05-10 10:41:26 -0400

    Separated Functions and added overdraft

--- atm.js
+++ atm.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+updateBalance()// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
 });
 
 checkingBalance = 0
@@ -7,50 +7,71 @@ savingsBalance = 0
 
 checkingBalanceDisplay = $("#checking .balance")
 savingsBalanceDisplay = $("#savings .balance")
-depositButton = $(".deposit")
-withdrawButton = $(".withdraw")
+depositCheckingButton = $("#checking .deposit")
+withdrawCheckingButton = $("#checking .withdraw")
+depositSavingsButton = $("#savings .deposit")
+withdrawSavingsButton = $("#savings .withdraw")
 checkingInput = $("#checking .input")
 savingsInput = $("#savings .input")
 
 
 //$("body").css("background", "red")
 
-depositButton.on("click",deposit)
-withdrawButton.on("click",withdraw)
+depositCheckingButton.on("click",depositChecking)
+withdrawCheckingButton.on("click",withdrawChecking)
+
+depositSavingsButton.on("click",depositSavings)
+withdrawSavingsButton.on("click",withdrawSavings)
 
 
 
-function deposit() {
+function depositChecking() {
   var depositAmount = parseInt(checkingInput.val())
-  var accountId = $(this).parent().attr('id')
-
-  if (accountId == "checking") {
-    checkingBalance += depositAmount
-  }else if (accountId == "savings") {
-    savingsBalance += depositAmount
-  }
-
+  //var accountId = $(this).parent().attr('id')
+  checkingBalance += depositAmount
   updateBalance()
-  console.log("You made a deposit")
 }
 
-function withdraw(){
-  var withdrawAmount = parseInt(checkingInput.val())
-  var accountId = $(this).parent().attr('id')
 
-  if (accountType < withdrawAmount) {
-    alert("You need more moneyz!")
+function depositSavings() {
+  var depositAmount = parseInt(savingsInput.val())
+  //var accountId = $(this).parent().attr('id')
+  savingsBalance += depositAmount
+  updateBalance()
+}
+
+
+function withdrawChecking(){
+  var withdrawAmount = parseInt(checkingInput.val())
+  //var accountId = $(this).parent().attr('id')
+
+  if (checkingBalance < withdrawAmount) {
+    if (confirm("Would you like to enable overdraft protection and withdraw from you Savings?")) {
+      overdraftChecking(withdrawAmount)
+    }
     return;
   }
 
-  if (accountId == "checking") {
-    checkingBalance -= withdrawAmount
-  } else if (accountId == "savings") {
-    savingsBalance -= withdrawAmount
-  }
-
+  checkingBalance -= withdrawAmount
   updateBalance()
 }
+
+
+function withdrawSavings(){
+  var withdrawAmount = parseInt(savingsInput.val())
+  //var accountId = $(this).parent().attr('id')
+
+  if (savingsBalance  < withdrawAmount) {
+    if (confirm("Would you like to enable overdraft protection and withdraw from you Checking?")) {
+      overdraftSavings(withdrawAmount)
+    }
+    return;
+  }
+
+  savingsBalance -= withdrawAmount
+  updateBalance()
+}
+
 
 function updateBalance(){
   checkingBalanceDisplay.text("$" + checkingBalance)
@@ -58,15 +79,44 @@ function updateBalance(){
   checkforZero()
 }
 
+
 function checkforZero(){
   if (checkingBalance == 0){
     checkingBalanceDisplay.addClass("zero")
   }else{
     checkingBalanceDisplay.removeClass("zero")
   }
-  if (savingsBalanceDisplay == 0){
+  if (savingsBalance == 0){
     savingsBalanceDisplay.addClass("zero")
   }else{
     savingsBalanceDisplay.removeClass("zero")
   }
+}
+
+
+function overdraftChecking(amount){
+  var totalBalance = checkingBalance + savingsBalance
+  var remainder = checkingBalance - amount
+
+  if (totalBalance >= amount) {
+    checkingBalance = 0
+    savingsBalance += remainder
+  }else{
+    alert("You need more moneyz!")
+  }
+  updateBalance()
+}
+
+function overdraftSavings(amount){
+  var totalBalance = checkingBalance + savingsBalance
+  var remainder = savingsBalance - amount
+
+  if (totalBalance >= amount) {
+    savingsBalance = 0
+    checkingBalance += remainder
+  }else{
+    alert("You need more moneyz!")
+  }
+  updateBalance()
+
 }
