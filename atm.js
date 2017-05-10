25b59dd653ceba3622b5ee10720e3a67cb264a83
Tim Chase <tchase4544@gmail.com>
2017-05-10 09:38:04 -0400

    setup and deposit feature working

--- atm.js
+++ atm.js
@@ -1,3 +1,31 @@
 $(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+	var userAccounts = {
+		checkings : 0,
+		savings : 0
+	};
+	//deposit checking
+	$('#checking .deposit').on("click",function(){
+		moneyCheck()
+	})
+	//deposit saving
+	$('#savings .deposit').on("click",function(){
+		moneySave()
+	})
+
+
+	//function to deposit $$$ checking
+	function moneyCheck() {
+		var deposit = $('#checking .input').val()
+		deposit = parseInt(deposit,10)
+		userAccounts.checkings += deposit;
+		$('#checking .balance').text("$"+userAccounts.checkings)
+	}
+	//function to deposit $$$ saving
+	function moneySave() {
+		var deposit = $('#savings .input').val()
+		deposit = parseInt(deposit,10)
+		userAccounts.savings += deposit;
+		$('#savings .balance').text("$"+userAccounts.savings)
+	}
+	//
 });
