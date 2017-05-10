d858afd39c58f2b4e4eed7f031bfe3e9b7459308
Tim Chase <tchase4544@gmail.com>
2017-05-10 11:31:38 -0400

    functioning ATM

--- atm.js
+++ atm.js
@@ -6,11 +6,13 @@ $(document).ready(function(){
 
 	//deposit checking
 	$('#checking .deposit').on("click",function(){
+
 		moneyCheck()
 		inTheRedCheckings()
 	})
 	//deposit saving
 	$('#savings .deposit').on("click",function(){
+
 		moneySave()
 		inTheRedSavings()
 	})
@@ -18,15 +20,12 @@ $(document).ready(function(){
 	$('#checking .withdraw').on("click", function(){
 		
 		withdrawlCheck()
-
 		inTheRedCheckings()
-
 	})
 	//withdrawl form savings
 	$('#savings .withdraw').on("click", function(){
 
 		withdrawlSave()
-
 		inTheRedSavings()
 	})
 
@@ -42,24 +41,21 @@ $(document).ready(function(){
 			displayCheck()
 		}
 	}
-/////////////////////////////////////
-function overDraftProtection() {
-	var withdrawlAmount = $('#checking .input').val()
-	var theDifference = withdrawlAmount - userAccounts.checkings
-	console.log(theDifference)
-	console.log(theDifference - userAccounts.savings)
-	if(userAccounts.savings - theDifference < 0){
-		alert("Error! you do not have enough funds");
-		return
-	}else{
-		userAccounts.savings -= theDifference
-		displaySave();
-		userAccounts.checkings = 0;
-		displayCheck();
-		
+	//OverDraft protection
+	function overDraftProtection() {
+		var withdrawlAmount = $('#checking .input').val()
+		var theDifference = withdrawlAmount - userAccounts.checkings
+		if(userAccounts.savings - theDifference < 0){
+			alert("Error! you do not have enough funds");
+			return
+		}else{
+			userAccounts.savings -= theDifference
+			displaySave();
+			userAccounts.checkings = 0;
+			displayCheck();
+		}
 	}
-}
-///////////////////////////////////////
+
 	function displayCheck() {
 		$('#checking .balance').text("$"+userAccounts.checkings)
 	}
@@ -70,7 +66,7 @@ function overDraftProtection() {
 	function withdrawlSave() {
 		var withdrawlAmount = $('#savings .input').val()
 		if(userAccounts.savings - withdrawlAmount < 0){
-			// alert("You do not have enough funds");
+			alert("You do not have enough funds");
 			return
 		}else{
 			userAccounts.savings -= withdrawlAmount
