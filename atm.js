a57b0b091bfad4d48b67c893c879d034e87d8c69
Mark Moeller <MarkMoellerUVA@gmail.com>
2017-05-10 09:19:27 -0400

    Deposit button responsive

--- atm.js
+++ atm.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
 });
+
+depositButton = $(".deposit")
+withdrawButton = $("withdraw")
+checkingInput = $(".checking .input")
+savingsInput = $(".savings .input")
+
+
+//$("body").css("background", "red")
+
+depositButton.on("click",deposit)
+
+
+
+function deposit() {
+  console.log("You made a deposit")
+}
