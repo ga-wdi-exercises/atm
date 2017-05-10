057319cccdec21732555fe9741c4a7ee85570e7c
Scott Pierce <scottpiercew@gmail.com>
2017-05-10 11:26:46 -0400

    background red, 1st attempt at checking

--- atm.js
+++ atm.js
@@ -1,3 +1,33 @@
 $(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+  $("body").css("background-color", "red");
+
+
+//user types in dollar amount and clicks Deposit or Withdraw
+//if Deposit is clicked amount typed in is added to account
+
+$(".deposit").on("click", function(){
+  // event.preventDefault()
+
+  var value = $(".input").val()
+  var noDollarInput = parseInt(value)
+console.log(noDollarInput)
+
+  var currentBalance = $(".balance").val()
+  var noDollarCurrent = parseInt(currentBalance)
+console.log(currentBalance)
+
+  var newBalance = noDollarCurrent + noDollarInput
+
+console.log(newBalance)
+  // console.log(typeof value)
+})
+
+
+//if Withdraw is clicked balance of account is checked for sufficient funds
+//if sufficient funds allow withdraw
+//if amount typed in is not available in account give user an error message
+
+
+//if account balance is zero turn .zero red
+
 });
