b83f494d4e0a5e09cfcd0370ec59ff048d516cc1
Perry Fustero <pjfust@gmail.com>
2017-07-10 11:08:27 -0400

    on deposit, balance is increased by deposit ammount

--- atm.js
+++ atm.js
@@ -1,11 +1,19 @@
 /* global $ */
 
 $(document).ready(function () {
+  //user clicks the deposit button
   $('#checking .deposit').on('click', function () {
+
+    //get deposit input change to number
     depositInputCheck = $('#checking .input').val()
+    depositInputCheck = parseInt(depositInputCheck)
+
+    //get current balance, turn from string to number
     checkingBalance = $('#checking .balance').text()
     checkingBalance = parseInt((checkingBalance.slice(1, checkingBalance.length)))
-    $('#checking .balance').text('$' + depositInputCheck)
-    console.log(checkingBalance)
+    
+    //update balance
+    checkingBalance += depositInputCheck
+    $('#checking .balance').text('$' + checkingBalance)
   })
 })
