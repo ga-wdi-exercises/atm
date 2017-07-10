922519cf0e317c4f9a0db60befa60c7093bc9132
Jessa Marie Barre <jmlbarre@gmail.com>
2017-07-10 13:10:36 -0400

    refactored code to be more maintainable and less redundant

--- atm.js
+++ atm.js
@@ -1,11 +1,12 @@
 /* global $ */
 
 $(document).ready(function () {
-  // check if a number
+  /* check if a number */
   function isNumber (n) {
     return !isNaN(parseFloat(n)) && isFinite(n)
   }
 
+/* Informs the user they inputed an invalid amount */
   function addErrorMessage () {
     var errorMessage = $('<div class="error"></div>')
     errorMessage.text('invalid amount')
@@ -13,29 +14,49 @@ $(document).ready(function () {
     errorMessage.appendTo($('.header'))
   }
 
-  function makeDeposit (input, currentBalance, $self) {
+/* returns 'deposit' or 'withdrawal'*/
+  function getTransactionType ($el) {
+    return $el.attr('class')
+  }
+
+/* checks for the type of transaction and sends it to the proper function */
+  function changeBalance (type, input, currentBalance, $el) {
     var inputAsNumber
+
+    // convert input to number
     if (isNumber(input)) {
       inputAsNumber = parseInt(input)
-      $self.siblings('.balance').text(`\n$${currentBalance + inputAsNumber}`)
+      // perform transaction
+      if (type === 'deposit') {
+        makeDeposit(inputAsNumber, currentBalance, $el)
+      } else if (type === 'withdraw') {
+        makeWithdrawal(inputAsNumber, currentBalance, $el)
+      }
+      // remove error if any
       $('.error').remove()
     } else {
       addErrorMessage()
     }
   }
 
-  function makeWithdrawal (input, currentBalance, $self) {
-    var inputAsNumber
-    if (isNumber(input)) {
-      inputAsNumber = parseInt(input)
-      $self.siblings('.balance').text(`\n$${currentBalance - inputAsNumber}`)
-      $('.error').remove()
-    } else {
-      addErrorMessage()
-    }
+  /* add to balance */
+  function makeDeposit (amount, currentBalance, $el) {
+    var result = currentBalance + amount
+
+    $el.siblings('.balance').text(`\n$${result}`)
   }
 
-  $('.deposit').on('click', function () {
+/* withdrawal from balance */
+  function makeWithdrawal (amount, currentBalance, $el) {
+    var result = currentBalance - amount
+
+    $el.siblings('.balance').text(`\n$${result}`)
+  }
+
+  // get all inputs of type='button' and add a listener
+  var buttons = $("input:button")
+
+  buttons.on('click', function () {
     var $self = $(this)
     // get the current balance as a number without '$'
     var currentBalance = parseInt($self.siblings('.balance').text().replace('$', ''))
@@ -44,21 +65,9 @@ $(document).ready(function () {
     // get the user's input
     var input = $self.siblings('.input').val()
     console.log('input=' + input)
-
-    makeDeposit(input, currentBalance, $self)
-  }) // end event listener
-
-  $('.withdraw').on('click', function () {
-    var $self = $(this)
-    // get the current balance as a number without '$'
-    var currentBalance = parseInt($self.siblings('.balance').text().replace('$', ''))
-    console.log('currentBalance=' + currentBalance)
-
-    // get the user's input
-    var input = $self.siblings('.input').val()
-    console.log('input=' + input)
-
-    makeWithdrawal(input, currentBalance, $self)
+    // get the tranaction id
+    var type = getTransactionType($self)
+    changeBalance(type, input, currentBalance, $self)
   }) // end event listener
 
   // $('.balance').attr('class', 'balance zero')
