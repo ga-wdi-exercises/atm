34b09b9bde890dd6ddb2dcd54c34a0596145edb9
Perry Fustero <pjfust@gmail.com>
2017-07-10 11:16:11 -0400

    create withdraw button, subtract from checking balance

--- atm.js
+++ atm.js
@@ -1,19 +1,35 @@
 /* global $ */
 
 $(document).ready(function () {
-  //user clicks the deposit button
+  // Checking Deposit
+  // user clicks the deposit button
   $('#checking .deposit').on('click', function () {
+    // get deposit input change from string to number
+    depositChecking = $('#checking .input').val()
+    depositChecking = parseInt(depositChecking)
 
-    //get deposit input change to number
-    depositInputCheck = $('#checking .input').val()
-    depositInputCheck = parseInt(depositInputCheck)
-
-    //get current balance, turn from string to number
+    // get current balance, turn from string to number
     checkingBalance = $('#checking .balance').text()
     checkingBalance = parseInt((checkingBalance.slice(1, checkingBalance.length)))
-    
-    //update balance
-    checkingBalance += depositInputCheck
+
+    // update balance
+    checkingBalance += depositChecking
+    $('#checking .balance').text('$' + checkingBalance)
+  })
+
+  // Checking Withdraw
+  // user clicks the withraw button
+  $('#checking .withdraw').on('click', function () {
+    // get withdraw input and change from string to number
+    withdrawChecking = $('#checking .input').val()
+    withdrawChecking = parseInt(withdrawChecking)
+
+    // get current balance, turn from string to number
+    checkingBalance = $('#checking .balance').text()
+    checkingBalance = parseInt((checkingBalance.slice(1, checkingBalance.length)))
+
+    // update balance
+    checkingBalance -= withdrawChecking
     $('#checking .balance').text('$' + checkingBalance)
   })
 })
