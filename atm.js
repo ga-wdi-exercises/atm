7c066ec115d5a8ec3368a594976d72ce773c1c21
Kendall <adkins.kendall90@gmail.com>
2017-07-10 11:07:11 -0400

    create class

--- atm.js
+++ atm.js
@@ -8,17 +8,28 @@ $(document).ready(function () {
 $('html').css('background', 'red')
 
 // create a class = atm
-class ATM {
+class Atm {
+  // constructor includes (name, starting balance = 0,
+  // backupAccount = null)
   constructor (name, begBal = 0, backupAcc = null) {
-
+    // name
+    this.name = name
+    // starting balance
+    this.bal = begBal
+    // backup
+    this.backupAcc = backupAcc
+  }
+  // methods: deposit, withdraw (calls verBal [integrated in withdraw])
+  deposit (amt) {
+    console.log(`Depositing ${amt} to balance.`)
+    return this.bal += amt
+  }
+  withdraw (amt) {
+    console.log(`Verifying amount in account...`)
+    // ternary
+    this.bal > amt ? this.bal -= amt : alert('Insufficient funds, transaction cancelled') && console.log('Insufficient Funds, Transaction Cancelled')
+    return `current balance is ${this.bal}.`
   }
 }
-  // constructor includes (name, starting balance = 0, backupAccount = null)
-    // name
-    // starting balance
-    // backup
-  // methods
-    // deposit
-    // withdraw (calls verify balance)
 
-    // verify balance () = checks the money in the account
+var checking = new Atm('checking', 100)
