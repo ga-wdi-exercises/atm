6254291f65e917be40e35fb28273b3e9692fb475
MATT FILBERT <mcfilbert@email.wm.edu>
2017-07-10 11:25:18 -0400

    made basic framework

--- atm.js
+++ atm.js
@@ -1,3 +1,23 @@
-$(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-});
+var depositChecking = $('#checking .deposit') // define variable for deposit button (checking)
+var balanceChecking = $('#checking .balance')
+var inputChecking = $('#checking .input')
+var withdrawChecking = $('#checking .withdraw')
+var depositSavings = $('#savings .deposit') // define variable for deposit button (checking)
+var balanceSavings = $('#savings .balance')
+var inputSavings = $('#savings .input')
+var withdrawSavings = $('#savings .withdraw')
+$(document).ready(function (balance) { // update balance div for checking account
+  depositChecking.on('click', function () {
+    var balance = parseInt(balanceChecking.text().replace('$', ''))
+    var deposit = parseInt(inputChecking.val())
+    var total = balance + deposit
+    balanceChecking.text('$' + total)
+  })
+
+  withdrawChecking.on('click', function () {
+    var balance = parseInt(balanceChecking.text().replace('$', ''))
+    var withdraw = parseInt(inputChecking.val())
+    var total = balance - withdraw
+    balanceChecking.text('$' + total)
+  })
+})
