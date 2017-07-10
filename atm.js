3fac6f065487a8b5e73816fa18ea9006e973ce9d
Perry Fustero <pjfust@gmail.com>
2017-07-10 10:52:47 -0400

    when deposit click, deposit balance updates to deposit input

--- atm.js
+++ atm.js
@@ -3,6 +3,7 @@
 $(document).ready(function () {
   $('#checking .deposit').on('click', function () {
     depositInput = $('#checking .input').val()
+    $('#checking .balance').text('$' + depositInput)
     console.log(depositInput)
   })
 })
