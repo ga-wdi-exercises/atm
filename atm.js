060095a32fe03d000d4aa11926068f9baccd3cd8
Charles Cox <cox.charles.w@gmail.com>
2017-07-10 12:15:57 -0400

    add functionality for savings and overdraft protection

--- atm.js
+++ atm.js
@@ -1,25 +1,70 @@
-
+// Variables
 var checkWithdrawBut = $('#checking').children('.withdraw')
 var checkDepositBut = $('#checking').children('.deposit')
 var saveWithdrawBut = $('#savings').children('.withdraw')
 var saveDepositBut = $('#savings').children('.deposit')
 var checkInputVal = $('#checking').children('.input')
-var depositInputVal = $('#savings').children('.input')
+var saveInputVal = $('#savings').children('.input')
 var checkBalVal = $('#checking').children('.balance')
-var saveBalVal = $('#checking').children('.balance')
+var saveBalVal = $('#savings').children('.balance')
 var checkBalance = 0
 var saveBalance = 0
 var amount = 0
 
-function deposit () {
-  console.log(checkInputVal.val())
+//Functions
+  //Functions for checking
+function depositCheck () {
   checkBalance += parseInt(checkInputVal.val())
-  updateBal()
-  checkInputVal.val('enter an ammount')
+  updateBalCheck()
+  checkInputVal.val('')
 }
 
-function updateBal () {
+function withdrawCheck () {
+  var diff = checkBalance - checkInputVal.val()
+  if (diff < 0 && checkBalance >= -diff) {
+    negativeCheck()
+  } else if (diff < 0 && checkBalance < -diff) {
+    alert(`Sorry you dont have $${checkInputVal.val()} available.`)
+  } else {
+    checkBalance -= parseInt(checkInputVal.val())
+    updateBalCheck()
+    checkInputVal.val('')
+  }
+}
+
+function negativeCheck () {
+  var diff = checkBalance - checkInputVal.val()
+  saveBalance += diff
+  checkBalance = 0
+  updateBalCheck()
+  updateBalSave()
+  checkInputVal.val('')
+}
+
+function updateBalCheck () {
   checkBalVal.text('$' + checkBalance)
 }
 
-checkDepositBut.on('click', deposit)
+  //Functions for savings
+function depositSave () {
+  saveBalance += parseInt(saveInputVal.val())
+  updateBalSave()
+  saveInputVal.val('')
+}
+
+function withdrawSave () {
+  saveBalance -= parseInt(saveInputVal.val())
+  updateBalSave()
+  saveInputVal.val('')
+}
+
+function updateBalSave () {
+  saveBalVal.text('$' + saveBalance)
+}
+
+//Listeners
+checkDepositBut.on('click', depositCheck)
+checkWithdrawBut.on('click', withdrawCheck)
+
+saveDepositBut.on('click', depositSave)
+saveWithdrawBut.on('click', withdrawSave)
