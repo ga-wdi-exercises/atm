25922ffe180eb344ce86ff436cbb90f34680ae45
Jacob Greenwald <jeg4t@virginia.edu>
2017-07-10 10:24:37 -0500

    got checking deposit

--- atm.js
+++ atm.js
@@ -1,3 +1,20 @@
-$(document).ready(function(){
-$('body').css('background-color', 'red')
-});
+var depositChecking = $('#checking .deposit') // define variable for deposit button (checking)
+var balanceChecking = $('#checking .balance')
+var inputChecking = $('#checking .input')
+
+var depositSavings = $('')
+var balanceSavings = $('')
+var inputSavings = $('')
+
+
+$(document).ready(function (balance) { //update balance div for checking account
+depositChecking.on('click', function () {
+  var balance = parseInt(balanceChecking.text().replace("$", ''))
+  var deposit = parseInt(inputChecking.val())
+  var total = balance + deposit
+  balanceChecking.text('$' + total)
+})
+depositSavings.on ('click', function() {  //do same for savings account
+
+})
+})
