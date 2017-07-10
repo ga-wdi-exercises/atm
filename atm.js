30d8a6fe6eb0159c274777529e30dca34106946c
Charles Cox <cox.charles.w@gmail.com>
2017-07-10 11:27:50 -0400

    get value from input

--- atm.js
+++ atm.js
@@ -11,35 +11,9 @@ var checkBalance = 0
 var saveBalance = 0
 var amount = 0
 
-function changeBrushColor (evt) {
-   evt.preventDefault()
-   brush.css('background', input.val())
+function getValue () {
+  console.log(checkInputVal.val())
+  checkInputVal.val('enter an ammount')
 }
 
-// function addDeposit () {
-//   var value = checkInputVal.val()
-//   console.log(value)
-// }
-
-// possible functions
-//   increment balance
-//   decrease balance
-//   change balance color based on balance
-
-
-// function test1 () {
-//   console.log('worked')
-//   $('body').css('background', 'red')
-// }
-//
-// function test2 () {
-//   console.log(checkBalVal.text())
-//   $('body').css('background', 'red')
-// }
-//
-checkWithdrawBut.on('click', getValue)
-// checkDepositBut.on('click', test1)
-// saveWithdrawBut.on('click', test1)
-// saveDepositBut.on('click', test1)
-// checkInputVal.on('keypress', test2)
-// checkWithdrawBut.on('click', test)
+checkDepositBut.on('click', getValue)
