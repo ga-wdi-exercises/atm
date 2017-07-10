0c2714ad395661ba06d2dcc68ecd20ccbe671d4a
Josue Rosales <jhosue.rosales@gmail.com>
2017-07-10 11:46:42 -0400

    everything works including bonus

--- atm.js
+++ atm.js
@@ -2,21 +2,66 @@ $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
 });
 
-$('body').css('background','red')
+//
+
+var balanceChecking =0;
+var balanceSavings =0;
+
+
+
 
 
 var inputChecking = $('#checking .input');
-var inputSaving   = $('#savings .input');
+var inputSavings   = $('#savings .input');
 var outputChecking = $('#checking .balance')
 var outputSavings = $('#savings .balance')
 
 
 
-$('#checking .deposit').on('click',write)
+$('#checking .deposit').on('click',depositChecking)
 
-function write ()
+function depositChecking (){
+  balanceChecking += parseInt(inputChecking.val());
+  outputChecking.text("$" + balanceChecking);
+}
+
+$('#checking .withdraw').on('click',withdrawChecking)
+
+function withdrawChecking(){
+  if(inputChecking.val()<=balanceChecking){
+  balanceChecking -= parseInt(inputChecking.val());
+  outputChecking.text("$" + balanceChecking);
+}else overdraft(parseInt(inputChecking.val()))}
+
+
+$('#savings .deposit').on('click',depositSavings)
+
+function depositSavings (){
+  balanceSavings += parseInt(inputSavings.val());
+  outputSavings.text("$" + balanceSavings);
+}
+
+$('#savings .withdraw').on('click',withdrawSavings)
+
+function withdrawSavings(){
+  if(inputSavings.val()<=balanceSavings){
+  balanceSavings -= parseInt(inputSavings.val());
+  outputSavings.text("$" + balanceSavings);
+}}
+
+
+
+function overdraft(num)
 {
-  console.log(inputChecking.val())
-  outputChecking.text("$" + inputChecking.val());
-
+  console.log(num+" "+balanceChecking +"   "+balanceSavings)
+  if(num<=(balanceChecking + balanceSavings))
+  {
+    balanceSavings -= num - balanceChecking;
+    balanceChecking =0;
+    outputChecking.text("$" + balanceChecking);
+    outputSavings.text("$" + balanceSavings);
+  }
+  else {
+    alert("transaction is not possible, both your checking and savings account are not enough, try another amount.");
+  }
 }
