1bc918ad400d53806fba5e40156acbe9d8f15ac7
Alex Wandres <alex.r.wandres@gmail.com>
2017-07-10 10:44:58 -0400

    checking functionality

--- atm.js
+++ atm.js
@@ -1,3 +1,19 @@
-$(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-});
+
+
+var cDeposit = $('#checking .deposit')
+var cWithdraw = $('#checking .withdraw')
+var cBalance = $('#checking .balance')
+var inputValue = $('.input')
+var cBalanceNum = 0
+
+cDeposit.on('click', function() {
+  cBalanceNum += parseInt(inputValue.val())
+  cBalance.text('$' + cBalanceNum)
+})
+
+cWithdraw.on('click', function() {
+  if (cBalanceNum >= parseInt(inputValue.val())) {
+  cBalanceNum -= parseInt(inputValue.val())
+  cBalance.text('$' + cBalanceNum)
+}
+})
