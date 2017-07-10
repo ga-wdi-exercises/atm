08ac5a8a1df8d7e8eeab46ff8dff1abff31d69bb
Alex Wandres <alex.r.wandres@gmail.com>
2017-07-10 10:51:13 -0400

    savings functionality

--- atm.js
+++ atm.js
@@ -3,17 +3,35 @@
 var cDeposit = $('#checking .deposit')
 var cWithdraw = $('#checking .withdraw')
 var cBalance = $('#checking .balance')
-var inputValue = $('.input')
+var cInput= $('#checking .input')
 var cBalanceNum = 0
 
+var sDeposit = $('#savings .deposit')
+var sWithdraw = $('#savings .withdraw')
+var sBalance = $('#savings .balance')
+var sInput = $('#savings .input')
+var sBalanceNum = 0
+
 cDeposit.on('click', function() {
-  cBalanceNum += parseInt(inputValue.val())
+  cBalanceNum += parseInt(cInput.val())
   cBalance.text('$' + cBalanceNum)
 })
 
 cWithdraw.on('click', function() {
-  if (cBalanceNum >= parseInt(inputValue.val())) {
-  cBalanceNum -= parseInt(inputValue.val())
+  if (cBalanceNum >= parseInt(cInput.val())) {
+  cBalanceNum -= parseInt(cInput.val())
   cBalance.text('$' + cBalanceNum)
 }
+})
+
+sDeposit.on('click', function() {
+  sBalanceNum += parseInt(sInput.val())
+  sBalance.text('$' + sBalanceNum)
+})
+
+sWithdraw.on('click', function() {
+  if (sBalanceNum >= parseInt(sInput.val())) {
+    sBalanceNum -= parseInt(sInput.val())
+    sBalance.text('$' + sBalanceNum)
+  }
 })
