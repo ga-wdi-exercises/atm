6cc6827f6589fd1048f32fd04214c26819324861
Perry Fustero <pjfust@gmail.com>
2017-07-10 10:33:13 -0400

    set up files, tested jquery

--- atm.js
+++ atm.js
@@ -1,3 +1,3 @@
-$(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-});
+$(document).ready(function () {
+  $('body').css('background', 'red')
+})
