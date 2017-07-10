6dd0bbb5950f5b4957c72224d0f164c800fe4518
Kendall <adkins.kendall90@gmail.com>
2017-07-10 10:44:43 -0400

    setup and check

--- atm.js
+++ atm.js
@@ -1,3 +1,24 @@
-$(document).ready(function(){
+/* global $ */
+
+$(document).ready(function () {
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-});
+})
+
+// make background red as a test
+$('html').css('background', 'red')
+
+// create a class = atm
+class ATM {
+  constructor (name, begBal = 0, backupAcc = null) {
+
+  }
+}
+  // constructor includes (name, starting balance = 0, backupAccount = null)
+    // name
+    // starting balance
+    // backup
+  // methods
+    // deposit
+    // withdraw (calls verify balance)
+
+    // verify balance () = checks the money in the account
