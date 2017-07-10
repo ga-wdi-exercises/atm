fde59ca525a2537c6d6665b108789c153951f2d3
Rami Taha <ramisn3@gmail.com>
2017-07-10 15:29:27 -0400

    atm

--- atm.js
+++ atm.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
-});
+  $('body').css({"background-color": "red"});
+//checking account
+ var button1 = $("#checking .deposit")
+   function Hello() {
+     console.log("hello!")
+ }
+button1.on("click", Hello)
+
+var getText = $('#checking .deposit').text()
+console.log(getText)
+
+})
