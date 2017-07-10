42e6868dd30c2a2afe33b0809f0108a55d0fbb4c
Jessa Marie Barre <jmlbarre@gmail.com>
2017-07-10 11:36:25 -0400

    Add user input to balance and error check

--- atm.js
+++ atm.js
@@ -1,11 +1,36 @@
 /* global $ */
 
 $(document).ready(function () {
-  $('body').css('background-color', 'red')
-
   $('#checking .deposit').on('click', function () {
+    var currentBalance = parseInt($('#checking .balance').text().replace('$', ''))
+    console.log(currentBalance)
+
     var input = $('#checking .input').val()
-    $('#checking .balance').text(`\n$${input}`)
+    var number
+
+    function isNumber (n) {
+      return !isNaN(parseFloat(n)) && isFinite(n)
+    }
+
+    if (isNumber(input)) {
+      number = parseInt(input)
+      $('#checking .balance').text(`\n$${number + currentBalance}`)
+      $('.error').remove()
+    } else {
+      var errorMessage = $('<div class="error"></div>')
+      errorMessage.text('invalid number, enter an amount')
+      errorMessage.css('color', 'red')
+      errorMessage.appendTo($('.header'))
+    }
     console.log(input)
   })
 })
+
+// On "Deposit", it updates the balance
+// Now add the user input to the balance, and make it show up in the "balance" element
+
+// On "Withdraw", it updates the balance
+// Follow the same steps as before, except you're subtracting instead of adding
+
+// Refactor the existing code
+// Challenge: Try to have no function() that's longer than 5 lines. (Sandi Metz's Rule 2)
