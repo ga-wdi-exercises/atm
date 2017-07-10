70c00adf293d9dee49ea7304f51dbb4e14dac189
Jessa Marie Barre <jmlbarre@gmail.com>
2017-07-10 11:05:18 -0400

    changed checking balance with input

--- atm.js
+++ atm.js
@@ -1,7 +1,11 @@
-$(document).ready(function(){
+/* global $ */
+
+$(document).ready(function () {
   $('body').css('background-color', 'red')
 
   $('#checking .deposit').on('click', function () {
-    console.log('hi')
+    var input = $('#checking .input').val()
+    $('#checking .balance').text(`\n$${input}`)
+    console.log(input)
   })
-});
+})
