ad3bdc95fb0e37a0c43cd8cfee6bd31dfa25b4c6
Jacob Greenwald <jeg4t@virginia.edu>
2017-07-10 10:41:13 -0500

    deposit and withdraw for checking and savings

--- atm.js
+++ atm.js
@@ -1,23 +1,36 @@
 var depositChecking = $('#checking .deposit') // define variable for checking
 var balanceChecking = $('#checking .balance')
 var inputChecking = $('#checking .input')
+var withdrawChecking = $('#checking .withdraw')
 
-var depositSavings = $('#savings .deposit')  //define variables for savings
+var depositSavings = $('#savings .deposit')  // define variables for savings
 var balanceSavings = $('#savings .balance')
 var inputSavings = $('#savings .input')
+var withdrawSavings = $('#savings .withdraw')
 
-
-$(document).ready(function (balance) { //update balance div for checking account
-depositChecking.on('click', function () {
-  var balance = parseInt(balanceChecking.text().replace("$", ''))
-  var deposit = parseInt(inputChecking.val())
-  var total = balance + deposit
-  balanceChecking.text('$' + total)
-})
-depositSavings.on ('click', function() {  //do same for savings account
-  var balance = parseInt(balanceSavings.text().replace("$", ''))
-  var deposit = parseInt(inputSavings.val())
-  var total = balance + deposit
-  balanceSavings.text('$' + total)
-})
+$(document).ready(function (balance) { // update balance div for checking account
+  depositChecking.on('click', function () {
+    var balance = parseInt(balanceChecking.text().replace('$', ''))
+    var deposit = parseInt(inputChecking.val())
+    var total = balance + deposit
+    balanceChecking.text('$' + total)
+  })
+  withdrawChecking.on('click', function () { // withdraw for checking
+    var balance = parseInt(balanceChecking.text().replace('$', ''))
+    var withdraw = parseInt(inputChecking.val())
+    var total = balance - withdraw
+    balanceChecking.text('$' + total)
+  })
+  depositSavings.on('click', function () {  // deposit for savings
+    var balance = parseInt(balanceSavings.text().replace('$', ''))
+    var deposit = parseInt(inputSavings.val())
+    var total = balance + deposit
+    balanceSavings.text('$' + total)
+  })
+  withdrawSavings.on('click', function () { //withdraw for savings
+    var balance = parseInt(balanceSavings.text().replace('$', ''))
+    var withdraw = parseInt(inputSavings.val())
+    var total = balance - withdraw
+    balanceSavings.text('$' + total)
+  })
 })
