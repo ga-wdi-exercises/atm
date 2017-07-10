1a8055fd079ac797a38a62f3ab099ef6b3f39d53
Jacob Greenwald <jeg4t@virginia.edu>
2017-07-10 11:03:15 -0500

    done without bonus

--- atm.js
+++ atm.js
@@ -15,12 +15,18 @@ $(document).ready(function (balance) { // update balance div for checking accoun
     var total = balance + deposit
     balanceChecking.text('$' + total)
   })
+
   withdrawChecking.on('click', function () { // withdraw for checking
     var balance = parseInt(balanceChecking.text().replace('$', ''))
     var withdraw = parseInt(inputChecking.val())
     var total = balance - withdraw
-    balanceChecking.text('$' + total)
+
+    if (total >= 0) {
+      balanceChecking.text('$' + total)
+      balanceChecking.addClass('zero')
+    }
   })
+
   depositSavings.on('click', function () {  // deposit for savings
     var balance = parseInt(balanceSavings.text().replace('$', ''))
     var deposit = parseInt(inputSavings.val())
@@ -31,6 +37,9 @@ $(document).ready(function (balance) { // update balance div for checking accoun
     var balance = parseInt(balanceSavings.text().replace('$', ''))
     var withdraw = parseInt(inputSavings.val())
     var total = balance - withdraw
+    if (total >= 0) {
     balanceSavings.text('$' + total)
+    balanceSavings.addClass('zero')
+  }
   })
 })
