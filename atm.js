13940cebd85d92926289de0c7aae10f8f76c8543
Luis Wiese <luiswiese@gmail.com>
2017-09-04 00:24:51 -0400

    checking deposit

--- atm.js
+++ atm.js
@@ -1,3 +1,40 @@
+/* global $ */
 $(document).ready(function(){
 // Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
 });
+$('body').css('background', 'gray')
+
+var button1 = $('#checking .deposit')
+var button2 = $('#checking .withdraw')
+var button3 = $('#savings .deposit')
+var button4 = $('#savings .withdraw')
+
+// Checking
+button1.on("click", function(){
+    var deposit = parseInt($("#checking .input").val())
+    var balance = parseInt($("#checking .balance").text().replace("$", ""))
+    var newBalance = "$" + (balance + deposit)
+    $("#checking .balance").text(newBalance)
+  })
+
+  button2.on("click", function(){
+      var deposit = parseInt($("#checking .input").val())
+      var balance = parseInt($("#checking .balance").text().replace("$", ""))
+      var newBalance = "$" + (balance - deposit)
+      $("#checking .balance").text(newBalance)
+    })
+
+    // Savings
+    button3.on("click", function(){
+        var deposit = parseInt($("#savings .input").val())
+        var balance = parseInt($("#savings .balance").text().replace("$", ""))
+        var newBalance = "$" + (balance + deposit)
+        $("#savings .balance").text(newBalance)
+      })
+
+      button4.on("click", function(){
+          var deposit = parseInt($("#savings .input").val())
+          var balance = parseInt($("#savings .balance").text().replace("$", ""))
+          var newBalance = "$" + (balance - deposit)
+          $("#savings .balance").text(newBalance)
+        })
