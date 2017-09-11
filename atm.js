b41ea217b5eaa6fe4e012276be55dbbfcfab0206
Luis Wiese <luiswiese@gmail.com>
2017-09-10 22:44:39 -0400

    atm

--- atm.js
+++ atm.js
@@ -14,31 +14,45 @@ button1.on("click", function(){
     var deposit = parseInt($("#checking .input").val())
     var balance = parseInt($("#checking .balance").text().replace("$", ""))
     var newBalance = "$" + (balance + deposit)
-    $("#checking .balance").text(newBalance)
-    $('#checking .input').val('');
+    $("#checking .balance").text(newBalance)&$('#checking .input').val('');
   })
 
-  button2.on("click", function(){
-      var deposit = parseInt($("#checking .input").val())
-      var balance = parseInt($("#checking .balance").text().replace("$", ""))
-      var newBalance = "$" + (balance - deposit)
-      $("#checking .balance").text(newBalance)
-      $('#checking .input').val('');
-    })
+button2.on("click", function(){
+  var withdraw = parseInt($("#checking .input").val())
+  var balance = parseInt($("#checking .balance").text().replace("$", ""))
+  // Overdraft Protection
+      if (withdraw > balance) {
+        var overDraft = confirm('Not enough money in account, would you like to proceed and overdraft the account')
+        if (overDraft == true) {
+          var newBalance = "$" + (balance - withdraw);
+          $("#checking .balance").text(newBalance)&$('#checking .input').val('');
+          $("#checking .balance").css('background-color', 'red');
+          } else {
+          alert('Transaction Canceled')
+        }
+      }
+  })
 
     // Savings
-    button3.on("click", function(){
-        var deposit = parseInt($("#savings .input").val())
-        var balance = parseInt($("#savings .balance").text().replace("$", ""))
-        var newBalance = "$" + (balance + deposit)
-        $("#savings .balance").text(newBalance)
-        $('#savings .input').val('');
-      })
+button3.on("click", function(){
+    var deposit = parseInt($("#savings .input").val())
+    var balance = parseInt($("#savings .balance").text().replace("$", ""))
+    var newBalance = "$" + (balance + deposit)
+    $("#savings .balance").text(newBalance)&$('#savings .input').val('');
+  })
 
-      button4.on("click", function(){
-          var deposit = parseInt($("#savings .input").val())
-          var balance = parseInt($("#savings .balance").text().replace("$", ""))
-          var newBalance = "$" + (balance - deposit)
-          $("#savings .balance").text(newBalance)
-          $('#savings .input').val('');
-        })
+button4.on("click", function(){
+    var withdraw = parseInt($("#savings .input").val())
+    var balance = parseInt($("#savings .balance").text().replace("$", ""))
+    // Overdraft Protection
+    if (withdraw > balance) {
+      var overDraft = confirm('Not enough money in account, would you like to proceed and overdraft the account')
+      if (overDraft == true) {
+        var newBalance = "$" + (balance - withdraw)
+        $("#savings .balance").text(newBalance)&$('#savings .input').val('');
+        $("#savings .balance").css('background-color', 'red')
+      } else {
+      alert('transaction canceled')
+    }
+  }
+  })
